3fc69e3d43daa473ea7c9e989818d2324dc3cfbf
Noah D. Brenowitz <nbren12@gmail.com>
2019-07-17 15:59:28 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Improving the GFDL FV3 model physics with machine learning',
+    author='Vulcan Inc.',
+    license='MIT',
+)
