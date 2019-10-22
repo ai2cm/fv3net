45db2981ffcd100368c96e0ee29a55167e58d29e
Spencer Clark <spencerkclark@gmail.com>
2019-10-22 14:47:19 +0000

    Minor update to C48_submit_job.sh

--- assets/C48_submit_job.sh
+++ assets/C48_submit_job.sh
@@ -3,5 +3,5 @@
 ulimit -s unlimited
 cd /FV3/rundir
 cp /FV3/fv3.exe /FV3/rundir/fv3.exe
-mpirun -np 6 --allow-run-as-root  --mca btl_vader_single_copy_mechanism none --oversubscribe /FV3/rundir/fv3.exe
+mpirun -np 6 /FV3/rundir/fv3.exe
 
