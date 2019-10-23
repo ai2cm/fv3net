d90756bb27905d1ec297e0333008ace0b026096b
Noah D. Brenowitz <nbren12@gmail.com>
2019-10-22 23:53:08 -0700

    Provision nodes with ssd, might be faster

--- provision_cluster.sh
+++ provision_cluster.sh
@@ -1,4 +1,5 @@
 gcloud container clusters create snakemake-cluster \
    --num-nodes=1 --scopes storage-rw \
    --machine-type n1-standard-4 \
-   --zone us-central1-a
+   --zone us-central1-a \
+   --disk-type pd-ssd
