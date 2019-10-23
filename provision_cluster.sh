c18464a4a913012e2ab765977026386e8a57edff
Noah D. Brenowitz <nbren12@gmail.com>
2019-10-23 18:39:29 +0000

    Add memory limit to argo run

--- provision_cluster.sh
+++ provision_cluster.sh
@@ -3,3 +3,10 @@ gcloud container clusters create snakemake-cluster \
    --machine-type n1-standard-1 \
    --zone us-central1-a \
    --disk-type pd-ssd
+
+# node pool for the model running step
+gcloud container node-pools create big-mem --cluster snakemake-cluster \
+--machine-type n1-standard-2 --num-nodes 1 --zone us-central1-a
+
+# gcloud container node-pools delete big-mem --cluster snakemake-cluster \
+# --machine-type n1-standard-2 --num-nodes 1 --zone us-central1-a
