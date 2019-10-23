a32da243828450a4d20e65119946944c4a1b4aae
Noah D. Brenowitz <nbren12@gmail.com>
2019-10-23 00:49:00 -0700

    run argo workflow for several time steps

--- provision_cluster.sh
+++ provision_cluster.sh
@@ -1,5 +1,5 @@
 gcloud container clusters create snakemake-cluster \
-   --num-nodes=1 --scopes storage-rw \
-   --machine-type n1-standard-4 \
+   --num-nodes=4 --scopes storage-rw \
+   --machine-type n1-standard-1 \
    --zone us-central1-a \
    --disk-type pd-ssd
