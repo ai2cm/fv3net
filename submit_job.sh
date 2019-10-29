5c58150648d9a374a408dad04f08d1cdc5f1d6c4
Noah D. Brenowitz <nbren12@gmail.com>
2019-10-29 19:48:06 +0000

    use larger worker

--- submit_job.sh
+++ submit_job.sh
@@ -1,5 +1,5 @@
 python dataflow/pipeline.py \
-    --job_name coarsen-sfc-data-test-1 \
+    --job_name coarsen-sfc-data-test \
     --project vcm-ml \
     --region us-central1 \
     --runner DataFlow \
@@ -7,5 +7,6 @@ python dataflow/pipeline.py \
     --temp_location gs://vcm-ml-data/tmp_dataflow \
     --num_workers 1 \
     --max_num_workers 4 \
-    --disk_size_gb 80 
+    --disk_size_gb 80  \
+    --worker_machine_type n1-standard-4
     #--service_account_email noah-vm-sa@vcm-ml.iam.gserviceaccount.com
