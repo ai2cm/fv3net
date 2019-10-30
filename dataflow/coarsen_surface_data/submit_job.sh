b287382074ba57551ca99faf4ef20bc7a983c766
Noah D. Brenowitz <nbren12@gmail.com>
2019-10-30 03:58:12 +0000

    increase number of workers

--- dataflow/coarsen_surface_data/submit_job.sh
+++ dataflow/coarsen_surface_data/submit_job.sh
@@ -5,8 +5,8 @@ python -m coarsen_surface_data  \
     --runner DataFlow \
     --setup_file ./setup.py \
     --temp_location gs://vcm-ml-data/tmp_dataflow \
-    --num_workers 20 \
-    --max_num_workers 20 \
+    --num_workers 40 \
+    --max_num_workers 40 \
     --disk_size_gb 80 \
     --worker_machine_type n1-standard-1
     #--service_account_email noah-vm-sa@vcm-ml.iam.gserviceaccount.com
