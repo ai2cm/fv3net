2706c1036c55a58bd3dfd963ac241043af12a0a8
Noah D. Brenowitz <nbren12@gmail.com>
2019-10-31 16:38:34 -0700

    Add script to generate the argo jobs

---
+++ k8s-workflows/scale-snakemake/scale_snakemake_argo.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env sh
+
+timesteps=$(bash list_timesteps_to_run.sh)
+
+ITERATION="$(date +%s)"
+printf "iteration: %s\n" "$ITERATION" >&2
+
+export ITERATION
+
+rm -f argo_jobs.yml
+for time in $timesteps; do
+  export TIMESTEP="$time" && envsubst < argo.yml >> argo_jobs.yml
+  echo "---" >> argo_jobs.yml
+done
