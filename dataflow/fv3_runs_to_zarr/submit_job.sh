1a993763907e990952bc18b8dc0fc5363db57439
Noah D Brenowitz <nbren12@gmail.com>
2019-11-06 16:23:20 -0800

    Feature/fix zarr dim names (#56)
    * Fixed dimension renaming bug in fv_core files and updated dataflow pipeline run scripts
    
    * reimplemented dimension handling and naming in restart files in zarr creation pipeline
    
    * cleanup
    
    * change dataflow output dir
    
    * format with black

--- dataflow/fv3_runs_to_zarr/submit_job.sh
+++ dataflow/fv3_runs_to_zarr/submit_job.sh
@@ -1,12 +1,12 @@
 python pipeline.py  \
-    --job_name convert-rundirs-to-zarr-$(uuid) \
+    --job_name convert-rundirs-to-zarr-$(uuidgen) \
     --project vcm-ml \
     --region us-central1 \
-    --runner DataFlow \
+    --runner DataflowRunner \
     --setup_file ./setup.py \
     --temp_location gs://vcm-ml-data/tmp_dataflow \
     --num_workers 64 \
     --max_num_workers 128 \
     --disk_size_gb 50 \
-    --worker_machine_type n1-standard-1
-    #--service_account_email noah-vm-sa@vcm-ml.iam.gserviceaccount.com
+    --worker_machine_type n1-standard-1 
+   # --service_account_email brian-vm-sa@vcm-ml.iam.gserviceaccount.com
