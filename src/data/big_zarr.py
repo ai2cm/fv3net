faf3812a9b1b165f0af245d7949f2c3328afe807
brianhenn <brianhenn@gmail.com>
2019-11-07 21:03:31 +0000

    reformat with black

--- src/data/big_zarr.py
+++ src/data/big_zarr.py
@@ -2,12 +2,13 @@ from src import gcs
 import xarray as xr
 from datetime import datetime
 
-BASE_PATH = 'gs://vcm-ml-data/2019-10-28-X-SHiELD-2019-10-05-multiresolution-extracted/zarr_new_dims/C48/'
-OUTPUT_PATH = f'gs://vcm-ml-data/2019-10-28-X-SHiELD-2019-10-05-multiresolution-extracted/big_zarr/{datetime.now().isoformat()}.zarr'
+BASE_PATH = "gs://vcm-ml-data/2019-10-28-X-SHiELD-2019-10-05-multiresolution-extracted/zarr_new_dims/C48/"
+OUTPUT_PATH = f"gs://vcm-ml-data/2019-10-28-X-SHiELD-2019-10-05-multiresolution-extracted/big_zarr/{datetime.now().isoformat()}.zarr"
 N_ZARR = None
 
+
 def process(counter, zarr):
-    print(f"{counter}: finished stitching {zarr}", flush = True)
+    print(f"{counter}: finished stitching {zarr}", flush=True)
     return gcs.open_remote_zarr(zarr)
 
 
@@ -23,25 +24,26 @@ def to_big_zarr(gcs_base_dir: str, gcs_output_zarr: str, n_zarr: int = None) -> 
     Returns:
         None
     """
-    
+
     zarr_list = gcs.list_matches(gcs_base_dir)
     print(f"Found {len(zarr_list) - 1} .zarr directories on path.")
-    
+
     if n_zarr:
         n_zarr = min(n_zarr, len(zarr_list))
     else:
         n_zarr = len(zarr_list)
-        
+
     print(f"Starting to stitch {n_zarr - 1} datasets...")
-        
+
     big_ds = xr.combine_by_coords(
         [process(i, zarr) for i, zarr in enumerate(zarr_list[1:n_zarr])]
     )
-        
-    print("Writing to GCS...", flush = True)
+
+    print("Writing to GCS...", flush=True)
     gcs.write_remote_zarr(big_ds, gcs_output_zarr)
     print("...finished.")
-    
-if __name__=='__main__':
-    
+
+
+if __name__ == "__main__":
+
     to_big_zarr(BASE_PATH, OUTPUT_PATH, N_ZARR)
--- src/gcs.py
+++ src/gcs.py
@@ -7,10 +7,14 @@ import gcsfs
 
 def authenticate(key):
     logging.debug("authenticating with key at {key}")
-    ret = subprocess.call(['gcloud', 'auth', 'activate-service-account', '--key-file', key])
+    ret = subprocess.call(
+        ["gcloud", "auth", "activate-service-account", "--key-file", key]
+    )
     if ret == 0:
-        logging.warning("Authentication failed. could lead to "
-                        "errors if no other authentication has been configured")
+        logging.warning(
+            "Authentication failed. could lead to "
+            "errors if no other authentication has been configured"
+        )
     else:
         logging.debug("authentication succeeded.")
 
@@ -18,26 +22,26 @@ def authenticate(key):
 @delayed
 def upload_to_gcs(src, dest, save_op):
     logging.info("uploading %s to %s" % (src, dest))
-    subprocess.check_call(['gsutil', '-q', 'cp',  src, dest])
+    subprocess.check_call(["gsutil", "-q", "cp", src, dest])
     logging.info("uploading %s done" % dest)
 
 
 def exists(url):
-    proc = subprocess.call(['gsutil', 'ls', url], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    proc = subprocess.call(
+        ["gsutil", "ls", url], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+    )
     return proc == 0
 
 
 def list_matches(pattern):
     """Call `gsutil ls <pattern>`"""
-    files = subprocess.check_output(
-        ['gsutil', 'ls', pattern]
-    )
-    return [arg.decode('UTF-8') for arg in files.split()]
+    files = subprocess.check_output(["gsutil", "ls", pattern])
+    return [arg.decode("UTF-8") for arg in files.split()]
 
 
 def copy(src, dest):
     logging.debug(f"copying {src} to {dest}")
-    command = ['gsutil', '-m', 'cp', '-r', src, dest]
+    command = ["gsutil", "-m", "cp", "-r", src, dest]
     subprocess.check_call(command)
     logging.debug(f"copying {src} to {dest} done")
 
@@ -51,11 +55,11 @@ def copy_directory_contents(src: str, dest):
 
 
 def copy_many(urls, dest):
-    command = ['gsutil', '-m', 'cp', '-r'] + urls + [dest]
+    command = ["gsutil", "-m", "cp", "-r"] + urls + [dest]
     subprocess.check_call(command)
-    
-    
-def open_remote_zarr(zarr_path: str, project: str = 'vcm-ml') -> xr.Dataset:
+
+
+def open_remote_zarr(zarr_path: str, project: str = "vcm-ml") -> xr.Dataset:
     """Open a zarr dataset on GCS
     
     Args:
@@ -70,6 +74,7 @@ def open_remote_zarr(zarr_path: str, project: str = 'vcm-ml') -> xr.Dataset:
     gcsmap = fs.get_mapper(zarr_path)
     return xr.open_zarr(store=gcsmap)
 
+
 def write_remote_zarr(ds: xr.Dataset, gcs_path: str) -> None:
     """Writes an xarray dataset to a zarr on GCS
     
@@ -78,6 +83,6 @@ def write_remote_zarr(ds: xr.Dataset, gcs_path: str) -> None:
         gcs_path: the GCS path to the zarr to be written, beginning with tbe bucket name
     
     """
-    fs = gcsfs.GCSFileSystem(project='vcm-ml')
+    fs = gcsfs.GCSFileSystem(project="vcm-ml")
     gcsmap = fs.get_mapper(gcs_path)
-    ds.to_zarr(store=gcsmap, mode='w')
+    ds.to_zarr(store=gcsmap, mode="w")
