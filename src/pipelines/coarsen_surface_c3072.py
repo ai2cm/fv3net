1efd809eb0b18c1c3252dfd9a1a8aff05169e210
Noah D. Brenowitz <nbren12@gmail.com>
2019-11-09 18:22:06 -0800

    Debug subtile combining code.

--- src/pipelines/coarsen_surface_c3072.py
+++ src/pipelines/coarsen_surface_c3072.py
@@ -1,18 +1,15 @@
 import apache_beam as beam
-from typing import Iterator, Tuple
-import io
 from vcm import cubedsphere
-from vcm.cloud import gsutil, gcs
+from vcm.cloud import gcs
+from src.pipelines import common
 import xarray as xr
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.io import fileio
 import logging
 import os
 import tempfile
 
 logger = logging.getLogger(__name__)
 
-logging.basicConfig(level=logging.DEBUG)
 
 INPUT = 'gs://vcm-ml-data/2019-10-03-X-SHiELD-C3072-to-C384-diagnostics'
 OUTPUT = 'gs://vcm-ml-data/2019-11-06-X-SHiELD-gfsphysics-diagnostics-coarsened'
@@ -54,7 +51,7 @@ def download_to_file(url: str, dest: str):
     logging.info(f"Done downloading to {dest}")
 
 
-def download_subtile(suffix: str) -> Iterator[Tuple[xr.DataArray, xr.DataArray]]:
+def download_subtile(suffix: str):
     with tempfile.NamedTemporaryFile() as fdata, tempfile.NamedTemporaryFile() as fgrid:
         download_to_file(url(INPUT, PREFIX_DATA, suffix), fdata.name)
         download_to_file(url(INPUT, PREFIX_GRID_SPEC, suffix), fgrid.name)
@@ -62,48 +59,40 @@ def download_subtile(suffix: str) -> Iterator[Tuple[xr.DataArray, xr.DataArray]]
         logger.info(f"{suffix}: opening xarray file")
         ds = xr.open_dataset(fdata.name)
         weights = xr.open_dataset(fgrid.name)[AREA]
-        ds = ds.assign_coords(tile=int(suffix[4]))
+        tile = suffix[4]
+        ds = ds
 
     for var in ds:
-        yield weights, ds[var]
+        yield {'tile': tile, 'name': var}, (weights, ds[var].load())
 
 
-def coarsen(args: Tuple[xr.DataArray, xr.DataArray]) -> Tuple[str, xr.DataArray]:
-    weights, da = args
+def coarsen(key, val):
+    weights, da = val
     ds_var_coarse = cubedsphere.weighted_block_average(
         da, weights, COARSENING, **DIM_KWARGS)
-    return da.name, ds_var_coarse.rename(da.name)
+    return key, ds_var_coarse.to_dataset(name=da.name)
 
 
-def upload_nc(ds: xr.Dataset, upload_path):
-    with tempfile.NamedTemporaryFile() as f:
-        blob = gcs.init_blob_from_gcs_url(upload_path)
-        logger.info(f"Saving {ds.name} to disk")
-        ds.to_netcdf(f.name)
-        logger.info(f"Start Uploading {ds.name} to {upload_path}")
-        blob.upload_from_filename(f.name)
-        logger.info(f"Done Uploading {ds.name} to {upload_path}")
-
-
-def upload(ds: xr.Dataset):
-    upload_path = os.path.join(OUTPUT, ds.name + '.nc')
-    upload_nc(ds, upload_path)
+def _name(key):
+    file = "{name}.tile{tile}.nc".format(**key)
+    return os.path.join(output_subdir, file)
 
 
 def run(beam_options):
     suffixes = get_suffixes()
     print(f"Processing {len(suffixes)} files")
+
     with beam.Pipeline(options=beam_options) as p:
         (p | beam.Create(suffixes)
          | "Download Data" >> beam.ParDo(download_subtile)
-         | "Coarsen" >> beam.Map(coarsen)
-         | "Combine Subtiles" >> beam.CombinePerKey(cubedsphere.combine_subtiles)
-         | beam.Values()
-         | "Upload" >> beam.Map(upload)
-         )
+         | "Coarsen" >> beam.MapTuple(coarsen)
+         | common.CombineSubtilesByKey()
+         | common.WriteToNetCDFs(_name))
+
 
 
 if __name__ == '__main__':
     """Main function"""
+    logging.basicConfig(level=logging.DEBUG)
     beam_options = PipelineOptions(save_main_session=True)
     run(beam_options=beam_options)
