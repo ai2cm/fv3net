935e5b8695fdec1c1daaddb8e5b4310baf12a0d6
Noah D. Brenowitz <nbren12@gmail.com>
2019-11-11 13:32:08 -0800

    Regridding to lat lon works

--- workflows/regrid_c3072_diag/regrid.sh
+++ workflows/regrid_c3072_diag/regrid.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-field=PRATEsfc
+set -e
+set -x
 
+field="$1"
+input_bucket=gs://vcm-ml-data/2019-11-06-X-SHiELD-gfsphysics-diagnostics-coarsened/C384
+output_bucket=gs://vcm-ml-data/2019-11-06-X-SHiELD-gfsphysics-diagnostics-coarsened
+
+
+# download the data
 for tile in {1..6}
 do
-    outputFile=$field.tile${tile}.nc
-    rm $outputFile
-    mppnccombine $outputFile $field/*tile${tile}.nc.* 
+    fileName=${field}.tile${tile}.nc
+    inputUrl=$url/$fileName
+
+    [ -f $fileName ] || gsutil cp $inputUrl $fileName
 done
 
 mosaic=2019-10-05-coarse-grids-and-orography-data/C384/grid_spec.nc
+nlat=180
+nlon=180
 
 fregrid \
  --input_mosaic $mosaic \
- --nlat 180 --nlon 90 \
- --remap_file c34_to_180x90.nc \
+ --nlat $nlat --nlon $nlon \
+ --remap_file c34_to_${nlat}x${nlon}.nc \
  --input_file  $field \
  --output_file $field.nc  \
  --LstepBegin 1 --LstepEnd 480 \
  --scalar_field $field
  #--scalar_field areat
+
+
+gsutil cp $field.nc $output_bucket/nLat${nlat}_nLon${nLon}/$field.nc
