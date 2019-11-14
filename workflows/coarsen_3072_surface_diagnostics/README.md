a1eb15668478d3c15d9fb4723610929be1343632
Noah D. Brenowitz <nbren12@gmail.com>
2019-11-13 16:17:00 -0800

    Finally get a working combination of sdist and setup.py

--- workflows/coarsen_3072_surface_diagnostics/README.md
+++ workflows/coarsen_3072_surface_diagnostics/README.md
@@ -6,11 +6,12 @@ This workflow produced the outputs at
 `gs://vcm-ml-data/2019-11-06-X-SHiELD-gfsphysics-diagnostics-coarsened/C384/`.
 
 To test this job locally, run 
-
+    
     bash submit_local.sh
 
 To submit with many workers on google cloud dataflow run
 
     bash submit_job.sh
     
-This script can be edited to change things like the number of workers to use in dataflow.
+This script can be edited to change things like the number of workers to use in
+dataflow. It serves as a good template for future dataflow pipelines.
--- workflows/coarsen_3072_surface_diagnostics/submit_job.sh
+++ workflows/coarsen_3072_surface_diagnostics/submit_job.sh
@@ -2,11 +2,21 @@
 
 SETUP_PY=../../setup.py
 VCM_PATH=../../external/vcm
-NUM_WORKERS=96
+# Have a few more workers than the 96 input files present to avoid having all
+# the workers wait for a single file to download
+NUM_WORKERS=100
 BEAM_MAIN_MODULE=fv3net.pipelines.coarsen_surface_c3072
 
 # build the sdist for vcm
-python $VCM_PATH/setup.py sdist
+#
+# Notes:
+# Calling the setup.py from this current directory and then using the tarball from the
+# local the local dists folder led to import errors when submitting to the remote
+# Therefore, we run the setup.py for vcm in the external/vcm directory.
+(
+cd $VCM_PATH
+python setup.py sdist
+)
 
 
 # submit the job
@@ -21,4 +31,4 @@ python -m $BEAM_MAIN_MODULE  \
     --max_num_workers $NUM_WORKERS \
     --disk_size_gb 100 \
     --worker_machine_type n1-standard-2 \
-    --extra_package dist/vcm*.tar.gz
+    --extra_package $VCM_PATH/dist/vcm*.tar.gz
