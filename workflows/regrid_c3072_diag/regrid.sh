e38fa075027ab952a34c8a8061088b833d059ce3
Noah D Brenowitz <nbren12@gmail.com>
2019-11-14 11:28:07 -0800

    Regrid c3072 diagnostics to lat/lon (#61)
    This PR introduces a kubeflow pipeline for regridding arbitrary GFDL cubed sphere data to netCDF files. The intended interface is via the kubeflow web ui: https://kf-ml.endpoints.vcm-ml.cloud.goog/_/pipeline/#/pipelines, but the regrid.sh script should work from within a docker container alone.
    
    An example output of this script is available here:
    
    gs://vcm-ml-data/2019-11-12-fv3gfs-C48-five-day-nudged-run/nLat180_nLon360/sfc_dt_atmos.nc
    
    Commits include:
    
    * initialize project stub
    
    * Add stub test directory for
    
    * add pipelines stub
    
    * Move most of src/ into vcm/vcm
    
    * Get tests working again in external/vcm
    
    - I also added some requirements.txt
    
    * Refactor the rerun_fv3 to the pipelines module
    
    * Move k8s-workflows to workflows per discussion with frodre
    
    * Move dataflow files to src and utils to vcm
    
    * Add tests for cubedsphere
    
    * Fix GCS utility names to vcm with tests passing
    
    * Delete old files and folders
    
    * Add TODO files in directories that still need some thought
    
    * Rename tests to clarify target module
    
    * Move docker installation scripts to docker/
    
    * Add __init__.py to vcm.cloud
    
    * Add workflow for tar extraction.   with Dataflow
    
    * Adjust directory and file layout in readme
    
    * Move relevant vcm.cloud.gcs tests into vcm tests
    
    * add some debugging output to gsutil commands
    
    * add hello world beam example
    
    * Finish moving old dataflow submission scripts into workflow. Remove extraneous dirs
    
    * Fix CircleCI testing?
    
    * Added docs folder
    
    * Rename to fv3net and remove accidental symlink copy into fv3_runs_to_zarr
    
    * finish renameing of references to src to fv3net
    
    * Remove snakemake from circleCI tests
    
    * Fix gsutil import in rerun_fv3
    
    * Add gsutil to requirements.txt
    
    * Fix reference to short_datatag_def.yml
    
    * Add dataflow requirements to requirements.txt
    
    * Fix requirements.txt formatting error
    
    * Start work on regridding C348 to lat/lon
    
    * Regridding to lat lon works
    
    * Process the data in a non-reproducible way
    
    I could not get either
    1. gsutil to work in a local docker container, or
    2. gsutil to work in K8s via kubeflow
    
    So for now I downloaded the data by hand, processed it in docker with
    `regrid_hacky.sh`
    and reuploaded to the appropriate place.
    
    * Authenticate gcloud within script
    
    gcloud has a different authentication protocol than the google python api. In
    particular, it won't use the GOOGLE_APPLICATION_DEFAULT environmental variable
    automatically.
    
    * Clean up regrid script
    
    * Add missing dockerfile and makeifle
    
    * Add local google creds to docker image using bindmounts
    
    * Run-regridding on google cloud
    
    * Generalize the regridding interface
    
    This is needed to work with the nudged input data
    
    * Enhance README
    
    * Change resolution to 360
    
    * Remove extraneous files and decribe the purpose of enter.sh
    
    * Fix typo
    
    * Add table of parameters to the README
    
    * fix table
    
    * Mention REST API in the README
    
    * Expand some Kubeflow explanation to the README.md
    
    * add extra_args paramater to change the target resolution

---
+++ workflows/regrid_c3072_diag/regrid.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+set -x
+
+
+src_prefix="$1"; shift;
+outputBucket="$1"; shift;
+resolution="$1"; shift;
+scalarFields="$1"; shift;
+
+
+# authenticate with gcloud
+if [ -f $GOOGLE_APPLICATION_CREDENTIALS ]
+then
+    gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
+fi
+
+# download the tile data
+localPrefix=data
+for tile in {1..6}
+do
+    inputUrl=$src_prefix.tile${tile}.nc
+    fileName=$localPrefix.tile${tile}.nc
+
+    [ -f $fileName ] || gsutil cp $inputUrl $fileName
+done
+
+# download orographic data
+mosaic=2019-10-05-coarse-grids-and-orography-data/$resolution/grid_spec.nc
+if [ ! -f $mosaic ]
+then
+       gsutil cp gs://vcm-ml-data/2019-10-05-coarse-grid-and-orography-data.tar grid.tar
+       tar xf grid.tar
+       rm grid.tar
+fi
+remapFile=${resolution}_to_${nlat}x${nlon}.nc
+
+
+# fregrid is a GFDL command line tool for regridding FMS model output
+# it works for cubed-sphere and many other grids.
+fregrid  --input_mosaic $mosaic \
+ --remap_file $remapFile \
+ --input_file  $localPrefix \
+ --output_file $localPrefix.nc \
+ --scalar_field $scalarFields \
+ $@
+
+gsutil cp $localPrefix.nc $outputBucket
