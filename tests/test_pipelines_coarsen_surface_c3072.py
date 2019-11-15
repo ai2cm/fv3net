56b03e64514f64a92ae8fcbc3a8d84a817fb05b3
Noah D Brenowitz <nbren12@gmail.com>
2019-11-14 18:22:51 -0800

    Coarsen surface diagnostics from C3072 SHIELD run (#64)
    This PR adds a dataflow pipeline and corresponding tests for coarse-graining the C3072 SHIELD data. It also includes some common apache beam functionality (in fv3net.common) that could be shared across different pipelines.
    
    The old draft PR (#60) was difficult to merge with the great-refactor, so I just moved the relevant files over to this new branch. I will close the other ones.
    
    Todo
    
     add documentation to the common.py module.
     close #60
     delete the various old branches.
     manually checked that the dataflow job still works.
     tests pass
    
    Changes include:
    
    * Add surface coarening pipeline
    
    This is the only code contributed by the surface coarsening branch
    
    * Add test
    
    * Fix import errors
    
    * Run automatic formatter and add readme
    
    * Update readme and add local script
    
    * Document the routines in common.py
    
    * Add nose to requirements
    
    This is needed by the apache_beam TestPipeline object.
    
    * Finally get a working combination of sdist and setup.py
    
    * Simplify and comment the code
    
    Removes the netCDF sink class, which was confusing.
    
    * Add type hint
    
    * fix broken link in docstring
    
    * Update workflows/coarsen_3072_surface_diagnostics/submit_job.sh
    
    Co-Authored-By: Oliver Watt-Meyer <oliwm@vulcan.com>
    
    * remove stupid line

---
+++ tests/test_pipelines_coarsen_surface_c3072.py
@@ -0,0 +1,51 @@
+import numpy as np
+import xarray as xr
+from vcm import cubedsphere
+import apache_beam as beam
+from apache_beam.testing.test_pipeline import TestPipeline
+from fv3net.pipelines import common
+from fv3net.pipelines import coarsen_surface_c3072
+
+
+def _test_data():
+    n = 6
+    x = np.arange(n) + 1
+    da = xr.DataArray(x, dims=["x"], name="a", coords={"x": x})
+    yield "a", da.isel(x=slice(0, n // 2))
+    yield "a", da.isel(x=slice(n // 2 - 1, None))
+
+
+def data_from_disk():
+    suffix = "tileX.nc.0000"
+    prefix = "PRATEsfc/gfsphysics_15min_fine_PRATEsfc"
+    for fname in cubedsphere.all_filenames(prefix):
+        tile = int(fname[-9])
+        yield {"name": "PRATEsfc", "tile": tile}, xr.open_dataset(fname)
+
+
+def mock_data():
+    for tile in range(6):
+        for name, ds in _test_data():
+            yield {"name": name, "tile": tile}, ds.to_dataset()
+
+
+def test_CombineSubtilesByKey(tmpdir):
+    def _name(key):
+        return "gs://vcm-ml-data/TESTDELETE/{name}.tile{tile}.nc".format(**key)
+
+    with TestPipeline() as p:
+        data = (
+            p
+            | beam.Create(mock_data())
+            | common.CombineSubtilesByKey()
+            | common.WriteToNetCDFs(_name)
+        )
+
+
+def test__name():
+    key = {"tile": 3, "name": "a"}
+    path = coarsen_surface_c3072._name(key)
+    assert (
+        path
+        == f"gs://vcm-ml-data/2019-11-06-X-SHiELD-gfsphysics-diagnostics-coarsened/C384/a.tile3.nc"
+    )
