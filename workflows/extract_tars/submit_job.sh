d4f93da39b1275bb42430ac46590845e366d37d6
Andre Perkins <frodre@gmail.com>
2019-11-19 22:17:13 +0000

    Fix README for creating external vcm package and submitting to Dataflow

--- workflows/extract_tars/submit_job.sh
+++ workflows/extract_tars/submit_job.sh
@@ -23,5 +23,5 @@ python -m fv3net.pipelines.extract_tars \
     --type_check_strictness 'ALL_REQUIRED' \
     --worker_machine_type n1-standard-1 \
     --setup_file ./setup.py \
-    --extra_package dist/vcm-0.1.0.tar.gz
+    --extra_package external/vcm/dist/vcm-0.1.0.tar.gz
 
