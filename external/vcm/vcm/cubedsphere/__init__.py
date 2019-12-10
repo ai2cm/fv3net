23fd066222e8043c533cbbb8294da291de25ad75
Oliver Watt-Meyer <oliwm@vulcan.com>
2019-12-10 22:44:12 +0000

    fix __init__ for cubedsphere

--- external/vcm/vcm/cubedsphere/__init__.py
+++ external/vcm/vcm/cubedsphere/__init__.py
@@ -2,8 +2,10 @@ from .coarsen import (
     block_coarsen,
     block_edge_sum,
     block_median,
+    coarsen_coords,
     edge_weighted_block_average,
     horizontal_block_reduce,
+    weighted_block_average,
     xarray_block_reduce,
 )
 from .io import all_filenames, open_cubed_sphere, save_tiles_separately
