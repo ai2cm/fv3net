779132b6400f420048609568a6a16936f4ac6b31
Oliver Watt-Meyer <oliwm@vulcan.com>
2019-12-18 09:47:15 -0800

    Rename functions in test_calc_thermo.py

--- external/vcm/tests/test_calc_thermo.py
+++ external/vcm/tests/test_calc_thermo.py
@@ -3,21 +3,21 @@ import xarray as xr
 
 from vcm.calc.thermo import (
     VERTICAL_DIM,
-    absolute_pressure_interface,
-    absolute_height_interface,
+    pressure_on_interface,
+    height_on_interface,
 )
 
 
 def test_absolute_pressure_interface():
-    dp = xr.DataArray(np.arange(1, 10), dims=[VERTICAL_DIM])
-    pabs = absolute_pressure_interface(dp)
+    delp = xr.DataArray(np.arange(1, 10), dims=[VERTICAL_DIM])
+    pabs = pressure_on_interface(delp)
     pabs = xr.DataArray(pabs)
-    xr.testing.assert_allclose(pabs.diff(VERTICAL_DIM), dp)
+    xr.testing.assert_allclose(pabs.diff(VERTICAL_DIM), delp)
 
 
 def test_absolute_height_interface():
     dz = xr.DataArray(np.arange(1, 10), dims=[VERTICAL_DIM])
     phis = xr.DataArray(0)
-    zabs = absolute_height_interface(dz, phis)
+    zabs = height_on_interface(dz, phis)
     zabs = xr.DataArray(zabs)
     xr.testing.assert_allclose(zabs.diff(VERTICAL_DIM), dz)
