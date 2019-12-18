1293a61b4d64c258e4e56a6a81eb536eb96d2ce7
Anna Kwa <akwa@uci.edu>
2019-12-18 11:07:36 -0800

    Refactor train data pipeline

--- fv3net/pipelines/create_training_data/pipeline.py
+++ fv3net/pipelines/create_training_data/pipeline.py
@@ -1,45 +1,32 @@
+import numba
 import argparse
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+import gcsfs
 import logging
 import os
+import xarray as xr
 
-import apache_beam as beam
-import gcsfs
-import numba
-from apache_beam.options.pipeline_options import PipelineOptions
-from vcm.convenience import get_timestep_from_filename
+from vcm.calc import apparent_source
+from vcm.cloud import gsutil
+from vcm.cubedsphere import rename_centered_xy_coords, shift_edge_var_to_center
 
-from fv3net.pipelines.create_training_data.dataset_creator import (
-    create_training_dataset,
-    write_to_zarr,
-)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler("dataset.log")
+fh.setLevel(logging.INFO)
+logger.addHandler(fh)
+
 
 # There have been issues where python crashes immediately unless numba gets
 # imported explicitly before vcm, hence the import and del
 del numba
 
-logging.basicConfig(level=logging.INFO)
 
-
-class CreateTrainZarr(beam.DoFn):
-    """Applies process to elements of beam ProcessCollection to create
-    batch dataset with feature/target variables and write it to GCS
-
-    """
-
-    def __init__(self, args):
-        self.mask_to_surface_type = args.mask_to_surface_type
-        self.gcs_output_data_dir = args.gcs_output_data_dir
-        self.gcs_bucket = args.gcs_bucket
-        self.gcs_project = args.gcs_project
-
-    def process(self, data_urls):
-        ds = create_training_dataset(
-            data_urls, self.mask_to_surface_type, project=self.gcs_project
-        )
-        zarr_file = get_timestep_from_filename(data_urls[0]) + ".zarr"
-        write_to_zarr(
-            ds, self.gcs_output_data_dir, zarr_file, bucket=self.gcs_bucket,
-        )
+TIME_DIM = 'initialization_time'
+GRID_VARS = ["grid_lon", "grid_lat", "grid_lont", "grid_latt"]
+INPUT_VARS = ["sphum", "T", "delp", "u", "v", "slmsk"]
+TARGET_VARS = ["Q1", "Q2", "QU", "QV"]
 
 
 def run(args, pipeline_args):
@@ -63,8 +50,105 @@ def run(args, pipeline_args):
         (
             p
             | beam.Create(data_urls)
-            | "CreateSubsetZarr" >> beam.ParDo(CreateTrainZarr(args))
+            | "LoadCloudData" >> beam.Map(_load_cloud_data, fs=fs)
+            | "CreateTrainingCols" >> beam.Map(_create_train_cols)
+            | "MaskToSurfaceType" >> beam.Map(
+                _mask_to_surface_type, surface_type=args.mask_to_surface_type)
+            | "WriteToZarr" >> beam.Map(
+                _write_to_zarr,
+                gcs_dest_dir=args.gcs_output_data_dir)
         )
+
+
+def _write_to_zarr(
+    ds, gcs_dest_dir, bucket="vcm-ml-data",
+):
+    """Writes temporary zarr on worker and moves it to GCS
+
+    Args:
+        ds: xr dataset for single training batch
+        gcs_dest_path: write location on GCS
+        zarr_filename: name for zarr, use first timestamp as label
+        bucket: GCS bucket
+    Returns:
+        None
+    """
+    logger.info("Writing to zarr...")
+    zarr_filename = _filename_from_first_timestep(ds)
+    output_path = os.path.join(bucket, gcs_dest_dir, zarr_filename)
+    ds.to_zarr(zarr_filename, mode="w")
+    gsutil.copy(zarr_filename, output_path)
+    logger.info(f"Done writing zarr to {output_path}")
+
+
+def _filename_from_first_timestep(ds):
+    timestep = min(ds[TIME_DIM].values).strftime("%Y%m%d.%H%M%S")
+    return timestep + ".zarr"
+
+
+def _load_cloud_data(fs, gcs_urls):
+    """
+
+    Args:
+        fs: GCSFileSystem
+        gcs_urls: list of GCS urls to open
+
+    Returns:
+        xarray dataset of concatenated zarrs in url list
+    """
+    gcs_zarr_mappings = [fs.get_mapper(url) for url in gcs_urls]
+    ds = xr.concat(map(xr.open_zarr, gcs_zarr_mappings), "initialization_time")[
+        INPUT_VARS + GRID_VARS
+    ]
+    return ds
+
+
+def _mask_to_surface_type(ds, surface_type):
+    """
+
+    Args:
+        ds: xarray dataset, must have variable slmsk
+        surface_type: one of ['sea', 'land', 'seaice']
+
+    Returns:
+        input dataset masked to the surface_type specified
+    """
+    if not surface_type:
+        return ds
+    elif surface_type not in ["sea", "land", "seaice"]:
+        raise ValueError("Must mask to surface_type in ['sea', 'land', 'seaice'].")
+    surface_type_codes = {"sea": 0, "land": 1, "seaice": 2}
+    mask = ds.slmsk == surface_type_codes[surface_type]
+    ds_masked = ds.where(mask)
+    return ds_masked
+
+
+def _create_train_cols(ds):
+    """
+
+    Args:
+        ds: xarray dataset, must have variables ['u', 'v', 'T', 'sphum']
+
+    Returns:
+        xarray dataset with variables in INPUT_VARS + TARGET_VARS + GRID_VARS
+    """
+    da_centered_u = rename_centered_xy_coords(shift_edge_var_to_center(ds["u"]))
+    da_centered_v = rename_centered_xy_coords(shift_edge_var_to_center(ds["v"]))
+    ds["u"] = da_centered_u
+    ds["v"] = da_centered_v
+    ds["QU"] = apparent_source(ds.u)
+    ds["QV"] = apparent_source(ds.v)
+    ds["Q1"] = apparent_source(ds.T)
+    ds["Q2"] = apparent_source(ds.sphum)
+    num_slices = len(ds.initialization_time.values) - 1
+    ds = (
+        ds[INPUT_VARS + TARGET_VARS + GRID_VARS]
+        .isel(forecast_time=0)
+        .squeeze()
+        .drop("forecast_time")
+        .isel(initialization_time=slice(None, num_slices))
+    )
+    return ds
 
 
 if __name__ == "__main__":
