a90029298275ac7feff726f317322daf94042892
Oliver Watt-Meyer <oliwm@vulcan.com>
2019-12-19 15:00:58 -0800

    Move mappm.f90 to external and add setup.py

---
+++ external/vcm/external/mappm/setup.py
@@ -0,0 +1,11 @@
+from numpy.distutils.core import Extension, setup
+
+mappm_extension = Extension(name="mappm", sources=["mappm.f90"])
+
+setup(
+    name="mappm",
+    description="Python wrapper of mappm routine from FV3GFS model",
+    author="Vulcan Technologies, LLC",
+    author_email="oliwm@vulcan.com",
+    ext_modules=[mappm_extension],
+)
