7e92faa6eae7f1afd2a9b1696eadbc16daceef26
Oliver Watt-Meyer <oliwm@vulcan.com>
2019-12-24 20:28:27 +0000

    Add _mask_weights test

--- external/vcm/tests/test_remapz.py
+++ external/vcm/tests/test_remapz.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 import xarray as xr
-from vcm.cubedsphere.remapz import remap_levels
+from vcm.cubedsphere.remapz import remap_levels, _mask_weights
 
 
 def input_dataarray(shape):
@@ -31,3 +31,22 @@ def test_remap_levels(p_in_shape, f_in_shape, p_out_shape, expected):
         pytest.skip("mappm import failed. Skipping test_remap_levels.")
     f_out = f_out.transpose(*f_in.dims)
     assert f_out.shape == expected
+
+
+def test__mask_weights():
+    weights = input_dataarray((2, 2, 1)).isel(z=0)
+    phalf_coarse_on_fine = input_dataarray((2, 2, 3))
+    phalf_fine = input_dataarray((2, 2, 3))
+    phalf_coarse_on_fine[:, :, 0] = 1.0
+    phalf_coarse_on_fine[:, :, 1] = 2.0
+    phalf_coarse_on_fine[:, :, 2] = 3.0
+    phalf_fine[:, :, 0] = 1.0
+    phalf_fine[:, :, 1] = 2.0
+    phalf_fine[0, :, 2] = 2.5
+    phalf_fine[1, :, 2] = 3.5
+    expected_weights = weights.broadcast_like(phalf_fine.isel(z=slice(None, -1))).copy()
+    expected_weights[0, :, 1] = 0.0
+    masked_weights = _mask_weights(
+        weights, phalf_coarse_on_fine, phalf_fine, dim_center="z", dim_outer="z"
+    )
+    xr.testing.assert_allclose(expected_weights, masked_weights)
