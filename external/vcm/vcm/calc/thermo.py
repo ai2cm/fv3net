8d334c9d033bfb853165b504e7668a0134bc2a52
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-01-03 16:51:39 +0000

    Fix midpoint pressure calc and add docstrings

--- external/vcm/vcm/calc/thermo.py
+++ external/vcm/vcm/calc/thermo.py
@@ -15,7 +15,20 @@ REVERSE = slice(None, None, -1)
 def pressure_at_interface(
     delp, toa_pressure=TOA_PRESSURE, dim_center=COORD_Z_CENTER, dim_outer=COORD_Z_OUTER
 ):
-    """ Compute pressure at layer interfaces """
+    """ Compute pressure at layer interfaces
+
+    Args:
+        delp (xr.DataArray): pressure thicknesses
+        toa_pressure (float, optional): pressure at the top of atmosphere.
+            Defaults to 300Pa.
+        dim_center (str, optional): name of vertical dimension for delp.
+            Defaults to "pfull".
+        dim_outer (str, optional): name of vertical dimension for output pressure.
+            Defaults to "phalf".
+
+    Returns:
+        xr.DataArray: atmospheric pressure at layer interfaces
+    """
     top = xr.full_like(delp.isel({dim_center: [0]}), toa_pressure).variable
     delp_with_top = top.concat([top, delp.variable], dim=dim_center)
     pressure = delp_with_top.cumsum(dim_center)
@@ -23,7 +36,19 @@ def pressure_at_interface(
 
 
 def height_at_interface(dz, phis, dim_center=COORD_Z_CENTER, dim_outer=COORD_Z_OUTER):
-    """ Compute geopotential height at layer interfaces """
+    """ Compute geopotential height at layer interfaces
+
+    Args:
+        dz (xr.DataArray): layer height thicknesses
+        phis (xr.DataArray): surface geopotential
+        dim_center (str, optional): name of vertical dimension for dz.
+            Defaults to "pfull".
+        dim_outer (str, optional): name of vertical dimension for output heights.
+            Defaults to "phalf".
+
+    Returns:
+        xr.DataArray: height at layer interfaces
+    """
     bottom = phis.broadcast_like(dz.isel({dim_center: [0]})) / GRAVITY
     dzv = -dz.variable  # dz is negative in model
     dz_with_bottom = dzv.concat([dzv, bottom], dim=dim_center)
@@ -36,6 +61,8 @@ def height_at_interface(dz, phis, dim_center=COORD_Z_CENTER, dim_outer=COORD_Z_O
 
 
 def _add_coords(dv, da, dim_center=COORD_Z_CENTER, dim_outer=COORD_Z_OUTER):
+    """ Assign coords from da to dv, except use vertical coordinate that is
+    one element longer than da's """
     coords_except_z = da.drop(dim_center).coords
     z_coord_center = da.coords[dim_center]
     z_coord_outer = xr.concat(
@@ -48,45 +75,84 @@ def _add_coords(dv, da, dim_center=COORD_Z_CENTER, dim_outer=COORD_Z_OUTER):
     )
 
 
-def pressure_at_midpoint(delp, dim=COORD_Z_CENTER):
-    """ Compute pressure at layer midpoints """
-    pi = pressure_at_interface(delp, dim_center=dim)
-    pc = _interface_to_midpoint(pi, dim_center=dim)
-    return xr.DataArray(pc, coords=delp.coords)
+def pressure_at_midpoint(delp, toa_pressure=TOA_PRESSURE, dim=COORD_Z_CENTER):
+    """ Compute pressure at layer midpoints by linear interpolation
+
+    Args:
+        delp (xr.DataArray): pressure thicknesses
+        toa_pressure (float, optional): pressure at the top of atmosphere.
+            Defaults to 300Pa.
+        dim (str, optional): name of vertical dimension for delp. Defaults to "pfull".
+
+    Returns:
+        xr.DataArray: atmospheric pressure at layer midpoints
+    """
+    pi = pressure_at_interface(delp, toa_pressure=toa_pressure, dim_center=dim)
+    return _interface_to_midpoint(pi, dim_center=dim)
 
 
 def height_at_midpoint(dz, phis, dim=COORD_Z_CENTER):
-    """ Compute geopotential height at layer midpoints """
+    """ Compute geopotential height at layer midpoints by linear interpolation
+
+    Args:
+        dz (xr.DataArray): layer height thicknesses
+        phis (xr.DataArray): surface geopotential
+        dim (str, optional): name of vertical dimension for dz. Defaults to "pfull".
+
+    Returns:
+        xr.DataArray: height at layer midpoints
+    """
     zi = height_at_interface(dz, phis, dim_center=dim)
-    zc = _interface_to_midpoint(zi, dim_center=dim)
-    return xr.DataArray(zc, coords=dz.coords)
+    return _interface_to_midpoint(zi, dim_center=dim)
 
 
 def _interface_to_midpoint(da, dim_center=COORD_Z_CENTER, dim_outer=COORD_Z_OUTER):
     da_mid = (
-        da.isel({dim_outer: slice(0, -1)}) + da.isel({dim_outer: slice(1, None)})
+        da.isel({dim_outer: slice(0, -1)})
+        + da.isel({dim_outer: slice(1, None)}).drop(dim_outer)
     ) / 2
     return da_mid.rename({dim_outer: dim_center})
 
 
-def pressure_at_midpoint_log(delp, dim=COORD_Z_CENTER):
+def pressure_at_midpoint_log(delp, toa_pressure=TOA_PRESSURE, dim=COORD_Z_CENTER):
     """ Compute pressure at layer midpoints following Eq. 3.17 of Simmons
-    and Burridge (1981), MWR."""
-    pi = pressure_at_interface(delp, dim_center=dim)
-    dlogp = xr.DataArray(np.log(pi)).diff(dim)
-    return xr.DataArray(delp / dlogp, coords=delp.coords)
+    and Burridge (1981), MWR.
+
+    Args:
+        delp (xr.DataArray): pressure thicknesses
+        toa_pressure (float, optional): pressure at the top of atmosphere.
+            Defaults to 300Pa.
+        dim (str, optional): name of vertical dimension for delp. Defaults to "pfull".
+
+    Returns:
+        xr.DataArray: atmospheric pressure at layer midpoints
+    """
+    pi = pressure_at_interface(
+        delp, toa_pressure=toa_pressure, dim_center=dim, dim_outer=dim
+    )
+    dlogp = np.log(pi).diff(dim, label="lower")
+    return delp / dlogp
 
 
 def hydrostatic_dz(T, q, delp, dim=COORD_Z_CENTER):
-    """ Compute layer thickness assuming hydrostatic balance """
+    """ Compute layer thickness assuming hydrostatic balance
+
+    Args:
+        T (xr.DataArray): temperature
+        q (xr.DataArray): specific humidity
+        delp (xr.DataArray): pressure thickness
+        dim (str, optional): name of vertical dimension. Defaults to "pfull".
+
+    Returns:
+        xr.DataArray: layer thicknesses dz
+    """
     pi = pressure_at_interface(delp, dim_center=dim, dim_outer=dim)
     epsilon = RDGAS / RVGAS
     tv = T * (1 + q / epsilon) / (1 + q)
-    # tv = (1 + 0.61 * q) * T
     dlogp = np.log(pi).diff(dim, label="lower")
     return -dlogp * RDGAS * tv / GRAVITY
 
 
 def dz_and_top_to_phis(top_height, dz, dim=COORD_Z_CENTER):
-    """ Compute surface geopotential from model top height and layer thicknesses """
+    """ Compute surface geopotential from model top height and layer thicknesses"""
     return GRAVITY * (top_height + dz.sum(dim=dim))
