0fefcbfa3ee009b4818daa6ed58c2f9fc482d647
Anna Kwa <akwa@uci.edu>
2020-01-03 13:12:03 -0800

    fix failing tests

--- tests/test_sklearn_wrapper.py
+++ tests/test_sklearn_wrapper.py
@@ -1,10 +1,10 @@
 import numpy as np
-import pytest
 import xarray as xr
 
 from fv3net.regression.sklearn.wrapper import _pack
 
-def test_flatten(test_packer):
+
+def test_flatten():
     x = np.ones((3, 4, 5))
     shape = (3, 4, 5)
     dims = "x y z".split()
@@ -15,11 +15,11 @@ def test_flatten(test_packer):
     a = xr.DataArray(x, dims=dims)
     ds = xr.Dataset({"a": a, "b": a})
 
-    ans = _pack.flatten(ds, sample_dim)
+    ans = _pack(ds, sample_dim)[0]
     assert ans.shape == (nz, 2 * nx * ny)
 
 
-def test_flatten_1d_input(test_packer):
+def test_flatten_1d_input():
     x = np.ones((3, 4, 5))
     shape = (3, 4, 5)
     dims = "x y z".split()
@@ -30,17 +30,17 @@ def test_flatten_1d_input(test_packer):
     a = xr.DataArray(x, dims=dims)
     ds = xr.Dataset({"a": a, "b": a.isel(x=0, y=0)})
 
-    ans = _pack(ds, sample_dim)
+    ans = _pack(ds, sample_dim)[0]
     assert ans.shape == (nz, nx * ny + 1)
 
 
-def test_flatten_same_order(test_packer):
+def test_flatten_same_order():
     nx, ny = 10, 4
     x = xr.DataArray(np.arange(nx * ny).reshape((nx, ny)), dims=["sample", "feature"])
 
     ds = xr.Dataset({"a": x, "b": x.T})
     sample_dim = "sample"
-    a = _pack(ds[["a"]], sample_dim)
-    b = _pack(ds[["b"]], sample_dim)
+    a = _pack(ds[["a"]], sample_dim)[0]
+    b = _pack(ds[["b"]], sample_dim)[0]
 
     np.testing.assert_allclose(a, b)
