9c5ba8dc667dc99275b409455619bc6e007f6be5
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-01-10 14:08:46 -0800

    Update submit_jobs.py for new one_step workflow

--- workflows/one_step_jobs/submit_jobs.py
+++ workflows/one_step_jobs/submit_jobs.py
@@ -12,50 +12,47 @@ import fv3config
 
 logger = logging.getLogger("run_jobs")
 
+RUN_DURATION = timedelta(minutes=15)
+RUN_TIMESTEP = 60  # seconds
 
-PARENT_BUCKET = (
-    "gs://vcm-ml-data/2019-10-28-X-SHiELD-2019-10-05-multiresolution-extracted/"
-)
-
-INPUT_BUCKET = PARENT_BUCKET + "restart/C48/"
+PARENT_BUCKET = "gs://vcm-ml-data/2020-01-06-X-SHiELD-2019-12-02-restarts-and-rundirs/"
+INPUT_BUCKET = PARENT_BUCKET + "restarts/C48/"
 OUTPUT_BUCKET = PARENT_BUCKET + "one_step_output/C48/"
 CONFIG_BUCKET = PARENT_BUCKET + "one_step_config/C48/"
 DOCKER_IMAGE = "us.gcr.io/vcm-ml/fv3gfs-python"
-LOCAL_RUNFILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "runfile.py")
-LOCAL_DIAG_TABLE = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "diag_table"
-)
+LOCAL_PARENT_DIR = os.path.dirname(os.path.abspath(__file__))
+LOCAL_FILES_TO_UPLOAD = ["runfile.py", "diag_table", "fv_core.res.nc"]
+
+RESTART_CATEGORIES = ["fv_core.res", "fv_srf_wnd.res", "fv_tracer.res", "sfc_data"]
+TILES = range(1, 7)
 
 SECONDS_IN_MINUTE = 60
-SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60
-SECONDS_IN_DAY = SECONDS_IN_HOUR * 24
-
-
-def set_run_duration(config: dict, duration: timedelta) -> dict:
-    return_config = copy.deepcopy(config)
-    coupler_nml = return_config["namelist"]["coupler_nml"]
-    total_seconds = duration.total_seconds()
-    if total_seconds % 1 != 0:
-        raise ValueError("duration must be an integer number of seconds")
-    coupler_nml["months"] = 0
-    coupler_nml["hours"] = 0
-    coupler_nml["minutes"] = 0
-    days = int(total_seconds / SECONDS_IN_DAY)
-    coupler_nml["days"] = days
-    coupler_nml["seconds"] = int(total_seconds - (days * SECONDS_IN_DAY))
-    return return_config
+SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE
 
 
 def timestep_from_url(url: str) -> str:
     return str(Path(url).name)
 
 
-def list_timesteps(bucket: str) -> List[str]:
+def list_timesteps(bucket: str) -> set:
     """Returns the unique timesteps in a bucket"""
-    ls_output = subprocess.check_output(["gsutil", "ls", bucket])
+    try:
+        ls_output = subprocess.check_output(["gsutil", "ls", bucket])
+    except subprocess.CalledProcessError:
+        return set()
     file_list = ls_output.decode("UTF-8").strip().split()
     timesteps = map(timestep_from_url, file_list)
     return set(timesteps)
+
+
+def time_list_from_timestep(timestep: str) -> List[int]:
+    year = int(timestep[:4])
+    month = int(timestep[4:6])
+    day = int(timestep[6:8])
+    hour = int(timestep[9:11])
+    minute = int(timestep[11:13])
+    second = int(timestep[13:15])
+    return [year, month, day, hour, minute, second]
 
 
 def timesteps_to_process() -> List[str]:
@@ -67,16 +64,60 @@ def timesteps_to_process() -> List[str]:
     return sorted(list(set(to_do) - set(done)))
 
 
+def turn_off_physics(config):
+    new_config = copy.deepcopy(config)
+    new_config["namelist"]["atmos_model_nml"]["dycore_only"] = True
+    new_config["namelist"]["fv_core_nml"]["do_sat_adj"] = False
+    new_config["namelist"]["gfdl_cloud_microphysics_nml"]["fast_sat_adj"] = False
+    return new_config
+
+
+def get_initial_condition_patch_files(timestep):
+    patch_files = [
+        fv3config.get_asset_dict(
+            os.path.join(INPUT_BUCKET, timestep),
+            f"{timestep}.{category}.tile{tile}.nc",
+            target_location="INPUT",
+            target_name=f"{category}.tile{tile}.nc",
+        )
+        for category in RESTART_CATEGORIES
+        for tile in TILES
+    ]
+    patch_files += [
+        fv3config.get_asset_dict(
+            os.path.join(CONFIG_BUCKET, timestep),
+            "fv_core.res.nc",
+            target_location="INPUT",
+        ),
+    ]
+    return patch_files
+
+
 def get_config(timestep):
     config = fv3config.get_default_config()
     config = fv3config.enable_restart(config)
     config["experiment_name"] = f"one-step.{timestep}.{uuid.uuid4()}"
-    config["initial_conditions"] = os.path.join(INPUT_BUCKET, timestep, "rundir/INPUT")
-    config = set_run_duration(config, timedelta(minutes=15))
-    config["namelist"]["coupler_nml"].update({"dt_atmos": 60, "dt_ocean": 60})
+    config["diag_table"] = os.path.join(CONFIG_BUCKET, timestep, "diag_table")
+    # all files from restart_example initial conditions (except coupler.res) will
+    # be overwritten by patch_files. But coupler.res will be ignored since
+    # force_date_from_namelist=True
+    config["initial_conditions"] = "restart_example"
+    config = fv3config.set_run_duration(config, RUN_DURATION)
+    config["patch_files"] = get_initial_condition_patch_files(timestep)
+    config["namelist"]["coupler_nml"].update(
+        {
+            "dt_atmos": RUN_TIMESTEP,
+            "dt_ocean": RUN_TIMESTEP,
+            "restart_secs": RUN_TIMESTEP,
+            "current_date": time_list_from_timestep(timestep),
+            "force_date_from_namelist": True,
+        }
+    )
+    config["namelist"]["atmos_model_nml"]["fhout"] = RUN_TIMESTEP / SECONDS_IN_HOUR
     config["namelist"]["fv_core_nml"].update(
         {"external_eta": True, "npz": 79, "k_split": 1, "n_split": 1}
     )
+    config = turn_off_physics(config)
     return config
 
 
@@ -85,11 +126,10 @@ def submit_jobs(timestep_list: List[str]) -> None:
     for timestep in timestep_list:
         config = get_config(timestep)
         job_name = config["experiment_name"]
-        runfile_location = os.path.join(CONFIG_BUCKET, timestep, "runfile.py")
-        fs.put(LOCAL_RUNFILE, runfile_location)
-        diag_table_location = os.path.join(CONFIG_BUCKET, timestep, "diag_table")
-        fs.put(LOCAL_DIAG_TABLE, diag_table_location)
-        config["diag_table"] = diag_table_location
+        for filename in LOCAL_FILES_TO_UPLOAD:
+            file_local = os.path.join(LOCAL_PARENT_DIR, filename)
+            file_remote = os.path.join(CONFIG_BUCKET, timestep, filename)
+            fs.put(file_local, file_remote)
         config_location = os.path.join(CONFIG_BUCKET, timestep, "fv3config.yml")
         with fs.open(config_location, "w") as config_file:
             config_file.write(yaml.dump(config))
@@ -98,7 +138,7 @@ def submit_jobs(timestep_list: List[str]) -> None:
             config_location,
             outdir,
             DOCKER_IMAGE,
-            runfile=runfile_location,
+            runfile=os.path.join(CONFIG_BUCKET, timestep, "runfile.py"),
             jobname=job_name,
             namespace="default",
             memory_gb=3.6,
