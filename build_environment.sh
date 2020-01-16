d246401a38655bf6d6b66a2f75f287d574cc0a6a
Noah D. Brenowitz <nbren12@gmail.com>
2020-01-16 15:30:14 -0800

    rename activate_conda script

--- build_environment.sh
+++ build_environment.sh
@@ -7,6 +7,6 @@ CONDA_BASE=$(conda info --base)
 conda env create -n $CONDA_ENV -f environment.yml  2> /dev/null || \
 	echo "Conda env already exists proceeding to VCM package installation"
 
-source activateEnv fv3net
+source activate_conda_from_subshell fv3net
 conda develop . external/vcm external/fv3config
 
