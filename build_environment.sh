6551587a656e61e5cdcb2b91c1b289adabc489da
Noah D. Brenowitz <nbren12@gmail.com>
2020-01-16 12:18:32 -0800

    Use conda-develop over setup.py develop

--- build_environment.sh
+++ build_environment.sh
@@ -3,9 +3,6 @@
 CONDA_ENV=$1
 CONDA_BASE=$(conda info --base)
 
-function setupPythonPackage {
-( cd $1 && python setup.py develop)
-}
 
 function activateEnv {
     source $CONDA_BASE/etc/profile.d/conda.sh
@@ -17,7 +14,5 @@ conda env create -n $CONDA_ENV -f environment.yml  2> /dev/null || \
 	echo "Conda env already exists proceeding to VCM package installation"
 
 activateEnv
-setupPythonPackage .
-setupPythonPackage external/vcm
-setupPythonPackage external/fv3config
+conda develop . external/vcm external/fv3config
 
