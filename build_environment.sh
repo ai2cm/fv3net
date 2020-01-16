36703fe1aed60d10629237144c85eeec4ebe9bfd
Noah D. Brenowitz <nbren12@gmail.com>
2020-01-16 15:05:01 -0800

    make script to activate conda from within subshell

--- build_environment.sh
+++ build_environment.sh
@@ -4,15 +4,9 @@ CONDA_ENV=$1
 CONDA_BASE=$(conda info --base)
 
 
-function activateEnv {
-    source $CONDA_BASE/etc/profile.d/conda.sh
-    conda activate $CONDA_ENV
-}
-
-
 conda env create -n $CONDA_ENV -f environment.yml  2> /dev/null || \
 	echo "Conda env already exists proceeding to VCM package installation"
 
-activateEnv
+source activateEnv fv3net
 conda develop . external/vcm external/fv3config
 
