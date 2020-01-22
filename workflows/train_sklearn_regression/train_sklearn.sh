3bd6258efd330f33804d095c197d470fb00667fc
Anna Kwa <akwa@uci.edu>
2020-01-22 14:58:42 -0800

    Add workflow for ML pipeline

---
+++ workflows/train_sklearn_regression/train_sklearn.sh
@@ -0,0 +1,5 @@
+ python -m fv3net.regression.sklearn.train \
+  --train-config-file rf_train_config.yml \
+  --model-output-filename rf_model.pkl \
+  --target-normalization-file default \
+  --output-dir outputs_sklearn_regression
