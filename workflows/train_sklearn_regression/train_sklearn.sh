40169acc4df99d279e2aa02ab4e9d4bb12c6966b
Anna Kwa <annak@vulcan.com>
2020-01-24 13:32:04 -0800

    Feature/ml pipeline (#81)
    Adds a workflow to train an sklearn model on batches of data.

---
+++ workflows/train_sklearn_regression/train_sklearn.sh
@@ -0,0 +1,4 @@
+ python -m fv3net.regression.sklearn.train \
+  --train-config-file example_rf_training_config.yml \
+  --target-normalization-file default \
+  --output-dir outputs_sklearn_regression
