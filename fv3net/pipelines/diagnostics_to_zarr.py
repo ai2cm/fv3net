1a1e2e9234a1a8dd8792c781b22282dcdbf4b207
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-01-24 09:52:36 -0800

    Determine diagnostic categories if none provided

--- fv3net/pipelines/diagnostics_to_zarr.py
+++ fv3net/pipelines/diagnostics_to_zarr.py
@@ -14,14 +14,7 @@ logger = logging.getLogger(__name__)
 
 INITIAL_CHUNKS = {"time": 12}
 TILES = range(1, 7)
-PARENT_BUCKET = "gs://vcm-ml-data/2019-12-12-baseline-FV3GFS-runs/"
-DIAGNOSTIC_CATEGORIES = [
-    "atmos_8xdaily",
-    "tracers_8xdaily",
-    "nudge_dt_8xdaily",
-    "atmos_hourly",
-    "sfc_hourly"
-]
+COMMON_SUFFIX = ".tile1.nc"
 
 
 def run(args, pipeline_args):
@@ -30,17 +23,41 @@ def run(args, pipeline_args):
         diagnostic_dir = os.path.join(_get_parent_dir(rundir), "diagnostic_zarr")
     else:
         diagnostic_dir = args.diagnostic_dir
+    if args.diagnostic_categories is None:
+        diagnostic_categories = _get_all_diagnostic_categories(rundir, _get_fs(rundir))
+    else:
+        diagnostic_categories = args.diagnostic_categories
 
     beam_options = PipelineOptions(flags=pipeline_args, save_main_session=True)
     with beam.Pipeline(options=beam_options) as p:
         (
             p
-            | beam.Create(args.diagnostic_categories)
+            | beam.Create(diagnostic_categories)
             | "OpenConvertSave"
             >> beam.ParDo(
                 open_convert_save, rundir=rundir, diagnostic_dir=diagnostic_dir
             )
         )
+
+
+def _get_fs(path):
+    """Return the fsspec filesystem required to handle a given path."""
+    if path.startswith("gs://"):
+        return fsspec.filesystem("gs")
+    else:
+        return fsspec.filesystem("file")
+
+
+def _get_all_diagnostic_categories(rundir, fs):
+    """ get full paths for all files in rundir that end in COMMON_SUFFIX """
+    full_paths = fs.glob(os.path.join(rundir, f"*{COMMON_SUFFIX}"))
+    return [_get_category_from_path(path) for path in full_paths]
+
+
+def _get_category_from_path(path):
+    """ get part of filename before COMMON_SUFFIX """
+    basename = os.path.basename(path)
+    return basename[:-len(COMMON_SUFFIX)]
 
 
 def _get_parent_dir(path):
@@ -78,16 +95,16 @@ if __name__ == "__main__":
         "--diagnostic-dir",
         type=str,
         default=None,
-        help="Location to save zarr store. Defaults to the parent of rundir.",
+        help="Location to save zarr stores. Defaults to the parent of rundir.",
     )
     parser.add_argument(
         "--diagnostic-categories",
         type=str,
-        default=DIAGNOSTIC_CATEGORIES,
+        default=None,
         nargs="+",
-        help="One or more categories of diagnostic files. Provide everything before "
-        ".tile*.nc. Defaults to atmos_8xdaily, tracers_8xdaily, nudge_dt_8xdaily, "
-        "atmos_hourly, sfc_hourly.",
+        help="Optionally specify one or more categories of diagnostic files. Provide "
+        "part of filename before .tile*.nc. Defaults to all diagnostic categories in "
+        "rundir.",
     )
     args, pipeline_args = parser.parse_known_args()
 
--- workflows/diagnostics_to_zarr/submit_local.sh
+++ workflows/diagnostics_to_zarr/submit_local.sh
@@ -3,6 +3,5 @@
 PIPELINE=fv3net.pipelines.diagnostics_to_zarr
 EXAMPLE_RUNDIR=gs://vcm-ml-data/2019-12-12-baseline-FV3GFS-runs/free/C48/free-2016.93319727-0c4b-4658-8f15-b279b38ae360/output
 
-
 # submit the job
 python -m $PIPELINE --rundir $EXAMPLE_RUNDIR
