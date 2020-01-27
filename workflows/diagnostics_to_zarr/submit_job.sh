ea581781a7fdd3089a806dd811d1e6d5d7463a05
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-01-27 23:25:23 +0000

    Use bigger worker, pass vcm extra package

--- workflows/diagnostics_to_zarr/submit_job.sh
+++ workflows/diagnostics_to_zarr/submit_job.sh
@@ -1,7 +1,6 @@
 # submit the job
 PIPELINE=fv3net.pipelines.diagnostics_to_zarr
 RUNDIR=$1
-NUM_WORKERS=6
 
 python -m $PIPELINE  \
     --rundir $RUNDIR \
@@ -11,7 +10,8 @@ python -m $PIPELINE  \
     --runner DataflowRunner \
     --setup_file workflows/diagnostics_to_zarr/setup.py \
     --temp_location gs://vcm-ml-data/tmp_dataflow \
-    --num_workers $NUM_WORKERS \
-    --max_num_workers $NUM_WORKERS \
-    --disk_size_gb 120 \
-    --worker_machine_type n1-highmem-4
+    --num_workers 1 \
+    --max_num_workers 5 \
+    --disk_size_gb 256 \
+    --worker_machine_type n1-highmem-16 \
+    --extra_package external/vcm/dist/vcm-0.1.0.tar.gz
