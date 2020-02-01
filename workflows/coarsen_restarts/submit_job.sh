e13fd32f0d118fd37219505e648113c33aff047e
Andre Perkins <frodre@gmail.com>
2020-02-01 01:02:38 +0000

    Fix GCS_DST to correct directory from coarsening 40-day operation

--- workflows/coarsen_restarts/submit_job.sh
+++ workflows/coarsen_restarts/submit_job.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 GCS_SRC="gs://vcm-ml-data/2019-12-02-40-day-X-SHiELD-simulation-C384-restart-files"
-GCS_DST="gs://vcm-ml-data/2020-01-06-X-SHiELD-2019-12-02-coarsened-restarts"
+GCS_DST="gs://vcm-ml-data/2020-01-16-X-SHiELD-2019-12-02-pressure-coarsened-rundirs/restarts"
 GCS_GRIDSPEC="gs://vcm-ml-data/2020-01-06-C384-grid-spec-with-area-dx-dy"
 
 SRC_RESOLUTION=384
