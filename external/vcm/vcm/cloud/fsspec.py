3813ee732c554a08edaa80ced94212dcd4b693c0
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-02-05 15:24:20 -0800

    Update one-step-jobs for model config and success checking (#110)
    * allow multiple model configurations for one-step jobs without having to edit job submission script
    
    * provide two examples of configurations, one with all physics off, one with just microphysics and deep convection off
    
    * check whether runs that exist in output_url successfully completed instead of just checking whether output directory exists for a particular timestep (thanks @frodre)
    
    * allow user to specify input/output locations when submitting workflow
    
    * add better documentation
    
    Co-authored-by: W. Andre Perkins <frodre@gmail.com>

---
+++ external/vcm/vcm/cloud/fsspec.py
@@ -0,0 +1,23 @@
+import fsspec
+
+
+def get_protocol(path: str) -> str:
+    """Return fsspec filesystem protocol corresponding to path
+
+    Args:
+        path: local or remote path
+
+    Returns:
+        "file" unless "://" exists in path, in which case returns part of
+        path preceding "://"
+    """
+    if "://" in path:
+        protocol = path.split("://")[0]
+    else:
+        protocol = "file"
+    return protocol
+
+
+def get_fs(path: str) -> fsspec.AbstractFileSystem:
+    """Return fsspec filesystem object corresponding to path"""
+    return fsspec.filesystem(get_protocol(path))
