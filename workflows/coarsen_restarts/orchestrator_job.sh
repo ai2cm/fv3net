d7fb50b47a5de31f4763726ef92a5aac7654c51f
Andre Perkins <frodre@gmail.com>
2020-02-06 22:35:49 +0000

    Add orchestrator specific job script

--- workflows/coarsen_restarts/orchestrator_job.sh
+++ workflows/coarsen_restarts/orchestrator_job.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-GCS_SRC="gs://vcm-ml-data/2019-12-02-40-day-X-SHiELD-simulation-C384-restart-files"
-GCS_DST="gs://vcm-ml-data/2020-01-16-X-SHiELD-2019-12-02-pressure-coarsened-rundirs/restarts"
+GCS_SRC=$1
+GCS_DST=$2
 GCS_GRIDSPEC="gs://vcm-ml-data/2020-01-06-C384-grid-spec-with-area-dx-dy"
 
-SRC_RESOLUTION=384
-TARGET_RESOLUTION=48
+SRC_RESOLUTION=$3
+TARGET_RESOLUTION=$4
 
 python -m fv3net.pipelines.coarsen_restarts\
     --gcs-src-dir $GCS_SRC \
