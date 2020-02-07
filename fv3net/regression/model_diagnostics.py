47ec1479570fff47342774041532f4887658d7a3
Anna Kwa <akwa@uci.edu>
2020-02-06 16:10:27 -0800

    Add main func and run script

--- fv3net/regression/model_diagnostics.py
+++ fv3net/regression/model_diagnostics.py
@@ -25,6 +25,19 @@ STACK_DIMS = ["tile", INIT_TIME_DIM, COORD_X_CENTER, COORD_Y_CENTER]
 OUTPUT_FIG_DIR = "model_performance_plots"
 
 
+def _predict_on_test_data(
+        test_data_path, model_path, num_test_zarrs, model_type="rf"):
+    if model_type=="rf":
+        from .sklearn.test import load_test_dataset, load_model, predict_dataset
+        ds_test = load_test_dataset(test_data_path, num_test_zarrs)
+        sk_wrapped_model = load_model(model_path)
+        ds_pred = predict_dataset(sk_wrapped_model, ds_test)
+        return ds_test, ds_pred
+    else:
+        raise ValueError("Cannot predict using model type {model_type},"
+                         "only 'rf' is currently implemented.")
+
+
 def _load_high_res_dataset(coarsened_hires_diags_path, init_times):
     fs = fsspec.filesystem("gs")
     ds_hires = xr.open_zarr(
@@ -153,7 +166,7 @@ def _plot_diurnal_cycle(
     plt.show()
 
 
-def make_all_plots(ds_pred, ds_target, ds_hires, grid):
+def make_all_plots(ds_pred, ds_target, ds_hires, grid, output_dir):
     for ds in [ds_pred, ds_target, ds_hires]:
         if not set(STACK_DIMS).issubset(ds.dims):
             raise ValueError(f"Make sure all datasets are unstacked,"
@@ -162,13 +175,12 @@ def make_all_plots(ds_pred, ds_target, ds_hires, grid):
     ds_pred["P-E"] = mass_integrate(-ds_pred["Q2"], ds_pred.delp) * kg_m2s_to_mm_day
     ds_target["P-E"] = mass_integrate(-ds_target["Q2"], ds_target.delp) * kg_m2s_to_mm_day
 
+    # for convenience, separate the land/sea data
     slmsk = ds_target.isel({COORD_Z_CENTER: -1, INIT_TIME_DIM: 0}).slmsk
-
     ds_pred_sea = mask_to_surface_type(xr.merge([ds_pred, slmsk]), "sea").drop("slmsk")
     ds_target_sea = mask_to_surface_type(xr.merge([ds_target, slmsk]), "sea").drop("slmsk")
     ds_pred_land = mask_to_surface_type(xr.merge([ds_pred, slmsk]), "land").drop("slmsk")
     ds_target_land  = mask_to_surface_type(xr.merge([ds_target, slmsk]), "land").drop("slmsk")
-
     ds_pe = _merge_comparison_datasets(
         "P-E",
         ds_pred,
@@ -176,48 +188,100 @@ def make_all_plots(ds_pred, ds_target, ds_hires, grid):
         ds_hires,
         grid)
 
-    ds_pe["solar_time"] = solar_time(ds_pe)
-    matplotlib.rcParams['figure.dpi'] = 80
-    _plot_diurnal_cycle(
-        mask_to_surface_type(ds_pe, "sea"),
-        "P-E",
-        title="ocean",
-        plot_filename="diurnal_cycle_P-E_sea.png")
-    _plot_diurnal_cycle(
-        mask_to_surface_type(ds_pe, "land"),
-        "P-E",
-        title="land",
-        plot_filename="diurnal_cycle_P-E_land.png")
-
-    matplotlib.rcParams['figure.dpi'] = 200
-    fig_pe = plot_cube(
-        mappable_var(ds_pe.isel({INIT_TIME_DIM: 0}), "P-E"),
-        col="dataset",
-        cbar_label="P-E [mm/day]")[0]
-    fig_pe.savefig(os.path.join(OUTPUT_FIG_DIR, "P-E.png"))
-    plt.show()
-
+    # R^2 vs pressure plots
     matplotlib.rcParams['figure.dpi'] = 70
     _make_r2_plot(
         ds_pred,
         ds_target,
         ["Q1", "Q2"],
-        output_dir=OUTPUT_FIG_DIR,
+        output_dir=output_dir,
         plot_filename="r2_vs_pressure_level_global.png",
         title="$R^2$, global")
     _make_land_sea_r2_plot(
         ds_pred_sea, ds_pred_land, ds_target_sea, ds_target_land, vars=["Q1", "Q2"]
     )
 
+    # plot a variable across the diurnal cycle
+    ds_pe["solar_time"] = solar_time(ds_pe)
+    matplotlib.rcParams['figure.dpi'] = 80
+    _plot_diurnal_cycle(
+        mask_to_surface_type(ds_pe, "sea"),
+        "P-E",
+        title="ocean",
+        output_dir=output_dir,
+        plot_filename="diurnal_cycle_P-E_sea.png")
+    _plot_diurnal_cycle(
+        mask_to_surface_type(ds_pe, "land"),
+        "P-E",
+        title="land",
+        output_dir=output_dir,
+        plot_filename="diurnal_cycle_P-E_land.png")
+
+    # map plot a variable and compare across prediction/ C48 /coarsened high res data
+    matplotlib.rcParams['figure.dpi'] = 200
+    fig_pe = plot_cube(
+        mappable_var(ds_pe.isel({INIT_TIME_DIM: 0}), "P-E"),
+        col="dataset",
+        cbar_label="P-E [mm/day]")[0]
+    fig_pe.savefig(os.path.join(output_dir, "P-E.png"))
+    plt.show()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--test-data-path",
+        type=str,
+        required=True,
+        help="Path to directory containing test data zarrs. Can be local or remote."
+    )
+    parser.add_argument(
+        "--model-path",
+        type=str,
+        required=True,
+        help="Model file location. Can be local or remote."
+    )
+    parser.add_argument(
+        "--high-res-data-path",
+        type=str,
+        required=True,
+        help="Path to C48 coarsened high res diagnostic data."
+    )
+    parser.add_argument(
+        "--num-test-zarrs",
+        type=int,
+        default=5,
+        help="Number of zarrs to concat together for use as test set."
+    )
+    parser.add_argument(
+        "--model-type",
+        type=str,
+        default="rf",
+        help="Type of model to use. Default is random forest 'rf'. "
+             "The only type implement right now is 'rf'."
+    )
     parser.add_argument(
         "--output-dir-suffix",
         type=str,
         default="sklearn_regression",
         help="Directory suffix to write files to. Prefixed with today's timestamp."
     )
-    args=parser.parse_args()
+    args = parser.parse_args()
+
     timestamp = datetime.now().strftime("%Y%m%d.%H%M%S")
     output_dir = f"{timestamp}_{args.output_dir_suffix}"
+
+    ds_test, ds_pred = _predict_on_test_data(
+        args.test_data_path,
+        args.model_path,
+        args.num_test_zarrs,
+        args.model_type
+    )
+    init_times = list(set(ds_test[INIT_TIME_DIM].values))
+    ds_hires = _load_high_res_dataset(args.high_res_data_path, init_times)
+
+    grid_path = os.path.join(os.path.dirname(args.test_data_path), "grid_spec.zarr")
+    fs = fsspec.filesystem("gs")
+    grid = xr.open_zarr(fs.get_mapper(grid_path))
+    make_all_plots(ds_pred, ds_test, ds_hires, grid, output_dir)
+
