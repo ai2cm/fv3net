fa28bfda785b8e4b8217dfc3d50e30f3722efca3
Andre Perkins <frodre@gmail.com>
2020-02-08 22:56:55 +0000

    Update orchestrate script with submit_utils updates after merge

--- workflows/one_step_jobs/orchestrate_jobs.py
+++ workflows/one_step_jobs/orchestrate_jobs.py
@@ -2,28 +2,35 @@ import argparse
 import os
 import logging
 import subprocess
+import yaml
+from pathlib import Path
 
 import submit_utils
 
 PWD = os.path.dirname(os.path.abspath(__file__))
-DOCKER_IMAGE = "us.gcr.io/vcm-ml/fv3gfs-python"
-LOCAL_RUNFILE = os.path.join(PWD, "runfile.py")
-LOCAL_DIAG_TABLE = os.path.join(PWD, "diag_table")
+CONFIG_DIRECTORY_NAME = "one_step_config"
 
 
 def _create_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "input_gcs_path",
+        "one_step_yaml",
         type=str,
         required=True,
-        help="Source input restart files for one-step simulations",
+        help="Path to local run configuration yaml.",
     )
     parser.add_argument(
-        "output_gcs_path",
+        "input_url",
         type=str,
         required=True,
-        help="Output destination for one-step configurations and simulation output"
+        help="Remote url to initial conditions. Initial conditions are assumed to be "
+        "stored as INPUT_URL/{timestamp}/{timestamp}.{restart_category}.tile*.nc",
+    )
+    parser.add_argument(
+        "output_url",
+        type=str,
+        required=True,
+        help="Remote url where model output will be saved."
     )
     parser.add_argument(
         "experiment_label",
@@ -32,31 +39,26 @@ def _create_arg_parser():
         help="Label to group the individual Kubernetes jobs with",
     )
     parser.add_argument(
-        "--config-bucket",
+        "-o",
+        "--overwrite",
+        required=False,
+        action="store_true",
+        help="Overwrite successful timesteps in OUTPUT_URL.",
+    )
+    parser.add_argument(
+        "--n-steps",
+        type=int,
+        default=None,
+        required=False,
+        help="Number of timesteps to process. By default all timesteps "
+        "found in INPUT_URL for which successful runs do not exist in "
+        "OUTPUT_URL will be processed. Useful for testing.",
+    )
+    parser.add_argument(
+        "--config-url",
         type=str,
         required=False,
         help="Storage path for job configuration files"
-    )
-    parser.add_argument(
-        "--local-runfile",
-        type=str,
-        required=False,
-        default=LOCAL_RUNFILE,
-        help="Path to local runfile for performing a simulation using an FV3GFS Docker image",
-    )
-    parser.add_argument(
-        "--local-diag-table",
-        type=str,
-        required=False,
-        default=LOCAL_DIAG_TABLE,
-        help="Path to local diag table file for the FV3GFS run",
-    )
-    parser.add_argument(
-        "--docker-image",
-        type=str,
-        required=False,
-        default=DOCKER_IMAGE,
-        help="Docker image to pull for the kubernetes job",
     )
 
     return parser
@@ -74,16 +76,21 @@ if __name__ == "__main__":
     parser = _create_arg_parser()
     args = parser.parse_args()
 
-    if not args.config_bucket:
-        config_bucket = args.output_gcs_path + "/config"
-    else:
-        config_bucket = args.config_bucket
+    with open(args.one_step_yaml) as file:
+        one_step_config = yaml.load(file, Loader=yaml.FullLoader)
+    workflow_name = Path(args.one_step_yaml).with_suffix("").name
 
-    timestep_list = submit_utils.timesteps_to_process(args.input_gcs_path, args.output_gcs_path)
+    if not args.config_url:
+        config_url = os.path.join(args.output_gcs_path, CONFIG_DIRECTORY_NAME)
+    else:
+        config_url = args.config_url
+
+    timestep_list = submit_utils.timesteps_to_process(
+        args.input_url, args.output_url, args.n_steps, args.overwrite
+    )
     submit_utils.submit_jobs(
-        timestep_list, config_bucket, args.local_runfile,
-        args.local_diag_table, args.docker_image, args.output_gcs_path,
-        experiment_label=args.experiment_label
+        timestep_list, workflow_name, one_step_config, args.input_url, args.output_url,
+        config_url, experiment_label=args.experiment_label
     )
 
     wait_for_complete(args.experiment_label)
