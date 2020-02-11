de7ba4c02a71a101448164d0682f7cab99f958a8
Andre Perkins <frodre@gmail.com>
2020-02-11 01:07:24 +0000

    Optional unique ids and add ability to match unique id on experiment label

--- workflows/end_to_end/get_experiment_args.py
+++ workflows/end_to_end/get_experiment_args.py
@@ -18,7 +18,7 @@ def get_experiment_args(args):
     return json.dumps(all_step_arguments)
 
 
-def _get_experiment_path(config):
+def _get_experiment_path(config, unique_id=None):
 
     proto = config['storage_proto']
     root = config['storage_root']
@@ -29,19 +29,28 @@ def _get_experiment_path(config):
 
     experiment_name = f"{experiment['name']}-{str(uuid.uuid4())[-7:]}"
 
+    if unique_id is not None:
+        experiment_name = f"{experiment_name}-{unique_id}"
+
     return f"{proto}://{root}/{experiment_name}"
 
 
 def _get_all_step_arguments(workflow_steps, config):
     """Get a dictionary of each step with i/o and methedological arguments"""
-    root_experiment_path = _get_experiment_path(config)
+    
+    if config['experiment']['unique_id']:
+        unique_id = {str(uuid.uuid4())[-8:]
+    else:
+        unique_id = None
+    
+    root_experiment_path = _get_experiment_path(config, unique_id=unique_id)
     steps_config = config["experiment"]["steps"]
     all_step_arguments = {}
     for i, step in enumerate(workflow_steps):
         curr_config = steps_config[step]
         input_location = _get_input_location(step, curr_config, steps_config, root_experiment_path)
         output_location = _get_output_location(step, curr_config, root_experiment_path)
-        method_args = _generate_method_args(curr_config)
+        method_args = _generate_method_args(curr_config, unique_id=unique_id)
         step_args = " ".join([input_location, output_location, method_args])
         all_step_arguments[step] = step_args
 
@@ -103,15 +112,24 @@ def _generate_output_path_from_config(step_name, step_config, max_config_stubs=3
     return output_str
 
 
-def _generate_method_args(step_config):
+def _generate_method_args(step_config, unique_id=None):
     """generate the methodlogical arguments for the step"""
     method_config = step_config.get("method", None)
-    if method_config is not None:
-        method_args = " ".join([str(val) for val in method_config.values()])
-    else:
-        method_args = ""
+    methods_to_add_unique_id = step_config.get("add_unique_id", [])
 
-    return method_args
+    if method_config is not None:
+        method_args = []
+        for key, val in method_config.items():
+            if key in methods_to_add_unique_id and unique_id is not None:
+                val = f"{val}-{unique_id}"
+            else:
+                val = str(val)
+            method_args.append(val)
+        combined_method_args = " ".join(method_args)
+    else:
+        combined_method_args = ""
+
+    return combined_method_args
 
 
 if __name__ == "__main__":
