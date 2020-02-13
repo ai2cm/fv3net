c563f99568eea7b14dad525417f76678fe148373
Andre Perkins <frodre@gmail.com>
2020-02-13 00:50:54 +0000

    Refactor configuration for more flexible I/O value and top-level value linking

--- workflows/end_to_end/get_experiment_args.py
+++ workflows/end_to_end/get_experiment_args.py
@@ -11,14 +11,100 @@ def get_experiment_args(args):
     with open(args.config_file, 'r') as f:
         config = yaml.safe_load(f)
 
-    experiment_config = config['experiment']
-    workflow_steps = experiment_config['workflow_steps']
+    _apply_config_transforms(config)
+    workflow_steps = config["experiment"]['workflow_steps']
     all_step_arguments = _get_all_step_arguments(workflow_steps, config)
 
     return json.dumps(all_step_arguments)
 
 
-def _get_experiment_path(config, unique_id=None):
+def _apply_config_transforms(config):
+
+    _add_unique_id(config)
+    _use_top_level_var(config)
+    _resolve_output_location(config)
+    _resolve_input_from(config)
+
+
+def _add_unique_id(config):
+
+    exp_config = config['experiment']
+    if not exp_config['unique_id']:
+        return
+
+    unique_id = str(uuid.uuid4())[-8:]
+    exp_name = exp_config['name']
+    exp_config['name'] = exp_name + f"-{unique_id}"
+
+    for step_name, step_config in exp_config['steps'].items():
+        config_transforms = step_config.get('config_transforms', None)
+
+        if config_transforms and 'add_unique_id' in config_transforms:
+            vars_to_add_unique = step_config['config_transforms']['add_unique_id']
+            for config_var in vars_to_add_unique:
+                method_val = step_config['method'][config_var]
+                method_val += f"-{unique_id}"
+                step_config['method'][config_var] = method_val
+
+
+def _resolve_output_location(config):
+
+    root_exp_path = _get_experiment_path(config)
+    all_steps_config = config['experiment']['steps']
+
+    for step_name, step_config in all_steps_config.items():
+
+        if "output_location" in step_config:
+            continue
+        else:
+            output_stub = _generate_output_path_from_config(step_name, step_config)
+            location = os.path.join(root_exp_path, output_stub)
+            step_config['output_location'] = location
+
+
+def _resolve_input_from(config):
+
+    all_steps_config = config['experiment']['steps']
+
+    for step_name, step_config in all_steps_config.items():
+        input_config = step_config['inputs']
+
+        for input_source, source_info in input_config.items():
+            location = source_info.get('location', None)
+            from_key = source_info.get('from', None)
+            
+            if location is not None:
+                continue
+            elif from_key is not None:
+                source_info['location'] = all_steps_config[from_key]["output_location"]
+            else:
+                raise KeyError(
+                    f"Input section of {step_name} should have either 'location' "
+                    "or 'from' specified in the orchestration configuration"
+                )
+
+
+def _use_top_level_var(config):
+
+    experiment_vars = config['experiment']['experiment_vars']
+    all_steps_config = config['experiment']['steps']
+
+    for step_name, step_config in all_steps_config.items():
+        
+        if "config_transforms" not in step_config:
+            continue
+        elif "use_top_level" not in step_config["config_transforms"]:
+            continue
+
+        vars_to_transform = step_config["config_transforms"]["use_top_level"]
+
+        for varname in vars_to_transform:
+
+            exp_var_key = step_config["method"][varname]
+            step_config["method"][varname] = experiment_vars[exp_var_key]
+
+
+def _get_experiment_path(config):
 
     proto = config['storage_proto']
     root = config['storage_root']
@@ -29,62 +115,27 @@ def _get_experiment_path(config, unique_id=None):
 
     experiment_name = f"{experiment['name']}"
 
-    if unique_id is not None:
-        experiment_name = f"{experiment_name}-{unique_id}"
-
     return f"{proto}://{root}/{experiment_name}"
 
 
 def _get_all_step_arguments(workflow_steps, config):
     """Get a dictionary of each step with i/o and methedological arguments"""
-    
-    if config['experiment']['unique_id']:
-        unique_id = str(uuid.uuid4())[-8:]
-    else:
-        unique_id = None
-    
-    root_experiment_path = _get_experiment_path(config, unique_id=unique_id)
+
     steps_config = config["experiment"]["steps"]
     all_step_arguments = {}
     for i, step in enumerate(workflow_steps):
         curr_config = steps_config[step]
-        input_location = _get_input_location(step, curr_config, steps_config, root_experiment_path)
-        output_location = _get_output_location(step, curr_config, root_experiment_path)
-        method_args = _generate_method_args(curr_config, unique_id=unique_id)
-        step_args = " ".join([input_location, output_location, method_args])
+        all_input_locations = [
+            input_info["location"] for input_info in curr_config["inputs"].values()
+        ]
+        output_location = curr_config["output_location"]
+        method_args = _generate_method_args(curr_config)
+
+        input_args = " ".join(all_input_locations)
+        step_args = " ".join([input_args, output_location, method_args])
         all_step_arguments[step] = step_args
 
     return all_step_arguments
-
-
-def _get_input_location(step, step_config, all_steps_config, root_path):
-    """get an individual steps input location"""
-    
-    if "input_location" in step_config:
-        # User-defined
-        input_location = step_config["input_location"]
-    elif "input_from" in step_config:
-        # Auto-generated
-        prev_step = step_config["input_from"]
-        prev_config = all_steps_config[prev_step]
-        input_location = _get_output_location(prev_step, prev_config, root_path)
-    else:
-        raise KeyError(f"Missing specification of input location for step: {step}")
-
-    return input_location
-
-
-def _get_output_location(step_name, step_config, root_path):
-    """Get a steps output location or generate if not specified"""
-    if "output_location" in step_config:
-        # User-defined
-        output_location = step_config["output_location"]
-    else:
-        # Auto-generated
-        output_stub = _generate_output_path_from_config(step_name, step_config)
-        output_location = os.path.join(root_path, output_stub)
-
-    return output_location
     
 
 def _generate_output_path_from_config(step_name, step_config, max_config_stubs=3):
@@ -112,19 +163,12 @@ def _generate_output_path_from_config(step_name, step_config, max_config_stubs=3
     return output_str
 
 
-def _generate_method_args(step_config, unique_id=None):
+def _generate_method_args(step_config):
     """generate the methodlogical arguments for the step"""
     method_config = step_config.get("method", None)
-    methods_to_add_unique_id = step_config.get("add_unique_id", [])
 
     if method_config is not None:
-        method_args = []
-        for key, val in method_config.items():
-            if key in methods_to_add_unique_id and unique_id is not None:
-                val = f"{val}-{unique_id}"
-            else:
-                val = str(val)
-            method_args.append(val)
+        method_args = [str(val) for val in method_config.values()]
         combined_method_args = " ".join(method_args)
     else:
         combined_method_args = ""
