512c7572e48471494397911c06fa6cc1b7be1cbe
brianhenn <brianhenn@gmail.com>
2020-02-14 00:05:14 +0000

    refactored workflow submit script to loop through steps defined in config

--- workflows/end_to_end/submit_workflow.sh
+++ workflows/end_to_end/submit_workflow.sh
@@ -5,34 +5,25 @@ set -e
 config_file=$1
 
 # get arguments for all steps
-all_step_args=$(python workflows/end_to_end/get_experiment_args.py $config_file)
-echo $all_step_args
+ALL=$(python workflows/end_to_end/get_experiment_steps_and_args.py $config_file)
+WORKFLOW=$(echo $ALL | jq -r .workflow)
+COMMANDS=$(echo $ALL | jq -r .commands)
+ALL_ARGUMENTS=$(echo $ALL | jq -r .arguments)
 
-# # extra fv3net packages
-# cd external/vcm
-# python setup.py sdist
+# extra fv3net packages
+cd external/vcm
+python setup.py sdist
 
-# cd external/mappm
-# python setup.py sdist
+cd external/mappm
+python setup.py sdist
 
-# cd ../../../..
+cd ../../../..
 
-# # Coarsening step
-# coarsen_args=$(echo $all_step_args | jq -r .coarsen_restarts)
-# workflows/coarsen_restarts/orchestrator_job.sh $coarsen_args
-
-# # One-step jobs
-# one_step_args=$(echo $all_step_args | jq -r .one_step_run)
-# python workflows/one_step_jobs/orchestrate_submit_jobs.py $one_step_args
-
-# # ML training data creation step
-# training_data_args=$(echo $all_step_args | jq -r .create_training_data)
-# workflows/create_training_data/orchestrator_job_directrunner_test.sh $training_data_args
-
-# # train ML model step
-# train_model_args=$(echo $all_step_args | jq -r .train_sklearn_model)
-# workflows/sklearn_regression/orchestrator_train_sklearn.sh $train_model_args
-
-# test ML model step
-test_model_args=$(echo $all_step_args | jq -r .test_sklearn_model)
-workflows/sklearn_regression/orchestrator_test_sklearn.sh $test_model_args
+echo "Starting workflow to execute the following steps: "$WORKFLOW
+for STEP in $WORKFLOW; do
+  echo "Starting command "$STEP"..."
+  COMMAND=$(echo $COMMANDS | jq -r .${STEP})
+  ARGUMENTS=$(echo $ALL_ARGUMENTS | jq -r .${STEP})
+  echo "Running command \""${COMMAND}" "${ARGUMENTS}"\""
+  $($COMMAND $ARGUMENTS)
+done
