a4a9417ef8e6a08df91acdd0a24e9eac82338bdb
brianhenn <brianhenn@gmail.com>
2020-02-18 22:29:25 +0000

    fixed circular reference in fv3net.regression.sklearn

--- fv3net/regression/sklearn/__init__.py
+++ fv3net/regression/sklearn/__init__.py
@@ -1,6 +1,3 @@
 from .train import load_data_generator, load_model_training_config, train_model
 
 __all__ = [load_data_generator, load_model_training_config, train_model]
-
-MODEL_CONFIG_FILENAME = "training_config.yml"
-MODEL_FILENAME = "sklearn_model.pkl"
