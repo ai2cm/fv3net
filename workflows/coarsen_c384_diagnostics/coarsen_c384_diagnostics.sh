ca085c171075dab8b4683c0a1f6358654da47e8f
brianhenn <brianhenn@gmail.com>
2020-02-18 01:00:37 +0000

    removed coarsen diags shell script

--- workflows/coarsen_c384_diagnostics/coarsen_c384_diagnostics.sh
+++
@@ -1,9 +0,0 @@
-INPUT_PATH=$1
-OUTPUT_PATH=$2
-CONFIG_PATH=$3
-
-python workflows/coarsen_c384_diagnostics/coarsen_c384_diagnostics.py \
---input-path=$INPUT_PATH \
---output-path=$OUTPUT_PATH \
---config-path=$CONFIG_PATH
-
