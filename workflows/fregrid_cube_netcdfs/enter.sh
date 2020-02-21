d49e734fb05a8faf981e389470cdc46cbac4926b
Noah D. Brenowitz <nbren12@gmail.com>
2020-02-21 15:37:09 -0800

    write pytohn version of the regridding script

--- workflows/fregrid_cube_netcdfs/enter.sh
+++ workflows/fregrid_cube_netcdfs/enter.sh
@@ -4,4 +4,4 @@
 
 credentials=$HOME/keys/noahb-vm.json
 bindMounts="-v $credentials:/creds.json -e GOOGLE_APPLICATION_CREDENTIALS=/creds.json  -v /home/noahb:/home/noahb -w $(pwd) -ti "
-docker run $bindMounts us.gcr.io/vcm-ml/fretools  bash
+docker run $bindMounts us.gcr.io/vcm-ml/regrid_c3072_diag bash
---
+++ workflows/fregrid_cube_netcdfs/regrid.py
@@ -0,0 +1,79 @@
+import argparse
+import fsspec
+import os
+import xarray as xr
+import tarfile
+import sh
+import logging
+from subprocess import check_call
+
+
+grid = "gs://vcm-ml-data/2019-10-05-coarse-grid-and-orography-data.tar"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("src_prefix")
+parser.add_argument("outputBucket")
+parser.add_argument("resolution")
+parser.add_argument("--scalar_fields")
+parser.add_argument("--mosaic", default=grid)
+parser.add_argument("--args", default="--nlat 180 --nlon 360")
+
+args = parser.parse_args()
+
+
+fs = fsspec.filesystem("gs")
+
+
+def download_tile_data(src_prefix):
+    tiles = []
+    for k in range(6):
+        tile_suffix = f".tile{k+1}.nc"
+        tile_path = src_prefix + tile_suffix
+        output = "data" + tile_suffix
+        logging.info(f"Downloading {tile_path}")
+        fs.get(tile_path, output)
+        tiles.append(output)
+    return tiles
+
+
+def get_mosaic(mosaic, resolution):
+    mosaic = (
+        f"2019-10-05-coarse-grids-and-orography-data/{args.resolution}/grid_spec.nc"
+    )
+    if not os.path.exists(mosaic):
+        with fsspec.open(mosaic) as f:
+            tar = tarfile.open(fileobj=f)
+            tar.extractall()
+    return mosaic
+
+
+def get_scalar_fields(data):
+    ds = xr.open_dataset(data[0])
+    vars = []
+    for var in ds:
+        if set(ds[var].dims) >= {"grid_yt", "grid_xt"}:
+            vars.append(var)
+    return ",".join(vars)
+
+
+files = download_tile_data(args.src_prefix)
+mosaic = get_mosaic(args.mosaic, args.resolution)
+remap_file = "remap_file.nc"
+scalar_fields = args.scalar_fields if args.scalar_fields else get_scalar_fields(files)
+
+check_call(
+    [
+        "fregrid",
+        "--input_mosaic",
+        mosaic,
+        "--remap_file",
+        remap_file,
+        "--input_file",
+        "data",
+        "--output_file",
+        "data.nc",
+        "--scalar_field",
+        scalar_fields,
+    ]
+    + args.args.split()
+)
