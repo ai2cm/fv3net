20d04b29fb3d7d746ac1e86f5e95c250665e6c1d
Andre Perkins <frodre@gmail.com>
2020-02-24 02:55:28 +0000

    Linting

--- fv3net/pipelines/kube_jobs/utils.py
+++ fv3net/pipelines/kube_jobs/utils.py
@@ -42,7 +42,7 @@ def update_tiled_asset_names(
     source_filename: str,
     target_url: str,
     target_filename: str,
-    **kwargs
+    **kwargs,
 ) -> List[dict]:
 
     """
@@ -78,27 +78,30 @@ def wait_for_kubejob_complete(job_labels: Mapping[str, str]):
     """
 
     selector_format_labels = [f"{key}={value}" for key, value in job_labels.items()]
-    combined_selectors = ",".join(selector_format_labels) 
+    combined_selectors = ",".join(selector_format_labels)
 
     kubernetes.config.load_kube_config()
     batch = kubernetes.client.BatchV1Api()
 
     # Check active jobs
     while True:
-        
+
         jobs = batch.list_job_for_all_namespaces(label_selector=combined_selectors)
         active_jobs = [
             job_info.metadata.name for job_info in jobs.items if job_info.status.active
         ]
 
         if active_jobs:
-            logger.info(f"Active Jobs at {time.strftime('%Y-%m-%d %H:%M:%S')}\n" + "\n".join(active_jobs))
+            logger.info(
+                f"Active Jobs at {time.strftime('%Y-%m-%d %H:%M:%S')}\n"
+                + "\n".join(active_jobs)
+            )
             time.sleep(30)
             continue
         else:
             logger.info("All kubernetes jobs finished!")
             break
-    
+
     # Get failed and successful jobs
     failed = []
     success = []
@@ -108,7 +111,7 @@ def wait_for_kubejob_complete(job_labels: Mapping[str, str]):
         elif job_info.status.succeeded:
             # tuples for delete operation
             success.append((job_info.metadata.name, job_info.metadata.namespace))
-    
+
     logger.info("Failed Jobs from EXP_LABEL:\n" + "\n".join(failed))
 
     # Delete successful jobs
