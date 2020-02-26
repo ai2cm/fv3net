d3d9acab301364a769436640ec03c2df9200b967
Andre Perkins <frodre@gmail.com>
2020-02-26 19:00:47 +0000

    Tests for common

---
+++ tests/test_pipeline_common.py
@@ -0,0 +1,67 @@
+import pytest
+import tempfile
+import pathlib
+import os
+from fv3net.pipelines.common import (
+    parse_timestep_from_path,
+    list_timesteps,
+    get_unique_tag,
+)
+
+@pytest.fixture
+def timestep_dir():
+
+    timesteps = ['20160801.001500', '20160801.003000', '20160801.004500']
+    extra_file = "not_a_timestep.nc"
+    extra_dir = "what_a_config_dir"
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        for out_dir in timesteps + [extra_dir]:
+            os.makedirs(os.path.join(tmpdir, out_dir))
+
+        pathlib.Path(tmpdir, extra_file).touch()
+
+        yield tmpdir, timesteps
+
+
+def test_timestep_lister(timestep_dir):
+    
+    tmpdir, timesteps = timestep_dir
+    timesteps_found = list_timesteps(tmpdir)
+    assert len(timesteps_found) == len(timesteps)
+    for check_timestep in timesteps_found:
+        assert check_timestep in timesteps
+
+        
+def test_timestep_lister_sorted(timestep_dir):
+
+    tmpdir, timesteps = timestep_dir
+    timesteps.sort()
+    timesteps_found = list_timesteps(tmpdir)
+    for i, ref_timestep in enumerate(timesteps):
+        assert timesteps_found[i] == ref_timestep
+
+
+def test_extract_timestep_from_path():
+
+    timestep = "20160801.001500"
+    good_path = f"gs://path/to/timestep/{timestep}/"
+    assert parse_timestep_from_path(good_path) == timestep
+
+
+def test_extract_timestep_from_path_with_no_timestep_in_path():
+
+    with pytest.raises(ValueError):
+        bad_path = "gs://path/to/not/a/timestep/"
+        parse_timestep_from_path(bad_path)
+
+
+def test_unique_tag_length():
+
+    tlen = 8
+    tag = get_unique_tag(tlen)
+    assert len(tag) == tlen
+
+    with pytest.raises(ValueError):
+        get_unique_tag(0)
+
