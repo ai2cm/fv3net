77861ed5b38ef86aac653bc0751a41ba81987e54
Jeremy McGibbon <jeremym@vulcan.com>
2020-02-27 12:43:04 -0800

    changes following reviews from oliwm and nbren

--- workflows/nudging/runfile.py
+++ workflows/nudging/runfile.py
@@ -1,17 +1,15 @@
 import os
+import functools
 from datetime import datetime, timedelta
 import yaml
+import fv3config
 
 if __name__ == "__main__":
     import fv3gfs
     from mpi4py import MPI
 
 
-REFERENCE_DIR = (
-    "gs://vcm-ml-data/"
-    "2020-01-16-X-SHiELD-2019-12-02-pressure-coarsened-rundirs/restarts/C48/"
-)
-TENDENCY_OUT_FILENAME = "nudging_tendencies.nc"
+TENDENCY_OUT_FILENAME = "tendencies.zarr"
 RUN_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
@@ -27,10 +25,7 @@ def get_timescales_from_config(config):
 
 
 def time_to_label(time):
-    return (
-        f"{time.year:04d}{time.month:02d}{time.day:02d}."
-        f"{time.hour:02d}{time.minute:02d}{time.second:02d}"
-    )
+    return f"{time.year:04d}{time.month:02d}{time.day:02d}.{time.hour:02d}{time.minute:02d}{time.second:02d}"
 
 
 def test_time_to_label():
@@ -39,19 +34,19 @@ def test_time_to_label():
     assert result == label
 
 
-def get_restart_directory(label):
-    return os.path.join(REFERENCE_DIR, label)
+def get_restart_directory(reference_dir, label):
+    return os.path.join(reference_dir, label)
 
 
-def get_reference_state(time, partitioner, only_names):
+def get_reference_state(time, reference_dir, partitioner, only_names):
     label = time_to_label(time)
-    dirname = get_restart_directory(label)
+    dirname = get_restart_directory(reference_dir, label)
     return fv3gfs.open_restart(dirname, partitioner, label=label, only_names=only_names)
 
 
-def nudge_to_reference(state, partitioner, timescales, timestep):
+def nudge_to_reference(state, partitioner, reference_dir, timescales, timestep):
     reference = get_reference_state(
-        state["time"], partitioner, only_names=timescales.keys()
+        state["time"], reference_dir, partitioner, only_names=timescales.keys()
     )
     tendencies = fv3gfs.apply_nudging(state, reference, timescales, timestep)
     tendencies = append_key_label(tendencies, "_tendency_due_to_nudging")
@@ -71,12 +66,22 @@ if __name__ == "__main__":
     current_dir = os.getcwd()
     with open("fv3config.yml", "r") as config_file:
         config = yaml.safe_load(config_file)
+    reference_dir = config["nudging"]["restarts_path"]
     MPI.COMM_WORLD.barrier()  # wait for master rank to write run directory
-    partitioner = fv3gfs.Partitioner.from_namelist(config["namelist"])
+    partitioner = fv3gfs.CubedSpherePartitioner.from_namelist(config["namelist"])
     nudging_timescales = get_timescales_from_config(config)
     timestep = get_timestep(config)
+
+    nudge = functools.partial(
+        nudge_to_reference,
+        partitioner=partitioner,
+        reference_dir=reference_dir,
+        timescales=nudging_timescales,
+        timestep=timestep,
+    )
+
     tendency_monitor = fv3gfs.ZarrMonitor(
-        os.path.join(RUN_DIR, "tendencies.zarr"),
+        os.path.join(RUN_DIR, TENDENCY_OUT_FILENAME),
         partitioner,
         mode="w",
         mpi_comm=MPI.COMM_WORLD,
@@ -89,9 +94,7 @@ if __name__ == "__main__":
         fv3gfs.save_intermediate_restart_if_enabled()
 
         state = fv3gfs.get_state(names=["time"] + list(nudging_timescales.keys()))
-        tendencies = nudge_to_reference(
-            state, partitioner, nudging_timescales, timestep
-        )
+        tendencies = nudge(state)
         tendency_monitor.store(tendencies)
         fv3gfs.set_state(state)
     fv3gfs.cleanup()
