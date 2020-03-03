ef6563b6c9851af15a72a5401494f6f806a0c507
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-03 15:11:47 -0800

    add optional output to gcs

--- workflows/prognostic_run_diags/run.sh
+++ workflows/prognostic_run_diags/run.sh
@@ -1,12 +1,18 @@
-usage="Usage: run.sh <url>"
+usage="Usage: run.sh <url> [gcs]"
 
 if [[ $# < 1 ]]
 then
 	echo $usage
 fi
 
+
 url=$1
 (
 	export PROG_RUN_LOCATION=$1
 	jupyter nbconvert --execute prognostic-run-diags-v1.ipynb
 )
+
+if [[ $# > 1]]
+then
+	gsutil cp prognostic-run-diags-v1.html $2
+fi
