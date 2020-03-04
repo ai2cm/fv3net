362a1cd35ea1fc159d762899752998f1e3753bf8
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-04 14:39:13 -0800

    removed __all__ from __init__.py
    It was out of date anyway, and many popular libraries don't use __all__

--- external/vcm/vcm/__init__.py
+++ external/vcm/vcm/__init__.py
@@ -12,34 +12,3 @@ from .coarsen import coarsen_restarts_on_pressure, coarsen_restarts_on_sigma
 from .visualize import plot_cube, mappable_var, plot_cube_axes
 
 from .xarray_loaders import open_tiles
-
-__all__ = [
-    "TOP_LEVEL_DIR",
-    "calc",
-    "casting",
-    "cloud",
-    "coarsen",
-    "coarsen_restarts_on_pressure",
-    "coarsen_restarts_on_sigma",
-    "combine_array_sequence",
-    "combining",
-    "complex_sfc_data_coarsening",
-    "convenience",
-    "cubedsphere",
-    "extract",
-    "extract_tarball_to_path",
-    "fv3_restarts",
-    "mappable_var",
-    "open_restarts",
-    "open_tiles",
-    "parse_datetime_from_str",
-    "parse_timestep_str_from_path",
-    "plot_cube",
-    "plot_cube_axes",
-    "regrid",
-    "schema",
-    "schema_registry",
-    "visualize",
-    "xarray_loaders",
-    "xarray_utils",
-]
