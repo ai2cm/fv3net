28b82f5fb85363e0f4483856ce784dabc99620c0
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-06 23:49:26 +0000

    run for physics off and deep-conv off

--- workflows/prognostic_c48_run/_submit_baseline_jobs.sh
+++ workflows/prognostic_c48_run/_submit_baseline_jobs.sh
@@ -4,7 +4,7 @@ set -x
 
 STAMP=$(date +%F)-$(uuid | head -c 6)
 output=gs://vcm-ml-data/testing-2020-02
-urls=("gs://vcm-ml-data/2020-02-28-X-SHiELD-2019-12-02-deep-and-mp-off")
+urls=("gs://vcm-ml-data/2020-03-03-X-SHiELD-2019-12-02-deep-conv-off" "gs://vcm-ml-data/2020-02-26-X-SHiELD-2019-12-02-physics-off")
 ic=20160803.061500
 image=us.gcr.io/vcm-ml/prognostic-run-orchestration:fv3py_v2.3-mp-off-switch
 
