51c72b28eeda784d02b8d6e27129c9713a3eaea8
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-16 19:52:09 +0000

    Allow opening the html file from browser
    I had to set the mime type to allow this

--- workflows/prognostic_run_diags/putfile.py
+++ workflows/prognostic_run_diags/putfile.py
@@ -1,5 +1,16 @@
 import sys
-import fsspec
+from google.cloud import storage
 
-fs = fsspec.filesystem("gs")
-fs.put(*sys.argv[1:])
+def parse_gs_string(url):
+    stripped = url.lstrip('gs://')
+    for k, ch in enumerate(stripped):
+        if ch == '/':
+            break
+    return stripped[:k], stripped[k+1:]
+
+bucket_name, blob_name = parse_gs_string(sys.argv[2])
+
+client = storage.Client()
+bucket = client.get_bucket(bucket_name)
+blob = storage.Blob(blob_name, bucket)
+blob.upload_from_filename(sys.argv[1], content_type='text/html')
