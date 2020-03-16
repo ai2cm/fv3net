baa903347f1615da6e2ce0b8a8aac2cc0940df3b
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-16 20:04:49 +0000

    fix linting errors

--- workflows/prognostic_run_diags/putfile.py
+++ workflows/prognostic_run_diags/putfile.py
@@ -1,16 +1,18 @@
 import sys
 from google.cloud import storage
 
+
 def parse_gs_string(url):
-    stripped = url.lstrip('gs://')
+    stripped = url.lstrip("gs://")
     for k, ch in enumerate(stripped):
-        if ch == '/':
+        if ch == "/":
             break
-    return stripped[:k], stripped[k+1:]
+    return stripped[:k], stripped[k + 1 :]
+
 
 bucket_name, blob_name = parse_gs_string(sys.argv[2])
 
 client = storage.Client()
 bucket = client.get_bucket(bucket_name)
 blob = storage.Blob(blob_name, bucket)
-blob.upload_from_filename(sys.argv[1], content_type='text/html')
+blob.upload_from_filename(sys.argv[1], content_type="text/html")
