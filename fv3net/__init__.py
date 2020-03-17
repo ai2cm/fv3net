31930d9484b95e9df4d4b93eee9286f4e8491cbf
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-17 22:24:18 +0000

    black

--- fv3net/__init__.py
+++ fv3net/__init__.py
@@ -3,4 +3,4 @@ import pathlib
 TOP_LEVEL_DIR = pathlib.Path(__file__).parent.parent.absolute()
 COARSENED_DIAGS_ZARR_NAME = "gfsphysics_15min_coarse.zarr"
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
