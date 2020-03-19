8a7a49cd6cb4ad083b01fe10de8da4e16435e842
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-19 19:06:07 +0000

    print more info in zarr_stat

--- workflows/one_step_jobs/zarr_stat.py
+++ workflows/one_step_jobs/zarr_stat.py
@@ -1,9 +1,24 @@
 import fsspec
+import xarray as xr
 import zarr
 
 url = "gs://vcm-ml-data/testing-noah/one-step/big.zarr/"
 m = fsspec.get_mapper(url)
-g = zarr.open_group(m, mode='r')
+ds = xr.open_zarr(m)
 
-print(g['air_temperature'][:].std())
+print("output structure:")
+print()
+for root, dirname, filename in fsspec.filesystem('gs').walk("gs://vcm-ml-data/testing-noah/one-step"):
+    if not 'big.zarr' in root:
+        for name in filename:
+            print(f"{root}/{name}")
+        for name in dirname:
+            print(f"{root}/{name}/")
+
+print()
+print("big.zarr info:")
+print()
+print(ds.info())
+print(ds.air_temperature.std().compute())
+
 
