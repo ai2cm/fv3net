ae2e38308dda957a378b1196dc448b93701f596d
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-20 22:22:23 +0000

    change image version and chunking

--- workflows/one_step_jobs/_run_steps.sh
+++ workflows/one_step_jobs/_run_steps.sh
@@ -1,7 +1,7 @@
 workdir=$(pwd)
  src=gs://vcm-ml-data/orchestration-testing/test-andrep/coarsen_restarts_source-resolution_384_target-resolution_48/
  output=gs://vcm-ml-data/testing-noah/one-step
- image=us.gcr.io/vcm-ml/prognostic_run:0.1.0
+ image=us.gcr.io/vcm-ml/prognostic_run:v0.1.0-a1
  yaml=$PWD/deep-conv-off.yml
 
  gsutil -m rm -r $output > /dev/null
