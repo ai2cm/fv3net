6f698bc69c263d70283b97ba2e678f286f7cf707
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-24 17:42:54 +0000

    remove get_runfile_config

--- fv3net/runtime/config.py
+++ fv3net/runtime/config.py
@@ -10,16 +10,10 @@ class dotdict(dict):
     __delattr__ = dict.__delitem__
 
 
-def get_runfile_config():
+def get_config():
     with open("fv3config.yml") as f:
         config = yaml.safe_load(f)
     return config
-
-
-def get_runfile_config():
-    with open("fv3config.yml") as f:
-        config = yaml.safe_load(f)
-    return dotdict(config["scikit_learn"])
 
 
 def get_namelist():
