85947a5098d40cf1674e1179d2692cf8b6dc7408
Anna Kwa <annak@vulcan.com>
2020-03-27 00:39:41 +0000

    update test script

--- workflows/sklearn_regression/test_sklearn.sh
+++ workflows/sklearn_regression/test_sklearn.sh
@@ -1,6 +1,6 @@
 python -m fv3net.diagnostics.sklearn_model_performance \
-  20200205.205016_model_training_files/20200205.205016_sklearn_model.pkl \
-  gs://vcm-ml-data/test-annak/2020-02-20_train_data_pipeline_downsampled/test \
-  gs://vcm-ml-data/2019-12-05-40-day-X-SHiELD-simulation-C384-diagnostics/C48_gfsphysics_15min_coarse.zarr \
+  gs://vcm-ml-data/experiments-2020-03/deep-conv-off-mp-off-8c96d2ad/train_sklearn_model_train-config-file_example_base_rf_training_config.yml/sklearn_model.pkl \
+  gs://vcm-ml-data/experiments-2020-03/deep-conv-off-mp-off-8c96d2ad/create_training_data_timesteps-per-output-file_1_train-fraction_0.5/test \
+  gs://vcm-ml-data/orchestration-testing/shield-coarsened-diags-2019-12-04 \
   gs://vcm-ml-public/test-annak/test-new-diags \
-  48
+  4
