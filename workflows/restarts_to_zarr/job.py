f0ecaad9c62aca79bfff3084aa0bc4ced5fd145f
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-27 15:57:46 -0700

    black

--- workflows/restarts_to_zarr/job.py
+++ workflows/restarts_to_zarr/job.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from itertools import product
-from typing import Callable, Hashable, Iterable, Sequence, Tuple, TypeVar
+from typing import Callable, Tuple
 
 import fsspec
 import xarray as xr
@@ -15,7 +15,12 @@ from fv3net.pipelines import list_timesteps
 
 logging.basicConfig(level=logging.INFO)
 
-CATEGORIES=('fv_srf_wnd_coarse.res', 'sfc_data_coarse', 'fv_tracer_coarse.res', 'fv_core_coarse.res')
+CATEGORIES = (
+    "fv_srf_wnd_coarse.res",
+    "sfc_data_coarse",
+    "fv_tracer_coarse.res",
+    "fv_core_coarse.res",
+)
 
 
 def _file(url: str, time: str, category: str, tile: int) -> str:
@@ -31,7 +36,11 @@ def get_timestep(fs, url, time, category, tile) -> xr.Dataset:
         return vcm.standardize_metadata(ds)
 
 
-def insert_timestep(output: fv3net.ZarrMapping, get: Callable[[str, str, int], xr.Dataset], key: Tuple[str, str, int]):
+def insert_timestep(
+    output: fv3net.ZarrMapping,
+    get: Callable[[str, str, int], xr.Dataset],
+    key: Tuple[str, str, int],
+):
     time, category, tile = key
     data = get(time, category, tile)
     logging.info(f"Setting data at {time} for {category} tile {tile}")
@@ -44,28 +53,29 @@ def get_schema(fs: fsspec.AbstractFileSystem, url: str) -> xr.Dataset:
         return vcm.standardize_metadata(xr.open_dataset(f))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # TODO Parameterize these settings
     url = "gs://vcm-ml-data/2020-03-16-5-day-X-SHiELD-simulation-C384-restart-files/"
     times = list_timesteps(url)
 
-    fs = fsspec.filesystem('gs')
+    fs = fsspec.filesystem("gs")
     categories = CATEGORIES
     tiles = [1, 2, 3, 4, 5, 6]
 
     # get schema for first timestep
     time = times[0]
-    schema = xr.merge([
-        get_schema(fs, _file(url, time, category, tile=1))
-        for category in categories
-    ])
+    schema = xr.merge(
+        [get_schema(fs, _file(url, time, category, tile=1)) for category in categories]
+    )
     print("Schema:")
     print(schema)
 
     #
     store = "big.zarr"
     group = zarr.open_group(store, mode="w")
-    output_m = fv3net.ZarrMapping(group, schema, dims=['time', 'tile'], coords={'tile': tiles, 'time': times})
+    output_m = fv3net.ZarrMapping(
+        group, schema, dims=["time", "tile"], coords={"tile": tiles, "time": times}
+    )
     load_timestep = get_timestep(fs, url)
     map_fn = curry(insert_timestep, output_m, load_timestep)
 
