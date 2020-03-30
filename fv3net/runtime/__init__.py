79f4be0540b98052fa96ad6f9ca8b152681bc743
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-03-30 21:11:49 +0000

    Fix fv3net runtime __init__

--- fv3net/runtime/__init__.py
+++ fv3net/runtime/__init__.py
@@ -1,4 +1,3 @@
 from . import sklearn_interface as sklearn
 from .state_io import init_writers, append_to_writers, CF_TO_RESTART_MAP
 from .config import get_config, get_namelist, get_timestep
-from .mean_nudging import get_current_nudging_tendency
