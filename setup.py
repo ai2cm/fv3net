cb9a94b95dadbec304dd22defa6972b0221b62c1
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-31 21:50:48 +0000

    update setup.py again

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+import os
 
 dependencies = [
     "dask==2.13.0",
@@ -13,13 +14,20 @@ dependencies = [
     "xarray==0.15.1",
     "xgcm==0.3.0",
     "zarr==2.4.0",
+    "numba==0.48.0",
+    "cftime-1.1.1.2"
 ]
 
-packages = find_packages() + find_packages("external/vcm")
+
+packages = find_packages() + find_packages("external/vcm", exclude='tests')
+package_dir = {'vcm': f'external/vcm/vcm/', 'fv3net': 'fv3net'}
+
+print(f"Found packages: {packages}")
 setup(
     name="fv3net",
-    packages=find_packages(),
+    packages=packages,
     install_requires=dependencies,
+    package_dir=package_dir,
     version="0.1.1",
     description="Improving the GFDL FV3 model physics with machine learning",
     author="Vulcan Inc.",
