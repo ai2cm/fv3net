f8492ceb47d72782efcb96bd8ce0c1df43e8db0c
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-31 23:26:55 +0000

    add development run scripts

---
+++ workflows/restarts_to_zarr/_run.sh
@@ -0,0 +1,18 @@
+NUM_WORKERS=32
+
+cd /home/noahb/fv3net
+
+python -m fv3net.pipelines.restarts_to_zarr  \
+    --setup $(pwd)/setup.py \
+    --job_name test-$(uuid) \
+    --project vcm-ml \
+    --region us-central1 \
+    --runner DataFlow \
+    --temp_location gs://vcm-ml-data/tmp_dataflow \
+    --num_workers $NUM_WORKERS \
+    --max_num_workers $NUM_WORKERS \
+    --autoscaling_algorithm=NONE \
+    --worker_machine_type n1-standard-1 \
+    --n-steps 10  \
+    --url gs://vcm-ml-data/2020-03-16-5-day-X-SHiELD-simulation-C384-restart-files \
+    --output gs://vcm-ml-data/testing-noah/big1.zarr
---
+++ workflows/restarts_to_zarr/_run_remote.sh
@@ -0,0 +1,2 @@
+python job.py gs://vcm-ml-data/2020-03-16-5-day-X-SHiELD-simulation-C384-restart-files gs://vcm-ml-data/testing-noah/big1.zarr --n-steps 10 --direct_num_workers 8  | tee err 2>&1
+
