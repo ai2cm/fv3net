af6120b614db59ecb0a0cfa36fabf5e52ef121d0
Anna Kwa <annak@vulcan.com>
2020-04-01 22:31:31 +0000

    add comments to name file and update run scirpt

--- workflows/create_training_data/submit_directrunner_test.sh
+++ workflows/create_training_data/submit_directrunner_test.sh
@@ -2,4 +2,5 @@ python -m fv3net.pipelines.create_training_data \
 gs://vcm-ml-data/test-annak/2020-03-27_test_one_step/big.zarr \
 gs://vcm-ml-data/orchestration-testing/shield-coarsened-diags-2019-12-04 \
 gs://vcm-ml-data/test-annak/2020-03-27_test_bigzarr_train_pipeline \
+./pipelines/create_training_data/variable_names.yml \
   --runner DirectRunner
