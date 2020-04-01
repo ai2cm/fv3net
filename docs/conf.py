5f0511b7c53ea2cd90904a84301f971b969359c6
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-01 09:13:40 -0700

    lint

--- docs/conf.py
+++ docs/conf.py
@@ -15,20 +15,18 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../external/vcm'))
 
-import vcm
-
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../external/vcm"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'fv3net'
-copyright = '2020, Vulcan, Inc.'
-author = 'Vulcan, Inc.'
+project = "fv3net"
+copyright = "2020, Vulcan, Inc."
+author = "Vulcan, Inc."
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -36,16 +34,22 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.autosummary",
-"sphinx.ext.graphviz", "m2r"]
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.graphviz",
+    "m2r",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -58,11 +62,11 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Graphviz
 graphviz_output_format = "svg"
 
 
 # M2r
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
--- docs/link_readmes.py
+++ docs/link_readmes.py
@@ -3,28 +3,25 @@ import os
 
 workflows = []
 for root, dirs, files in os.walk(".."):
-  for file in files:
-    if file == "README.md":
-       if root == "..":
-         workflow = 'readme'
-       else:
-         workflow = os.path.split(root)[-1]
-       workflows.append((workflow, os.path.join(root, file)))
+    for file in files:
+        if file == "README.md":
+            if root == "..":
+                workflow = "readme"
+            else:
+                workflow = os.path.split(root)[-1]
+            workflows.append((workflow, os.path.join(root, file)))
 
 
 links = []
 for workflow, readme in workflows:
-  link = f"{workflow}_link"
-  links.append(link)
-  with open(link + '.rst', "w") as f:
-    f.write(f".. mdinclude:: {readme}")
+    link = f"{workflow}_link"
+    links.append(link)
+    with open(link + ".rst", "w") as f:
+        f.write(f".. mdinclude:: {readme}")
 
 
 print(".. toctree::")
 print("    :maxdepth: 2")
 print("")
 for link in links:
-	print(f"    {link}")
-  
-
-
+    print(f"    {link}")
