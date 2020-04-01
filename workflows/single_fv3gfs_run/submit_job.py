8655c93faf681ea097a873e4dcf7c2dda5e27169
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-03-31 18:08:27 -0700

    Use common transfer_local_to_remote function

--- workflows/single_fv3gfs_run/submit_job.py
+++ workflows/single_fv3gfs_run/submit_job.py
@@ -7,6 +7,7 @@ import yaml
 import fv3config
 from fv3net.pipelines.kube_jobs import (
     get_base_fv3config,
+    transfer_local_to_remote,
     update_nested_dict,
     update_config_for_nudging,
 )
@@ -25,16 +26,8 @@ KUBERNETES_CONFIG_DEFAULT = {
 
 
 def get_kubernetes_config(config_update):
-    """Get default kubernetes config and updatedwith provided config_update"""
+    """Get default kubernetes config and update with provided config_update"""
     return update_nested_dict(KUBERNETES_CONFIG_DEFAULT, config_update)
-
-
-def _upload_if_necessary(path, bucket_url):
-    if not path.startswith("gs://"):
-        remote_path = os.path.join(bucket_url, os.path.basename(path))
-        fsspec.filesystem("gs").put(path, remote_path)
-        path = remote_path
-    return path
 
 
 def _get_and_upload_run_config(bucket, run_config, base_model_config):
@@ -45,10 +38,10 @@ def _get_and_upload_run_config(bucket, run_config, base_model_config):
     # if necessary, upload runfile and diag_table. In future, this should be
     # replaced with an fv3config function to do the same for all elements of config
     if kubernetes_config["runfile"] is not None:
-        kubernetes_config["runfile"] = _upload_if_necessary(
+        kubernetes_config["runfile"] = transfer_local_to_remote(
             kubernetes_config["runfile"], config_bucket
         )
-    model_config["diag_table"] = _upload_if_necessary(
+    model_config["diag_table"] = transfer_local_to_remote(
         model_config["diag_table"], config_bucket
     )
     if model_config["namelist"]["fv_core_nml"].get("nudge", False):
