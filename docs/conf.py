6ee2d0b7e71f9c5feab0f9427edeea1f717264fa
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-01 15:20:32 -0700

    lint

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("../external/vcm"))
 
-import fv3net
+import fv3net  # noqa
 
 # -- Project information -----------------------------------------------------
 
