6f3e299377e46b116fad4d949d757d4cc4d44e85
Anna Kwa <annak@vulcan.com>
2020-04-02 22:54:09 +0000

    fix dim name mismatches with high res and edge vars

--- tests/test_pipelines_training_dataset.py
+++ tests/test_pipelines_training_dataset.py
@@ -3,13 +3,14 @@ import pytest
 import xarray as xr
 
 from fv3net.pipelines.create_training_data.pipeline import (
-    _add_apparent_sources, _preprocess_one_step_data)
+    _add_apparent_sources,
+    _preprocess_one_step_data,
+)
 
 
 @pytest.fixture
 def test_training_raw_ds():
     centered_coords = {
-        "step": ["begin", "after_physics"],
         "initial_time": [
             np.datetime64("2020-01-01T00:00").astype("M8[ns]"),
             np.datetime64("2020-01-01T00:15").astype("M8[ns]"),
@@ -19,33 +20,21 @@ def test_training_raw_ds():
 
     # hi res changes by 0 K, C48 changes by 1 K and 6 K
     T_da = xr.DataArray(
-        [
-            [[273.0, 274.0, 300.0], [273.0, 275.0, 310.0]],
-            [[0.0, 1.0, 3.0], [2.0, 3.0, 4.0]],
-        ],
-        dims=["step", "initial_time", "forecast_time"],
+        [[273.0, 274.0, 300.0], [273.0, 275.0, 310.0]],
+        dims=["initial_time", "forecast_time"],
         coords=centered_coords,
     )
 
     return xr.Dataset({"air_temperature": T_da})
 
 
-def test__create_train_cols(test_training_raw_ds):
-    ds = _preprocess_one_step_data(
-        test_training_raw_ds,
-        radiation_vars=[],
-        forecast_time_dim="forecast_time",
-        suffix_coarse_train="train",
-        step_time_dim="step",
-        coord_begin_step="begin",
-    )
+def test__add_apparent_sources(test_training_raw_ds):
     train_ds = _add_apparent_sources(
-        ds,
+        test_training_raw_ds,
         tendency_tstep_onestep=1,
         tendency_tstep_highres=1,
         init_time_dim="initial_time",
         forecast_time_dim="forecast_time",
-
         var_source_name_map={"air_temperature": "dQ1"},
     )
     assert train_ds.dQ1.values == pytest.approx(1.0 / (15.0 * 60) - 26.0 / 60)
