c30e669c84f4266eeedc5687aff0cbb03c9ab00a
Andre Perkins <frodre@gmail.com>
2020-04-03 00:55:40 +0000

    Login in entrypoint script if app creds env var is set

--- docker/fv3net/init_kubeconfig.sh
+++ docker/fv3net/init_kubeconfig.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [[ ! -z $GOOGLE_APPLICATION_CREDENTIALS ]]; then
+    gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+fi
+
 # get cluster credentials
 cluster_zone=$(gcloud container clusters list --format="value(location)")
 cluster_name=$(gcloud container clusters list --format="value(name)")
