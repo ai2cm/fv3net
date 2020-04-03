85ec55b83c0e896ef0cd67c59c3822cd8a4dce70
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-04-03 17:39:24 +0000

    Update runfile for n_proc>6

--- workflows/run_with_learned_nudging/mean_nudging_runfile.py
+++ workflows/run_with_learned_nudging/mean_nudging_runfile.py
@@ -36,48 +36,51 @@ def get_current_nudging_tendency(variables, time, ds_nudging):
     return nudging_tendency
 
 
-def apply_nudging_tendency(variables, ds_nudging, dt, communicator):
-    state = fv3gfs.get_state(names=["time"] + variables)
+def apply_nudging_tendency(nudging_state, dt, communicator):
+    variables = list(nudging_state.keys())
+    state = fv3gfs.get_state(names=variables)
     tile = communicator.partitioner.tile_index(communicator.rank)
     if communicator.tile.rank == 0:
-        logger.info(f"Getting nudging tendency for current timestep on tile {tile}")
-        tendency = get_current_nudging_tendency(variables, state["time"], ds_nudging)
-        logger.info(f"Converting nudging tendency to fv3gfs state on tile {tile}")
-        tendency_as_state = {
-            variable: fv3util.Quantity.from_data_array(tendency[variable])
-            for variable in variables
-        }
-    if communicator.tile.rank == 0:
-        logger.info(f"Scattering nudging tendency to all ranks for tile {tile}")
-    tendency_as_state = communicator.tile.scatter_state(tendency_as_state)
-    if communicator.tile.rank == 0:
         logger.info(f"Adding nudging tendency to state for tile {tile}")
-    for variable in variables:
-        state[variable].view[:] += tendency_as_state[variable].view[:] * dt
+    for variable in nudging_state:
+        state[variable].view[:] += nudging_state[variable].view[:] * dt
     if communicator.tile.rank == 0:
         logger.info(f"Updating fv3gfs state for tile {tile}")
     fv3gfs.set_state(state)
 
 
+def open_nudging_tendency(url, communicator, variables):
+    rename_dict = {CF_TO_NUDGE[var]: var for var in variables}
+    nudging_state = {}
+    mapper = fsspec.get_mapper(url)
+    rank = communicator.rank
+    tile = communicator.partitioner.tile_index(rank)
+    if communicator.tile.rank == 0:
+        logger.info(f"Loading tile-{tile} nudging tendencies on rank {rank}")
+        ds_nudging = xr.open_zarr(mapper).isel(tile=tile, time=0)
+        ds_nudging = ds_nudging.rename(rename_dict)
+        ds_nudging = ds_nudging[variables].load()
+        nudging_state = {
+            variable: fv3util.Quantity.from_data_array(ds_nudging[variable])
+            for variable in variables
+        }
+    nudging_state = communicator.tile.scatter_state(nudging_state)
+    return nudging_state
+
+
 if __name__ == "__main__":
     config = runtime.get_config()
     nudging_zarr_url = config["runtime"]["nudging_zarr_url"]
-    mapper = fsspec.get_mapper(nudging_zarr_url)
     variables_to_nudge = config["runtime"]["variables_to_nudge"]
     dt = runtime.get_timestep()
     communicator = fv3gfs.CubedSphereCommunicator(
         MPI.COMM_WORLD, fv3gfs.CubedSpherePartitioner.from_namelist(config["namelist"])
     )
-    # tile = fv3util.get_tile_index(rank, communicator.partitioner.total_ranks)
     rank = communicator.rank
-    tile_index = communicator.partitioner.tile_index(rank)
     if rank == 0:
         logger.info(f"Nudging following variables: {variables_to_nudge}")
-    if communicator.tile.rank == 0:
-        logger.info(f"My rank is {rank} and my tile_rank is {communicator.tile.rank}")
-        logger.info(f"Loading tile-{tile_index} nudging tendencies from {nudging_zarr_url}")
-        ds_nudging = xr.open_zarr(mapper).isel(tile=tile_index).load()
-
+        logger.info(f"Using nudging tendencies from: {nudging_zarr_url}")
+    nudging_tendency = open_nudging_tendency(nudging_zarr_url, communicator, variables_to_nudge)
     fv3gfs.initialize()
     for i in range(fv3gfs.get_step_count()):
         do_logging = rank == 0 and i % 10 == 0
@@ -89,9 +92,8 @@ if __name__ == "__main__":
         fv3gfs.compute_physics()
         if do_logging:
             logger.info(f"Adding nudging tendency for timestep {i}")
-        if communicator.tile.rank == 0:
-            apply_nudging_tendency(variables_to_nudge, ds_nudging, dt, communicator)
+        apply_nudging_tendency(nudging_tendency, dt, communicator)
         if do_logging:
-            logger.info(f"Update atmospheric prognostic state for timestep {i}")
+            logger.info(f"Updating atmospheric prognostic state for timestep {i}")
         fv3gfs.apply_physics()
     fv3gfs.cleanup()
