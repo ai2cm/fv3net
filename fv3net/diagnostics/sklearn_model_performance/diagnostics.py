0ee8b5a7a74e852c1af95307e0b83ec7a7a3b275
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-04-06 14:29:25 -0700

    Add integration test for end-to-end workflow (#199)
    * Add initial configs for integration test
    
    * Update docker image in end-to-end config
    
    * Make snapshot indices same for precip and heating
    
    * Update end-to-end test config and add diag_table
    
    * Make open_diagnostic explicitly load
    
    * Uncomment commented steps
    
    * Dont do coarsen_restarts step for now
    
    * Start of CI running tests
    
    * Add basic tests of GCP firewall and authentication
    
    * Delete firewall on completion
    
    * Ignore reg tests for now
    
    * Add project default to gcloud
    
    * Fix config.yml
    
    * Fix firewall create args
    
    * Fix firewall name
    
    * Update service account creds to use
    
    * Remove destination range
    
    * Fix allow port number
    
    * Add debug statements after kubectl
    
    * Fix cluster name
    
    * Fix cluster name add kubectl cluster name
    
    * Add jq to fv3net dockerfile
    
    * Add initial integration scripts for CI
    
    * Update test config filenames
    
    * Fix list_timesteps for gcsfs==v0.6.1
    
    The update from gcsfs v0.6.1 breaks timestep lister for non-existing
    directories.  This fixes handling the FileNotFoundError and adds a
    test for this case.
    
    * Update dependencies for test cache
    
    * switch image to test_integration tag
    
    * Update integration test submit to use k8s job yaml
    
    * Update dockerfile for fv3net to include jq
    
    * Working submit from VM to k8s cluster for integration
    
    * Add unique job name to CI submit shell script
    
    * Update script for submitting jobs and waiting for CI
    
    * Fix while loop syntax add toleration for bigger orchestration node
    
    * add toleration
    
    * Fix success check
    
    * Move time in wait loop echo to front
    
    * Use blocking run submission script in CircleCI
    
    * Fix CI dependency install of yq
    
    * Add -y to apt-get install statements
    
    * pin gcsfs=0.6.0 due to change that raises exceptions on ls of empty/non-existent fs
    
    * Finalize circle ci run of integration tests after image builds, add slack integration
    
    * Linting
    
    * Update installed deps file for gcsfs==0.6.0, remove test slack notification
    
    * Rename e2e config file
    
    * Add usage of circleCI version tag for integration test submission
    
    * Update dockerfile to include login for creds
    
    * Troubleshooting kubectl config init from docker build w/in CI
    
    * Use entrypoint script to set up kubectl configuration
    
    * Login in entrypoint script if app creds env var is set
    
    * Use different google creds in CI for in image tests
    
    * Add read permissions for secrets outside of root user group for jovyan
    
    * Handle k8s yaml overwriting entrypoint, clean up yml
    
    * Move kubeconfig initializing entry point to /usr/bin/init_kubeconfig.sh
    
    * Combine apt-get install statements in fv3net dockerfile
    
    * Move environment variable deps out of run_integratioN_with_wait.sh
    
    * Move k8s job yml into template directory to discourage public use
    
    * Reset fv3config to tagged commite
    
    * Update history log with integration test
    
    Co-authored-by: Andre Perkins <frodre@gmail.com>

--- fv3net/diagnostics/sklearn_model_performance/diagnostics.py
+++ fv3net/diagnostics/sklearn_model_performance/diagnostics.py
@@ -181,7 +181,7 @@ def plot_diagnostics(ds_pred, ds_fv3, ds_shield, output_dir, dpi_figures):
     _plot_comparison_maps(
         ds,
         "net_precipitation",
-        time_index_selection=[0, 2],
+        time_index_selection=[0, -1],
         plot_cube_kwargs={"cbar_label": "timestep snapshot, P-E [mm/day]"},
     ).savefig(
         os.path.join(output_dir, "P-E_time_snapshots.png"),
