9da7352e3868f8e5e6a8e26627c205d1c91eb0da
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-04-06 23:11:36 +0000

    Add back runtime get_runfile_config function

--- fv3net/runtime/config.py
+++ fv3net/runtime/config.py
@@ -10,6 +10,12 @@ class dotdict(dict):
     __delattr__ = dict.__delitem__
 
 
+def get_runfile_config():
+    with open("fv3config.yml") as f:
+        config = yaml.safe_load(f)
+    return dotdict(config["scikit_learn"])
+
+
 def get_config():
     with open("fv3config.yml") as f:
         config = yaml.safe_load(f)
