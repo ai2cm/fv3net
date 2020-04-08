96f8f36a9f22662fdbfc176eade9809877df33a4
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-08 21:54:16 +0000

    add runtime to list of local environments

--- .environment-scripts/install_local_packages.sh
+++ .environment-scripts/install_local_packages.sh
@@ -4,7 +4,7 @@ CONDA_ENV=$1
 
 source activate $CONDA_ENV
 
-local_packages_to_install=(. external/fv3config external/vcm external/vcm/external/mappm)
+local_packages_to_install=(. external/fv3config external/vcm external/vcm/external/mappm external/runtime )
 for package  in ${local_packages_to_install[@]}
 do
   pip install --no-deps -e $package
