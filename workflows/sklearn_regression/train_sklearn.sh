1b7f8df6e0b76372bf606d38dadfab67cbed2978
Anna Kwa <annak@vulcan.com>
2020-04-08 23:49:56 +0000

    update test script paths

--- workflows/sklearn_regression/train_sklearn.sh
+++ workflows/sklearn_regression/train_sklearn.sh
@@ -1,4 +1,4 @@
 python -m fv3net.regression.sklearn.train \
-  gs://vcm-ml-data/test-annak/2020-04-03_test_bigzarr_train_pipeline \
+  gs://vcm-ml-data/test-annak/2020-04-08_test_bigzarr_train_pipeline/ \
   workflows/sklearn_regression/example_rf_training_config.yml \
-  gs://vcm-ml-data/test-annak/2020-04-06_sklearn_train
+  gs://vcm-ml-data/test-annak/2020-04-08_sklearn_train
