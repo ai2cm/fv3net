0f422da96f0b2d32e119c95b3c97de04dfde3b07
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-08 00:05:01 +0000

    add mpi friendly function

--- workflows/one_step_jobs/capture.py
+++ workflows/one_step_jobs/capture.py
@@ -13,6 +13,10 @@ import logging
 
 @contextlib.contextmanager
 def capture_stream(stream):
+    """fork is not compatible with mpi so this won't work
+    
+    The wait line below hangs indefinitely.
+    """
 
     # create a pipe to communicate with the fork
     r, w = os.pipe()
@@ -34,8 +38,8 @@ def capture_stream(stream):
             # restore the original file handle
             os.dup2(orig_file_handle, stream.fileno())
 
-            # close the pipe
             os.close(w)
+            os.wait()
     else:
         # child process:
         # close the writing end, we won't need this
@@ -44,6 +48,30 @@ def capture_stream(stream):
             for line in f:
                 logging.debug(line.strip())
         sys.exit(0)
+
+
+@contextlib.contextmanager
+def capture_stream_mpi(stream, logger_name="fv3gfs"):
+
+    # parent process:
+    # close the reading end, we won't need this
+    with tempfile.TemporaryFile() as out:
+        try:
+            orig_file_handle = os.dup(stream.fileno())
+            # overwrite the streams fileno with a the pipe to be read by the forked 
+            # process below
+            os.dup2(out.fileno(), stream.fileno())
+            yield
+        finally:
+            # restore the original file handle
+            os.dup2(orig_file_handle, stream.fileno())
+
+            # print logging info
+            logger = logging.getLogger(logger_name)
+            out.seek(0)
+            for line in out:
+                logger.debug(line.strip().decode('UTF-8'))
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
