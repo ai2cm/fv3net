ffb256013bfdffdd4a284d968c43f384ada4c313
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-08 21:59:14 +0000

    change package installation

--- .environment-scripts/install_local_packages.sh
+++ .environment-scripts/install_local_packages.sh
@@ -4,8 +4,19 @@ CONDA_ENV=$1
 
 source activate $CONDA_ENV
 
-local_packages_to_install=(. external/fv3config external/vcm external/vcm/external/mappm external/runtime )
+local_packages_to_install=(. external/fv3config external/vcm external/vcm/external/mappm )
 for package  in ${local_packages_to_install[@]}
 do
   pip install --no-deps -e $package
+done
+
+poetry_packages=( external/runtime )
+for package in ${poetry_packages[@]}
+do
+  (
+    cd $package
+    rm -f dist/*.whl
+    poetry build
+    pip install dist/*.whl
+  )
 done
