0e9b9caafbeb9cb30906865d2c7fe620a4db554e
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-09 06:10:21 +0000

    lint

--- external/runtime/runtime/__init__.py
+++ external/runtime/runtime/__init__.py
@@ -3,4 +3,4 @@ from .state_io import init_writers, append_to_writers, CF_TO_RESTART_MAP
 from .config import get_namelist, get_config
 from .capture import capture_stream, capture_stream_mpi, capture_fv3gfs_funcs
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
