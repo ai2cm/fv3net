94d62aa7bc64ecb8117cef06ffe46dffd4e5650c
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-10 09:27:14 -0700

    satisfy shellcheck

--- tests/end_to_end_integration/run_integration_with_wait.sh
+++ tests/end_to_end_integration/run_integration_with_wait.sh
@@ -6,32 +6,32 @@ SLEEP_TIME=60
 
 function waitForComplete {
     # Sleep while job is active
-    job_name=$1
+    jobName=$1
     NAMESPACE=$2
     timeout=$(date -ud "30 minutes" +%s)
-    job_active=$(kubectl get job -n $NAMESPACE $job_name -o json | jq --raw-output .status.active)
-    echo $job_active
+    job_active=$(kubectl get job -n "$NAMESPACE" "$jobName" -o json | jq --raw-output .status.active)
+    echo "$job_active"
     while [[ $(date +%s) -le $timeout ]] && [[ $job_active == "1" ]]
     do
-        echo \[$(date "+%Y-%m-%d %H:%M")\] Job active: $job_name ... sleeping ${SLEEP_TIME}s
+        echo "$(date \"+%Y-%m-%d %H:%M\")" Job active: "$jobName" ... sleeping ${SLEEP_TIME}s
         sleep $SLEEP_TIME
-        job_active=$(kubectl get job -n $NAMESPACE $job_name -o json | jq --raw-output .status.active)
+        job_active=$(kubectl get job -n "$NAMESPACE" "$jobName" -o json | jq --raw-output .status.active)
     done
 
     # Check for job success
-    job_succeed=$(kubectl get job -n $NAMESPACE $job_name -o json | jq --raw-output .status.succeeded)
-    job_fail=$(kubectl get job -n $NAMESPACE $job_name -o json | jq --raw-output .status.failed)
+    job_succeed=$(kubectl get job -n "$NAMESPACE" "$jobName" -o json | jq --raw-output .status.succeeded)
+    job_fail=$(kubectl get job -n "$NAMESPACE" "$jobName" -o json | jq --raw-output .status.failed)
     if [[ $job_succeed == "1" ]]
     then
-        echo Job successful: $job_name
+        echo Job successful: "$jobName"
         echo Deleting job...
-        kubectl delete job $job_name
+        kubectl delete job "$jobName"
     elif [[ $job_fail == "1" ]]
     then
-        echo Job failed: $job_name
+        echo Job failed: "$jobName"
         exit 1
     else
-        echo Job timed out or success ambiguous: $job_name
+        echo Job timed out or success ambiguous: "$jobName"
         exit 1
     fi
 }
@@ -39,18 +39,27 @@ function waitForComplete {
 export PROGNOSTIC_RUN_IMAGE=$1
 export FV3NET_IMAGE=$2
 
-TESTDIR=tests/end_to_end_integration/
+TESTDIR=$(pwd)/tests/end_to_end_integration
 NAMESPACE=integration-tests
-export JOBNAME=integration-test-$(date +%F)-$(openssl rand -hex 6)
-export CONFIGMAP=integration-test-$(date +%F)-$(openssl rand -hex 6)
+JOBNAME=integration-test-$(date +%F)-$(openssl rand -hex 6)
+CONFIGMAP=integration-test-$(date +%F)-$(openssl rand -hex 6)
+export JOBNAME CONFIGMAP
 
-K8S_TEMPLATE=$TESTDIR/job.yml
-E2E_TEMPLATE=$TESTDIR/end_to_end_configuration.yml
+K8S_TEMPLATE=$TESTDIR/job_template.yml
+E2E_TEMPLATE=$TESTDIR/end_to_end_template.yml
 
-envsubst < $E2E_TEMPLATE > end-to-end.yml
-envsubst < $K8S_TEMPLATE > job.yml
 
-# use config map to make the end to end yaml available to the job
-kubectl create configmap -n $NAMESPACE $CONFIGMAP --from-file=end-to-end.yml
-kubectl apply -n $NAMESPACE -f job.yml
-waitForComplete $JOBNAME $NAMESPACE
+workdir=$(mktemp -d)
+
+(
+    cd "$workdir"
+    envsubst < "$E2E_TEMPLATE" > end-to-end.yml
+    envsubst < "$K8S_TEMPLATE" > job.yml
+
+    # use config map to make the end to end yaml available to the job
+    kubectl create configmap -n $NAMESPACE "$CONFIGMAP" --from-file=end-to-end.yml
+    kubectl apply -n $NAMESPACE -f job.yml
+    waitForComplete "$JOBNAME" "$NAMESPACE"
+)
+
+rm -rf "$workdir"
