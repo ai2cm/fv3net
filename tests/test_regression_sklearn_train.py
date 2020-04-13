355fbf97da59e4eb7287e1e47af017ddbf6cbc6d
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-04-13 14:13:03 -0700

    Fix test

--- tests/test_regression_sklearn_train.py
+++ tests/test_regression_sklearn_train.py
@@ -5,4 +5,5 @@ def test_train_save_output_succeeds(tmpdir):
     model = object()
     config = {"a": 1}
     url = str(tmpdir)
-    train.save_output(url, model, config)
+    timesteps = ['20160801.001500', '20160801.003000']
+    train.save_output(url, model, config, timesteps)
