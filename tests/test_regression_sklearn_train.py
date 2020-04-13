e73bdebaac6ab6b9810e8a28b0edec8deba6f668
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-04-13 14:33:03 -0700

    Lint

--- tests/test_regression_sklearn_train.py
+++ tests/test_regression_sklearn_train.py
@@ -5,5 +5,5 @@ def test_train_save_output_succeeds(tmpdir):
     model = object()
     config = {"a": 1}
     url = str(tmpdir)
-    timesteps = ['20160801.001500', '20160801.003000']
+    timesteps = ["20160801.001500", "20160801.003000"]
     train.save_output(url, model, config, timesteps)
