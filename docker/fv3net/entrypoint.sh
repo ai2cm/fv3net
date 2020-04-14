388bd12cad3e9911ecca69dfb1f63b237669f7b7
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-14 12:28:05 -0700

    Fix gsutil auth errors

--- docker/fv3net/entrypoint.sh
+++ docker/fv3net/entrypoint.sh
@@ -6,10 +6,11 @@ if [[ ! -z $GOOGLE_APPLICATION_CREDENTIALS ]]; then
     gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 fi
 
-# get cluster credentials
-cluster_zone=$(gcloud container clusters list --format="value(location)")
-cluster_name=$(gcloud container clusters list --format="value(name)")
-gcloud container clusters get-credentials --zone $cluster_zone \
-    --internal-ip  $cluster_name
+gsutilPath=$(which gsutil)
+echo "Path to gsutil: $gsutilPath"
+if [[ "$gsutilPath" != /usr/bin/gsutil ]]; then
+    echo "gsutil is not the system version...which could lead to auth issues. deleting the package."
+    rm $gsutilPath
+fi
 
 exec "$@"
