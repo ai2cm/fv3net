c49ec8c1942f1b4235e278f2540b9f184abcd1cd
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-15 13:05:28 -0700

    add train_sklearn as configmap too

--- tests/end_to_end_integration/run_integration_with_wait.sh
+++ tests/end_to_end_integration/run_integration_with_wait.sh
@@ -57,7 +57,8 @@ workdir=$(mktemp -d)
     envsubst < "$K8S_TEMPLATE" > job.yml
 
     # use config map to make the end to end yaml available to the job
-    kubectl create configmap -n $NAMESPACE "$CONFIGMAP" --from-file=end-to-end.yml --from-file $TESTDIR/create_training_data_variable_names.yml
+    kubectl create configmap -n $NAMESPACE "$CONFIGMAP" --from-file=end-to-end.yml --from-file $TESTDIR/create_training_data_variable_names.yml \
+        --from-file $TESTDIR/train_sklearn_model.yml
     kubectl apply -n $NAMESPACE -f job.yml
     waitForComplete "$JOBNAME" "$NAMESPACE"
 )
