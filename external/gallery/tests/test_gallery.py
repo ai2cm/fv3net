822446d3c4569ae904d2bd4bb5a8a4ee3c806226
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-04-15 17:51:11 -0700

    Add poetry.lock files

--- external/gallery/tests/test_gallery.py
+++ external/gallery/tests/test_gallery.py
@@ -1,5 +1,11 @@
-from gallery import __version__
+from datetime import datetime
+from gallery import __version__, plot_daily_and_hourly_hist
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_plot_daily_and_hourly_hist():
+    timesteps = [datetime(2016, 8, 1), datetime(2016, 8, 2)]
+    plot_daily_and_hourly_hist(timesteps)
