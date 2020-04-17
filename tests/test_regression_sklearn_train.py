083b1f13fb3e3cc35b7d7695301ed2ffe7004354
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-04-17 18:37:08 +0000

    Update HISTORY

--- tests/test_regression_sklearn_train.py
+++ tests/test_regression_sklearn_train.py
@@ -2,12 +2,12 @@ import xarray as xr
 import numpy as np
 
 from fv3net.regression.sklearn import train
-from datetime import datetime
 from fv3net.regression.dataset_handler import _shuffled
 
 
 def test_train_save_model_succeeds(tmpdir):
     model = object()
+    url = str(tmpdir)
     filename = "filename.pkl"
     train.save_model(url, model, filename)
 
