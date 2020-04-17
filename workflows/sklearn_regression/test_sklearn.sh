c94a8e894a9b7f148d3ed0ca12a4807c8280b20b
Anna Kwa <annak@vulcan.com>
2020-04-17 17:59:48 +0000

    fix config var list

--- workflows/sklearn_regression/test_sklearn.sh
+++ workflows/sklearn_regression/test_sklearn.sh
@@ -1,7 +1,7 @@
 python -m fv3net.diagnostics.sklearn_model_performance \
   gs://vcm-ml-data/test-annak/2020-04-08_sklearn_train \
-  gs://vcm-ml-data/test-annak/2020-04-08_test_bigzarr_train_pipeline  \
+  gs://vcm-ml-data/test-annak/2020-04-16_debug_intgr/create_training_data_  \
   gs://vcm-ml-data/orchestration-testing/shield-coarsened-diags-2019-12-04 \
-  gs://vcm-ml-public/test-annak/test-new-diags \
-  ./fv3net/diagnostics/sklearn_model_performance/variable_names.yml \
-  --num_test_zarrs 4
+  ./fv3net/tests/end_to_end_integration/config/test_sklearn_variable_names.yml \
+  gs://vcm-ml-public/test-annak/2020-04-17_scalarmetrics \
+  --num_test_zarrs 2
