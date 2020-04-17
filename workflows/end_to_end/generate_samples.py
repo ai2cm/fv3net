09aa6537560051eefcec5b694c22fa94cee3e478
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-17 15:30:06 -0700

    lint

--- workflows/end_to_end/generate_samples.py
+++ workflows/end_to_end/generate_samples.py
@@ -17,10 +17,7 @@ fs = fsspec.filesystem("gs")
 url = args.pop("url")
 urls = sorted(fs.ls(url))
 steps = [vcm.parse_timestep_str_from_path(url) for url in urls]
-splits = vcm.train_test_split_sample(
-    steps,
-    **args
-)
+splits = vcm.train_test_split_sample(steps, **args)
 
 all_steps = sorted(set(flatten(flatten(splits.values()))))
 data = {"one_step": list(all_steps), "train": splits}
