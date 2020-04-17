6f9e26b60bb253418157ce9973c2269fad21d285
Anna Kwa <annak@vulcan.com>
2020-04-17 14:21:57 -0700

    Feature/train data use pairs (#256)
    * use input step pairs instead of splitting in run.py
    
    * fixes
    
    * delete yml in pipelines.create_training_data
    
    * edit arg description
    
    * linting
    
    * rm deprecated arg, fix func name
    
    * rm window test
    
    * rm import
    
    * linting

--- fv3net/pipelines/create_training_data/__main__.py
+++ fv3net/pipelines/create_training_data/__main__.py
@@ -24,30 +24,20 @@ if __name__ == "__main__":
         help="yaml file for providing data variable names",
     )
     parser.add_argument(
+        "timesteps_file",
+        type=str,
+        help="File containing paired timesteps for test and/or train sets.",
+    )
+    parser.add_argument(
         "gcs_output_data_dir",
         type=str,
         help="Write path for train data in Google Cloud Storage bucket. "
         "Don't include bucket in path.",
     )
-    parser.add_argument(
-        "--timesteps-per-output-file",
-        type=int,
-        default=1,
-        help="Number of consecutive timesteps to calculate features/targets for in "
-        "a single process and save to output file."
-        "When the full output is shuffled at the data generator step, these"
-        "timesteps will always be in the same training data batch.",
-    )
-    parser.add_argument(
-        "--train-fraction",
-        type=float,
-        default=0.5,
-        help="Fraction of batches to save as training set. "
-        "Output zarr files will be saved in either 'train' or 'test' subdir of "
-        "gcs-output-data-dir",
-    )
 
     args, pipeline_args = parser.parse_known_args()
-    with open(args.variable_namefile, "r") as stream:
-        names = yaml.safe_load(stream)
-    run(args=args, pipeline_args=pipeline_args, names=names)
+    with open(args.variable_namefile, "r") as f:
+        names = yaml.safe_load(f)
+    with open(args.timesteps_file, "r") as f:
+        timesteps = yaml.safe_load(f)
+    run(args=args, pipeline_args=pipeline_args, names=names, timesteps=timesteps)
