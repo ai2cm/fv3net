8e197a0cfb30a55cee9ca56ea59a6bcc9ddea067
Noah D. Brenowitz <nbren12@gmail.com>
2020-04-19 10:25:08 -0700

    slightly update tests

--- tests/create_training_data/test.sh
+++ tests/create_training_data/test.sh
@@ -3,7 +3,7 @@ coarsen_diags=gs://vcm-ml-data/testing-noah/2020-04-18/25b5ec1a1b8a9524d2a021198
 python -m fv3net.pipelines.create_training_data \
     gs://vcm-ml-data/test-end-to-end-integration/integration-debug/one_step_run_ \
     $coarsen_diags \
-    /etc/config/create_training_data_variable_names.yml \
+    create_training_data_variable_names.yml \
     /home/jovyan/fv3net/training_times.json \
     gs://vcm-ml-data/test-end-to-end-integration/integration-debug/create_training_data_ \
     --timesteps-per-output-file 1
