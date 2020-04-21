e4e23744e0362b87e90dfdb923062a23e8091f98
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-04-21 08:40:25 -0700

    Add force_include_one_step option

--- workflows/end_to_end/generate_samples.py
+++ workflows/end_to_end/generate_samples.py
@@ -18,10 +18,12 @@ url = args.pop("url")
 urls = sorted(fs.ls(url))
 steps = [vcm.parse_timestep_str_from_path(url) for url in urls]
 spinup = args.pop("spinup", steps[0])
+include_one_step = args.pop("force_include_one_step", [])
 steps = vcm.remove_spinup_period(steps, spinup)
 splits = vcm.train_test_split_sample(steps, **args)
 
 all_steps = sorted(set(flatten(flatten(splits.values()))))
+all_steps += include_one_step
 data = {"one_step": list(all_steps), "train_and_test": splits}
 
 print(json.dumps(data))
