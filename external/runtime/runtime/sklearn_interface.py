2f2a967ef6a92e34ebc72a08add93b282a4a0465
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-05-01 10:45:21 -0700

    Add docstrings, remove type hints

--- external/runtime/runtime/sklearn_interface.py
+++ external/runtime/runtime/sklearn_interface.py
@@ -13,19 +13,36 @@ def open_model(url):
         return joblib.load(f)
 
 
-def predict(model: SklearnWrapper, state: xr.Dataset) -> xr.Dataset:
+def predict(model, state):
+    """Given ML model and state, make tendency prediction.
+
+    Args:
+        model (fv3net.regression.sklearn.wrapper.SklearnWrapper): trained ML model
+        state (xr.Dataset): atmospheric state
+
+    Returns:
+        (xr.Dataset): ML model prediction
+    """
     stacked = state.stack(sample=["x", "y"])
     with parallel_backend("threading", n_jobs=1):
         output = model.predict(stacked, "sample").unstack("sample")
     return output
 
 
-def update(
-    model: SklearnWrapper, state: xr.Dataset, dt: float
-) -> (xr.Dataset, xr.Dataset):
+def update(model, state, dt):
+    """Update state with ML model prediction of tendencies.
+
+    Args:
+        model (fv3net.regression.sklearn.wrapper.SklearnWrapper): trained ML model
+        state (xr.Dataset): atmospheric state
+        dt (float): timestep (seconds)
+
+    Returns:
+        (xr.Dataset, xr.Dataset): tuple of updated state and predicted tendencies
+    """
     tend = predict(model, state)
     updated = state.assign(
         specific_humidity=state["specific_humidity"] + tend["dQ2"] * dt,
         air_temperature=state["air_temperature"] + tend["dQ1"] * dt,
     )
-    return {key: updated[key] for key in updated}, {key: tend[key] for key in tend}
+    return updated, tend
