5f8841bcf28208ad898c1f8b8f3171b50158a711
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-05 16:39:28 -0700

    Rename .installed_build_deps to environment.lock

--- .environment-scripts/build_environment.sh
+++ .environment-scripts/build_environment.sh
@@ -3,6 +3,5 @@
 CONDA_ENV=$1
 CONDA_BASE=$(conda info --base)
 
-conda env create -n $CONDA_ENV -f environment.yml && \
-	conda list -n $CONDA_ENV > .circleci/.installed_build_env_deps || \
+conda env create -n $CONDA_ENV -f environment.yml || \
 	echo "Conda env already exists proceeding to VCM package installation"
