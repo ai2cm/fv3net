956dd07b08f5fbc8cf5c2cb8dc0a53004641c0b0
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-07 16:42:47 -0700

    Add argo workflow for restarts to zarr

--- workflows/fine_res_budget/test.py
+++ workflows/fine_res_budget/test.py
@@ -1,16 +1,19 @@
-import merge_restarts_and_diags
+from merge_restarts_and_diags import *
 import budgets
 import logging
 
 
 physics_url = "gs://vcm-ml-data/2020-03-16-5-day-X-SHiELD-simulation-C384-diagnostics/atmos_15min_coarse_ave.zarr/"
-restart_url = "gs://vcm-ml-data/2019-12-02-40-day-X-SHiELD-simulation-C384-restart-files.zarr/"
+restart_url = "gs://vcm-ml-data/testing-noah/big_local.zarr/"
 
 # TODO remove this function, not good to open and merge data in the same step.
-merged = merge_restarts_and_diags.open_merged_data(
-    restart_url,
-    physics_url
-)
+restarts = open_restart_data(restart_url)
+diag = open_diagnostic_output(physics_url)
+
+# merged = merge_restarts_and_diags.open_merged_data(
+#     restart_url,
+#     physics_url
+# )
 
 logging.info("Computing coarse-grained budgets")
 c48_budget = budgets.compute_recoarsened_budget(merged, factor=8)
