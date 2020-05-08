59b9e5bd95cd714f4f7786db4d9981e01746ab08
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-08 12:17:40 -0700

    Address security concern with testing

--- fv3net/pipelines/restarts_to_zarr/__main__.py
+++ fv3net/pipelines/restarts_to_zarr/__main__.py
@@ -1,22 +1,7 @@
-import argparse
-import fsspec
-from fv3net.pipelines.common import list_timesteps
 import logging
-import xarray as xr
-import vcm
-
-from itertools import product
-import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions
-
+import argparse
 from . import funcs
 
-CATEGORIES = (
-    "fv_srf_wnd_coarse.res",
-    "sfc_data_coarse",
-    "fv_tracer_coarse.res",
-    "fv_core_coarse.res",
-)
 
 if __name__ == "__main__":
 
@@ -28,42 +13,4 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--n-steps", default=-1, type=int)
     parser.add_argument("--no-init", action="store_true")
     args, pipeline_args = parser.parse_known_args()
-
-    times = list_timesteps(args.url)
-    if args.n_steps != -1:
-        times = times[: args.n_steps]
-
-    fs = fsspec.filesystem("gs")
-    categories = CATEGORIES
-    tiles = [1, 2, 3, 4, 5, 6]
-
-    # get schema for first timestep
-    if not args.no_init:
-        logging.info("Getting the schema")
-        time: str = times[0]
-        schema = xr.merge(
-            [
-                funcs.get_schema(fs, funcs._file(args.url, time, category, tile=1))
-                for category in categories
-            ]
-        )
-        print("Schema:")
-        print(schema)
-
-        logging.info("Initializing output zarr")
-        store = funcs._get_store(args.output)
-        output_m = vcm.ZarrMapping.from_schema(
-            store, schema, dims=["time", "tile"], coords={"tile": tiles, "time": times}
-        )
-
-    items = product(times, categories, tiles)
-    with beam.Pipeline(options=PipelineOptions(pipeline_args)) as p:
-
-        (
-            p
-            | beam.Create(items)
-            | "OpenFiles" >> beam.ParDo(funcs.get_timestep, fs, args.url)
-            | "Insert" >> beam.Map(funcs.insert_timestep, args.output)
-        )
-
-    logging.info("Job completed succesfully!")
+    funcs.run(args, pipeline_args)
--- fv3net/pipelines/restarts_to_zarr/funcs.py
+++ fv3net/pipelines/restarts_to_zarr/funcs.py
@@ -1,12 +1,24 @@
 import logging
 import os
+from itertools import product
 from typing import Tuple
 
+import apache_beam as beam
 import fsspec
 import xarray as xr
+from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.utils import retry
 
 import vcm
+from fv3net.pipelines.common import list_timesteps
+
+
+CATEGORIES = (
+    "fv_srf_wnd_coarse.res",
+    "sfc_data_coarse",
+    "fv_tracer_coarse.res",
+    "fv_core_coarse.res",
+)
 
 
 def _file(url: str, time: str, category: str, tile: int) -> str:
@@ -47,3 +59,45 @@ def get_schema(fs: fsspec.AbstractFileSystem, url: str) -> xr.Dataset:
 
 def _get_store(output: str):
     return fsspec.get_mapper(output)
+
+
+def run(args, pipeline_args):
+
+    times = list_timesteps(args.url)
+    if args.n_steps != -1:
+        times = times[: args.n_steps]
+
+    fs = fsspec.filesystem("gs")
+    categories = CATEGORIES
+    tiles = [1, 2, 3, 4, 5, 6]
+
+    # get schema for first timestep
+    if not args.no_init:
+        logging.info("Getting the schema")
+        time: str = times[0]
+        schema = xr.merge(
+            [
+                get_schema(fs, _file(args.url, time, category, tile=1))
+                for category in categories
+            ]
+        )
+        print("Schema:")
+        print(schema)
+
+        logging.info("Initializing output zarr")
+        store = _get_store(args.output)
+        vcm.ZarrMapping.from_schema(
+            store, schema, dims=["time", "tile"], coords={"tile": tiles, "time": times}
+        )
+
+    items = product(times, categories, tiles)
+    with beam.Pipeline(options=PipelineOptions(pipeline_args)) as p:
+
+        (
+            p
+            | beam.Create(items)
+            | "OpenFiles" >> beam.ParDo(get_timestep, fs, args.url)
+            | "Insert" >> beam.Map(insert_timestep, args.output)
+        )
+
+    logging.info("Job completed succesfully!")
