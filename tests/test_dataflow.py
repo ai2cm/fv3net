10d1a208f0e8dde67c94ce8ed950aaed85b48e2a
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-12 17:35:55 -0700

    Add a regression test for dataflow submission
    This ensures that the dataflow job can be submitted by the remote
    instance.

---
+++ tests/test_dataflow.py
@@ -0,0 +1,21 @@
+import subprocess
+import pytest
+
+submission = """
+python tests/simple_dataflow.py  \
+    --setup $(pwd)/setup.py \
+    --job_name test-$(uuid) \
+    --project vcm-ml \
+    --region us-central1 \
+    --runner DataFlow \
+    --temp_location gs://vcm-ml-data/tmp_dataflow \
+    --num_workers 1 \
+    --autoscaling_algorithm=NONE \
+    --worker_machine_type n1-standard-1 \
+    --disk_size_gb 30
+"""
+
+
+@pytest.mark.regression()
+def test_submit_dataflow():
+    subprocess.check_call(["bash", "-c", submission])
