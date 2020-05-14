96bde745a79aae53cc4f65dd0ff04e527d7475b0
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-13 18:26:58 -0700

    Revert "Copy the package_dir from poetry generated setup.py"
    This reverts commit 0008e1fe7cb299aeed52a1e4f52ec9a27cda5322.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ dependencies = [
 
 
 packages = find_packages() + find_packages("external/vcm", exclude="tests")
-package_dir = {"": ["*"]}
+package_dir = {"vcm": f"external/vcm/vcm/", "fv3net": "fv3net"}
 
 print(f"Found packages: {packages}")
 setup(
