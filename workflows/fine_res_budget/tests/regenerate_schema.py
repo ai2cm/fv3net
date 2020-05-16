b1eafed354c99a9a443326125740696b81475a7b
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-16 10:26:04 -0700

    Lint

--- workflows/fine_res_budget/tests/regenerate_schema.py
+++ workflows/fine_res_budget/tests/regenerate_schema.py
@@ -8,7 +8,7 @@ import synth
 logging.basicConfig(level=logging.INFO)
 
 
-diagurl = "gs://vcm-ml-data/2020-03-16-5-day-X-SHiELD-simulation-C384-diagnostics/atmos_15min_coarse_ave.zarr"
+diagurl = "gs://vcm-ml-data/2020-03-16-5-day-X-SHiELD-simulation-C384-diagnostics/atmos_15min_coarse_ave.zarr"  # noqa
 restart_url = "gs://vcm-ml-data/2020-03-16-5-day-X-SHiELD-simulation-C384-restart-files.zarr"  # noqa
 
 lo_res_coords = ("time", "tile", "grid_xt", "grid_yt", "pfull")
