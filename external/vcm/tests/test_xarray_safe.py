fd3c6ffa1a7a723a7e3173f0f869030dbcdcf28e
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-16 14:34:13 -0700

    Remove xarray warnings
    This functionality seems brittle and over-engineered for such a
    a simple concept. The warning messages do not provide a very useful
    signal, and engineering them to the point where they do will take
    substantial effort to do in SOLID way.

--- external/vcm/tests/test_xarray_safe.py
+++ external/vcm/tests/test_xarray_safe.py
@@ -2,12 +2,7 @@ import xarray as xr
 import numpy as np
 import pytest
 
-from vcm.safe import (
-    _validate_stack_dims,
-    warner,
-    _is_in_module,
-)
-from vcm import testing
+from vcm.safe import _validate_stack_dims
 
 
 def test__validate_stack_dims_ok():
@@ -27,41 +22,3 @@ def test__validate_stack_dims_not_ok():
         _validate_stack_dims(ds, ["x", "y", "z"])
 
     _validate_stack_dims(ds, ["x", "y", "z"], allowed_broadcast_dims=["z"])
-
-
-def test_warnings():
-    arr_2d = np.ones((10, 10))
-    ds = xr.Dataset({"a": (["x", "y"], arr_2d), "b": (["x", "y"], arr_2d)})
-
-    with pytest.warns(UserWarning):
-        ds["a"]
-
-    with pytest.warns(UserWarning):
-        ds[["a", "b"]]
-
-    with testing.no_warning(None):
-        with warner.allow():
-            ds["a"]
-
-
-@pytest.mark.parametrize(
-    "path, module, expected",
-    [
-        (xr.__file__, xr, True),
-        (xr.testing.__file__, xr, True),
-        # Test long file name to make sure path inequality works as expected
-        ("aadjfoaisdjfa/" * 20 + "localfile.py", xr, False),
-        ("localfile.py", xr, False),
-    ],
-)
-def test__is_in_module(path, module, expected):
-    assert _is_in_module(path, module)
-
-
-def test__internal_usage_doesnt_warn():
-    """Internal usages of blacklisted functions within xarray should not emit warnings
-    """
-    ds = xr.Dataset({"a": (["x", "y"], np.ones((10, 5)))})
-    a = ds["a"]
-    with testing.no_warning(None):
-        a.stack(new=["x", "y"])
--- external/vcm/vcm/safe.py
+++ external/vcm/vcm/safe.py
@@ -1,73 +1,7 @@
-from functools import wraps
 from typing import cast, Sequence, Hashable
-import traceback
 import xarray as xr
-import warnings
-from contextlib import contextmanager
-import pathlib
 
 
-def _is_in_module(path, module):
-    path = pathlib.Path(path)
-    module_root = pathlib.Path(module.__file__).parent
-    return path > module_root
-
-
-def _called_from_module(module):
-    stack = traceback.extract_stack()
-    for frame in stack[::-1]:
-        ans = _is_in_module(frame.filename, module)
-        if ans:
-            return True
-    return False
-
-
-class Warner:
-    """Class for managing warnings for unsafe functions in commonly used libraries"""
-
-    def __init__(self):
-        self.warn = True
-
-    @contextmanager
-    def allow(self):
-        self.warn = False
-        yield
-        self.warn = True
-
-    def warn_on_use(self, class_, method):
-        func = getattr(class_, method)
-
-        def myfunc(*args, **kwargs):
-            called_from_xarray = _called_from_module(xr)
-            if self.warn and not called_from_xarray:
-                warnings.warn(
-                    name + " is unsafe. Please avoid use in long-running code."
-                )
-            return func(*args, **kwargs)
-
-        name = class_.__name__ + "." + method
-        setattr(class_, method, myfunc)
-
-    def allow_use(self, func):
-        @wraps(func)
-        def myfunc(*args, **kwargs):
-            with self.allow():
-                return func(*args, **kwargs)
-
-        return myfunc
-
-
-blacklist = [
-    (xr.Dataset, "stack"),
-    (xr.Dataset, "__getitem__"),
-]
-
-warner = Warner()
-for class_, method in blacklist:
-    warner.warn_on_use(class_, method)
-
-
-@warner.allow_use
 def get_variables(ds: xr.Dataset, variables: Sequence[Hashable]) -> xr.Dataset:
     """ds[...] is very confusing function from a typing perspective and should be
     avoided in long-running pipeline codes. This function introduces a type-stable
@@ -94,7 +28,6 @@ def _validate_stack_dims(ds, dims, allowed_broadcast_dims=()):
             )
 
 
-@warner.allow_use
 def stack_once(
     ds: xr.Dataset,
     dim,
