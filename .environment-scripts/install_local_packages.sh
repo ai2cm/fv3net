12b47472d0d30d3734593de060435b5ac111800a
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-16 11:06:41 -0700

    Build mappm

--- .environment-scripts/install_local_packages.sh
+++ .environment-scripts/install_local_packages.sh
@@ -4,7 +4,9 @@ CONDA_ENV=$1
 
 source activate $CONDA_ENV
 
-local_packages_to_install=()
+local_packages_to_install=( 
+  external/vcm/external/mappm 
+)
 for package  in "${local_packages_to_install[@]}"
 do
   pip install --no-deps -e "$package"
@@ -13,7 +15,6 @@ done
 poetry_packages=( external/runtime external/report external/gallery . 
   external/fv3config 
   external/vcm 
-  external/vcm/external/mappm 
   external/synth 
   workflows/one_step_diags 
   workflows/fine_res_budget
