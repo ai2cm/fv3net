4bbafe28d52d898c6b0ff5c9b21a21557f1c5ec5
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-16 10:46:22 -0700

    Add fine_res_budget to local packages list

--- .environment-scripts/install_local_packages.sh
+++ .environment-scripts/install_local_packages.sh
@@ -5,17 +5,24 @@ CONDA_ENV=$1
 source activate $CONDA_ENV
 
 local_packages_to_install=()
-for package  in ${local_packages_to_install[@]}
+for package  in "${local_packages_to_install[@]}"
 do
-  pip install --no-deps -e $package
+  pip install --no-deps -e "$package"
 done
 
-poetry_packages=( external/runtime external/report external/gallery . external/fv3config external/vcm external/vcm/external/mappm external/synth workflows/one_step_diags )
+poetry_packages=( external/runtime external/report external/gallery . 
+  external/fv3config 
+  external/vcm 
+  external/vcm/external/mappm 
+  external/synth 
+  workflows/one_step_diags 
+  workflows/fine_res_budget
+)
 
-for package in ${poetry_packages[@]}
+for package in "${poetry_packages[@]}"
 do
   (
-    cd $package
+    cd "$package" || exit
     conda develop .
   )
 done
