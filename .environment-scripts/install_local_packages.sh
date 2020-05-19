345087f538327425aad4b360bab581f4b0a0ae93
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-05-18 19:00:37 -0700

    Add fv3kube to poetry packages

--- .environment-scripts/install_local_packages.sh
+++ .environment-scripts/install_local_packages.sh
@@ -10,7 +10,9 @@ do
   pip install --no-deps -e $package
 done
 
-poetry_packages=( external/runtime external/report external/gallery . external/fv3config external/vcm external/vcm/external/mappm external/synth workflows/one_step_diags )
+poetry_packages=( external/runtime external/report external/gallery . external/fv3config \
+                  external/vcm external/vcm/external/mappm external/synth workflows/one_step_diags \
+                  external/fv3kube )
 
 for package in ${poetry_packages[@]}
 do
