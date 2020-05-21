5841927433058c2d94920fcd039dbfd140938528
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-21 16:07:39 -0700

    Add support for GCS urls

--- fv3net/regression/loaders/_fine_resolution_budget.py
+++ fv3net/regression/loaders/_fine_resolution_budget.py
@@ -2,17 +2,16 @@ import os
 import re
 import vcm
 import xarray as xr
+from toolz import groupby
 
 
 class FineResolutionBudgetTiles:
     """An Mapping interface to a fine-res-q1-q2 dataset"""
 
     def __init__(self, url):
-        super(FineResolutionBudget, self).__init__()
-        fs = vcm.cloud.get_fs(url)
+        self._fs = vcm.cloud.get_fs(url)
         self._url = url
-        self.files = fs.glob(os.path.join(url, "*.nc"))
-
+        self.files = self._fs.glob(os.path.join(url, "*.nc"))
         if len(self.files) == 0:
             raise ValueError("No file detected")
 
@@ -24,7 +23,7 @@ class FineResolutionBudgetTiles:
         return date, int(tile)
 
     def __getitem__(self, key: str) -> xr.Dataset:
-        return xr.open_dataset(self._find_file(key))
+        return vcm.open_remote_nc(self._fs, self._find_file(key))
 
     def _find_file(self, key):
         return [file for file in self.files if self._parse_file(file) == key][-1]
@@ -39,16 +38,53 @@ class FineResolutionBudgetTiles:
         return len(self.keys())
 
 
+# TODO this name is too specific. This operation will work for any Mapping keyed by
+# (time, tile) tuples
 class FineResolutionBudget:
-    # TODO this name is too specific. This operation will work for any Mapping keyed by
-    # (time, tile) tuples
+    """
+
+    Example:
+
+        >>> loader = loaders.FineResolutionBudget.from_url('gs://vcm-ml-scratch/noah/2020-05-19/')
+        >>> loader['20160805.202230']
+        <xarray.Dataset>
+        Dimensions:                         (grid_xt: 48, grid_yt: 48, pfull: 79, tile: 6)
+        Coordinates:
+            step                            <U6 'middle'
+            time                            object 2016-08-05 20:22:30
+        * tile                            (tile) int64 1 2 3 4 5 6
+        Dimensions without coordinates: grid_xt, grid_yt, pfull
+        Data variables:
+            air_temperature                 (tile, pfull, grid_yt, grid_xt) float32 235.28934 ... 290.56107
+            air_temperature_convergence     (tile, grid_yt, grid_xt, pfull) float32 4.3996937e-07 ... 1.7985441e-06
+            air_temperature_eddy            (tile, pfull, grid_yt, grid_xt) float32 -2.3193044e-05 ... 0.0004279223
+            air_temperature_microphysics    (tile, pfull, grid_yt, grid_xt) float32 0.0 ... -5.5472506e-06
+            air_temperature_nudging         (tile, pfull, grid_yt, grid_xt) float32 0.0 ... 2.0156076e-06
+            air_temperature_physics         (tile, pfull, grid_yt, grid_xt) float32 2.3518855e-06 ... -3.3252392e-05
+            air_temperature_resolved        (tile, pfull, grid_yt, grid_xt) float32 0.26079428 ... 0.6763954
+            air_temperature_storage         (tile, pfull, grid_yt, grid_xt) float32 0.000119928314 ... 5.2825694e-06
+            specific_humidity               (tile, pfull, grid_yt, grid_xt) float32 5.7787e-06 ... 0.008809893
+            specific_humidity_convergence   (tile, grid_yt, grid_xt, pfull) float32 -6.838638e-14 ... -1.7079346e-08
+            specific_humidity_eddy          (tile, pfull, grid_yt, grid_xt) float32 -1.0437861e-13 ... -2.5796332e-06
+            specific_humidity_microphysics  (tile, pfull, grid_yt, grid_xt) float32 0.0 ... 1.6763515e-09
+            specific_humidity_physics       (tile, pfull, grid_yt, grid_xt) float32 -1.961625e-14 ... 5.385441e-09
+            specific_humidity_resolved      (tile, pfull, grid_yt, grid_xt) float32 6.4418755e-09 ... 2.0072384e-05
+            specific_humidity_storage       (tile, pfull, grid_yt, grid_xt) float32 -6.422655e-11 ... -5.3609618e-08
+    """
 
     def __init__(self, tiles: FineResolutionBudgetTiles) -> "FineResolutionBudget":
         self._tiles = tiles
+        self._time_lookup = groupby(lambda x: x[0], self._tiles.keys())
+
+    def keys(self):
+        return self._time_lookup.keys()
+
+    def __len__(self):
+        return len(self.keys())
 
     def __getitem__(self, time: str) -> xr.Dataset:
         tiles = list(range(1, 7))
-        datasets = [self._tiles[(time, tile)] for tile in tiles]
+        datasets = [self._tiles[key] for key in self._time_lookup[time]]
         return xr.concat(datasets, dim="tile").assign_coords(tile=tiles)
 
     @classmethod
