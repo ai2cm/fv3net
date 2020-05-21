2749e915f5cacd63e03ced8b1d542e638af801b1
Anna Kwa <annak@vulcan.com>
2020-05-21 23:03:49 +0000

    move transform out of _one_step.py

--- fv3net/regression/loaders/_one_step.py
+++ fv3net/regression/loaders/_one_step.py
@@ -10,6 +10,7 @@ import xarray as xr
 import vcm
 from vcm import cloud, safe
 from ._sequences import FunctionOutputSequence
+from ._transforms import stack_and_format
 from ..constants import SAMPLE_DIM_NAME, TIME_NAME
 
 __all__ = ["load_one_step_batches"]
@@ -45,7 +46,6 @@ def load_one_step_batches(
     random_seed: int = 1234,
     mask_to_surface_type: str = None,
     init_time_dim_name: str = "initial_time",
-    z_dim_name: str = "z",
     rename_variables: Mapping[str, str] = None,
 ) -> Sequence:
     """Get a sequence of batches from one-step zarr stores.
@@ -61,7 +61,6 @@ def load_one_step_batches(
         random_seed (optional): seed value for random number generator
         mask_to_surface_type: mask data points to ony include the indicated surface type
         init_time_dim_name: name of the initialization time dimension
-        z_dim_name: name of the vertical dimension
         rename_variables: mapping of variables to rename,
             from data names to standard names
     """
@@ -93,9 +92,8 @@ def load_one_step_batches(
             init_time_dim_name,
         )
         input_formatted_batch = functools.partial(
-            _stack_and_format,
+            stack_and_format,
             init_time_dim_name,
-            z_dim_name,
             mask_to_surface_type,
             copy.deepcopy(random),  # each sequence must be shuffled the same!
         )
@@ -129,33 +127,6 @@ def _load_one_step_batch(
     return ds.load()
 
 
-def _stack_and_format(
-    init_time_dim_name: str,
-    z_dim_name: str,
-    mask_to_surface_type: str,
-    random,
-    ds: xr.Dataset,
-) -> xr.Dataset:
-    if mask_to_surface_type is not None:
-        ds = vcm.mask_to_surface_type(ds, mask_to_surface_type)
-    stack_dims = [dim for dim in ds.dims if dim != z_dim_name]
-    ds_stacked = safe.stack_once(
-        ds,
-        SAMPLE_DIM_NAME,
-        stack_dims,
-        allowed_broadcast_dims=[z_dim_name, init_time_dim_name],
-    )
-
-    ds_no_nan = ds_stacked.dropna(SAMPLE_DIM_NAME)
-
-    if len(ds_no_nan[SAMPLE_DIM_NAME]) == 0:
-        raise ValueError(
-            "No Valid samples detected. Check for errors in the training data."
-        )
-    ds = ds_no_nan.load()
-    return _shuffled(ds, SAMPLE_DIM_NAME, random)
-
-
 
 @backoff.on_exception(backoff.expo, (ValueError, RuntimeError), max_tries=3)
 def _load_datasets(fs, urls):
@@ -184,26 +155,3 @@ def _validated_num_batches(total_num_input_files, files_per_batch, num_batches=N
     else:
         num_train_batches = num_batches
     return num_train_batches
-
-
-def _shuffled(dataset, dim, random):
-    chunks_default = (len(dataset[dim]),)
-    chunks = dataset.chunks.get(dim, chunks_default)
-    indices = _chunk_indices(chunks)
-    shuffled_inds = _shuffled_within_chunks(indices, random)
-    return dataset.isel({dim: shuffled_inds})
-
-
-def _chunk_indices(chunks):
-    indices = []
-
-    start = 0
-    for chunk in chunks:
-        indices.append(list(range(start, start + chunk)))
-        start += chunk
-    return indices
-
-
-def _shuffled_within_chunks(indices, random):
-    # We should only need to set the random seed once (not every time)
-    return np.concatenate([random.permutation(index) for index in indices])
