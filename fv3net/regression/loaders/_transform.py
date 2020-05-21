6429d6f4c0f01960e446cc77b1d09994e6a36bf0
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-21 16:27:12 -0700

    Make the type signature accurate

--- fv3net/regression/loaders/_transform.py
+++ fv3net/regression/loaders/_transform.py
@@ -4,13 +4,12 @@ from toolz import groupby
 
 
 K = TypeVar("K")
-V = TypeVar("V")
 
 
 class GroupByKey:
     def __init__(
-        self, tiles: Mapping[K, V], fn: Callable[Tuple[K], Hashable]
-    ) -> Mapping[K, V]:
+        self, tiles: Mapping[K, xr.Dataset], fn: Callable[[K], Hashable]
+    ) -> Mapping[K, xr.Dataset]:
         self._tiles = tiles
         self._time_lookup = groupby(fn, self._tiles.keys())
 
@@ -21,6 +20,7 @@ class GroupByKey:
         return len(self.keys())
 
     def __getitem__(self, time: str) -> xr.Dataset:
+        # TODO generalize this function
         tiles = list(range(1, 7))
         datasets = [self._tiles[key] for key in self._time_lookup[time]]
         return xr.concat(datasets, dim="tile").assign_coords(tile=tiles)
