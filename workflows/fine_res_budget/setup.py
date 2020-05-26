0f8a709a42d096e63093c9d789cc9d140310abd4
Noah D Brenowitz <nbren12@gmail.com>
2020-05-26 13:12:54 -0700

    Scale fine-res budget calculation with Google Dataflow (#351)
    The apache beam pipeline to compute the fine-res q1 and q2 was previously only run for local instances. This work scales it to a remote dataflow job.
    
    Changes:
    - submit the remote beam job using `dataflow.sh`, adding some necessary sdist commands
    - Respect chunking of ShiELD physics diagnostics when loading the data. This dataset uses a time chunks size of 12, so the previous implementation would require loading each chunk 12 times.

---
+++ workflows/fine_res_budget/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = ["budget"]
+
+package_data = {"": ["*"]}
+
+install_requires = []
+
+setup_kwargs = {
+    "name": "budget",
+    "version": "0.1.0",
+    "description": "",
+    "long_description": None,
+    "author": "Noah D. Brenowitz",
+    "author_email": "nbren12@gmail.com",
+    "maintainer": None,
+    "maintainer_email": None,
+    "url": None,
+    "packages": packages,
+    "package_data": package_data,
+    "install_requires": install_requires,
+    "python_requires": ">=3.7,<4.0",
+}
+
+
+setup(**setup_kwargs)
