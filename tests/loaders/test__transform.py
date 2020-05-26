c6dae6e2909aa50120dac1dcc8c34a67a91dd7c0
Anna Kwa <annak@vulcan.com>
2020-05-26 19:29:05 +0000

    move transform test to loader test dir

--- tests/loaders/test__transform.py
+++ tests/loaders/test__transform.py
@@ -1,32 +1,31 @@
 from fv3net.regression.loaders._transform import _shuffled, _chunk_indices
 import numpy as np
-import pytest
 import xarray as xr
 
 
-@pytest.fixture
-def test_dataset():
-    m, n = 10, 2
-    x = "x"
-    sample = "sample"
-    return xr.Dataset(
-        {"a": ([sample, x], np.ones((m, n))), "b": ([sample], np.ones((m)))},
-        coords={x: np.arange(n), sample: np.arange(m)},
-    )
-
-
-def test__shuffled(test_dataset):
-    dataset = test_dataset.isel(sample=1)
-    _shuffled(dataset, "sample", np.random.RandomState(1))
-
-
-def test__shuffled_dask(test_dataset):
-    dataset = test_dataset.chunk()
-    _shuffled(dataset, "sample", np.random.RandomState(1))
-
-
-def test__chunk_indices(test_dataset):
+def test__chunk_indices():
     chunks = (2, 3)
     expected = [[0, 1], [2, 3, 4]]
     ans = _chunk_indices(chunks)
     assert ans == expected
+
+
+def _dataset(sample_dim):
+    m, n = 10, 2
+    x = "x"
+    sample = sample_dim
+    return xr.Dataset(
+        {"a": ([sample, x], np.ones((m, n))), "b": ([sample], np.ones((m)))},
+        coords={x: np.arange(n), sample_dim: np.arange(m)},
+    )
+
+
+def test__shuffled():
+    dataset = _dataset("sample")
+    dataset.isel(sample=1)
+    _shuffled(dataset, "sample", np.random.RandomState(1))
+
+
+def test__shuffled_dask():
+    dataset = _dataset("sample").chunk()
+    _shuffled(dataset, "sample", np.random.RandomState(1))
