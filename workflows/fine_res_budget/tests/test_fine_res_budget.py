118214e110f8b4e749bdb0bd42ad567f18654378
Noah D. Brenowitz <nbren12@gmail.com>
2020-05-27 10:52:25 -0700

    Use linear extrapolation in convergence calculation

--- workflows/fine_res_budget/tests/test_fine_res_budget.py
+++ workflows/fine_res_budget/tests/test_fine_res_budget.py
@@ -59,13 +59,24 @@ def test_OpenTimeChunks():
         chunks | "Assert no time or tile" >> beam.Map(_assert_no_time_or_tile)
 
 
-def test__convergence():
+def test__convergence_constant():
     nz = 5
     delp = np.ones(nz).reshape((1, 1, nz))
 
-    expected = np.array([-1, 0, 0, 0, 1]).reshape((1, 1, nz))
+    expected = np.array([0, 0, 0, 0, 0]).reshape((1, 1, nz))
 
     ans = _convergence(delp, delp)
+    np.testing.assert_almost_equal(ans, expected)
+
+
+def test__convergence_linear():
+    nz = 5
+    f = np.arange(nz).reshape((1, 1, nz))
+    delp = np.ones(nz).reshape((1, 1, nz))
+
+    expected = np.array([-1, -1, -1, -1, -1]).reshape((1, 1, nz))
+
+    ans = _convergence(f, delp)
     np.testing.assert_almost_equal(ans, expected)
 
 
