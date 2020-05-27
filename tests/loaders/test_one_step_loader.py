80cb2ccd247e54e57a62e3850ad78c55d5573277
Anna Kwa <annak@vulcan.com>
2020-05-27 22:42:55 +0000

    address Andre's PR comments

--- tests/loaders/test_one_step_loader.py
+++ tests/loaders/test_one_step_loader.py
@@ -13,38 +13,27 @@ from fv3net.regression.loaders._one_step import (
     TimestepMapper,
 )
 
-ONE_STEP_ZARR_SCHEMA = "tests/loaders/one_step_zarr_schema.json"
+ONE_STEP_ZARR_SCHEMA = "one_step_zarr_schema.json"
 Z_DIM_SIZE = 79
-TEMP_DIR = tempfile.TemporaryDirectory()
-
-
-def _cleanup_temp_dir(temp_dir):
-    print(f"Cleaning up temp dir {temp_dir.name}")
-    temp_dir.cleanup()
-
-
-def _write_synth_dataset(output_path):
-    with open(ONE_STEP_ZARR_SCHEMA) as f:
-        schema = synth.load(f)
-    one_step_dataset = synth.generate(schema)
-    one_step_dataset.to_zarr(output_path, consolidated=True)
-
-
-atexit.register(_cleanup_temp_dir, TEMP_DIR)
-for i in range(4):
-    output_path = os.path.join(TEMP_DIR.name, f"2020050{i}.000000.zarr")
-    _write_synth_dataset(output_path)
+TIMESTEP_LIST = [f"2020050{i}.000000" for i in range(4)]
+DATA_VARS = ["air_temperature", "specific_humidity"]
 
 
 @pytest.fixture
-def test_timestep_mapper():
-    timestep_mapper = TimestepMapper(TEMP_DIR.name)
+def test_timestep_mapper(datadir):
+    with open(os.path.join(datadir, ONE_STEP_ZARR_SCHEMA)) as f:
+        schema = synth.load(f)
+    one_step_dataset = synth.generate(schema)
+    for timestep in TIMESTEP_LIST:
+        output_path = os.path.join(datadir, f"{timestep}.zarr")
+        one_step_dataset.to_zarr(output_path, consolidated=True)    
+    timestep_mapper = TimestepMapper(str(datadir))
     return timestep_mapper
 
 
 def test_load_datasets(test_timestep_mapper):
     ds_list = _load_datasets(
-        test_timestep_mapper, ["20200500.000000", "20200501.000000"]
+        test_timestep_mapper, TIMESTEP_LIST[:2]
     )
     assert len(ds_list) == 2
 
@@ -55,18 +44,19 @@ def test__load_one_step_batch(test_timestep_mapper):
         data_vars=["air_temperature", "specific_humidity"],
         rename_variables={},
         init_time_dim_name="time",
-        timestep_list=["20200500.000000", "20200501.000000", "20200502.000000"],
+        timestep_list=TIMESTEP_LIST[:3],
     )
     assert len(ds["time"]) == 3
 
 
-def test_load_one_step_batches():
+def test_load_one_step_batches(datadir, test_timestep_mapper):
     batched_data_sequence = load_one_step_batches(
-        TEMP_DIR.name,
-        ["air_temperature", "specific_humidity"],
+        str(datadir),
+        DATA_VARS,
         files_per_batch=2,
         num_batches=2,
     )
     assert len(batched_data_sequence) == 2
     for i, batch in enumerate(batched_data_sequence):
         assert len(batch["z"]) == Z_DIM_SIZE
+        assert set(batch.data_vars) == set(DATA_VARS)
