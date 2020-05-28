96454282ee23b79096a56bf1a88309611b968acc
Andre Perkins <frodre@gmail.com>
2020-05-28 19:29:57 +0000

    Roll mapper back into loading_nudging_batches

--- fv3net/regression/loaders/_nudged.py
+++ fv3net/regression/loaders/_nudged.py
@@ -1,5 +1,6 @@
 import os
 import fsspec
+import logging
 import xarray as xr
 import pandas as pd
 import numpy as np
@@ -15,6 +16,8 @@ from vcm.cubedsphere.constants import TIME_FMT
 from . import _transform as transform
 from ._sequences import FunctionOutputSequence
 
+logger = logging.getLogger(__name__)
+
 TIMESCALE_OUTDIR_TEMPLATE = "outdir-*h"
 SIMULATION_TIMESTEPS_PER_HOUR = 4
 NUDGING_FILES = [
@@ -26,15 +29,16 @@ NUDGING_FILES = [
 
 SAMPLE_DIM = "sample"
 NUDGED_TIME_DIM = "time"
+Z_DIM = "z"
 
 BatchSequence = Sequence[xr.Dataset]
 
 
 def load_nudging_batches(
     data_path: str,
-    *variable_names: Iterable[str],
+    data_vars: Iterable[str],
     timescale_hours: Union[int, float] = 3,
-    num_samples_in_batch: int = 13824,
+    num_times_in_batch: int = 10,
     num_batches: int = None,
     random_seed: int = 0,
     mask_to_surface_type: str = None,
@@ -49,12 +53,10 @@ def load_nudging_batches(
 
     Args:
         data_path: location of directory containing zarr stores
-        *variable_names: any number of sequences of variable names. One Sequence will be
-            returned for each of the given sequences. The "sample" dimension will be
-            identical across each of these sequences.
+        data_vars: names of variables to include in the output batched dataset 
         timescale_hours (optional): timescale of the nudging for the simulation
             being used as input.
-        num_samples_in_batch (optional): number of samples to include
+        num_times_in_batch (optional): number of times to include
             in a single batch item.  Overridden by num_batches
         num_batches (optional): number of batches to split the
             input samples into.  Overrides num_samples_in_batch.
@@ -63,100 +65,73 @@ def load_nudging_batches(
             Requires "land_sea_mask" exists in the loaded dataset.  Note: as currently
             implemented NaN drop may reduce the batch size under requested
             number of samples.
-        z_dim_name (optional): vertical dimension name to retain in the dimension
-            stacking procedure
         rename_variables (optional): A mapping to update any variable names in the
             dataset prior to the selection of input/output variables
-        time_dim_name (optional): Time dimension name to use for selection from input
-            data
         initial_time_skip_hr (optional): Length of model inititialization (in hours)
             to omit from the batching operation
         n_times (optional): Number of times (by index) to include in the
             batch resampling operation
     """
 
-    nudged_output_path = _get_path_for_nudging_timescale(
-        nudged_output_dirs, timescale_hours
+    logger.info(f"Loading nudged mapper from sources in {data_path}")
+    nudged_mapper = open_nudged_mapper(data_path, timescale_hours, initial_time_skip_hr, n_times)
+    nudged_tstep_mapper = nudged_mapper.merge_sources(["after_physics", "nudging_tendencies"])
+
+    random = np.random.RandomState(random_seed)
+    timesteps = random.shuffle(nudged_tstep_mapper.keys())
+
+    # TODO: Overlaps with _validated_num_batches in _one_step
+    if num_batches is None:
+        num_batches = len(timesteps) // num_times_in_batch
+    elif num_batches * num_times_in_batch > len(timesteps):
+        raise ValueError(
+            f"Not enough timesteps (n={len(timesteps)} to create requested "
+            f"{num_batches} batches of size {num_times_in_batch}"
+        )
+
+    logger.info(f"Creating {num_batches} batches of size {num_times_in_batch}")
+    batched_timesteps = []
+    for i in range(num_batches):
+        start = i * num_times_in_batch
+        end = start + num_times_in_batch
+        batched_timesteps.append(timesteps[start:end])
+
+    batch_loader = partial(
+        _load_nudging_batch,
+        nudged_tstep_mapper,
+        data_vars,
+        rename_variables,
+        mask_to_surface_type,
     )
+    return FunctionOutputSequence(batch_loader, batched_timesteps)
 
-    datasets_to_batch = _load_datasets(
-        fs, nudged_output_path, variable_names, rename_variables
-    )
-
-    batched_sequences = []
-    for dataset in datasets_to_batch:
-        start = initial_time_skip_hr * SIMULATION_TIMESTEPS_PER_HOUR
-        end = start + n_times
-        dataset = dataset.isel({time_dim_name: slice(start, end)})
-
-        if mask_to_surface_type is not None:
-            dataset = vcm.mask_to_surface_type(dataset, mask_to_surface_type)
-
-        stack_dims = [dim for dim in dataset.dims if dim != z_dim_name]
-        ds_stacked = safe.stack_once(
-            dataset, SAMPLE_DIM, stack_dims, allowed_broadcast_dims=[z_dim_name]
-        )
-
-        total_samples = ds_stacked.sizes[SAMPLE_DIM]
-
-        sample_slice_sequence = _get_batch_slices(
-            total_samples, num_samples_in_batch, num_batches=num_batches
-        )
-        random = np.random.RandomState(random_seed)
-        random.shuffle(sample_slice_sequence)
-
-        loader_func = partial(_load_nudging_batch, ds_stacked, random)
-
-        batched_sequences.append(
-            FunctionOutputSequence(loader_func, sample_slice_sequence)
-        )
-
-    if len(batched_sequences) > 1:
-        return batched_sequences
-    else:
-        return batched_sequences[0]
+# TODO: changed toplevel constants to shared constants
 
 
 def _load_nudging_batch(
-    stacked_ds: xr.Dataset, random: np.random.RandomState, batch_slice: slice
+    timestep_mapper, data_vars, rename_variables, mask_to_surface_type, tstep_keys
 ) -> xr.Dataset:
 
-    batch = stacked_ds.isel({SAMPLE_DIM: batch_slice})
-    batch = batch.load()
-    batch_no_nan = batch.dropna(SAMPLE_DIM)
+    logger.debug(f"Loading batch with mapper keys: {tstep_keys}")
+    batch = [timestep_mapper[timestep] for timestep in tstep_keys]
+    batch_ds = xr.concat(batch, NUDGED_TIME_DIM)
+    batch_ds = batch_ds.rename(rename_variables)
+    batch_ds = vcm.mask_to_surface_type(batch_ds, mask_to_surface_type) 
+    batch_ds = safe.get_variables(batch_ds, data_vars)
 
-    if len(batch_no_nan[SAMPLE_DIM]) == 0:
+    # Into memory we go
+    batch_ds = batch_ds.load()
+
+    stack_dims = [dim for dim in batch_ds.dims if dim != Z_DIM]
+    stacked_batch_ds = safe.stack(batch_ds, SAMPLE_DIM, stack_dims, allowed_broadcast_dims=[Z_DIM])
+    stacked_batch_ds = stacked_batch_ds.dropna(SAMPLE_DIM)
+
+    if len(stacked_batch_ds[SAMPLE_DIM]) == 0:
         raise ValueError(
             "No Valid samples detected. Check for errors in the training data."
         )
 
-    return transform.shuffled(batch_no_nan, SAMPLE_DIM, random)
-
-
-def _get_batch_slices(
-    num_samples: int, samples_per_batch: int, num_batches: int = None
-):
-
-    if num_batches is not None:
-        batch_size = num_samples // num_batches
-    else:
-        batch_size = samples_per_batch
-        num_batches = num_samples // samples_per_batch
-
-    if batch_size == 0 or num_batches == 0:
-        raise ValueError(
-            "The number of input samples is insufficient to create a batch for "
-            f"requested samples_per_batch={samples_per_batch} or "
-            f"requested num_batches={num_batches}."
-        )
-
-    slices = []
-    for i in range(num_batches):
-        start = i * batch_size
-        end = start + batch_size
-        slices.append(slice(start, end))
-
-    return slices
+    return stacked_batch_ds
 
 
 def _get_path_for_nudging_timescale(nudged_output_dirs, timescale_hours, tol=1e-5):
@@ -248,7 +223,11 @@ class NudgedMapperAllSources(BaseMapper):
 
             combined_ds = combined_ds.merge(ds)
 
-        return NudgedTimestepMapper(combined_ds)         
+        return NudgedTimestepMapper(combined_ds)
+
+    # TODO: group by operations
+    #   groupby source is easy since we could just return the self._nudge_ds mapping
+    #   groupby time needs merged ds for all timesteps
 
     @staticmethod
     def _check_dvar_overlap(ds1, ds2):
