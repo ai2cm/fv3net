f245c5d725fde65d883d57cbbb2192599f37b340
brianhenn <brianhenn@gmail.com>
2020-06-03 21:57:05 +0000

    created separate branch and PR for fine res apparent source mapper

--- fv3net/regression/loaders/__init__.py
+++ fv3net/regression/loaders/__init__.py
@@ -1,9 +1,13 @@
 # This module should only contain loaders we mean to be able to specify in the
 # model training configuration
 from ._one_step import open_one_step
-from ._fine_resolution_budget import open_fine_resolution_budget
+from ._fine_resolution_budget import (
+    open_fine_resolution_budget,
+    open_fine_res_apparent_sources,
+)
 
 # TODO: replace with open mapping func for nudging data source
 from ._nudged import load_nudging_batches
 
+from ._batch import mapper_to_batches
 from .constants import TIME_NAME, SAMPLE_DIM_NAME
--- fv3net/regression/loaders/_fine_resolution_budget.py
+++ fv3net/regression/loaders/_fine_resolution_budget.py
@@ -3,7 +3,7 @@ import re
 import vcm
 from typing import Mapping
 import xarray as xr
-from ._transform import GroupByTime
+from ._transform import GroupByTime, FineResolutionSources
 
 
 class FineResolutionBudgetTiles:
@@ -76,3 +76,10 @@ def open_fine_resolution_budget(url: str) -> Mapping[str, xr.Dataset]:
     """  # noqa
     tiles = FineResolutionBudgetTiles(url)
     return GroupByTime(tiles)
+
+
+def open_fine_res_apparent_sources(url: str) -> Mapping[str, xr.Dataset]:
+    """Open a derived mapping interface to the fine resolution budget, grouped
+        by time and with derived apparent sources
+    """  # noqa
+    return FineResolutionSources(open_fine_resolution_budget(url))
