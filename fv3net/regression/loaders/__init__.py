47709eb55bac6fb5a5ec828ebed67d08c9349d9b
brianhenn <brianhenn@gmail.com>
2020-06-04 04:55:01 +0000

    tests passing

--- fv3net/regression/loaders/__init__.py
+++ fv3net/regression/loaders/__init__.py
@@ -10,5 +10,4 @@ from ._fine_resolution_budget import (
 # TODO: replace with open mapping func for nudging data source
 from ._nudged import load_nudging_batches
 
-from ._batch import mapper_to_batches
 from .constants import TIME_NAME, SAMPLE_DIM_NAME
