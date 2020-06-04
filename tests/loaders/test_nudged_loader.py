e1c48ea8c901bf3a9a6a6699d37120a6287a635f
Andre Perkins <frodre@gmail.com>
2020-06-04 22:59:34 +0000

    Get test_nudged_loader.py from before master merge

--- tests/loaders/test_nudged_loader.py
+++ tests/loaders/test_nudged_loader.py
@@ -1,44 +1,92 @@
 import pytest
 import os
 import xarray as xr
-import numpy as np
+import pandas as pd
+from itertools import chain
 
 import synth
 
 from fv3net.regression.loaders._nudged import (
-    _get_batch_slices,
-    load_nudging_batches,
+    TIME_NAME,
+    TIME_FMT,
+    _load_nudging_batches,
     _get_path_for_nudging_timescale,
+    NudgedTimestepMapper,
+    NudgedStateCheckpoints,
+    MergeNudged,
+    GroupByTime,
 )
 
-
-@pytest.fixture
-def xr_dataset():
-
-    dat = np.arange(120).reshape(5, 2, 3, 4)
-    ds = xr.Dataset(
-        data_vars={"data": (("time", "x", "y", "z"), dat)},
-        coords={
-            "time": np.arange(5),
-            "x": np.arange(2),
-            "y": np.arange(3),
-            "z": np.arange(4),
-        },
-    )
-
-    return ds
+NTIMES = 144
 
 
+@pytest.fixture(scope="module")
+def nudge_tendencies(datadir_module):
+
+    tendency_data_schema = datadir_module.join("nudging_tendencies.json")
+    with open(tendency_data_schema) as f:
+        schema = synth.load(f)
+    nudging_tend = synth.generate(schema)
+    nudging_tend = _int64_to_datetime(nudging_tend)
+
+    return nudging_tend.isel({TIME_NAME: slice(0, NTIMES)})
+
+
+@pytest.fixture(scope="module")
+def general_nudge_schema(datadir_module):
+    nudge_data_schema = datadir_module.join(f"after_physics.json")
+    with open(nudge_data_schema) as f:
+        schema = synth.load(f)
+
+    return schema
+
+
+@pytest.fixture(scope="module")
+def general_nudge_output(general_nudge_schema):
+
+    data = synth.generate(general_nudge_schema)
+    data = _int64_to_datetime(data)
+
+    return data.isel({TIME_NAME: slice(0, NTIMES)})
+
+
+@pytest.fixture(scope="module")
+def nudged_checkpoints(general_nudge_schema):
+
+    nudged_datasets = {}
+    datasets_sources = ["before_dynamics", "after_dynamics", "after_physics"]
+    for source in datasets_sources:
+        ds = synth.generate(general_nudge_schema)
+        ds = _int64_to_datetime(ds)
+        nudged_datasets[source] = ds.isel({TIME_NAME: slice(0, NTIMES)})
+
+    return nudged_datasets
+
+
+def _int64_to_datetime(ds):
+    time = pd.to_datetime(ds[TIME_NAME].values)
+    time = time.round("S")
+    return ds.assign_coords({TIME_NAME: time})
+
+
+@pytest.fixture(scope="module")
+def nudged_tstep_mapper(nudge_tendencies, general_nudge_output):
+
+    combined_ds = xr.merge([nudge_tendencies, general_nudge_output], join="inner")
+
+    timestep_mapper = NudgedTimestepMapper(combined_ds)
+
+    return timestep_mapper
+
+
+# Note: datadir_module fixture in conftest.py
 @pytest.mark.regression
-def test_load_nudging_batches(datadir):
+def test_load_nudging_batches(nudged_tstep_mapper):
 
-    xlim = 10
-    ylim = 10
-    zlim = 2
-    tlim = 144
     ntimes = 90
     init_time_skip_hr = 12  # 48 15-min timesteps
-    num_batches = 14
+    times_per_batch = 14
+    num_batches = 5
 
     rename = {
         "air_temperature_tendency_due_to_nudging": "dQ1",
@@ -47,32 +95,13 @@ def test_load_nudging_batches(datadir):
     input_vars = ["air_temperature", "specific_humidity"]
     output_vars = ["dQ1", "dQ2"]
     data_vars = input_vars + output_vars
-    local_vars_for_storag = input_vars + list(rename.keys())
-
-    synth_data = ["nudging_tendencies", "after_physics"]
-    for key in synth_data:
-        schema_path = datadir.join(f"{key}.json")
-        # output directory with nudging timescale expected
-        zarr_out = datadir.join(f"outdir-3h/{key}.zarr")
-
-        with open(str(schema_path)) as f:
-            schema = synth.load(f)
-
-        xr_zarr = synth.generate(schema)
-        reduced_ds = xr_zarr[[var for var in local_vars_for_storag if var in xr_zarr]]
-        decoded = xr.decode_cf(reduced_ds)
-        # limit data for efficiency (144 x 6 x 2 x 10 x 10)
-        decoded = decoded.isel(
-            time=slice(0, tlim), x=slice(0, xlim), y=slice(0, ylim), z=slice(0, zlim)
-        )
-        decoded.to_zarr(str(zarr_out))
 
     # skips first 48 timesteps, only use 90 timesteps
-    sequence = load_nudging_batches(
-        str(datadir),
+    sequence = _load_nudging_batches(
+        nudged_tstep_mapper,
         data_vars,
-        timescale_hours=3,
         num_batches=num_batches,
+        num_times_in_batch=times_per_batch,
         rename_variables=rename,
         initial_time_skip_hr=init_time_skip_hr,
         n_times=ntimes,
@@ -81,31 +110,10 @@ def test_load_nudging_batches(datadir):
     # 14 batches requested
     assert len(sequence._args) == num_batches
 
-    batch_samples_total = 0
     for batch in sequence:
-        batch_samples_total += batch.sizes["sample"]
-
-    total_samples = ntimes * 6 * xlim * ylim
-    expected_num_samples = (total_samples // num_batches) * num_batches
-    assert batch_samples_total == expected_num_samples
-
-
-@pytest.mark.parametrize(
-    "num_samples,samples_per_batch,num_batches", [(5, 2, None), (5, 4, 2)]
-)
-def test__get_batch_slices(num_samples, samples_per_batch, num_batches):
-
-    expected = [slice(0, 2), slice(2, 4)]
-    args = _get_batch_slices(num_samples, samples_per_batch, num_batches=num_batches)
-    assert args == expected
-
-
-@pytest.mark.parametrize(
-    "num_samples,samples_per_batch,num_batches", [(5, 6, None), (5, 2, 6)]
-)
-def test__get_batch_slices_failure(num_samples, samples_per_batch, num_batches):
-    with pytest.raises(ValueError):
-        _get_batch_slices(num_samples, samples_per_batch, num_batches=num_batches)
+        assert batch.sizes["sample"] == times_per_batch * 6 * 48 * 48
+        for var in data_vars:
+            assert var in batch
 
 
 @pytest.fixture
@@ -140,3 +148,133 @@ def test__get_path_for_nudging_timescale(nudging_output_dirs, timescale, expecte
 def test__get_path_for_nudging_timescale_failure(nudging_output_dirs, timescale):
     with pytest.raises(KeyError):
         _get_path_for_nudging_timescale(nudging_output_dirs, timescale, tol=1e-5)
+
+
+def test_NudgedTimestepMapper(general_nudge_output):
+
+    mapper = NudgedTimestepMapper(general_nudge_output)
+
+    assert len(mapper) == general_nudge_output.sizes[TIME_NAME]
+
+    single_time = general_nudge_output[TIME_NAME].values[0]
+    item = general_nudge_output.sel({TIME_NAME: single_time})
+    time_key = pd.to_datetime(single_time).strftime(TIME_FMT)
+    xr.testing.assert_equal(item, mapper[time_key])
+
+
+@pytest.fixture(params=["dataset_only", "nudged_tstep_mapper_only", "mixed", "empty"])
+def mapper_to_ds_case(request, nudge_tendencies, general_nudge_output):
+
+    if request.param == "dataset_only":
+        sources = (nudge_tendencies, general_nudge_output)
+    elif request.param == "nudged_tstep_mapper_only":
+        sources = (
+            NudgedTimestepMapper(nudge_tendencies),
+            NudgedTimestepMapper(general_nudge_output),
+        )
+    elif request.param == "mixed":
+        sources = (nudge_tendencies, NudgedTimestepMapper(general_nudge_output))
+    elif request.param == "empty":
+        sources = ()
+
+    return sources
+
+
+def test_MergeNudged__mapper_to_datasets(mapper_to_ds_case):
+
+    datasets = MergeNudged._mapper_to_datasets(mapper_to_ds_case)
+    for source in datasets:
+        assert isinstance(source, xr.Dataset)
+
+
+@pytest.fixture(params=["empty", "single", "no_overlap"])
+def overlap_check_pass_datasets(request, nudge_tendencies, general_nudge_output):
+
+    if request.param == "empty":
+        sources = ()
+    elif request.param == "single":
+        sources = (nudge_tendencies,)
+    elif request.param == "no_overlap":
+        sources = (nudge_tendencies, general_nudge_output)
+
+    return sources
+
+
+def test_MergeNudged__check_dvar_overlap(overlap_check_pass_datasets):
+
+    MergeNudged._check_dvar_overlap(*overlap_check_pass_datasets)
+
+
+@pytest.fixture(params=["single_overlap", "all_overlap"])
+def overlap_check_fail_datasets(request, nudge_tendencies, general_nudge_output):
+
+    if request.param == "single_overlap":
+        sources = (nudge_tendencies, general_nudge_output, general_nudge_output)
+    elif request.param == "all_overlap":
+        sources = (nudge_tendencies, nudge_tendencies, nudge_tendencies)
+
+    return sources
+
+
+def test_MergeNudged(nudge_tendencies, general_nudge_output):
+
+    merged = MergeNudged(nudge_tendencies, NudgedTimestepMapper(general_nudge_output))
+
+    assert len(merged) == NTIMES
+
+    item = merged[merged.keys()[0]]
+    source_vars = chain(
+        nudge_tendencies.data_vars.keys(), general_nudge_output.data_vars.keys(),
+    )
+    for var in source_vars:
+        assert var in item
+
+
+def test_MergeNudged__check_dvar_overlap_fail(overlap_check_fail_datasets):
+    with pytest.raises(ValueError):
+        MergeNudged._check_dvar_overlap(*overlap_check_fail_datasets)
+
+
+def test_NudgedStateCheckpoints(nudged_checkpoints):
+
+    mapper = NudgedStateCheckpoints(nudged_checkpoints)
+
+    ds_len = sum([ds.sizes[TIME_NAME] for ds in nudged_checkpoints.values()])
+    assert len(mapper) == ds_len
+
+    single_item = nudged_checkpoints["after_physics"].isel(time=0)
+    time_key = pd.to_datetime(single_item.time.values).strftime(TIME_FMT)
+    item_key = ("after_physics", time_key)
+    xr.testing.assert_equal(mapper[item_key], single_item)
+
+
+@pytest.fixture
+def checkpoint_mapping(general_nudge_output):
+
+    item = general_nudge_output.isel(time=slice(0, 1))
+    checkpoints = {
+        ("before_dynamics", "20160801.001500"): item,
+        ("after_dynamics", "20160801.001500"): item,
+        ("after_dynamics", "20160801.003000"): item,
+    }
+
+    return checkpoints
+
+
+def test_GroupByTime(checkpoint_mapping):
+
+    test_groupby = GroupByTime(checkpoint_mapping)
+    assert len(test_groupby) == 2
+    assert list(test_groupby.keys()) == ["20160801.001500", "20160801.003000"]
+
+
+@pytest.mark.parametrize(
+    "time_key, expected_size", [("20160801.001500", 2), ("20160801.003000", 1)]
+)
+def test_GroupByTime_items(time_key, expected_size, checkpoint_mapping):
+
+    test_groupby = GroupByTime(checkpoint_mapping)
+
+    item = test_groupby[time_key]
+    assert "checkpoint" in item.dims
+    assert item.sizes["checkpoint"] == expected_size
