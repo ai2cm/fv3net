b7a5838491fb013689e0fb58aabb87723f1ca96e
Andre Perkins <frodre@gmail.com>
2020-06-06 00:42:51 +0000

    Remove old nudging batch loader

--- fv3net/regression/loaders/_nudged.py
+++ fv3net/regression/loaders/_nudged.py
@@ -4,155 +4,22 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 import zarr.storage as zstore
-from typing import Sequence, Iterable, Mapping, Union, Tuple
-from functools import partial
+from typing import Sequence, Mapping, Union, Tuple
 from itertools import product
 from toolz import groupby
 from pathlib import Path
 
 import vcm
-from vcm import cloud, safe
+from vcm import cloud
 from vcm.convenience import round_time
 from vcm.cubedsphere.constants import TIME_FMT
-from ._sequences import FunctionOutputSequence
-from .constants import SAMPLE_DIM_NAME, TIME_NAME
+from .constants import TIME_NAME
 
 logger = logging.getLogger(__name__)
 
 TIMESCALE_OUTDIR_TEMPLATE = "outdir-*h"
 SIMULATION_TIMESTEPS_PER_HOUR = 4
 Z_DIM_NAME = "z"
-
-BatchSequence = Sequence[xr.Dataset]
-TimestepMapper = Mapping[str, xr.Dataset]
-
-
-def load_nudging_batches(
-    url: str,
-    data_vars: Iterable[str],
-    nudging_timescale_hr: Union[int, float] = 3,
-    num_times_in_batch: int = 10,
-    num_batches: int = None,
-    random_seed: int = 0,
-    mask_to_surface_type: str = None,
-    rename_variables: Mapping[str, str] = None,
-    initial_time_skip_hr: int = 0,
-    n_times: int = None,
-):
-    """temporary loader while transforms being developed"""
-
-    tstep_mapper = open_nudged(
-        url,
-        nudging_timescale_hr,
-        initial_time_skip_hr=initial_time_skip_hr,
-        n_times=n_times,
-    )
-
-    return _load_nudging_batches(
-        tstep_mapper,
-        data_vars,
-        num_times_in_batch=num_times_in_batch,
-        num_batches=num_batches,
-        random_seed=random_seed,
-        mask_to_surface_type=mask_to_surface_type,
-        rename_variables=rename_variables,
-        initial_time_skip_hr=initial_time_skip_hr,
-        n_times=n_times,
-    )
-
-
-def _load_nudging_batches(
-    nudged_tstep_mapper: TimestepMapper,
-    data_vars: Iterable[str],
-    num_times_in_batch: int = 10,
-    num_batches: int = None,
-    random_seed: int = 0,
-    mask_to_surface_type: str = None,
-    rename_variables: Mapping[str, str] = None,
-    initial_time_skip_hr: int = 0,
-    n_times: int = None,
-) -> Union[Sequence[BatchSequence], BatchSequence]:
-    """
-    Get a sequence of batches from a nudged-run zarr store.
-
-    Args:
-        nudged_tstep_mapper: Mapping object from fv3 time strings to nudged fv3
-            output data
-        data_vars: Variable names used to subset batched datasets
-        num_times_in_batch (optional): number of times to include
-            in a single batch item.  Overridden by num_batches
-        num_batches (optional): number of batches to split the
-            input samples into.  Overrides num_samples_in_batch.
-        random_seed (optional): A seed for the RNG state used in shuffling operations
-        mask_to_surface_type (optional): Flag selector for surface type masking.
-            Requires "land_sea_mask" exists in the loaded dataset.  Note: as currently
-            implemented NaN drop may reduce the batch size under requested
-            number of samples.
-        rename_variables (optional): A mapping to update any variable names in the
-            dataset prior to the selection of input/output variables
-        initial_time_skip_hr (optional): Length of model inititialization (in hours)
-            to omit from the batching operation
-        n_times (optional): Number of times (by index) to include in the
-            batch resampling operation
-    """
-
-    random = np.random.RandomState(random_seed)
-    timesteps = nudged_tstep_mapper.keys()
-    random.shuffle(timesteps)
-
-    # TODO: Overlaps with _validated_num_batches in _one_step
-    if num_batches is None:
-        num_batches = len(timesteps) // num_times_in_batch
-    elif num_batches * num_times_in_batch > len(timesteps):
-        raise ValueError(
-            f"Not enough timesteps (n={len(timesteps)} to create requested "
-            f"{num_batches} batches of size {num_times_in_batch}"
-        )
-
-    logger.info(f"Creating {num_batches} batches of size {num_times_in_batch}")
-    batched_timesteps = []
-    for i in range(num_batches):
-        start = i * num_times_in_batch
-        end = start + num_times_in_batch
-        batched_timesteps.append(timesteps[start:end])
-
-    batch_loader = partial(
-        _load_nudging_batch,
-        nudged_tstep_mapper,
-        data_vars,
-        rename_variables,
-        mask_to_surface_type,
-    )
-    return FunctionOutputSequence(batch_loader, batched_timesteps)
-
-
-def _load_nudging_batch(
-    timestep_mapper, data_vars, rename_variables, mask_to_surface_type, tstep_keys
-) -> xr.Dataset:
-
-    logger.debug(f"Loading batch with mapper keys: {tstep_keys}")
-    batch = [timestep_mapper[timestep] for timestep in tstep_keys]
-    batch_ds = xr.concat(batch, TIME_NAME)
-    batch_ds = batch_ds.rename(rename_variables)
-    if mask_to_surface_type is not None:
-        batch_ds = vcm.mask_to_surface_type(batch_ds, mask_to_surface_type)
-    batch_ds = safe.get_variables(batch_ds, data_vars)
-
-    # Into memory we go
-    batch_ds = batch_ds.load()
-
-    stack_dims = [dim for dim in batch_ds.dims if dim != Z_DIM_NAME]
-    stacked_batch_ds = safe.stack_once(
-        batch_ds, SAMPLE_DIM_NAME, stack_dims, allowed_broadcast_dims=[Z_DIM_NAME]
-    )
-    stacked_batch_ds = stacked_batch_ds.dropna(SAMPLE_DIM_NAME)
-
-    if len(stacked_batch_ds[SAMPLE_DIM_NAME]) == 0:
-        raise ValueError(
-            "No Valid samples detected. Check for errors in the training data."
-        )
-
-    return stacked_batch_ds
 
 
 def _get_path_for_nudging_timescale(nudged_output_dirs, timescale_hours, tol=1e-5):
