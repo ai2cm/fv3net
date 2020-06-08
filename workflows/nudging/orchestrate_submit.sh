c5c6e4b126251b50156558951436435ddf434970
Andre Perkins <frodre@gmail.com>
2020-06-08 23:48:23 +0000

    Add script for orchestration target nudging

---
+++ workflows/nudging/orchestrate_submit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+BASE_CONFIG=$1
+NUDGE_TIMESCALE_HOURS=$2
+FV3GFS_IMAGE=$3
+REMOTE_ROOT=$4
+
+RUN_LABEL=${NUDGE_TIMESCALE_HOURS}h
+REMOTE_OUTDIR=${REMOTE_ROOT}/outdir-${RUN_LABEL}
+RUNFILE=runfile.py
+REMOTE_RUNFILE=${REMOTE_ROOT}/${RUNFILE}
+CONFIG_FILE=fv3config-${RUN_LABEL}.yaml
+REMOTE_CONFIG_FILE=${REMOTE_ROOT}/config/${CONFIG_FILE}
+
+(
+    cd workflows/nudging
+
+    python prepare_config.py ${BASE_CONFIG} ${CONFIG_FILE} --timescale-hours ${NUDGE_TIMESCALE_HOURS}
+
+    gsutil cp ${CONFIG_FILE} ${REMOTE_CONFIG_FILE}
+    gsutil cp ${RUNFILE} ${REMOTE_RUNFILE}
+    ./submit_job.sh ${REMOTE_CONFIG_FILE} ${REMOTE_RUNFILE} ${REMOTE_OUTDIR} ${FV3GFS_IMAGE}
+)
+
