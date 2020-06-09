ab28d081cb565f4176800bcb8f3f24cd00bd3b88
Noah D. Brenowitz <nbren12@gmail.com>
2020-06-09 14:47:33 -0700

    Only rename existing variables
    job: prognostic-run-byu9h0g7

--- workflows/prognostic_c48_run/sklearn_runfile.py
+++ workflows/prognostic_c48_run/sklearn_runfile.py
@@ -42,13 +42,27 @@ class RenamingAdapter:
         else:
             self.rename_out = rename_out
 
+    @staticmethod
+    def _rename(ds: xr.Dataset, rename: Mapping[str, str]) -> xr.Dataset:
+        all_names = set(ds.data_vars) | set(ds.dims) | set(rename)
+        rename_restricted = {key: rename[key] for key in all_names}
+        return ds.rename(rename_restricted)
+
+    def _rename_inputs(self, ds):
+        # TODO typehints
+        return self._rename(ds, self.rename_in)
+
+    def _rename_outputs(self, ds):
+        return self._rename(ds, self.rename_out)
+
     @property
     def variables(self) -> Set[str]:
         return {self.rename_out.get(var, var) for var in self.model.input_vars_}
 
     def predict(self, arg: xr.Dataset, sample_dim: str) -> xr.Dataset:
-        input_ = arg.rename(self.rename_in)
-        return self.model.predict(input_, sample_dim).rename(self.rename_out)
+        input_ = self._rename_inputs(arg)
+        prediction = self.model.predict(input_, sample_dim)
+        return self._rename_outputs(prediction)
 
 
 def compute_diagnostics(state, diags):
