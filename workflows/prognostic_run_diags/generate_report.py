3a594e367614ac7c58400eb95b1d6a9c5aeca31c
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-06-10 12:27:12 -0700

    Add physics global mean and bias plots to report

--- workflows/prognostic_run_diags/generate_report.py
+++ workflows/prognostic_run_diags/generate_report.py
@@ -162,8 +162,22 @@ def rms_plots(time_series: Mapping[str, xr.Dataset]) -> hv.HoloMap:
 
 
 @diag_plot_manager.register
-def global_avg_plots(time_series: Mapping[str, xr.Dataset]) -> hv.HoloMap:
-    return HVPlot(holomap_filter(time_series, varfilter="global_avg").overlay("run"))
+def global_avg_dycore_plots(time_series: Mapping[str, xr.Dataset]) -> hv.HoloMap:
+    return HVPlot(
+        holomap_filter(time_series, varfilter="global_avg_dycore").overlay("run")
+    )
+
+
+@diag_plot_manager.register
+def global_avg_physics_plots(time_series: Mapping[str, xr.Dataset]) -> hv.HoloMap:
+    return HVPlot(
+        holomap_filter(time_series, varfilter="global_avg_physics").overlay("run")
+    )
+
+
+@diag_plot_manager.register
+def global_bias_plots(time_series: Mapping[str, xr.Dataset]) -> hv.HoloMap:
+    return HVPlot(holomap_filter(time_series, varfilter="bias_global").overlay("run"))
 
 
 def diurnal_cycle_plots(time_series: Mapping[str, xr.Dataset]) -> hv.HoloMap:
