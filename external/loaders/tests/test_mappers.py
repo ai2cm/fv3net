1ff814d75e7a37184f643c4bcb4e527a46ede1e2
brianhenn <brianhenn@gmail.com>
2020-06-24 21:08:17 +0000

    added base mapper tests

--- external/loaders/tests/test_mappers.py
+++ external/loaders/tests/test_mappers.py
@@ -2,6 +2,7 @@ import pytest
 from loaders.mappers._fine_resolution_budget import FineResolutionBudgetTiles, FineResolutionSources
 from loaders.mappers._nudged import GeoMapper, NudgedTimestepMapper, MergeNudged, NudgedStateCheckpoints, NudgedFullTendencies
 from loaders.mappers._one_step import TimestepMapper
+from vcm.convenience import parse_datetime_from_str
 
 base_mappers = [
     FineResolutionBudgetTiles,
@@ -13,4 +14,15 @@ base_mappers = [
     NudgedFullTendencies,
     TimestepMapper
 ]
+
+@pytest.fixture(params=base_mappers)
+def base_mapper(request):
+    return request.param
+
+
+def test_base_mapper(base_mapper):
+    assert len(base_mapper) == 2
+    for key in base_mapper:
+        assert parse_datetime_from_str(key)
+        
 
