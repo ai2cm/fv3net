3432c78e7469fac3fd000f2be8a355ce63ff8ec6
Anna Kwa <annak@vulcan.com>
2020-06-24 00:15:23 +0000

    cleanup output scratch before writing test data

--- tests/sklearn_regression/test_nudged_source.sh
+++ tests/sklearn_regression/test_nudged_source.sh
@@ -3,6 +3,8 @@
 TRAINING_DATA=gs://vcm-ml-scratch/andrep/test-nudging-workflow/nudging
 OUTPUT=gs://vcm-ml-scratch/annak/test-nudging-workflow/test-training/
 
+gsutil -m rm -r $OUTPUT
+
 python -m fv3net.regression.sklearn \
     $TRAINING_DATA \
     tests/sklearn_regression/train_sklearn_model_nudged_source.yaml  \
