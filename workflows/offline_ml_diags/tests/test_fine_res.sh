f6cd8363c1119e3d57ac610df48000888c5b5199
Anna Kwa <annak@vulcan.com>
2020-06-24 18:32:08 +0000

    cleanup scratch test dirs before each test run

--- workflows/offline_ml_diags/tests/test_fine_res.sh
+++ workflows/offline_ml_diags/tests/test_fine_res.sh
@@ -2,7 +2,7 @@
 
 MODEL=gs://vcm-ml-scratch/noah/2020-06-17-triad-round1/trained/sklearn_model.pkl
 OUTPUT=gs://vcm-ml-scratch/annak/test-offline-validation-workflow/fineres/
-
+gsutil -m rm -r $OUTPUT
 python -m offline_ml_diags.compute_diags \
     workflows/offline_ml_diags/tests/test_fine_res_config.yml \
     $MODEL \
--- workflows/offline_ml_diags/tests/test_nudging.sh
+++ workflows/offline_ml_diags/tests/test_nudging.sh
@@ -3,6 +3,7 @@
 MODEL=gs://vcm-ml-scratch/andrep/test-nudging-workflow/train_sklearn_model/sklearn_model.pkl
 OUTPUT=gs://vcm-ml-scratch/annak/test-offline-validation-workflow/nudging/
 
+gsutil -m rm -r $OUTPUT
 python -m offline_ml_diags.compute_diags \
     workflows/offline_ml_diags/tests/test_nudging_config.yml \
     $MODEL \
