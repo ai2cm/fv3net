52b8c12b78f7b3914d736eb28adb6af4c0c6a7cf
Anna Kwa <annak@vulcan.com>
2020-06-25 18:36:52 +0000

    only accept two input mappers, raise error if non null source name given for mapper with existing overlap dim

--- external/loaders/loaders/mappers/_merged.py
+++ external/loaders/loaders/mappers/_merged.py
@@ -14,8 +14,10 @@ def get_sample_dataset(mapper):
 class MergeOverlappingData(GeoMapper):
     def __init__(
         self,
-        mappers: Sequence,
-        source_names: Sequence[str],
+        mapper_left: GeoMapper,
+        mapper_right: GeoMapper,
+        source_name_left: str = None,
+        source_name_right: str = None,
         overlap_dim: str = DERIVATION_DIM,
         variables: Sequence[str] = None,
     ):
@@ -25,26 +27,25 @@ class MergeOverlappingData(GeoMapper):
         The coordinates of this dimension are the elements of the source_names
         arg. They must be given in the order corresponding to the mappers arg.
         Args:
-            mappers (Sequence): mappers to merge
-            source_names (Sequence[str]): source names for the mappers to be merged,
-                these are used as coordinates for overlapping variables in the merged
+            mappers_left: mapper to merge
+            mappers_right: mapper to merge
+            source_name_left (str): source names for the left mappers to be merged,
+                used as coordinate for overlapping variables in the merged
                 dataset if there is no existing overlap_dim coordinate for those
-                variables. Must be provided in order corresponding to mappers arg.
-            overlap_dim (str, optional): name of dimension for variable overlaps.
-                Defaults to DERIVATION_DIM.
+                variables. Must be provided if overlap_dim is not an existing
+                dimension in left mapper. If overlap_dim exists, give as None.
+                Defaults to None.
+            source_name_right (str): Same as source_name_left but for right mapper.
             variables (Sequence[str], optional): Subset of variables to return from
                 data. Defaults to None (return all variables).
         """
-        if len(mappers) < 2:
-            raise TypeError(
-                "MergeData should be instantiated with two or more mappers."
-            )
-        if len(source_names) != len(mappers):
-            raise ValueError("Need to provide same number of source names as mappers.")
-        self._mappers = mappers
-        self._source_names = source_names
+        self._mappers = [mapper_left, mapper_right]
+        self._source_names = [source_name_left, source_name_right]
         self._var_overlap = self._get_var_overlap(self._mappers)
         self._overlap_dim = overlap_dim
+        self._check_overlap_vars_dims(
+            self._mappers, self._source_names, self._var_overlap, self._overlap_dim
+        )
 
     def keys(self):
         mappers_keys = [mapper.keys() for mapper in self._mappers]
@@ -81,23 +82,26 @@ class MergeOverlappingData(GeoMapper):
         return overlap
 
     @staticmethod
-    def _check_overlap_vars_dims(mappers, overlap_vars, overlap_dim):
+    def _check_overlap_vars_dims(mappers, source_names, overlap_vars, overlap_dim):
         datasets = [get_sample_dataset(mapper) for mapper in mappers]
-        overlap_var_dims = [
-            safe.get_variables(ds, overlap_vars).dims for ds in datasets
-        ]
-        if not all(x == overlap_var_dims[0] for x in overlap_var_dims):
-            vars_missing_dim = []
-            # if a dataset already has overlap dim, get names of dataarrays
-            # that are missing this dimension
-            for ds in datasets:
-                ds_overlap_vars = safe.get_variables(ds, overlap_vars)
-                if overlap_dim in ds_overlap_vars.dims:
-                    for var in overlap_vars:
-                        if overlap_dim not in ds[var]:
-                            vars_missing_dim.append(var)
-            raise ValueError(
-                "If overlap dimension {overlap_dim} already exists in "
-                "one of the mappers it must be present for all overlapping variables. "
-                f"Variables {vars_missing_dim} are missing dimension {overlap_dim}."
-            )
+        for ds, source_coord in zip(datasets, source_names):
+            overlap_dim_occurence = 0
+            vars_missing_overlap_dim = []
+            for var in overlap_vars:
+                if overlap_dim in ds[var].dims:
+                    overlap_dim_occurence += 1
+                else:
+                    vars_missing_overlap_dim.append(var)
+            if 0 < overlap_dim_occurence < len(overlap_vars):
+                raise ValueError(
+                    f"If overlap dimension {overlap_dim} already exists in "
+                    "one of the mappers it must be present for all overlapping "
+                    f"variables. Variables {vars_missing_overlap_dim} are missing "
+                    f"dimension {overlap_dim}."
+                )
+            if overlap_dim in ds.dims and source_coord is not None:
+                raise ValueError(
+                    "Overlap dimension already exists in dataset with source name "
+                    f"given as {source_coord}. If overlap_dim exists, source name "
+                    "must be given as None."
+                )
