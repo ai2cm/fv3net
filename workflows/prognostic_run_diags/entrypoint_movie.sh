9cd08f372507bb1c36226eff66a74fa1d4dafef9
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-06-29 09:31:50 -0700

    Fix entrypoint script

--- workflows/prognostic_run_diags/entrypoint_movie.sh
+++ workflows/prognostic_run_diags/entrypoint_movie.sh
@@ -3,10 +3,11 @@
 set -e
 
 rundir=$1
+output=$2
 hash=$(echo $rundir | md5sum | awk '{print $1}')
+workdir=.cache/$hash
 
-python generate_movie_stills.py $rundir workdir/$hash
-
-ffmpeg -y -r 15 -i workdir/$hash/heating_and_moistening_%05d.png -vf fps=15 -pix_fmt yuv420p -s:v 1920x1080 workdir/$hash/heating_and_moistening.mp4
-gsutil cp workdir/$hash/heating_and_moistening.mp4 $output
-rm -r workdir/$hash
+python generate_movie_stills.py $rundir $workdir
+ffmpeg -y -r 15 -i $workdir/heating_and_moistening_%05d.png -vf fps=15 -pix_fmt yuv420p -s:v 1920x1080 $workdir/temp.mp4
+gsutil cp $workdir/temp.mp4 $output
+rm -r $workdir
