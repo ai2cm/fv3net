8dce0c53be410c572487c0c8efae1edc078cfab0
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-06-29 14:01:39 -0700

    Add movie-creation step to prognostic report argo.yaml

--- workflows/prognostic_run_diags/entrypoint_movie.sh
+++ workflows/prognostic_run_diags/entrypoint_movie.sh
@@ -9,5 +9,5 @@ workdir=.cache/$hash
 
 python generate_movie_stills.py $rundir $workdir
 ffmpeg -y -r 15 -i $workdir/heating_and_moistening_%05d.png -vf fps=15 -pix_fmt yuv420p -s:v 1920x1080 $workdir/temp.mp4
-gsutil cp $workdir/temp.mp4 $output
+gsutil cp $workdir/temp.mp4 $output/column_integrated_heating_and_moistening.mp4
 rm -r $workdir
