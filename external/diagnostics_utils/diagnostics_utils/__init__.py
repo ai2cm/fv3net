fbdc77006a831414619937294292be790c122fd4
Anna Kwa <annak@vulcan.com>
2020-07-01 23:21:59 +0000

    Revert "checkout pt 1 PR changes"
    This reverts commit 0aa995d4137fb26fa9f8ed61742b9e44697b02c4.

--- external/diagnostics_utils/diagnostics_utils/__init__.py
+++ external/diagnostics_utils/diagnostics_utils/__init__.py
@@ -8,7 +8,7 @@ from .utils import (
     snap_mask_to_type,
     insert_column_integrated_vars,
 )
-from ._diurnal_cycle import bin_diurnal_cycle, create_diurnal_cycle_dataset
+from ._diurnal_cycle import bin_diurnal_cycle
 
 __all__ = [
     "VARNAMES",
@@ -18,5 +18,4 @@ __all__ = [
     "snap_mask_to_type",
     "insert_column_integrated_vars",
     "bin_diurnal_cycle",
-    "create_diurnal_cycle_dataset",
 ]
