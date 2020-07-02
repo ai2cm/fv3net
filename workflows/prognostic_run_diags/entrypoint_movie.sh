ad50409c81e169f053ad908265275c328f8761d2
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-07-02 18:18:47 +0000

    Update movie entrypoint script

--- workflows/prognostic_run_diags/entrypoint_movie.sh
+++ workflows/prognostic_run_diags/entrypoint_movie.sh
@@ -2,12 +2,21 @@
 
 set -e
 
+usage="Usage: entrypoint_movie.sh rundir output"
+
+if [[ $# != 2 ]]; then
+    echo $usage
+    exit 2
+fi
+
 rundir=$1
 output=$2
+gridSpec=gs://vcm-ml-data/2020-01-06-C384-grid-spec-with-area-dx-dy/grid_spec
+
 hash=$(echo $rundir | md5sum | awk '{print $1}')
 workdir=.cache/$hash
 
-python generate_movie_stills.py $rundir $workdir
-ffmpeg -y -r 15 -i $workdir/heating_and_moistening_%05d.png -vf fps=15 -pix_fmt yuv420p -s:v 1920x1080 $workdir/temp.mp4
-gsutil cp $workdir/temp.mp4 $output/column_integrated_heating_and_moistening.mp4
-rm -r $workdir
+python generate_movie_stills.py $rundir $gridSpec $workdir
+ffmpeg -y -r 15 -i $workdir/heating_and_moistening_%05d.png -vf fps=15 -pix_fmt yuv420p -s:v 1920x1080 $workdir/movie.mp4
+gsutil cp $workdir/movie.mp4 $output/column_integrated_heating_and_moistening.mp4
+
