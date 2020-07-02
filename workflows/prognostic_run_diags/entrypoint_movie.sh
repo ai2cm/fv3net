9ea5350218afac70aeff232b19093bb16352bd3d
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-07-02 22:14:55 +0000

    Make movie entrypoint script work for arbitrary set of movies

--- workflows/prognostic_run_diags/entrypoint_movie.sh
+++ workflows/prognostic_run_diags/entrypoint_movie.sh
@@ -12,11 +12,17 @@ fi
 rundir=$1
 output=$2
 gridSpec=gs://vcm-ml-data/2020-01-06-C384-grid-spec-with-area-dx-dy/grid_spec
+imageSuffix=_00000.png
 
 hash=$(echo $rundir | md5sum | awk '{print $1}')
-workdir=.cache/$hash
+workDir=.cache/$hash
 
-python generate_movie_stills.py $rundir $gridSpec $workdir
-ffmpeg -y -r 15 -i $workdir/heating_and_moistening_%05d.png -vf fps=15 -pix_fmt yuv420p -s:v 1920x1080 $workdir/movie.mp4
-gsutil cp $workdir/movie.mp4 $output/column_integrated_heating_and_moistening.mp4
+python generate_movie_stills.py $rundir $gridSpec $workDir
+
+moviePaths=$(ls $workDir/*$imageSuffix)
+for movie in $moviePaths; do
+    base=$(basename $movie $imageSuffix)
+    ffmpeg -y -r 15 -i $workDir/${base}_%05d.png -vf fps=15 -pix_fmt yuv420p -s:v 1920x1080 $workDir/$base.mp4
+    gsutil cp $workDir/$base.mp4 $output/$base.mp4
+done
 
