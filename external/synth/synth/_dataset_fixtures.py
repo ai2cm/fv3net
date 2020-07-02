2ba6f3ae4e999f4041d3a0c26d00f969a2125f18
Jeremy McGibbon <jeremym@vulcan.com>
2020-07-01 17:02:19 -0700

    use ranges argument when generating grid data

--- external/synth/synth/_dataset_fixtures.py
+++ external/synth/synth/_dataset_fixtures.py
@@ -6,7 +6,7 @@ import intake
 import pytest
 from distutils import dir_util
 
-from .core import load, generate
+from .core import load, generate, Range
 
 
 timestep1 = "20160801.001500"
@@ -180,9 +180,8 @@ def data_source_path(dataset_fixtures_dir, data_source_name):
 
 @pytest.fixture
 def grid_dataset(dataset_fixtures_dir):
-    random = np.random.RandomState(0)
     with open(str(dataset_fixtures_dir.join("grid_schema.json"))) as f:
         grid_schema = load(f)
-    grid = generate(grid_schema).load()
-    grid["land_sea_mask"][:] = random.choice([0, 1, 2], size=grid["land_sea_mask"].shape)
+    grid_ranges = {"area": Range(1, 2), "land_sea_mask": Range(0, 2)}
+    grid = generate(grid_schema, ranges=grid_ranges).load()
     return grid
