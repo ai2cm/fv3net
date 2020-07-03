623f1e63851f8a8f63670c3960c90301715670d8
brianhenn <brianhenn@gmail.com>
2020-07-03 04:38:41 +0000

    added SHiELD data to diagnostic mapper for one-step, fine-res, nudging

--- external/loaders/loaders/constants.py
+++ external/loaders/loaders/constants.py
@@ -2,3 +2,16 @@ SAMPLE_DIM_NAME = "sample"
 TIME_NAME = "time"
 TIME_FMT = "%Y%m%d.%H%M%S"
 DERIVATION_DIM = "derivation"
+DERIVATION_SHiELD_COORD = "coarsened_SHiELD"
+RENAMED_SHIELD_DIAG_VARS = {
+    "DSWRFtoa_coarse": "total_sky_downward_shortwave_flux_at_top_of_atmosphere",
+    "DSWRFsfc_coarse": "total_sky_downward_shortwave_flux_at_surface",
+    "USWRFtoa_coarse": "total_sky_upward_shortwave_flux_at_top_of_atmosphere",
+    "USWRFsfc_coarse": "total_sky_upward_shortwave_flux_at_surface",
+    "DLWRFsfc_coarse": "total_sky_downward_longwave_flux_at_surface",
+    "ULWRFtoa_coarse": "total_sky_upward_longwave_flux_at_top_of_atmosphere",
+    "ULWRFsfc_coarse": "total_sky_upward_longwave_flux_at_surface",
+    "SHTFLsfc_coarse": "sensible_heat_flux",
+    "LHTFLsfc_coarse": "latent_heat_flux",
+    "PRATEsfc_coarse": "surface_precipitation_rate",
+}
--- external/loaders/loaders/mappers/__init__.py
+++ external/loaders/loaders/mappers/__init__.py
@@ -1,6 +1,6 @@
 # mapping functions we mean to be able to specify in the
 # model training configuration
-from ._one_step import open_one_step
+from ._one_step import open_one_step, open_one_step_with_diags
 from ._fine_resolution_budget import open_fine_res_apparent_sources
 from ._nudged import open_merged_nudged, open_merged_nudged_full_tendencies
 
