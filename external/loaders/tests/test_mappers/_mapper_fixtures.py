89216f4c66901960c15e5efe215aaabc2218342e
brianhenn <brianhenn@gmail.com>
2020-07-06 18:54:49 +0000

    added additional diagnostic mapper tests

--- external/loaders/tests/test_mappers/_mapper_fixtures.py
+++ external/loaders/tests/test_mappers/_mapper_fixtures.py
@@ -1,7 +1,13 @@
 import pytest
 import xarray as xr
 from loaders import mappers
-from loaders.mappers._fine_resolution_budget import FineResolutionSources
+from loaders.mappers._fine_resolution_budget import (
+    FineResolutionSources,
+    DERIVATION_FV3GFS_COORD,
+)
+from loaders.mappers._high_res_diags import open_high_res_diags
+from loaders.mappers._merged import MergeOverlappingData
+from loaders.constants import DERIVATION_SHiELD_COORD
 from typing import Mapping, Sequence
 
 training_mapper_names = ["FineResolutionSources", "SubsetTimes", "TimestepMapper"]
@@ -57,9 +63,10 @@ def diagnostic_mapper_data_source_path(
 
 def _open_fine_res_apparent_sources_patch(
     training_mapper_data_source_path: str,
-    rename_vars: Mapping[str, str],
-    drop_vars: Sequence[str],
-    dim_order: Sequence[str],
+    rename_vars: Mapping[str, str] = None,
+    drop_vars: Sequence[str] = None,
+    dim_order: Sequence[str] = None,
+    shield_diags_url: str = None,
 ):
     # this function is a patch for the actual one until synth is netcdf-compatible
     fine_res_ds = xr.open_zarr(training_mapper_data_source_path)
@@ -67,9 +74,21 @@ def _open_fine_res_apparent_sources_patch(
         fine_res_ds.time.values[0]: (fine_res_ds.isel(time=0)),
         fine_res_ds.time.values[1]: (fine_res_ds.isel(time=1)),
     }
-    return FineResolutionSources(
+
+    fine_resolution_sources_mapper = FineResolutionSources(
         time_mapper, rename_vars=rename_vars, drop_vars=drop_vars, dim_order=dim_order
     )
+
+    if shield_diags_url is not None:
+        shield_diags_mapper = open_high_res_diags(shield_diags_url)
+        fine_resolution_sources_mapper = MergeOverlappingData(
+            shield_diags_mapper,
+            fine_resolution_sources_mapper,
+            source_name_left=DERIVATION_SHiELD_COORD,
+            source_name_right=DERIVATION_FV3GFS_COORD,
+        )
+
+    return fine_resolution_sources_mapper
 
 
 @pytest.fixture
@@ -95,7 +114,9 @@ def diagnostic_mapper_helper_function(diagnostic_mapper_name):
 
 
 @pytest.fixture
-def training_mapper_helper_function_kwargs(training_mapper_name):
+def training_mapper_helper_function_kwargs(
+    training_mapper_name, C48_SHiELD_diags_dataset_path
+):
     if training_mapper_name == "TimestepMapper":
         return {}
     elif training_mapper_name == "SubsetTimes":
@@ -103,6 +124,7 @@ def training_mapper_helper_function_kwargs(training_mapper_name):
     elif training_mapper_name == "NudgedFullTendencies":
         return {
             "nudging_timescale_hr": 3,
+            "shield_diags_url": C48_SHiELD_diags_dataset_path,
         }
     elif training_mapper_name == "FineResolutionSources":
         return {
@@ -118,22 +140,22 @@ def training_mapper_helper_function_kwargs(training_mapper_name):
 
 
 @pytest.fixture
-def diagnostic_mapper_helper_function_kwargs(diagnostic_mapper_name):
+def diagnostic_mapper_helper_function_kwargs(
+    diagnostic_mapper_name, C48_SHiELD_diags_dataset_path
+):
     if diagnostic_mapper_name == "TimestepMapperWithDiags":
         kwargs = {}
     elif diagnostic_mapper_name == "NudgedFullTendencies":
         kwargs = {
             "nudging_timescale_hr": 3,
-            "shield_diags_url": (
-                "gs://vcm-ml-scratch/brianh/C48-SHiELD-diagnostics/"
-                "gfsphysics_15min_coarse.zarr"
-            ),
+            "shield_diags_url": C48_SHiELD_diags_dataset_path,
             "open_checkpoints_kwargs": {
                 "checkpoint_files": ("after_dynamics.zarr", "after_physics.zarr")
             },
         }
     elif diagnostic_mapper_name == "FineResolutionSources":
         kwargs = {
+            "shield_diags_url": C48_SHiELD_diags_dataset_path,
             "rename_vars": {
                 "delp": "pressure_thickness_of_atmospheric_layer",
                 "grid_xt": "x",
