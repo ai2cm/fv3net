4c7c56bbc1a4bfa22f834a318e959f4533e42f45
Anna Kwa <annak@vulcan.com>
2020-07-07 23:35:12 +0000

    tidy tests dir

--- workflows/offline_ml_diags/tests/test_nudging.sh
+++ workflows/offline_ml_diags/tests/test_nudging.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-MODEL=gs://vcm-ml-scratch/andrep/test-nudging-workflow/train_sklearn_model/sklearn_model.pkl
+MODEL=gs://vcm-ml-experiments/2020-06-17-triad-round-1/nudging/train_sklearn_model/sklearn_model.pkl
 OUTPUT=gs://vcm-ml-scratch/annak/test-offline-validation-workflow/nudging/
-
+gsutil -m rm -r $OUTPUT
 python -m offline_ml_diags.compute_diags \
     workflows/offline_ml_diags/tests/test_nudging_config.yml \
     $MODEL \
-    $OUTPUT \
-   --timesteps-file workflows/offline_ml_diags/tests/times_nudging.json
+    $OUTPUT
