d82642af3242a951ae8875e80fafef5da62a6493
Noah D. Brenowitz <nbren12@gmail.com>
2020-07-09 17:10:20 -0700

    Add a LocalMapper class and associated tools
    The ML training is dominated by I/O. this commit adds some tools for
    downloading data from one mapper in parallel, and returning a local
    mapper object representing the local data.

--- external/loaders/loaders/mappers/__init__.py
+++ external/loaders/loaders/mappers/__init__.py
@@ -1,12 +1,12 @@
 # mapping functions we mean to be able to specify in the
 # model training configuration
-from ._one_step import open_one_step
+# mapper classes used externally
+from ._base import GeoMapper, LongRunMapper
 from ._fine_resolution_budget import open_fine_res_apparent_sources
-from ._nudged import open_merged_nudged, open_merged_nudged_full_tendencies
 
 # additional open mapper functions
 from ._high_res_diags import open_high_res_diags
-
-# mapper classes used externally
-from ._base import GeoMapper, LongRunMapper
+from ._local import LocalMapper, mapper_to_local
 from ._merged import MergeOverlappingData
+from ._nudged import open_merged_nudged, open_merged_nudged_full_tendencies
+from ._one_step import open_one_step
---
+++ external/loaders/loaders/mappers/_local.py
@@ -0,0 +1,60 @@
+import logging as logger
+import os
+from functools import partial
+from multiprocessing import Pool
+
+import fsspec
+import xarray as xr
+
+from ._base import GeoMapper
+
+
+class LocalMapper(GeoMapper):
+    """A mapper for a directory of netCDFs. One per time-step.
+    """
+
+    def __init__(self, path: str):
+        self.fs = fsspec.get_fs_token_paths(path)[0]
+        self.path = path
+
+    def _files(self):
+        files = self.fs.glob(os.path.join(self.path, "*.nc"))
+        keys = [os.path.basename(file[: -len(".nc")]) for file in files]
+        return dict(zip(keys, files))
+
+    def keys(self):
+        return self._files().keys()
+
+    def __getitem__(self, key):
+        with self.fs.open(self._files()[key]) as f:
+            return xr.open_dataset(f)
+
+    def create_dir(self):
+        self.fs.makedirs(self.path, exist_ok=True)
+
+
+def process_item(key, path, mapper):
+    outputpath = os.path.join(path, f"{key}.nc")
+    logger.info(f"saving {key} to {outputpath}")
+    mapper[key].to_netcdf(outputpath)
+
+
+def mapper_to_local(mapper: GeoMapper, path: str, threads: int = 10) -> LocalMapper:
+    """Save a mapper to a local directory with multiprocessing
+
+    Args:
+        mapper: the mapper to save locally
+        path: a "local" path. Really any path fsspec uses works.
+        threads: number of processes to use for downloading
+
+    Returns:
+        a mapper representing the local data
+    """
+
+    local_mapper = LocalMapper(path)
+    local_mapper.create_dir()
+
+    with Pool(threads) as pool:
+        pool.map(partial(process_item, path=path, mapper=mapper), mapper.keys())
+
+    return local_mapper
