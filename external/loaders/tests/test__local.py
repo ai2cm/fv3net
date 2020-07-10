94820669b84a7b27fc19f2f9443d56e076fa75bb
Noah D. Brenowitz <nbren12@gmail.com>
2020-07-09 17:12:34 -0700

    Add tests

---
+++ external/loaders/tests/test__local.py
@@ -0,0 +1,19 @@
+import xarray as xr
+
+from loaders import LocalMapper
+
+
+def test_LocalMapper(tmpdir):
+    path = "a.nc"
+    xr.Dataset().to_netcdf(str(tmpdir.join(path)))
+
+    mapper = LocalMapper(str(tmpdir))
+
+    assert set(mapper) == {"a"}
+
+
+def test_LocalMapper_getitem(tmpdir):
+    path = "a.nc"
+    xr.Dataset().to_netcdf(str(tmpdir.join(path)))
+    mapper = LocalMapper(str(tmpdir))
+    assert isinstance(mapper["a"], xr.Dataset)
