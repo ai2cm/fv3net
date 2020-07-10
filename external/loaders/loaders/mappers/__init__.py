32dc03b94cc1e359be34fe9681f554da3c582e01
Noah D. Brenowitz <nbren12@gmail.com>
2020-07-09 17:13:38 -0700

    Undo isort

--- external/loaders/loaders/mappers/__init__.py
+++ external/loaders/loaders/mappers/__init__.py
@@ -1,12 +1,13 @@
 # mapping functions we mean to be able to specify in the
 # model training configuration
-# mapper classes used externally
-from ._base import GeoMapper, LongRunMapper
+from ._one_step import open_one_step
 from ._fine_resolution_budget import open_fine_res_apparent_sources
+from ._nudged import open_merged_nudged, open_merged_nudged_full_tendencies
+ from ._local import LocalMapper, mapper_to_local
 
 # additional open mapper functions
 from ._high_res_diags import open_high_res_diags
-from ._local import LocalMapper, mapper_to_local
+
+# mapper classes used externally
+from ._base import GeoMapper, LongRunMapper
 from ._merged import MergeOverlappingData
-from ._nudged import open_merged_nudged, open_merged_nudged_full_tendencies
-from ._one_step import open_one_step
