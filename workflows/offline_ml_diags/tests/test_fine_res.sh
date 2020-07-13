77cd919576d618670048d40abe79397291024331
brianhenn <brianhenn@gmail.com>
2020-07-13 05:25:28 +0000

    fine res diurnal entrypoint working with full variable set

--- workflows/offline_ml_diags/tests/test_fine_res.sh
+++ workflows/offline_ml_diags/tests/test_fine_res.sh
@@ -6,5 +6,4 @@ OUTPUT=gs://vcm-ml-scratch/annak/test-offline-validation-workflow/fineres/
 python -m offline_ml_diags.compute_diags \
     workflows/offline_ml_diags/tests/test_fine_res_config.yml \
     $MODEL \
-    $OUTPUT \
-   --timesteps-file workflows/offline_ml_diags/tests/times_fine_res.json
+    $OUTPUT
