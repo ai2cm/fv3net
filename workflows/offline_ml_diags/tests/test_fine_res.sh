3e619b3e874097e5d6ac185cdcf918f1329aea09
brianhenn <brianhenn@gmail.com>
2020-07-15 06:08:58 +0000

    reverted offline_ml_diags tests dev scripts and yamls

--- workflows/offline_ml_diags/tests/test_fine_res.sh
+++ workflows/offline_ml_diags/tests/test_fine_res.sh
@@ -6,4 +6,5 @@ OUTPUT=gs://vcm-ml-scratch/annak/test-offline-validation-workflow/fineres/
 python -m offline_ml_diags.compute_diags \
     workflows/offline_ml_diags/tests/test_fine_res_config.yml \
     $MODEL \
-    $OUTPUT
+    $OUTPUT \
+   --timesteps-file workflows/offline_ml_diags/tests/times_fine_res.json
