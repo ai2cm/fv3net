8bbab2759ef132c833849e79635b49a8c9de0d07
Jeremy McGibbon <jeremym@vulcan.com>
2020-07-15 12:42:48 -0700

    move shared fixtures to conftest.py

--- external/fv3fit/tests/training/conftest.py
+++ external/fv3fit/tests/training/conftest.py
@@ -1,3 +1,4 @@
+from typing import Iterable, Sequence
 from synth import (  # noqa: F401
     dataset_fixtures_dir,
     data_source_name,
@@ -7,3 +8,116 @@ from synth import (  # noqa: F401
     data_source_path,
     grid_dataset,
 )
+import xarray as xr
+from fv3fit._shared import ModelTrainingConfig, load_data_sequence
+import pytest
+import tempfile
+import yaml
+
+
+@pytest.fixture
+def input_variables() -> Iterable[str]:
+    return ["air_temperature", "specific_humidity"]
+
+
+@pytest.fixture
+def output_variables() -> Iterable[str]:
+    return ["dQ1", "dQ2"]
+
+
+@pytest.fixture()
+def batch_function(model_type: str) -> str:
+    return "batches_from_geodata"
+
+
+@pytest.fixture()
+def batch_kwargs(data_source_name: str) -> dict:  # noqa: F811
+    if data_source_name == "one_step_tendencies":
+        return {
+            "timesteps_per_batch": 1,
+            "init_time_dim_name": "initial_time",
+            "mapping_function": "open_one_step",
+            "timesteps": ["20160801.001500", "20160801.003000"],
+        }
+    elif data_source_name == "nudging_tendencies":
+        return {
+            "timesteps_per_batch": 1,
+            "mapping_function": "open_merged_nudged",
+            "timesteps": ["20160801.001500", "20160801.003000"],
+            "mapping_kwargs": {
+                "i_start": 0,
+                "rename_vars": {
+                    "air_temperature_tendency_due_to_nudging": "dQ1",
+                    "specific_humidity_tendency_due_to_nudging": "dQ2",
+                },
+            },
+        }
+    elif data_source_name == "fine_res_apparent_sources":
+        return {
+            "timesteps_per_batch": 1,
+            "mapping_function": "open_fine_res_apparent_sources",
+            "init_time_dim_name": "initial_time",
+            "timesteps": ["20160801.001500", "20160801.003000"],
+            "rename_variables": {},
+            "mapping_kwargs": {
+                "rename_vars": {
+                    "delp": "pressure_thickness_of_atmospheric_layer",
+                    "grid_xt": "x",
+                    "grid_yt": "y",
+                    "pfull": "z",
+                }
+            },
+        }
+
+
+@pytest.fixture
+def train_config(
+    model_type: str,
+    hyperparameters: dict,
+    input_variables: Iterable[str],
+    output_variables: Iterable[str],
+    batch_function: str,
+    batch_kwargs: dict,
+) -> ModelTrainingConfig:
+    return ModelTrainingConfig(
+        model_type=model_type,
+        hyperparameters=hyperparameters,
+        input_variables=input_variables,
+        output_variables=output_variables,
+        batch_function=batch_function,
+        batch_kwargs=batch_kwargs,
+    )
+
+
+@pytest.fixture
+def train_config_filename(
+    model_type: str,
+    hyperparameters: dict,
+    input_variables: Iterable[str],
+    output_variables: Iterable[str],
+    batch_function: str,
+    batch_kwargs: dict,
+) -> str:
+    with tempfile.NamedTemporaryFile(mode="w") as f:
+        yaml.dump(
+            {
+                "model_type": model_type,
+                "hyperparameters": hyperparameters,
+                "input_variables": input_variables,
+                "output_variables": output_variables,
+                "batch_function": batch_function,
+                "batch_kwargs": batch_kwargs,
+            },
+            f,
+        )
+        yield f.name
+
+
+@pytest.fixture
+def training_batches(
+    data_source_name: str,  # noqa: F811
+    data_source_path: str,  # noqa: F811
+    train_config: ModelTrainingConfig,
+) -> Sequence[xr.Dataset]:
+    batched_data = load_data_sequence(data_source_path, train_config)
+    return batched_data
