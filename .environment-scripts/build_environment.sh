1fcece923293e2ae030358fa2d9594cf01102651
Anna Kwa <annak@vulcan.com>
2020-07-16 20:18:32 +0000

    try fix build

--- .environment-scripts/build_environment.sh
+++ .environment-scripts/build_environment.sh
@@ -2,6 +2,5 @@
 
 CONDA_ENV=$1
 CONDA_BASE=$(conda info --base)
-
-conda env create -n $CONDA_ENV -f environment.yml || \
+source activate $CONDA_ENV || conda env create -n $CONDA_ENV -f environment.yml \
 	echo "Conda env already exists proceeding to VCM package installation"
