995a667d7d0a5be0fa903dfa2f580dc9ebfa0e00
Noah D Brenowitz <nbren12@gmail.com>
2020-07-16 12:21:46 -0700

    [VCMML-390] Make derived variables available to prognostic run (#472)
    Make the cos_zenith_angle available within the prognostic run file. 
    
    This PR does this by wrapping the `fv3gfs.get_state` calls with a custom class `DerivedFV3State` implementing the mutable mapping interface. This class supports registering new derived variables using its `register` method.
    
    Some other consequences of the new abstraction:
    - abstracts away the conversion of `fv3util.Quantity` to/from xarray DataArray
    - Add time as an attribute which allows the mapping to return a consistent type. `fv3gfs.get_state` effectively returns an object of type `Mapping[str, Union[datetime, xr.Dataset]]` because the "time" is a datetime object. Union return types often require some manual casting, which make typehinting a pain. Making the `time` an attribute rather than an element of the collection allows the user to lookup the time, while ensuring that the "state" is a container of objects of the same type. This will forestall any if-statements related to introspecting the types of objects returned by `fv3gfs.get_state`.

---
+++ external/runtime/tests/test_derived_state.py
@@ -0,0 +1,90 @@
+from datetime import datetime
+import xarray as xr
+import numpy as np
+
+import runtime
+import fv3util
+
+import pytest
+
+
+class MockFV3GFS:
+    def __init__(self):
+        self.set_state_called = False
+
+    def get_state(self, names):
+        # need this for the data to remain unchanged for equality tests
+        np.random.seed(0)
+
+        nx, ny = 10, 10
+
+        lat = fv3util.Quantity(np.random.rand(ny, nx), dims=["y", "x"], units="deg")
+        lon = fv3util.Quantity(np.random.rand(ny, nx), dims=["y", "x"], units="deg")
+
+        state = {
+            "time": datetime.now(),
+            "latitude": lat,
+            "longitude": lon,
+        }
+
+        return {name: state[name] for name in names}
+
+    def set_state(self, data):
+        self.set_state_called = True
+        for key, value in data.items():
+            assert isinstance(value, fv3util.Quantity)
+
+
+def test_DerivedFV3State():
+    fv3gfs = MockFV3GFS()
+    getter = runtime.DerivedFV3State(fv3gfs)
+    assert isinstance(getter["longitude"], xr.DataArray)
+
+
+def test_DerivedFV3State_cos_zenith():
+    fv3gfs = MockFV3GFS()
+    getter = runtime.DerivedFV3State(fv3gfs)
+    output = getter["cos_zenith_angle"]
+    assert isinstance(output, xr.DataArray)
+
+
+def test_DerivedFV3State_time():
+    fv3gfs = MockFV3GFS()
+    getter = runtime.DerivedFV3State(fv3gfs)
+    assert isinstance(getter.time, datetime)
+
+
+def test_DerivedFV3State_setitem():
+    fv3gfs = MockFV3GFS()
+    getter = runtime.DerivedFV3State(fv3gfs)
+    item = xr.DataArray([1.0], dims=["x"], attrs={"units": "m"})
+    # Check that data is passed to `MockFV3GFS.set_state` correctly
+    getter["a"] = item
+    assert fv3gfs.set_state_called
+
+
+def test_DerivedFV3State_getitem_time_raises():
+    fv3gfs = MockFV3GFS()
+    getter = runtime.DerivedFV3State(fv3gfs)
+    with pytest.raises(KeyError):
+        getter["time"]
+
+
+def test_DerivedFV3State_register():
+    fv3gfs = MockFV3GFS()
+
+    def xarray_func(arr):
+        return arr * 1.25
+
+    @runtime.DerivedFV3State.register("mock")
+    def mock(self):
+        return xarray_func(self["latitude"])
+
+    getter = runtime.DerivedFV3State(fv3gfs)
+
+    latitude = getter["latitude"]
+    expected = xarray_func(latitude)
+
+    output = getter["mock"]
+    assert isinstance(output, xr.DataArray)
+    xr.testing.assert_equal(output, expected)
