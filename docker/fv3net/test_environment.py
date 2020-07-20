6c5c16059919a701da1d5ce9425ce6ef69f9070c
Noah D. Brenowitz <nbren12@gmail.com>
2020-07-20 13:39:38 -0700

    lint

--- docker/fv3net/test_environment.py
+++ docker/fv3net/test_environment.py
@@ -5,9 +5,10 @@ import os
 
 
 def test_gsutil_not_from_conda():
-    gsutil_path = subprocess.check_output(['which', 'gsutil'])
+    gsutil_path = subprocess.check_output(["which", "gsutil"])
     conda_bin_dir = os.path.dirname(sys.executable)
     assert not gsutil_path.startswith(conda_bin_dir)
+
 
 if __name__ == "__main__":
     test_gsutil_not_from_conda()
