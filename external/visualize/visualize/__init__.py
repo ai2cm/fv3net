0ab6b9c6d53241f3ab3e9e5a57c7e9d79349a2e8
brianhenn <brianhenn@gmail.com>
2020-07-20 23:38:06 +0000

    removed dependencies on vcm in visualize

--- external/visualize/visualize/__init__.py
+++ external/visualize/visualize/__init__.py
@@ -1,9 +1,26 @@
-from .timestep_histograms import (
+from ._timestep_histograms import (
     plot_daily_and_hourly_hist,
     plot_daily_hist,
     plot_hourly_hist,
 )
-
 from ._plot_cube import plot_cube, mappable_var, plot_cube_axes, pcolormesh_cube
+from ._plot_diagnostics import (
+    plot_diurnal_cycle,
+    plot_diag_var_single_map,
+    plot_time_series,
+)
+
+__all__ = [
+    "plot_daily_and_hourly_hist",
+    "plot_daily_hist",
+    "plot_hourly_hist",
+    "plot_cube",
+    "mappable_var",
+    "plot_cube_axes",
+    "pcolormesh_cube",
+    "plot_diurnal_cycle",
+    "plot_diag_var_single_map",
+    "plot_time_series",
+]
 
 __version__ = "0.1.0"
