34509cb8dd7a803a004e66f5676dee8764b0ca7f
Noah D. Brenowitz <nbren12@gmail.com>
2020-07-20 13:38:01 -0700

    test location of gsutil

--- docker/fv3net/test_environment.py
+++ docker/fv3net/test_environment.py
@@ -1,1 +1,13 @@
 import tensorflow, fv3net, fv3fit  # noqa
+import subprocess
+import sys
+import os
+
+
+def test_gsutil_not_from_conda():
+    gsutil_path = subprocess.check_output(['which', 'gsutil'])
+    conda_bin_dir = os.path.dirname(sys.executable)
+    assert not gsutil_path.startswith(conda_bin_dir)
+
+if __name__ == "__main__":
+    test_gsutil_not_from_conda()
