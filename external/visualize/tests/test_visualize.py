5d555b5558464ee60e06fec19a18ba2e42543277
brianhenn <brianhenn@gmail.com>
2020-07-21 00:01:26 +0000

    visualize package tests passing"

--- external/visualize/tests/test_visualize.py
+++ external/visualize/tests/test_visualize.py
@@ -1,16 +1,27 @@
 import pytest
+from datetime import datetime
 import numpy as np
 import xarray as xr
 from cartopy import crs as ccrs
 from matplotlib import pyplot as plt
-from vcm.visualize.masking import (
+from visualize import __version__
+from visualize._masking import (
     _mask_antimeridian_quads,
     _periodic_equal_or_less_than,
     _periodic_greater_than,
     _periodic_difference,
 )
-from vcm.visualize import mappable_var, plot_cube_axes, plot_cube
-from vcm.visualize.plot_helpers import _get_var_label
+from visualize._plot_cube import mappable_var, plot_cube_axes, plot_cube
+from visualize._plot_helpers import _get_var_label
+from visualize._timestep_histograms import (
+    plot_daily_and_hourly_hist,
+    plot_daily_hist,
+    plot_hourly_hist,
+)
+
+
+def test_version():
+    assert __version__ == "0.1.0"
 
 
 @pytest.mark.parametrize(
@@ -303,3 +314,26 @@ def test_plot_cube_on_axis(sample_dataset, plotting_function):
 )
 def test__get_var_label(attrs, var_name, expected_label):
     assert _get_var_label(attrs, var_name) == expected_label
+
+
+example_timesteps = [
+    [datetime(2016, 8, 1), datetime(2016, 8, 2)],
+    (datetime(2016, 8, 1), datetime(2016, 8, 2)),
+    [np.datetime64("2016-08-01"), np.datetime64("2016-08-02")],
+    np.array(["2016-08-01", "2016-08-02"], dtype="datetime64"),
+]
+
+
+@pytest.mark.parametrize("timesteps", example_timesteps)
+def test_plot_daily_and_hourly_hist(timesteps):
+    plot_daily_and_hourly_hist(timesteps)
+
+
+@pytest.mark.parametrize("timesteps", example_timesteps)
+def test_plot_daily_hist(timesteps):
+    plot_daily_hist(plt.subplot(), timesteps)
+
+
+@pytest.mark.parametrize("timesteps", example_timesteps)
+def test_plot_hourly_hist(timesteps):
+    plot_hourly_hist(plt.subplot(), timesteps)
