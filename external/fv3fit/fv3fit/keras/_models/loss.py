fix how loss values are calculated, and allow loss normalization to be disabled

external/fv3fit/fv3fit/keras/_models/loss.py
@@ -8,16 +8,14 @@ import tensorflow as tf
 Weight = Union[int, float, np.ndarray]
 
 
-def _weighted_loss(weights, loss):
-    weights = tf.constant(weights.astype(np.float32))
-
+def _weighted_mse(weights, std):
     def custom_loss(y_true, y_pred):
-        return loss(tf.multiply(y_true, weights), tf.multiply(y_pred, weights))
+        return tf.math.reduce_mean(weights * tf.math.square((y_pred - y_true) / std))
 
     return custom_loss
 
 
-def _pack_weights(y_packer: ArrayPacker, y_std, **weights):
+def _pack_weights(y_packer: ArrayPacker, **weights):
     """Returns a size [1, n_features] array of stacked weights corresponding to a
     stacked array, based on values given in a weights dictionary. Default weight is
     1 split between all features of a quantity. Values can be scalar or an array giving
@@ -36,7 +34,7 @@ def _pack_weights(y_packer: ArrayPacker, y_std, **weights):
             array = np.zeros([1]) + weight
             dims = [y_packer.sample_dim_name]
         data_vars[name] = (dims, array)
-    return y_packer.to_array(xr.Dataset(data_vars)) / y_std  # type: ignore
+    return y_packer.to_array(xr.Dataset(data_vars))
 
 
 def _divide_scalar_weights_by_feature_counts(
@@ -53,17 +51,19 @@ def _divide_scalar_weights_by_feature_counts(
                 f"received weight of type {type(total_variable_weight)} for {name}, "
                 "only int, float, or ndarray are accepted"
             )
-        weights[name]
 
 
-def get_weighted_loss(
-    loss: Callable, y_packer: ArrayPacker, y_std: np.ndarray, **weights: Weight,
+def get_weighted_mse(
+    y_packer: ArrayPacker, y_std: np.ndarray, **weights: Weight,
 ) -> Callable:
-    """Retrieve a weighted loss function for a given set of weights.
+    """Retrieve a weighted mean squared error loss function for a given set of weights.
 
-    The weights are normalized by standard deviation of that feature, so that the weight
-    given as input to this function represents the overall contribution of that
-    feature to the loss, *independent of how much variance that feature has*.
+    Each feature is divided by the standard deviation before multiplying by the given
+    weights, so that the weight given as input to this function represents the overall
+    contribution of that feature to the loss, *independent of how much variance
+    that feature has*.
+
+    If you do not want scaling to occur, provide a standard deviation of 1.
 
     If a uniform weighting is given to specific
     humidity, and the variance of specific humidity is much smaller in upper levels,
@@ -71,13 +71,12 @@ def get_weighted_loss(
     to the loss as lower levels, despite the lower variance.
 
     Args:
-        loss: the basic loss function, such as tf.keras.losses.MSE
         y_packer: an object which creates stacked arrays
         y_std: the standard deviation of a stacked array of outputs
         **weights: for each variable, either a scalar weight which will be split between
             all features of that variable, or an array specifying the weight
             for each feature of that variable. Default weight is 1 split between all
-            features of that variable. Weights should be normalized and indicate
+            features of that variable. Weights indicate
             how much contribution that feature will give to the loss,
             independent of the variance of that feature.
 
@@ -85,5 +84,5 @@ def get_weighted_loss(
         weighted_loss
     """
     # convert scalar weights from total variable weight to per-feature weight
-    weights = _pack_weights(y_packer, y_std, **weights)
-    return _weighted_loss(weights, loss)
+    weights = _pack_weights(y_packer, **weights)
+    return _weighted_mse(weights, y_std)

external/fv3fit/fv3fit/keras/_models/models.py
@@ -8,7 +8,8 @@ import numpy as np
 import os
 from ._filesystem import get_dir, put_dir
 from .normalizer import StandardScaler
-from .loss import get_weighted_loss
+from .loss import get_weighted_mse
+import yaml
 
 logger = logging.getLogger(__file__)
 
@@ -90,6 +91,7 @@ class PackedKerasModel(Model):
     _Y_PACKER_FILENAME = "y_packer.json"
     _X_SCALER_FILENAME = "X_scaler.npz"
     _Y_SCALER_FILENAME = "y_scaler.npz"
+    _OPTIONS_FILENAME = "options.yml"
 
     def __init__(
         self,
@@ -97,6 +99,7 @@ class PackedKerasModel(Model):
         input_variables: Iterable[str],
         output_variables: Iterable[str],
         weights: Optional[Mapping[str, Union[int, float, np.ndarray]]] = None,
+        normalize_loss: bool = True,
     ):
         """Initialize the model.
 
@@ -110,6 +113,8 @@ class PackedKerasModel(Model):
                 weight of the variable, or a vector referring to the weight for each
                 feature of the variable. Default is a total weight of 1
                 for each variable.
+            normalize_loss: if True (default), normalize outputs by their standard
+                deviation before computing the loss function
         """
         super().__init__(sample_dim_name, input_variables, output_variables)
         self._model = None
@@ -125,6 +130,7 @@ class PackedKerasModel(Model):
             self.weights: Mapping[str, Union[int, float, np.ndarray]] = {}
         else:
             self.weights = weights
+        self._normalize_loss = normalize_loss
 
     @property
     def model(self) -> tf.keras.Model:
@@ -170,8 +176,9 @@ class PackedKerasModel(Model):
 
     def dump(self, path: str) -> None:
         with put_dir(path) as path:
-            model_filename = os.path.join(path, self._MODEL_FILENAME)
-            self.model.save(model_filename)
+            if self._model is not None:
+                model_filename = os.path.join(path, self._MODEL_FILENAME)
+                self.model.save(model_filename)
             with open(os.path.join(path, self._X_PACKER_FILENAME), "w") as f:
                 self.X_packer.dump(f)
             with open(os.path.join(path, self._Y_PACKER_FILENAME), "w") as f:
@@ -180,17 +187,22 @@ class PackedKerasModel(Model):
                 self.X_scaler.dump(f_binary)
             with open(os.path.join(path, self._Y_SCALER_FILENAME), "wb") as f_binary:
                 self.y_scaler.dump(f_binary)
+            with open(os.path.join(path, self._OPTIONS_FILENAME), "w") as f:
+                yaml.safe_dump({"normalize_loss": self._normalize_loss}, f)
 
     @property
     def loss(self):
         # putting this on a property method is needed so we can save and load models
         # using custom loss functions. If using a custom function, it must either
-        # be named custom_loss, as used in the load method below,
+        # be named "custom_loss", as used in the load method below,
         # or it must be registered with keras as a custom object.
+        # Do this by defining the function returned by the decorator as custom_loss.
         # See https://github.com/keras-team/keras/issues/5916 for more info
-        return get_weighted_loss(
-            tf.keras.losses.MSE, self.y_packer, self.y_scaler.std, **self.weights
-        )
+        std = self.y_scaler.std
+        std[std == 0] = 1.0
+        if not self._normalize_loss:
+            std[:] = 1.0
+        return get_weighted_mse(self.y_packer, std, **self.weights)
 
     @classmethod
     def load(cls, path: str) -> Model:
@@ -203,17 +215,24 @@ class PackedKerasModel(Model):
                 X_scaler = StandardScaler.load(f_binary)
             with open(os.path.join(path, cls._Y_SCALER_FILENAME), "rb") as f_binary:
                 y_scaler = StandardScaler.load(f_binary)
+            with open(os.path.join(path, cls._OPTIONS_FILENAME), "r") as f:
+                options = yaml.safe_load(f)
+                print(options)
             obj = cls(
-                X_packer.sample_dim_name, X_packer.pack_names, y_packer.pack_names
+                X_packer.sample_dim_name,
+                X_packer.pack_names,
+                y_packer.pack_names,
+                normalize_loss=options["normalize_loss"],
             )
             obj.X_packer = X_packer
             obj.y_packer = y_packer
             obj.X_scaler = X_scaler
             obj.y_scaler = y_scaler
             model_filename = os.path.join(path, cls._MODEL_FILENAME)
-            obj._model = tf.keras.models.load_model(
-                model_filename, custom_objects={"custom_loss": obj.loss}
-            )
+            if os.path.exists(model_filename):
+                obj._model = tf.keras.models.load_model(
+                    model_filename, custom_objects={"custom_loss": obj.loss}
+                )
             return obj
 
 
@@ -229,13 +248,20 @@ class DenseModel(PackedKerasModel):
         output_variables: Iterable[str],
         depth: int = 3,
         width: int = 16,
+        weights: Optional[Mapping[str, Union[int, float, np.ndarray]]] = None,
+        normalize_loss: bool = True,
     ):
         self._width = width
         self._depth = depth
-        super().__init__(sample_dim_name, input_variables, output_variables)
+        super().__init__(
+            sample_dim_name,
+            input_variables,
+            output_variables,
+            weights=weights,
+            normalize_loss=normalize_loss,
+        )
 
     def get_model(self, n_features_in: int, n_features_out: int) -> tf.keras.Model:
-        model = tf.keras.Sequential()
         inputs = tf.keras.Input(n_features_in)
         x = self.X_scaler.normalize_layer(inputs)
         for i in range(self._depth - 1):

external/fv3fit/fv3fit/keras/_models/normalizer.py
@@ -76,12 +76,17 @@ class StandardScaler:
         return self._denormalize_layer
 
     def dump(self, f: BinaryIO):
-        return np.savez(f, mean=self.mean, std=self.std)
+        data = {}
+        if self.mean is not None:
+            data["mean"] = self.mean
+        if self.std is not None:
+            data["std"] = self.std
+        return np.savez(f, **data)
 
     @classmethod
     def load(cls, f: BinaryIO):
         data = np.load(f)
         scaler = cls()
-        scaler.mean = data["mean"]
-        scaler.std = data["std"]
+        scaler.mean = data.get("mean")
+        scaler.std = data.get("std")
         return scaler

external/fv3fit/tests/test_loss.py
@@ -1,5 +1,5 @@
 from typing import Iterable
-from fv3fit.keras._models.loss import _pack_weights, _weighted_loss
+from fv3fit.keras._models.loss import _pack_weights, _weighted_mse
 from fv3fit._shared import ArrayPacker
 import numpy as np
 import xarray as xr
@@ -47,118 +47,88 @@ def packer(names: Iterable[str], dataset: xr.Dataset) -> ArrayPacker:
 
 
 @pytest.mark.parametrize(
-    "names,weights,features,std,reference",
+    "names,weights,features,reference",
     [
         pytest.param(
-            ["a"],
-            {"a": 1.0},
-            {"a": 1},
-            np.array([1]),
-            np.array([[1]]),
-            id="one_scalar_std_1_weight_1",
+            ["a"], {"a": 1.0}, {"a": 1}, np.array([[1]]), id="one_scalar_weight_1",
         ),
         pytest.param(
-            ["a"],
-            {"a": 1.0},
-            {"a": 1},
-            np.array([2]),
-            np.array([[0.5]]),
-            id="one_scalar_std_2_weight_1",
-        ),
-        pytest.param(
-            ["a"],
-            {"a": 2.0},
-            {"a": 1},
-            np.array([1]),
-            np.array([[2]]),
-            id="one_scalar_std_1_weight_2",
-        ),
-        pytest.param(
-            ["a"],
-            {"a": 1.0},
-            {"a": 4},
-            np.array([1.0, 1.0, 1.0, 1.0]),
-            np.array([[0.25, 0.25, 0.25, 0.25]]),
-            id="one_vector_std_1_weight_1",
-        ),
-        pytest.param(
-            ["a"],
-            {"a": 1.0},
-            {"a": 4},
-            np.array([0.25, 1.0, 5.0, 0.25]),
-            np.array([[1.0, 0.25, 0.05, 1.0]]),
-            id="one_vector_multiple_std_weight_1",
+            ["a"], {"a": 2.0}, {"a": 1}, np.array([[2]]), id="one_scalar_weight_2",
         ),
         pytest.param(
             ["a"],
             {"a": 4.0},
             {"a": 4},
-            np.array([1.0, 1.0, 1.0, 1.0]),
             np.array([[1.0, 1.0, 1.0, 1.0]]),
-            id="one_vector_std_1_weight_4",
-        ),
-        pytest.param(
-            ["a"],
-            {"a": 1.0},
-            {"a": 4},
-            np.array([0.25, 0.25, 0.25, 0.25]),
-            np.array([[1.0, 1.0, 1.0, 1.0]]),
-            id="one_vector_std_0_25_weight_1",
+            id="one_vector_weight_4",
         ),
         pytest.param(
             ["a", "b"],
             {"a": 1.0, "b": 2.0},
             {"a": 1, "b": 1},
-            np.array([2, 2]),
-            np.array([[0.5, 1]]),
+            np.array([[1.0, 2.0]]),
             id="two_scalars",
         ),
         pytest.param(
             ["a", "b"],
             {"a": 1.0, "b": 2.0},
             {"a": 2, "b": 1},
-            np.array([2, 2, 2]),
-            np.array([[0.25, 0.25, 1]]),
+            np.array([[0.5, 0.5, 2]]),
             id="one_scalar_one_vector",
         ),
         pytest.param(
             ["b", "a"],
             {"a": 1.0, "b": 2.0},
             {"a": 2, "b": 1},
-            np.array([2, 2, 2]),
-            np.array([[1, 0.25, 0.25]]),
+            np.array([[2.0, 0.5, 0.5]]),
             id="one_scalar_one_vector_reverse_order",
         ),
     ],
     indirect=["names", "weights", "features"],
 )
-def test_pack_weights(packer, std, weights, reference):
-    result = _pack_weights(packer, std, **weights)
+def test_pack_weights(packer, weights, reference):
+    result = _pack_weights(packer, **weights)
     np.testing.assert_array_equal(result, reference)
 
 
 @pytest.mark.parametrize(
-    "weights, loss, y_true, y_pred, reference",
+    "weights, std, y_true, y_pred, reference",
     [
         pytest.param(
             np.array([2.0]),
-            lambda x, y: abs(x - y),
+            np.array([1.0]),
             np.array([0.0]),
             np.array([1.0]),
             2.0,
             id="double_single_feature_loss",
         ),
         pytest.param(
+            np.array([2.0]),
+            np.array([0.5]),
+            np.array([0.0]),
+            np.array([1.0]),
+            8.0,
+            id="double_single_feature_loss_low_std",
+        ),
+        pytest.param(
             np.array([0.5, 1.0, 2.0]),
-            lambda x, y: sum(abs(x - y)),
+            np.array([1.0, 1.0, 1.0]),
             np.array([0.0, 0, 0]),
             np.array([1.0, 10, 100]),
-            210.5,
+            20100.5 / 3.0,
             id="varying_weight_loss",
+        ),
+        pytest.param(
+            np.array([1.0, 1.0, 1.0]),
+            np.array([0.5, 1.0, 2.0]),
+            np.array([0.0, 0, 0]),
+            np.array([1.0, 10, 100]),
+            (4 + 10 ** 2 + (100 / 2.0) ** 2) / 3.0,
+            id="varying_std_loss",
         ),
     ],
 )
-def test_weighted_loss(weights, loss, y_true, y_pred, reference):
-    loss = _weighted_loss(weights, loss)
+def test_weighted_mse(weights, std, y_true, y_pred, reference):
+    loss = _weighted_mse(weights, std)
     result = loss(y_true, y_pred)
-    np.testing.assert_array_equal(result, reference)
+    np.testing.assert_almost_equal(result, reference)