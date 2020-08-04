6f5b61004c161c07033957f9eda0a6a3cf432ab6
Jeremy McGibbon <jeremym@vulcan.com>
2020-08-04 11:32:30 -0700

    Add normalization to neural networks in fv3fit (#506)
    Motivation: Neural nets often require their inputs and outputs to be normalized to zero mean and standard deviation of 1 in order to train properly. We also need to be able to specify weights for different outputs features of our model.
    
    Description: This PR adds standard scaling to inputs and outputs of keras models, and normalized weights for arbitrary loss functions.
    
    Added public API:
    - PackedKerasModel and subclass DenseModel now accepts "weights" as a hyperparameter, referring to loss function weights. This is a dict whose keys are quantity names and values are either a scalar referring to the total weight of the quantity, or a vector referring to the weight for each feature of the quantity. Default is a total weight of 1 for each quantity.
    - PackedKerasModel and subclass DenseModel also accept "normalize_loss" as a flag. If set to True (default), features will be divided by their standard deviation (computed from the first training batch) before computing the loss
    
    Other code changes:
    - Defined an internal StandardScaler which normalizes and denormalizes data, and provides keras layers to normalize or denormalize data. This is serialized and deserialized alongside the other components of the PackedKerasModel.
    
    Documentation changes:
    - Added docstring to PackedKerasModel's `__init__` so it can explain the weights API. Documentation on using these weights in a workflow or an example workflow using these weights should be done when we (very soon) create a workflow which uses these features.

---
+++ external/fv3fit/fv3fit/keras/_models/loss.py
@@ -0,0 +1,88 @@
+from typing import Callable, Union, Mapping, MutableMapping
+from ..._shared import ArrayPacker
+import numpy as np
+import xarray as xr
+import tensorflow as tf
+
+
+Weight = Union[int, float, np.ndarray]
+
+
+def _weighted_mse(weights, std):
+    def custom_loss(y_true, y_pred):
+        return tf.math.reduce_mean(weights * tf.math.square((y_pred - y_true) / std))
+
+    return custom_loss
+
+
+def _pack_weights(y_packer: ArrayPacker, **weights):
+    """Returns a size [1, n_features] array of stacked weights corresponding to a
+    stacked array, based on values given in a weights dictionary. Default weight is
+    1 split between all features of a quantity. Values can be scalar or an array giving
+    a weight for each feature.
+    """
+    for name in y_packer.pack_names:
+        weights[name] = weights.get(name, 1)  # default weight of 1
+    _divide_scalar_weights_by_feature_counts(weights, y_packer.feature_counts)
+    data_vars = {}
+    for name in y_packer.pack_names:
+        weight = weights[name]
+        if isinstance(weight, np.ndarray):
+            array = weight[None, :]
+            dims = [y_packer.sample_dim_name, f"{name}_feature"]
+        else:
+            array = np.zeros([1]) + weight
+            dims = [y_packer.sample_dim_name]
+        data_vars[name] = (dims, array)
+    return y_packer.to_array(xr.Dataset(data_vars))  # type: ignore
+
+
+def _divide_scalar_weights_by_feature_counts(
+    weights: MutableMapping[str, Weight], feature_counts: Mapping[str, int]
+):
+    for name, total_variable_weight in weights.items():
+        if isinstance(total_variable_weight, (int, float)):
+            weights[name] = total_variable_weight / feature_counts[name]
+        elif isinstance(total_variable_weight, np.ndarray):
+            if total_variable_weight.shape == (1,):
+                weights[name] = total_variable_weight / feature_counts[name]
+        else:
+            raise TypeError(
+                f"received weight of type {type(total_variable_weight)} for {name}, "
+                "only int, float, or ndarray are accepted"
+            )
+
+
+def get_weighted_mse(
+    y_packer: ArrayPacker, y_std: np.ndarray, **weights: Weight,
+) -> Callable:
+    """Retrieve a weighted mean squared error loss function for a given set of weights.
+
+    Each feature is divided by the standard deviation before multiplying by the given
+    weights, so that the weight given as input to this function represents the overall
+    contribution of that feature to the loss, *independent of how much variance
+    that feature has*.
+
+    If you do not want scaling to occur, provide a standard deviation of 1.
+
+    If a uniform weighting is given to specific
+    humidity, and the variance of specific humidity is much smaller in upper levels,
+    a heavier weight will be given to upper levels so that it contributes just as much
+    to the loss as lower levels, despite the lower variance.
+
+    Args:
+        y_packer: an object which creates stacked arrays
+        y_std: the standard deviation of a stacked array of outputs
+        **weights: for each variable, either a scalar weight which will be split between
+            all features of that variable, or an array specifying the weight
+            for each feature of that variable. Default weight is 1 split between all
+            features of that variable. Weights indicate
+            how much contribution that feature will give to the loss,
+            independent of the variance of that feature.
+
+    Returns:
+        weighted_loss
+    """
+    # convert scalar weights from total variable weight to per-feature weight
+    weights = _pack_weights(y_packer, **weights)
+    return _weighted_mse(weights, y_std)
---
+++ external/fv3fit/tests/test_loss.py
@@ -0,0 +1,134 @@
+from typing import Iterable
+from fv3fit.keras._models.loss import _pack_weights, _weighted_mse
+from fv3fit._shared import ArrayPacker
+import numpy as np
+import xarray as xr
+import pytest
+
+
+SAMPLE_DIM = "sample"
+FEATURE_DIM = "z"
+
+
+@pytest.fixture
+def names(request):
+    return request.param
+
+
+@pytest.fixture
+def weights(request):
+    return request.param
+
+
+@pytest.fixture
+def features(request):
+    return request.param
+
+
+@pytest.fixture
+def dataset(features) -> xr.Dataset:
+    data_vars = {}
+    for name, n_features in features.items():
+        if n_features == 1:
+            data_vars[name] = ([SAMPLE_DIM], np.zeros([1]))
+        else:
+            data_vars[name] = (
+                [SAMPLE_DIM, f"{name}_{FEATURE_DIM}"],
+                np.zeros([1, n_features]),
+            )
+    return xr.Dataset(data_vars)
+
+
+@pytest.fixture
+def packer(names: Iterable[str], dataset: xr.Dataset) -> ArrayPacker:
+    packer = ArrayPacker(SAMPLE_DIM, names)
+    packer.to_array(dataset)  # must let packer know about array shapes
+    return packer
+
+
+@pytest.mark.parametrize(
+    "names,weights,features,reference",
+    [
+        pytest.param(
+            ["a"], {"a": 1.0}, {"a": 1}, np.array([[1]]), id="one_scalar_weight_1",
+        ),
+        pytest.param(
+            ["a"], {"a": 2.0}, {"a": 1}, np.array([[2]]), id="one_scalar_weight_2",
+        ),
+        pytest.param(
+            ["a"],
+            {"a": 4.0},
+            {"a": 4},
+            np.array([[1.0, 1.0, 1.0, 1.0]]),
+            id="one_vector_weight_4",
+        ),
+        pytest.param(
+            ["a", "b"],
+            {"a": 1.0, "b": 2.0},
+            {"a": 1, "b": 1},
+            np.array([[1.0, 2.0]]),
+            id="two_scalars",
+        ),
+        pytest.param(
+            ["a", "b"],
+            {"a": 1.0, "b": 2.0},
+            {"a": 2, "b": 1},
+            np.array([[0.5, 0.5, 2]]),
+            id="one_scalar_one_vector",
+        ),
+        pytest.param(
+            ["b", "a"],
+            {"a": 1.0, "b": 2.0},
+            {"a": 2, "b": 1},
+            np.array([[2.0, 0.5, 0.5]]),
+            id="one_scalar_one_vector_reverse_order",
+        ),
+    ],
+    indirect=["names", "weights", "features"],
+)
+def test_pack_weights(packer, weights, reference):
+    result = _pack_weights(packer, **weights)
+    np.testing.assert_array_equal(result, reference)
+
+
+@pytest.mark.parametrize(
+    "weights, std, y_true, y_pred, reference",
+    [
+        pytest.param(
+            np.array([2.0]),
+            np.array([1.0]),
+            np.array([0.0]),
+            np.array([1.0]),
+            2.0,
+            id="double_single_feature_loss",
+        ),
+        pytest.param(
+            np.array([2.0]),
+            np.array([0.5]),
+            np.array([0.0]),
+            np.array([1.0]),
+            8.0,
+            id="double_single_feature_loss_low_std",
+        ),
+        pytest.param(
+            np.array([0.5, 1.0, 2.0]),
+            np.array([1.0, 1.0, 1.0]),
+            np.array([0.0, 0, 0]),
+            np.array([1.0, 10, 100]),
+            20100.5 / 3.0,
+            id="varying_weight_loss",
+        ),
+        pytest.param(
+            np.array([1.0, 1.0, 1.0]),
+            np.array([0.5, 1.0, 2.0]),
+            np.array([0.0, 0, 0]),
+            np.array([1.0, 10, 100]),
+            (4 + 10 ** 2 + (100 / 2.0) ** 2) / 3.0,
+            id="varying_std_loss",
+        ),
+    ],
+)
+def test_weighted_mse(weights, std, y_true, y_pred, reference):
+    loss = _weighted_mse(weights, std)
+    result = loss(y_true, y_pred)
+    np.testing.assert_almost_equal(result, reference)
