89afa925dd9e441b5f4ab72a4edf4f494796b210
brianhenn <brianhenn@gmail.com>
2020-08-05 00:15:21 +0000

    both types of models opening in offline diags

--- workflows/offline_ml_diags/offline_ml_diags/_model_loaders.py
+++ workflows/offline_ml_diags/offline_ml_diags/_model_loaders.py
@@ -1,18 +1,27 @@
+from ._mapper import SklearnPredictionMapper, KerasPredictionMapper
 from vcm.cloud import get_fs
 from fv3fit import sklearn, keras
 import joblib
-from ._mapper import SklearnPredictionMapper, KerasPredictionMapper
 from typing import Tuple, Union
+import os
+
 PredictionMapper = Union[SklearnPredictionMapper, KerasPredictionMapper]
 
 
-def load_sklearn_model(model_path: str) -> Tuple[sklearn.SklearnWrapper, PredictionMapper]:
+def load_sklearn_model(
+    model_path: str,
+) -> Tuple[sklearn.SklearnWrapper, PredictionMapper]:
     fs_model = get_fs(model_path)
-    with fs_model.open(a.model_path, "rb") as f:
+    with fs_model.open(model_path, "rb") as f:
         model = joblib.load(f)
     return model, SklearnPredictionMapper
 
-def load_keras_model(model_path: str, model_type: str = 'DenseModel') -> Tuple[keras.Model, PredictionMapper]:
+
+def load_keras_model(
+    model_path: str,
+    model_type: str = "DenseModel",
+    model_datadir_name: str = "model_data",
+) -> Tuple[keras.Model, PredictionMapper]:
     model_class = keras.get_model_class(model_type)
-    model = model_class.load(model_path)
+    model = model_class.load(os.path.join(model_path, model_datadir_name))
     return model, KerasPredictionMapper
