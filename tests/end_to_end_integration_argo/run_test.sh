34ea0310dad78de87b27acb8fe673a98ef78a4f2
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-08-05 19:14:25 +0000

    Use our own job watching funcs, argo is buggy

--- tests/end_to_end_integration_argo/run_test.sh
+++ tests/end_to_end_integration_argo/run_test.sh
@@ -8,6 +8,41 @@ then
     exit 1
 fi
 
+SLEEP_TIME=60
+
+function getJob {
+    argo get $1 -n $2 -o json
+}
+
+function waitForComplete {
+    # Sleep while job is active
+    jobName=$1
+    NAMESPACE=$2
+    timeout=$(date -ud "30 minutes" +%s)
+    job_phase=$(getJob $jobName $NAMESPACE | jq -r .status.phase)
+    echo "$job_phase"
+    while [[ $(date +%s) -le $timeout ]] && [[ $job_phase == Running ]]
+    do
+        echo "$(date '+%Y-%m-%d %H:%M')" Job active: "$jobName" ... sleeping ${SLEEP_TIME}s
+        sleep $SLEEP_TIME
+        job_phase=$(getJob $jobName $NAMESPACE | jq -r .status.phase)
+    done
+
+    # Check job phase
+    job_phase=$(getJob $jobName $NAMESPACE | jq -r .status.phase)
+    if [[ $job_phase == Succeeded ]]
+    then
+        echo Job successful: "$jobName"
+    elif [[ $job_phase == Failed ]]
+    then
+        echo Job failed: "$jobName"
+        exit 1
+    else
+        echo Job timed out or success ambiguous: "$jobName"
+        exit 1
+    fi
+}
+
 random=$(openssl rand --hex 6)
 name=integration-test-$random
 
@@ -22,5 +57,8 @@ cd tests/end_to_end_integration_argo
 
 envsubst < "config_template.yaml" > "config.yaml"
 
-argo submit --watch argo.yaml -f config.yaml --name $name
-argo logs $name
+argo submit argo.yaml -f config.yaml --name $name
+
+trap "argo logs \"$name\"" EXIT
+# argo's wait/watch features are buggy, so roll our own
+waitForComplete $name default
