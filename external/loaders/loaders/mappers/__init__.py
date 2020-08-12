d36fb9ef6789f8ef80700abfe5e7b93227ed096c
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-08-12 00:18:02 +0000

    Remove one_step mapper and related tests

--- external/loaders/loaders/mappers/__init__.py
+++ external/loaders/loaders/mappers/__init__.py
@@ -1,6 +1,5 @@
 # mapping functions we mean to be able to specify in the
 # model training configuration
-from ._one_step import open_one_step
 from ._fine_resolution_budget import open_fine_res_apparent_sources
 from ._nudged import (
     open_merged_nudged,
