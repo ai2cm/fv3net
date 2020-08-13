697171bfec1c254514651877b6fa840508629e31
brianhenn <brianhenn@gmail.com>
2020-08-13 06:04:47 +0000

    modified fv3fit.sklearn wrapper and train routine to uniform API

--- external/fv3fit/fv3fit/sklearn/_wrapper.py
+++ external/fv3fit/fv3fit/sklearn/_wrapper.py
@@ -3,7 +3,9 @@ from copy import copy
 import numpy as np
 import xarray as xr
 from sklearn.base import BaseEstimator
-from .._shared import pack, unpack
+from .._shared import pack, unpack, Predictor
+
+from typing import Iterable
 
 
 class RegressorEnsemble:
@@ -53,35 +55,46 @@ class RegressorEnsemble:
 
 
 @dataclass
-class BaseXarrayEstimator:
-    def fit(
-        self, input_vars: tuple, output_vars: tuple, sample_dim: str, data: xr.Dataset
+class BaseXarrayEstimator(Predictor):
+    """Abstract base class for an estimator wich works with xarray datasets
+    """
+
+    def __init__(
+        self,
+        sample_dim_name: str,
+        input_variables: Iterable[str],
+        output_variables: Iterable[str],
     ):
         """
         Args:
-            input_vars: list of input variables
-            output_vars: list of output_variables
-            sample_dim: dimension over which samples are taken
+            sample_dim_name: dimension over which samples are taken
+            input_variables: list of input variables
+            output_variables: list of output variables
+        """
+        super().__init__(sample_dim_name, input_variables, output_variables)
+
+    def fit(self, data: xr.Dataset) -> None:
+        """
+        Args:
             data: xarray Dataset with dimensions (sample_dim, *)
 
-        Returns:
-            fitted model
         """
         raise NotImplementedError
 
-    def predict(self, data: xr.Dataset, sample_dim: str) -> xr.Dataset:
-        """
-        Make a prediction
 
-        Args:
-            data: xarray Dataset with the same feature dimensions as trained
-              data
-            sample_dim: dimension along which "samples" are defined. This could be
-              inferred, but explicity is not terrible.
-        Returns:
-            prediction:
-        """
-        raise NotImplementedError
+#     def predict(self, data: xr.Dataset, sample_dim: str) -> xr.Dataset:
+#         """
+#         Make a prediction
+
+#         Args:
+#             data: xarray Dataset with the same feature dimensions as trained
+#               data
+#             sample_dim: dimension along which "samples" are defined. This could be
+#               inferred, but explicity is not terrible.
+#         Returns:
+#             prediction:
+#         """
+#         raise NotImplementedError
 
 
 class SklearnWrapper(BaseXarrayEstimator):
@@ -89,30 +102,38 @@ class SklearnWrapper(BaseXarrayEstimator):
 
     """
 
-    def __init__(self, model: BaseEstimator):
+    def __init__(
+        self,
+        sample_dim_name: str,
+        input_variables: tuple,
+        output_variables: tuple,
+        model: BaseEstimator,
+    ):
         """
+        Initialize the wrapper
 
         Args:
+            sample_dim_name: dimension over which samples are taken
+            input_variables: list of input variables
+            output_variables: list of output variables
             model: a scikit learn regression model
         """
+        super().__init__(sample_dim_name, input_variables, output_variables)
         self.model = model
 
     def __repr__(self):
         return "SklearnWrapper(\n%s)" % repr(self.model)
 
-    def fit(
-        self, input_vars: tuple, output_vars: tuple, sample_dim: str, data: xr.Dataset
-    ):
+    def fit(self, data: xr.Dataset):
         # TODO the sample_dim can change so best to use feature dim to flatten
-        self.input_vars_ = input_vars
-        self.output_vars_ = output_vars
-        x, _ = pack(data[input_vars], sample_dim)
-        y, self.output_features_ = pack(data[output_vars], sample_dim)
+        x, _ = pack(data[self.input_variables], self.sample_dim_name)
+        y, self.output_features_ = pack(
+            data[self.output_variables], self.sample_dim_name
+        )
         self.model.fit(x, y)
-        return self
 
-    def predict(self, data, sample_dim):
-        x, _ = pack(data[self.input_vars_], sample_dim)
+    def predict(self, data):
+        x, _ = pack(data[self.input_variables], self.sample_dim_name)
         y = self.model.predict(x)
-        ds = unpack(y, sample_dim, self.output_features_)
-        return ds.assign_coords({sample_dim: data[sample_dim]})
+        ds = unpack(y, self.sample_dim_name, self.output_features_)
+        return ds.assign_coords({self.sample_dim_name: data[self.sample_dim_name]})
