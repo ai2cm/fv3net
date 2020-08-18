b873b2a8749f603bd055e85e5b9c3d6d4fdff37b
brianhenn <brianhenn@gmail.com>
2020-08-18 00:08:31 +0000

    added transpose to ensure sample dim is first in fv3fit keras packer

--- workflows/offline_ml_diags/offline_ml_diags/_model_loaders.py
+++ workflows/offline_ml_diags/offline_ml_diags/_model_loaders.py
@@ -1,6 +1,5 @@
 from fv3fit.sklearn import SklearnWrapper
 from fv3fit.keras import get_model_class, Model
-import os
 
 
 def load_sklearn_model(model_path: str) -> SklearnWrapper:
@@ -9,5 +8,5 @@ def load_sklearn_model(model_path: str) -> SklearnWrapper:
 
 def load_keras_model(model_path: str, keras_model_type: str = "DenseModel") -> Model:
     model_class = get_model_class(keras_model_type)
-    model = model_class.load(os.path.join(model_path))
+    model = model_class.load(model_path)
     return model
