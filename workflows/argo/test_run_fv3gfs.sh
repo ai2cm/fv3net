12afa9e311b24cd64cdd5563f9e89269a20e2c22
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-08-19 19:46:16 +0000

    Update test script to use latest post-process image

--- workflows/argo/test_run_fv3gfs.sh
+++ workflows/argo/test_run_fv3gfs.sh
@@ -6,8 +6,8 @@ FV3CONFIG=test_fv3config.yaml
 RUNFILE=../prognostic_c48_run/nudge_to_obs/runfile.py
 CHUNKS=test_chunks.yaml
 FV3GFS_IMAGE="us.gcr.io/vcm-ml/fv3gfs-python:v0.4.3"
-POST_PROCESS_IMAGE="us.gcr.io/vcm-ml/post_process_run:48814ed3752ac507c0e19fa001eef691c9f351e8"
-OUTPUT_URL="gs://vcm-ml-scratch/oliwm/test-fv3gfs-segments/v1"
+POST_PROCESS_IMAGE="us.gcr.io/vcm-ml/post_process_run:eaba512029771efe29a97820b257171ded401479"
+OUTPUT_URL="gs://vcm-ml-scratch/oliwm/test-fv3gfs-segments/v3"
 
 argo submit run-fv3gfs.yaml -p fv3config="$(cat $FV3CONFIG)" -p runfile="$(cat $RUNFILE)" -p chunks="$(cat $CHUNKS)" -p fv3gfs-image=${FV3GFS_IMAGE} -p post-process-image=${POST_PROCESS_IMAGE} -p output-url=${OUTPUT_URL} -p submission-count=2
 
