38662a7b42b988e1dfef120e5f32875396ed13ef
Jeremy McGibbon <jeremym@vulcan.com>
2020-08-20 14:12:36 -0700

    Update external/synth/setup.py
    Co-authored-by: Noah D Brenowitz <nbren12@gmail.com>

--- external/synth/setup.py
+++ external/synth/setup.py
@@ -12,7 +12,7 @@ setup(
     version="0.1.0",
     python_requires="==3.*,>=3.7.0",
     author="Noah D. Brenowitz",
-    author_email="nbren12@gmail.com",
+    author_email="noahb@vulcan.com",
     packages=["synth"],
     package_dir={"": "."},
     package_data={"synth": ["_dataset_fixtures/*.json"]},
