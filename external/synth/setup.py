134d2e5055f736af54246fe366b0cc379c34e955
Jeremy McGibbon <jeremym@vulcan.com>
2020-08-20 13:53:43 -0700

    convert synth from poetry to setuptools setup

---
+++ external/synth/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+readme = ""
+
+setup(
+    long_description=readme,
+    name="synth",
+    version="0.1.0",
+    python_requires="==3.*,>=3.7.0",
+    author="Noah D. Brenowitz",
+    author_email="nbren12@gmail.com",
+    packages=["synth"],
+    package_dir={"": "."},
+    package_data={"synth": ["_dataset_fixtures/*.json"]},
+    install_requires=[
+        "dask==2.*,>=2.15.0",
+        "fsspec==0.*,>=0.7.3",
+        "toolz==0.*,>=0.10.0",
+        "xarray==0.*,>=0.15.1",
+        "zarr==2.*,>=2.4.0",
+    ],
+    extras_require={"dev": ["pytest==5.*,>=5.2.0", "pytest-regtest==1.*,>=1.4.4"]},
+)
