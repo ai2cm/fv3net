3604e82af03db3e3a4952a38a53722d0a776595e
Jeremy McGibbon <jeremym@vulcan.com>
2020-08-20 12:31:57 -0700

    use absolute paths in docker image, install all external dirs

--- docker/fv3fit/entrypoint.sh
+++ docker/fv3fit/entrypoint.sh
@@ -3,11 +3,8 @@
 # install the needed fv3net packages
 for package in /external/*
 do
-    if [[ -f $package/setup.py ]]
-    then
-        echo "Setting up $package"
-        pip install -e "$package" --no-deps > /dev/null 2> /dev/null
-    fi
+    echo "Setting up $package"
+    pip install -e "$package" --no-deps > /dev/null 2> /dev/null
 done
 
 exec "$@"
