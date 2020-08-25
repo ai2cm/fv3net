3c90857b4266adb24c56c05095d630b89cd73044
brianhenn <brianh@vulcan.com>
2020-08-25 15:39:01 -0700

    Prognostic run with keras models
    Extends the functionality of `workflows/prognostic_c48_run` to work with `fv3fit.keras` ML models, in addition to the pre-existing ability to work with `fv3fit.sklearn` models. Uses the unified `fv3fit` API to allow near-identical behavior regardless of model type, with changes to the prognostic runfile consisting of only new model loaders specific to the model type. Also updates `workflows/prognostic_c48_run/orchestrate_submit_job.py` to configure both types of model jobs.
    
    Added public API:
    - `DummyModel` is now accessible via `fv3fit.keras.get_model_class`
    - `runtime.get_ml_model_class` returns the appropriate model class for loading based on the prognostic run config
    
    Refactored public API:
    - The former `fv3fit.sklearn._adapters.py` and its `RenamingAdapter` and `StackingAdapter` have been refactored to `workflows/prognostic_c48_run/runtime/adapters.py` since this workflow was the only place in which they were used, and they are no longer specific to `fv3fit.sklearn` (they now work with both sklearn and keras models). Their test suite has been refactored to `workflows/prognostic_c48_run/tests/test_runtime.py`
    
    Significant internal changes:
    - `workflows/prognostic_c48_run/sklearn_runfile.py` now calls an `open_model` function that uses new `runtime.get_ml_model_class` to load either type of model depending on config; the model is loaded on each MPI rank instead of broadcast due to the challenge of pickling/broadcasting keras models
    - `workflows/orchestrate_c48_run/orchestrate_submit_job.py` now handles reading model type from the user config and updating the fv3config.yml file appropriately. 
    - regression tests for prognostic ML have been rewritten to parameterize both model cases, using the `DummyModel` for the keras case
    - fv3fit.keras `ArrayPacker` objects on `.to_array` now check whether the first dimension of a 2D xr.DataArray is the sample dimension and transpose it to be the case if not
    - fv3fit now tests public model classes for being subclasses of the Predictor base class; new public model classes should be registered here
    
    - [X] Tests added
    
    Resolves [VCMML-418].
    
    
    
    
    
    [VCMML-418]: https://vulcan.atlassian.net/browse/VCMML-418

--- workflows/prognostic_c48_run/runtime/__init__.py
+++ workflows/prognostic_c48_run/runtime/__init__.py
@@ -1,6 +1,7 @@
-from .config import get_namelist, get_config
+from .config import get_namelist, get_config, get_ml_model
 from .capture import capture_stream, capture_stream_mpi, capture_fv3gfs_funcs
 from .derived_state import DerivedFV3State
 from .diagnostics import get_diagnostic_files
+from .adapters import RenamingAdapter, StackingAdapter
 
 __version__ = "0.1.0"
