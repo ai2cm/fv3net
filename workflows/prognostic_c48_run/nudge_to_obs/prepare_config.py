610a352f6f6a4a2e717d1a06556cb783440f08e0
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-08-27 20:24:02 +0000

    Allow linking and multiple segments in nudge-to-obs prepare_config

--- workflows/prognostic_c48_run/nudge_to_obs/prepare_config.py
+++ workflows/prognostic_c48_run/nudge_to_obs/prepare_config.py
@@ -14,13 +14,16 @@ def parse_args():
         description="prepare fv3config yaml file for nudge-to-obs run"
     )
     parser.add_argument("config", type=str, help="base yaml file to configure")
+    parser.add_argument(
+        "--segment-count", type=int, help="number of run-fv3gfs segmetns", default=1
+    )
     return parser.parse_args()
 
 
-def get_output_times(config):
+def get_output_times(config, segment_count):
     start_time = datetime(*config["namelist"]["coupler_nml"]["current_date"])
     output_frequency = timedelta(hours=config["namelist"]["atmos_model_nml"]["fhout"])
-    duration = fv3config.get_run_duration(config)
+    duration = segment_count * fv3config.get_run_duration(config)
     output_times = []
     current_time = start_time + output_frequency  # first output is after one interval
     while current_time <= start_time + duration:
@@ -44,8 +47,17 @@ def test_get_output_times():
             "atmos_model_nml": {"fhout": 1.0},
         }
     }
-    expected = ["20160801.010000", "20160801.020000", "20160801.030000"]
-    assert get_output_times(test_config) == expected
+    expected_one_segment = ["20160801.010000", "20160801.020000", "20160801.030000"]
+    expected_two_segments = [
+        "20160801.010000",
+        "20160801.020000",
+        "20160801.030000",
+        "20160801.040000",
+        "20160801.050000",
+        "20160801.060000",
+    ]
+    assert get_output_times(test_config, 1) == expected_one_segment
+    assert get_output_times(test_config, 2) == expected_two_segments
 
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -60,15 +72,15 @@ if __name__ == "__main__":
         user_config = yaml.safe_load(f)
 
     config = fv3kube.get_base_fv3config(user_config["base_version"])
-
+    config = vcm.update_nested_dict(config, user_config)
+    output_times = get_output_times(config, args.segment_count)
     config = vcm.update_nested_dict(
-        config,
-        {"runfile_output": {"output_times": get_output_times(config)}},
-        # user config takes precedence
-        user_config,
+        config, {"runfile_output": {"output_times": output_times}},
     )
 
     if config["namelist"]["fv_core_nml"].get("nudge", False):
-        config = fv3kube.enable_nudge_to_observations(config)
+        config = fv3kube.enable_nudge_to_observations(
+            config, segment_count=args.segment_count, link=True
+        )
 
     print(yaml.dump(config))
