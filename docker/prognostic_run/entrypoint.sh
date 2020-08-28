2b5f422a0b4166aba5b102c8baddde491c7a94bc
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-08-28 20:07:50 +0000

    Use setup.py instead of poetry for fv3kube

--- docker/prognostic_run/entrypoint.sh
+++ docker/prognostic_run/entrypoint.sh
@@ -7,13 +7,6 @@ do
     then
         echo "Setting up $package"
         pip install -e "$package" --no-deps > /dev/null 2> /dev/null
-    elif [[ -f $package/pyproject.toml ]]
-    then
-        echo "Setting up $package"
-        cwd=$PWD
-        cd $package
-        poetry install --no-dev -n
-        cd $cwd
     fi
 done
 
