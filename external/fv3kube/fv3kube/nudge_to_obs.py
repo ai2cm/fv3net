390c5324d87660f21377b60175f019eec9ad05e1
Noah D. Brenowitz <nbren12@gmail.com>
2020-09-07 16:34:34 -0700

    Refactor prepare_config into sequence of overlays

--- external/fv3kube/fv3kube/nudge_to_obs.py
+++ external/fv3kube/fv3kube/nudge_to_obs.py
@@ -1,10 +1,8 @@
-from copy import deepcopy
 from datetime import datetime, timedelta
 import numpy as np
 from typing import List, Mapping, Optional
 
 import fv3config
-import vcm
 
 # this module assumes that analysis files are at 00Z, 06Z, 12Z and 18Z
 SECONDS_IN_HOUR = 60 * 60
@@ -26,13 +24,11 @@ def _most_recent_hour(current_hour, hour_array=NUDGE_HOURS) -> int:
     return first_nudge_hour
 
 
-def _get_nudge_time_list(config: Mapping) -> List[datetime]:
+def _get_nudge_time_list(run_duration, current_date) -> List[datetime]:
     """Return list of datetime objects corresponding to times at which analysis files
     are required for nudging for a given model run configuration"""
-    current_date = config["namelist"]["coupler_nml"]["current_date"]
     start_time = datetime(*current_date)
     first_nudge_time = _most_recent_nudge_time(start_time)
-    run_duration = fv3config.get_run_duration(config)
     nudge_duration = run_duration + (start_time - first_nudge_time)
     nudge_duration_hours = int(
         np.ceil(nudge_duration.total_seconds() / SECONDS_IN_HOUR)
@@ -42,32 +38,16 @@ def _get_nudge_time_list(config: Mapping) -> List[datetime]:
     return [first_nudge_time + timedelta(hours=hour) for hour in nudging_hours]
 
 
-def _get_nudge_filename_list(config: Mapping) -> List[str]:
+def _get_target_filename_list(
+    run_duration, current_date, nudge_filename_pattern
+) -> List[str]:
     """Return list of filenames of all nudging files required"""
-    nudge_filename_pattern = config["gfs_analysis_data"]["filename_pattern"]
-    time_list = _get_nudge_time_list(config)
+    time_list = _get_nudge_time_list(run_duration, current_date)
     return [time.strftime(nudge_filename_pattern) for time in time_list]
 
 
-def _get_nudge_files_asset_list(config: Mapping) -> List[Mapping]:
-    """Return list of fv3config assets for all nudging files required for a given
-    model run configuration"""
-    nudge_url = config["gfs_analysis_data"]["url"]
-    return [
-        fv3config.get_asset_dict(nudge_url, file, target_location=NUDGE_FILE_TARGET)
-        for file in _get_nudge_filename_list(config)
-    ]
-
-
-def _get_nudge_files_description_asset(config: Mapping, config_url: str) -> Mapping:
-    """Return an fv3config asset pointing to the text file that the
-    model requires to describe the list of nudging files."""
-    fname_list_filename = config["namelist"]["fv_nwp_nudge_nml"]["input_fname_list"]
-    return fv3config.get_asset_dict(config_url, fname_list_filename)
-
-
-def _get_input_fname_list_asset(config: Mapping, filename: str) -> Mapping:
-    fname_list_contents = "\n".join(_get_nudge_filename_list(config))
+def _get_input_fname_list_asset(nudge_filename_list, filename: str) -> Mapping:
+    fname_list_contents = "\n".join(nudge_filename_list)
     data = fname_list_contents.encode()
     return fv3config.get_bytes_asset_dict(
         data, target_location="", target_name=filename
@@ -75,7 +55,10 @@ def _get_input_fname_list_asset(config: Mapping, filename: str) -> Mapping:
 
 
 def enable_nudge_to_observations(
-    config: Mapping,
+    duration,
+    current_date,
+    nudge_filename_pattern="%Y%m%d_%HZ_T85LR.nc",
+    nudge_url="gs://vcm-ml-data/2019-12-02-year-2016-T85-nudging-data",
     file_list_path="nudging_file_list",
     timescale_hours: Optional[Mapping[str, int]] = None,
 ) -> Mapping:
@@ -103,54 +86,48 @@ def enable_nudge_to_observations(
     Note:
         This function appends to patch_files and alters the namelist
     """
-    config = deepcopy(config)
 
-    default_analysis_overlay = {
+    nudge_file_list = _get_target_filename_list(
+        duration, current_date, nudge_filename_pattern
+    )
+    fname_list_asset = _get_input_fname_list_asset(nudge_file_list, file_list_path)
+    nudging_assets = [
+        fv3config.get_asset_dict(nudge_url, file, target_location=NUDGE_FILE_TARGET)
+        for file in nudge_file_list
+    ]
+
+    return {
         "gfs_analysis_data": {
-            "url": "gs://vcm-ml-data/2019-12-02-year-2016-T85-nudging-data",
-            "filename_pattern": "%Y%m%d_%HZ_T85LR.nc",
-        }
+            "url": nudge_url,
+            "filename_pattern": nudge_filename_pattern,
+        },
+        "namelist": _namelist(file_list_path),
+        "patch_files": [fname_list_asset] + nudging_assets,
     }
 
-    config = vcm.update_nested_dict(
-        default_analysis_overlay,
-        config,
-        _namelist_overlay(input_fname_list=file_list_path),
-    )
 
-    fname_list_asset = _get_input_fname_list_asset(config, file_list_path)
-
-    patch_files = config.setdefault("patch_files", [])
-    patch_files.append(fname_list_asset)
-    patch_files.extend(_get_nudge_files_asset_list(config))
-
-    return config
-
-
-def _namelist_overlay(input_fname_list="nudging_file_list",) -> Mapping:
+def _namelist(input_fname_list="nudging_file_list",) -> Mapping:
     return {
-        "namelist": {
-            "fv_core_nml": {"nudge": True},
-            "gfs_physics_nml": {"use_analysis_sst": True},
-            "fv_nwp_nudge_nml": {
-                "add_bg_wind": False,
-                "do_ps_bias": False,
-                "ibtrack": True,
-                "input_fname_list": input_fname_list,
-                "k_breed": 10,
-                "kbot_winds": 0,
-                "mask_fac": 0.2,
-                "nf_ps": 3,
-                "nf_t": 3,
-                "nudge_debug": True,
-                "r_hi": 5.0,
-                "r_lo": 3.0,
-                "r_min": 225000.0,
-                "t_is_tv": False,
-                "tc_mask": True,
-                "time_varying": False,
-                "track_file_name": "No_File_specified",
-                "use_high_top": True,
-            },
+        "fv_core_nml": {"nudge": True},
+        "gfs_physics_nml": {"use_analysis_sst": True},
+        "fv_nwp_nudge_nml": {
+            "add_bg_wind": False,
+            "do_ps_bias": False,
+            "ibtrack": True,
+            "input_fname_list": input_fname_list,
+            "k_breed": 10,
+            "kbot_winds": 0,
+            "mask_fac": 0.2,
+            "nf_ps": 3,
+            "nf_t": 3,
+            "nudge_debug": True,
+            "r_hi": 5.0,
+            "r_lo": 3.0,
+            "r_min": 225000.0,
+            "t_is_tv": False,
+            "tc_mask": True,
+            "time_varying": False,
+            "track_file_name": "No_File_specified",
+            "use_high_top": True,
         },
     }
