a395155fefcffb472bfc22bd843195afb7753807
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-09-08 23:40:05 +0000

    Update nudge-to-obs prepare config

--- workflows/prognostic_c48_run/nudge_to_obs/prepare_config.py
+++ workflows/prognostic_c48_run/nudge_to_obs/prepare_config.py
@@ -17,47 +17,31 @@ def parse_args():
     parser.add_argument(
         "--segment-count", type=int, help="number of run-fv3gfs segmetns", default=1
     )
+    parser.add_argument(
+        "--nudge-url",
+        type=str,
+        help="path to GFS analysis files",
+        default="gs://vcm-ml-data/2019-12-02-year-2016-T85-nudging-data",
+    )
     return parser.parse_args()
 
 
-def get_output_times(config, segment_count):
-    start_time = datetime(*config["namelist"]["coupler_nml"]["current_date"])
-    output_frequency = timedelta(hours=config["namelist"]["atmos_model_nml"]["fhout"])
-    duration = segment_count * fv3config.get_run_duration(config)
+def get_output_times(current_date, duration, frequency):
+    start_time = datetime(*current_date)
     output_times = []
-    current_time = start_time + output_frequency  # first output is after one interval
+    current_time = start_time + frequency  # first output is after one interval
     while current_time <= start_time + duration:
         output_times.append(vcm.encode_time(current_time))
-        current_time += output_frequency
+        current_time += frequency
     return output_times
 
 
 def test_get_output_times():
-    test_config = {
-        "namelist": {
-            "coupler_nml": {
-                "current_date": [2016, 8, 1, 0, 0, 0],
-                "hours": 3,
-                "years": 0,
-                "months": 0,
-                "days": 0,
-                "minutes": 0,
-                "seconds": 0,
-            },
-            "atmos_model_nml": {"fhout": 1.0},
-        }
-    }
-    expected_one_segment = ["20160801.010000", "20160801.020000", "20160801.030000"]
-    expected_two_segments = [
-        "20160801.010000",
-        "20160801.020000",
-        "20160801.030000",
-        "20160801.040000",
-        "20160801.050000",
-        "20160801.060000",
-    ]
-    assert get_output_times(test_config, 1) == expected_one_segment
-    assert get_output_times(test_config, 2) == expected_two_segments
+    expected = ["20160801.010000", "20160801.020000", "20160801.030000"]
+    assert (
+        get_output_times([2016, 8, 1, 0, 0, 0], timedelta(hours=3), timedelta(hours=1))
+        == expected
+    )
 
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -73,14 +57,28 @@ if __name__ == "__main__":
 
     config = fv3kube.get_base_fv3config(user_config["base_version"])
     config = vcm.update_nested_dict(config, user_config)
-    output_times = get_output_times(config, args.segment_count)
+
+    current_date = config["namelist"]["coupler_nml"]["current_date"]
+    output_frequency = timedelta(hours=config["namelist"]["atmos_model_nml"]["fhout"])
+    total_run_duration = args.segment_count * fv3config.get_run_duration(config)
+    output_times = get_output_times(current_date, total_run_duration, output_frequency)
+
     config = vcm.update_nested_dict(
         config, {"runfile_output": {"output_times": output_times}},
     )
 
     if config["namelist"]["fv_core_nml"].get("nudge", False):
-        config = fv3kube.enable_nudge_to_observations(
-            config, segment_count=args.segment_count, link=True
+        if nudge_url.startswith("gs://"):
+            copy_method = "copy"
+        else:
+            copy_method = "link"
+
+        nudge_overlay = fv3kube.enable_nudge_to_observations(
+            total_run_duration,
+            current_date,
+            nudge_url=args.nudge_url,
+            copy_method=copy_method,
         )
+        config = vcm.update_nested_dict(config, nudge_overlay)
 
     print(yaml.dump(config))
