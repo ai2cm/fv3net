Correct the mass weighting

external/fv3fit/tests/test_scalers.py
@@ -165,7 +165,7 @@ def test_get_mass_scaler():
         sample_dim_name=SAMPLE_DIM, pack_names=sorted(list(ds.data_vars))
     )
     y = packer.to_array(ds)
-    delp = np.array([1.0, 4.0])
+    delp = np.array([1.0, 0.25])
     scale_factors = {"y0": 100}
     scaler = get_mass_scaler(packer, delp, scale_factors, True,)
     expected_normalized = [