383ac2f475edc4f3e44272684e2f374a19b77267
Anna Kwa <annak@vulcan.com>
2020-09-10 00:17:15 +0000

    open fine res for comparison

---
+++ workflows/vertical_profile_diags/_utils.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+import numpy as np
+from typing import List
+import xarray as xr
+import vcm
+from vcm.convenience import round_time
+
+TIME_NAME = "time"
+TIME_FMT = "%Y%m%d.%H%M%S"
+
+
+
+def time_series(
+    da,
+    grid,
+    title=None,
+    units=None,
+    vmin=None,
+    vmax=None,
+    cmap=None
+):  
+    
+    units = units or da.units or ""
+    title = title or f"{da.name} [{units}]"
+    mean_dims = [dim for dim in ["tile", "x", "y"] if dim in da.dims]
+    da = da.mean(mean_dims)
+    if "z" in da.dims:
+        da.plot(x="time", vmin=vmin or None, vmax=vmax or None, cmap=cmap or "RdBu_r")
+        plt.ylabel("model level")
+        plt.gca().invert_yaxis()
+    else:
+        da.plot()
+    plt.title(title)
+    plt.show()
+
+
+def standardize_zarr_time_coord(ds: xr.Dataset) -> xr.Dataset:
+    """ Casts a datetime coord to to python datetime and rounds to
+    nearest even second (because cftime coords have small rounding
+    errors that makes it hard to other datasets join on time)
+
+    Args:
+        ds (xr.Dataset): time coordinate is datetime-like object
+
+    Returns:
+        xr.Dataset with standardized time coordinates
+    """
+    # Vectorize doesn't work on type-dispatched function overloading
+    times = np.array(list(map(vcm.cast_to_datetime, ds[TIME_NAME].values)))
+    times = np.vectorize(round_time)(times)
+    ds = ds.assign_coords({TIME_NAME: times})
+    return ds
+
+
+def dataset_from_timesteps(mapper, keys: Sequence[str], vars: List[str]):
+    time_coords = [datetime.strptime(key, TIME_FMT) for key in keys]
+    ds = xr.concat([mapper[key][vars] for key in keys], pd.Index(time_coords, name="time"))
+    return ds
--- workflows/vertical_profile_diags/regional_vertical_profile.py
+++ workflows/vertical_profile_diags/regional_vertical_profile.py
@@ -7,17 +7,21 @@ import xarray as xr
 import zarr.storage as zstore
 
 from diagnostics_utils import RegionOfInterest
+import ._utils as utils
 
 TIME_FMT = "%Y%m%d.%H%M%S"
 
-rename_diag_table_vars = {
+rename_vars = {
     "grid_xt": "x",
     "grid_x": "x_interface",
     "grid_yt": "y",
     "grid_y": "y_interface",
     "pfull": "z",
+    "delp": "pressure_thickness_of_atmospheric_layer",
     "temp": "air_temperature",
     "sphum": "specific_humidity",
+    "tendency_of_air_temperature_due_to_fv3_physics": "pQ1",
+    "tendency_of_specific_humidity_due_to_fv3_physics": "pQ2",
 }
 
 
@@ -25,9 +29,14 @@ def _create_arg_parser() -> argparse.ArgumentParser:
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "data_path",
+        "run_data_path",
         type=str,
-        help="Location of data."
+        help="Location of run data."
+    )
+    parser.add_argument(
+        "fine_res_reference_path",
+        type=str,
+        help="Location of reference fine res data."
     )
     parser.add_argument(
         "output_path",
@@ -78,19 +87,38 @@ def _create_arg_parser() -> argparse.ArgumentParser:
 
 def _open_zarr(
     url: str, time_bounds: Sequence[str], consolidated: bool = False
-) -> Sequence[xr.Dataset]:
+) :
     mapper = fsspec.get_mapper(url)
-    time_bounds = [datetime.strptime(t, TIME_FMT) for t in time_bounds]
-    return xr.open_zarr(
+    time_slice = slice(*[datetime.strptime(t, TIME_FMT) for t in time_bounds])
+    ds = xr.open_zarr(
         zstore.LRUStoreCache(mapper, 1024),
         consolidated=consolidated,
         mask_and_scale=False,
-    ).sel({"time": slice()})
-        
+    ) 
+    renamed = {
+        key: value for key, value in rename_vars.items()
+        if key in ds.data_vars}
+    ds = ds.rename(renamed) \
+        .pipe(utils.standardize_zarr_time_coord) \
+        .sel({"time": time_slice})
+    return ds
+
+
+def _fine_res_reference(
+    fine_res_path: str,
+    times: Sequence[datetime.datetime]
+):
+    mapper = open_fine_res_apparent_sources(fine_res_path)
+    times = [t.strftime(TIME_FMT) for t in times]
+    time_slice = slice(*[datetime.strptime(t, TIME_FMT) for t in time_bounds])
+    return utils.dataset_from_timesteps(
+        mapper, times, ["air_temperature", "specific_humidity"])
+
 
 if __name__ == "__main__":
     args = _create_arg_parser()
     if ".zarr" in args.data_path:
         ds = _open_zarr(args.data_paths, args.time_bounds, args.consolidated,)
+
+    fine_res = _fine_res_reference(args.fine_res_reference_path, ds.time.values)
     
-        
