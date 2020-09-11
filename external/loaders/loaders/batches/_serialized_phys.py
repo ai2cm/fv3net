e78cedcf92b82308a881b4cec7c73a650e5e40a1
Andre Perkins <frodre@gmail.com>
2020-09-11 01:46:57 +0000

    Completed flat item/slice loader from dataset sequence

--- external/loaders/loaders/batches/_serialized_phys.py
+++ external/loaders/loaders/batches/_serialized_phys.py
@@ -1,6 +1,10 @@
+import logging
 import xarray as xr
 
 from typing import Sequence, Union
+
+
+logger = logging.getLogger(__name__)
 
 
 class SerializedSequence(Sequence[xr.Dataset]):
@@ -8,9 +12,7 @@ class SerializedSequence(Sequence[xr.Dataset]):
     Create a sequence over savepoints for serialized physics data.
     """
 
-    def __init__(
-        self, xr_data: Union[xr.Dataset, xr.DataArray], item_dim: str = "savepoint"
-    ):
+    def __init__(self, xr_data: xr.Dataset, item_dim: str = "savepoint"):
         """
         Args
         ----
@@ -27,3 +29,110 @@ class SerializedSequence(Sequence[xr.Dataset]):
 
     def __len__(self):
         return self.data.sizes[self.item_dim]
+
+
+class FlatSerialSeq(SerializedSequence):
+    """
+    Flatten dimensions of sequence dataset item after retrieval
+    """
+
+    def __init__(
+        self,
+        serialized_seq: SerializedSequence,
+        dims_to_stack: Sequence[str],
+        dim_name: str = "sample",
+    ):
+        self.seq = serialized_seq
+        self.dims_to_stack = dims_to_stack
+        self.sample_dim_name = dim_name
+
+    def __getitem__(self, item):
+        ds = self.seq[item]
+        return self._flatten(ds)
+
+    def _flatten(self, ds: xr.Dataset):
+        # selection of single item removes sample dim from pool
+        dims_to_stack = [dim for dim in self.dims_to_stack if dim in ds.dims]
+        logging.debug(
+            f"Stacking dimensions into {self.sample_dim_name}: {dims_to_stack}"
+        )
+
+        stacked_sample_only = ds.stack({self.sample_dim_name: dims_to_stack})
+        flat_2d = _separate_by_extra_feature_dim(stacked_sample_only)
+        flat_2d = _check_sample_first(flat_2d, self.sample_dim_name)
+
+        return flat_2d
+
+
+def _separate_by_extra_feature_dim(ds):
+
+    """
+    The extra dimension is a tracer field.  Separate out into dataset vars.
+    Should only be called after sampling dims have been
+    """
+
+    for var, da in ds.items():
+        if da.ndim > 3:
+            raise ValueError(
+                "Expects at most 3 dimensions (sample, vertical, tracer_no)."
+            )
+        elif da.ndim == 3:
+            tracer_dim = _find_tracer_dim(da.dims)
+            logger.debug(
+                f"Separating variable ({var}) with extra feature dim ({tracer_dim})"
+            )
+            tracer_vars = {
+                f"{var}_{i}": da.isel({tracer_dim: i})
+                for i in range(da.sizes[tracer_dim])
+            }
+            ds = ds.drop(var)
+            ds.update(tracer_vars)
+
+    return ds
+
+
+def _find_tracer_dim(dims):
+    tracer_dims = [dim for dim in dims if "tracer" in dim]
+    if len(tracer_dims) > 1:
+        raise ValueError(
+            "Multiple tracer dimensions detected. Selection of single key is ambiguous."
+        )
+    else:
+        return tracer_dims[0]
+
+
+def _stack_extra_features(ds, sample_dim_name):
+
+    """
+    Need 2D variables for ML, but serialized turbulence had a tracer dim after
+    the vertical dimension.  This works for more than a single extra dimension.
+    Keeping around in case I want stack instead of
+    separation for these multi-feature variables
+    """
+
+    combined_feature_dims = {}
+    for var, da in ds.items():
+        if da.ndim > 2:
+            dims_to_stack = tuple(
+                sorted([dim for dim in da.dims if dim != sample_dim_name])
+            )
+            dim_name = combined_feature_dims.get(dims_to_stack, None)
+            if dim_name is None:
+                dim_name = f"stacked_feature{len(combined_feature_dims) + 1}"
+                combined_feature_dims[dims_to_stack] = dim_name
+
+            stack_map = {dim_name: dims_to_stack}
+            feature_stacked = da.stack(stack_map)
+            ds[var] = feature_stacked.drop(dim_name)
+
+    return ds
+
+
+def _check_sample_first(ds, sample_dim):
+    for var, da in ds.items():
+        if da.dims.index(sample_dim) != 0:
+            logger.debug(f"Transposing {var} to lead with sampling dimension")
+            da = da.transpose()
+            ds[var] = da
+
+    return ds
--- external/loaders/tests/test__serialized_phys.py
+++ external/loaders/tests/test__serialized_phys.py
@@ -2,25 +2,108 @@ import pytest
 import xarray as xr
 import numpy as np
 
-from loaders.batches._serialized_phys import SerializedSequence
+import loaders.batches._serialized_phys as sp
+
+SAMPLE_DIM_NAME = "sample"
 
 
 @pytest.fixture
 def xr_data():
+    data = np.random.randn(10, 15, 20, 8)
+    dims = ["savepoint", "horiz", "feature", "tracer"]
+    reg = xr.DataArray(data=data[:, :, :, 0], dims=dims[:3])
+    reg = reg.to_dataset(name="field")
+    reg["field_with_tracer"] = xr.DataArray(data=data, dims=dims)
+    return reg
 
-    data = np.random.randn(10, 15, 20, 25)
-    return xr.DataArray(data=data, dims=["savepoint", "rank", "horiz", "vert"])
+
+@pytest.mark.parametrize("idx", [0, -1])
+def test_Serilized_int_item(xr_data, idx):
+    seq = sp.SerializedSequence(xr_data=xr_data, item_dim="savepoint")
+    dat = seq[idx]
+    xr.testing.assert_equal(xr_data.isel(savepoint=idx), dat)
 
 
-@pytest.mark.parametrize("item_dim", ["savepoint", "rank", "horiz", "vert"])
-def test_SerializedSequence_int_item(xr_data, item_dim):
+@pytest.mark.parametrize("item_dim,", ["savepoint", "horiz"])
+def test_SerializedSequence_item_dim(xr_data, item_dim):
 
-    seq = SerializedSequence(xr_data, item_dim=item_dim)
+    seq = sp.SerializedSequence(xr_data, item_dim=item_dim)
     dat = seq[0]
     xr.testing.assert_equal(xr_data.isel({item_dim: 0}), dat)
 
 
+@pytest.mark.parametrize("selection_slice", [slice(3), slice(2, 4), slice(0, 6, 2)])
+def test_SerializedSequence_slice_item(xr_data, selection_slice):
+
+    seq = sp.SerializedSequence(xr_data, item_dim="savepoint")
+    dat = seq[selection_slice]
+    xr.testing.assert_equal(xr_data.isel({"savepoint": selection_slice}), dat)
+
+
 def test_SerializedSequence_len(xr_data):
 
-    seq = SerializedSequence(xr_data, item_dim="savepoint")
+    seq = sp.SerializedSequence(xr_data, item_dim="savepoint")
     assert len(seq) == 10
+
+
+def test__find_tracer_dim():
+
+    with pytest.raises(ValueError):
+        sp._find_tracer_dim(["tracer1", "tracer2"])
+
+    tracer_dim = sp._find_tracer_dim(["horiz", "tracer", "vert"])
+    assert tracer_dim == "tracer"
+
+
+@pytest.fixture()
+def tracer_dataset(xr_data):
+    return xr_data.stack({SAMPLE_DIM_NAME: ["savepoint", "horiz"]}).transpose()
+
+
+def test__separate_by_extra_feature_dim_all2d(tracer_dataset):
+
+    flattened = sp._separate_by_extra_feature_dim(tracer_dataset)
+    for da in flattened.values():
+        assert da.ndim <= 2
+
+
+def test__separate_by_extra_feature_dim_separated_var(tracer_dataset):
+
+    flattened = sp._separate_by_extra_feature_dim(tracer_dataset)
+
+    for i in range(tracer_dataset.dims["tracer"]):
+        separated_var = f"field_with_tracer_{i}"
+        assert separated_var in flattened
+        xr.testing.assert_equal(
+            tracer_dataset["field_with_tracer"].isel(tracer=i), flattened[separated_var]
+        )
+
+    assert "field_with_tracer" not in flattened
+
+
+def test_FlatSerialSeq(xr_data):
+    seq = sp.SerializedSequence(xr_data, item_dim="savepoint")
+    flat_seq = sp.FlatSerialSeq(seq, ["savepoint", "horiz"], dim_name=SAMPLE_DIM_NAME)
+
+    # single item
+    dat = flat_seq[0]
+    assert SAMPLE_DIM_NAME in dat.dims
+    assert dat.dims[SAMPLE_DIM_NAME] == 15  # single savepoint x 15 horizontal
+
+    for da in dat.values():
+        assert da.ndim <= 2
+
+    # slice
+    dat_slice = flat_seq[0:2]
+    assert dat_slice.dims[SAMPLE_DIM_NAME] == 30  # two savepoints x 15 horizontal
+
+
+def test__check_sample_first(tracer_dataset):
+    field = tracer_dataset.field.transpose()
+    # confirm sample not first
+    assert field.dims[0] != SAMPLE_DIM_NAME
+    tracer_dataset["field"] = field
+
+    res = sp._check_sample_first(tracer_dataset, SAMPLE_DIM_NAME)
+    for da in res.values():
+        assert da.dims[0] == SAMPLE_DIM_NAME
