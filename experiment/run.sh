f2dfe298b13da709abc04d2a889976bd0593acd5
Noah D. Brenowitz <nbren12@gmail.com>
2020-09-14 14:28:16 -0700

    Test off short sample
    Submit job
    
    train-diags-prog-wjp8h

--- experiment/run.sh
+++ experiment/run.sh
@@ -21,13 +21,15 @@ batch_kwargs:
   mapping_function: open_fine_res_apparent_sources
   mapping_kwargs:
     offset_seconds: 450
+    rename_vars:
+      delp: pressure_thickness_of_atmospheric_layer
 EOF
 
 cat << EOF > prognostic-run.yaml
 base_version: v0.5
 namelist:
   coupler_nml:
-    days: 10
+    days: 1
     hours: 0
     minutes: 0
     seconds: 0
@@ -44,14 +46,14 @@ EOF
 
 argo submit \
     --from workflowtemplate/train-diags-prog \
-    -p image-tag=dbf8db14a939ea59f460345e65cbe46c03c2c1f6 \
+    -p image-tag=bdafdfb3ee3c4a7e15849b01aefc28185c69defc \
     -p root=gs://vcm-ml-experiments/$EXPERIMENT \
     -p train-test-data=gs://vcm-ml-experiments/2020-07-30-fine-res \
     -p training-config="$(< training-config.yaml)" \
     -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
     -p initial-condition="20160805.000000" \
     -p prognostic-run-config="$(< prognostic-run.yaml)" \
-    -p train-times="$(<  ../train.json)" \
-    -p test-times="$(<  ../test.json)" \
+    -p train-times="$(<  ../train_short.json)" \
+    -p test-times="$(<  ../test_short.json)" \
     -p public-report-output=gs://vcm-ml-public/offline_ml_diags/$EXPERIMENT
 
