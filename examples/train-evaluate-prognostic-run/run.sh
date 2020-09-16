7417f770eeed608358030c244ace3c9fa48ad05a
Noah D. Brenowitz <nbren12@gmail.com>
2020-09-16 15:35:04 -0700

    Point to branch with ignore physics
    train-diags-prog-296fn

--- examples/train-evaluate-prognostic-run/run.sh
+++ examples/train-evaluate-prognostic-run/run.sh
@@ -6,7 +6,7 @@ EXPERIMENT=2020-09-03-ignore-physics
 
 argo submit \
     --from workflowtemplate/train-diags-prog \
-    -p image-tag=bdafdfb3ee3c4a7e15849b01aefc28185c69defc \
+    -p image-tag=dbf8db14a939ea59f460345e65cbe46c03c2c1f6 \
     -p root=gs://vcm-ml-scratch/noah/$EXPERIMENT \
     -p train-test-data=gs://vcm-ml-experiments/2020-06-17-triad-round-1/nudging/nudging/outdir-3h \
     -p training-config="$(< training-config.yaml)" \
