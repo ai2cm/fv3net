6f8b67de7bea5950092a8ccccb6114feca931b30
Noah D Brenowitz <nbren12@gmail.com>
2020-09-16 15:40:24 -0700

    Remove --no-train-subdir-append flag
    Since we deprecated the one step jobs, the `--no-train-subdir-append` should no longer be necessary. Every current training dataset/mapper expects data without `train/` appended to the path. This PR makes this behavior the default and removes this flag. This simplifies the configuration/running of the training.
    
    Added public API:
    - remove `--no-train-subdir-append` flag to `fvfit.sklearn` and `fv3fit.keras`. This is now the default behavior.

--- external/fv3fit/fv3fit/_shared/utils.py
+++ external/fv3fit/fv3fit/_shared/utils.py
@@ -1,14 +1,10 @@
-import os
 from typing import List
 
 
 def parse_data_path(args):
     # allows the data path to be provided as a sequence of urls,
     # which is useful for hybrid training data
-    if not args.no_train_subdir_append:
-        data_path = [os.path.join(path, "train") for path in args.train_data_path]
-    else:
-        data_path = args.train_data_path
+    data_path = args.train_data_path
     if isinstance(args.train_data_path, List) and len(data_path) == 1:
         return data_path[0]
     else:
