7c4d1e0bc6c78fef3b99b358f7fe4690d2217527
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-09-16 20:13:28 +0000

    Fix nudge to obs unit tests

--- external/fv3kube/tests_fv3kube/test_nudge_to_obs.py
+++ external/fv3kube/tests_fv3kube/test_nudge_to_obs.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import fv3kube
 
 
@@ -11,23 +12,21 @@ def test_enable_nudge_to_observations_namelist_options():
     duration = timedelta(days=10)
     output = fv3kube.enable_nudge_to_observations(duration, current_date)
     assert output["namelist"]["fv_core_nml"]["nudge"]
+    assert "url" in output["gfs_analysis_data"]
+    assert "filename_pattern" in output["gfs_analysis_data"]
 
 
-def test_enable_nudge_to_observations_adds_filelist_asset():
+def test_enable_nudge_to_observations_adds_nudging_asset():
     current_date = [2016, 1, 2, 1, 0, 0]
-    duration = timedelta(days=10)
+    duration = timedelta(days=1)
+    url = "/path/to/nudging/files"
+    pattern = "%Y%m%d_%HZ_T85LR.nc"
 
     file_list_path = "asdf"
 
     output = fv3kube.enable_nudge_to_observations(
-        duration, current_date, file_list_path=file_list_path
+        duration, current_date, nudge_url=url, nudge_filename_pattern=pattern
     )
 
-    # check patch files
     patch_files = output["patch_files"]
-    # search for filelist asset
-    for asset in patch_files:
-        if asset["target_name"] == file_list_path:
-            found = asset
-
-    assert "bytes" in found
+    assert any(asset["target_name"] == "20160102_00Z_T85LR.nc" for asset in patch_files)
