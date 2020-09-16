3b010711ac5939d35f5d69ce585f48b0dfb88c65
Noah D. Brenowitz <nbren12@gmail.com>
2020-09-16 12:09:46 -0700

    remove heredocs

--- experiment/run.sh
+++ experiment/run.sh
@@ -4,47 +4,6 @@ set -e
 
 EXPERIMENT=2020-09-03-ignore-physics
 
-cat << EOF > training-config.yaml
-model_type: sklearn_random_forest
-hyperparameters:
-  max_depth: 13
-  n_estimators: 1
-input_variables:
-- air_temperature
-- specific_humidity
-output_variables:
-- dQ1
-- dQ2
-batch_function: batches_from_geodata
-batch_kwargs:
-  timesteps_per_batch: 10
-  mapping_function: open_fine_res_apparent_sources
-  mapping_kwargs:
-    offset_seconds: 450
-    rename_vars:
-      delp: pressure_thickness_of_atmospheric_layer
-      grid_xt: x
-      grid_yt: y
-      pfull: z
-EOF
-
-cat << EOF > prognostic-run.yaml
-base_version: v0.5
-namelist:
-  coupler_nml:
-    days: 1
-    hours: 0
-    minutes: 0
-    seconds: 0
-  diag_manager_nml:
-    flush_nc_files: true
-  fv_core_nml:
-    do_sat_adj: false
-  gfdl_cloud_microphysics_nml:
-    fast_sat_adj: false
-EOF
-
-
 # https://github.com/VulcanClimateModeling/fv3net/pull/615
 
 argo submit \
