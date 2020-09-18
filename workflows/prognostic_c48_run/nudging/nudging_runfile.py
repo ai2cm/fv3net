05fc9aca75023d98e79e679673ec0d21e1a9a430
Anna Kwa <annak@vulcan.com>
2020-09-18 03:32:15 +0000

    Fix data type

--- workflows/prognostic_c48_run/nudging/nudging_runfile.py
+++ workflows/prognostic_c48_run/nudging/nudging_runfile.py
@@ -112,14 +112,14 @@ def append_key_label(d, suffix):
 
 
 def sst_from_reference(
-    reference_surface_temperature: xr.DataArray,
-    surface_temperature: xr.DataArray,
-    land_sea_mask: xr.DataArray,
-) -> xr.DataArray:
+    reference_surface_temperature: fv3gfs.util.Quantity,
+    surface_temperature: fv3gfs.util.Quantity,
+    land_sea_mask: fv3gfs.util.Quantity,
+) -> np.ndarray:
     return xr.where(
-        land_sea_mask == 0,
-        reference_surface_temperature,
-        surface_temperature).values
+        land_sea_mask.data_array == 0,
+        reference_surface_temperature.data_array,
+        surface_temperature.data_array).values
 
 
 def column_integrated_moistening(
@@ -320,7 +320,6 @@ if __name__ == "__main__":
                 tendencies["specific_humidity_tendency_due_to_nudging"],
                 timestep,
             )
-
         state[SST_NAME].view[:] = sst_from_reference(
             reference[SST_NAME], state[SST_NAME], state["land_sea_mask"])
         updated_state_members = {key: state[key] for key in updated_quantity_names}
