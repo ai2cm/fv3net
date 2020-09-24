99001bc616bcdf19625fc87f461f147af0342240
Andre Perkins <frodre@gmail.com>
2020-09-24 21:04:24 +0000

    First try at emulation main

--- external/fv3fit/fv3fit/emulation/__main__.py
+++ external/fv3fit/fv3fit/emulation/__main__.py
@@ -1,4 +1,69 @@
-import argparse
+"""
+k8s entrypoint for emulation
+"""
 
-args = argparse.ArgumentParser()
-args.parse_args()
+import argparse
+import logging
+import tensorflow as tf
+from pathlib import Path
+
+from fv3fit.keras import get_model
+from fv3fit._shared import load_model_training_config
+from loaders.batches import batches_from_serialized
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "train_data_path",
+        type=str,
+        help="Location of serialized physics data to train on",
+    )
+    parser.add_argument(
+        "train_config_file",
+        type=str,
+        help="Local path for training configuration yaml file",
+    )
+    parser.add_argument(
+        "output_data_path", type=str, help="Location to save config and trained model"
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger("fsspec").setLevel(logging.INFO)
+    logging.getLogger("urllib3.connectionpool").setLevel(logging.INFO)
+
+    config = load_model_training_config(args.train_config_file)
+
+    train_range = config.batch_kwargs.get("train_range", (None,))
+    batches = batches_from_serialized(args.train_data_path)
+    train = batches[slice(*train_range)]
+
+    lr = config.hyperparameters.get("learning_rate", 0.0001)
+    if lr == "exponential":
+        lr = tf.keras.optimizers.schedules.ExponentialDecay(0.001, 10_000, 0.96)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+
+    # TODO is var name handling okay?
+    sample = train[0]
+    input_vars = [var for var in sample if "input" in var]
+    output_vars = [var for var in sample if "output" in var]
+
+    fit_kwargs = config.hyperparameters.pop("fit_kwargs", {})
+    model = get_model(
+        config.model_type,
+        "sample",
+        input_vars if config.input_variables is None else config.input_variables,
+        output_vars if config.output_variables is None else config.output_variables,
+        **config.hyperparameters,
+    )
+    model.fit(train, **fit_kwargs)
+
+    model_output_path = Path(args.output_data_path, "keras_model")
+    model.dump(str(model_output_path.resolve()))
