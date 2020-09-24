afbf5ff215bc4f0731640aee1cf7f9df1d40c385
Noah D. Brenowitz <nbren12@gmail.com>
2020-09-24 14:45:34 -0700

    Start example prognostic run configuration

---
+++ examples/prognostic-run/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e 
+
+output=gs://vcm-ml-scratch/noah/test-free
+gsutil cp diag_Table $output/diag_table
+
+kubectl apply -k ../train-evaluate-prognostic-run/fv3net/workflows/argo
+argo submit argo.yaml -p diag_table=$output/diag_table -p output=$output
