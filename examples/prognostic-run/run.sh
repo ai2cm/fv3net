1f201dfdfaf919b1493f16ff844c1cea91581fc9
Noah D. Brenowitz <nbren12@gmail.com>
2020-09-24 15:27:57 -0700

    Run a test baseline run
    baseline-run-nxcmn

--- examples/prognostic-run/run.sh
+++ examples/prognostic-run/run.sh
@@ -3,7 +3,7 @@
 set -e 
 
 output=gs://vcm-ml-scratch/noah/test-free
-gsutil cp diag_Table $output/diag_table
+gsutil cp diag_table $output/diag_table
 
 kubectl apply -k ../train-evaluate-prognostic-run/fv3net/workflows/argo
 argo submit argo.yaml -p diag_table=$output/diag_table -p output=$output
