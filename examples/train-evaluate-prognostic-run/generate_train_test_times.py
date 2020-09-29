a8229fa4581ba3ccabef5e0248de1be28fbf1520
Noah D. Brenowitz <nbren12@gmail.com>
2020-09-28 18:20:32 -0700

    Production run
    - increased number of time steps for training
    - 40 days run
    - output: gs://vcm-ml-experiments/noah/2020-09-25-physics-on-free-debug
    - argo job: train-diags-prog-74wl6

--- examples/train-evaluate-prognostic-run/generate_train_test_times.py
+++ examples/train-evaluate-prognostic-run/generate_train_test_times.py
@@ -5,11 +5,11 @@ url = "gs://vcm-ml-archive/prognostic_runs/2020-09-25-physics-on-free/"
 data= loaders.mappers.open_baseline_emulator(url)
 times = sorted(data)
 
-# train_times = times[::5]
-# test_times = times[2::5]
+train_times = times[::5]
+test_times = times[2::5]
 
-train_times = times[:2]
-test_times = times[2:4]
+# train_times = times[:2]
+# test_times = times[2:4]
 
 with open("train.json", "w") as f:
     json.dump(train_times, f)
--- examples/train-evaluate-prognostic-run/run.sh
+++ examples/train-evaluate-prognostic-run/run.sh
@@ -7,7 +7,7 @@ EXPERIMENT=2020-09-25-physics-on-free-debug
 argo submit \
     --from workflowtemplate/train-diags-prog \
     -p image-tag=d51feb8cbc0acf6bad6dea84c3bb4a3f0accdf8e \
-    -p root=gs://vcm-ml-scratch/noah/$EXPERIMENT \
+    -p root=gs://vcm-ml-experiments/noah/$EXPERIMENT \
     -p train-test-data=gs://vcm-ml-archive/prognostic_runs/2020-09-25-physics-on-free/ \
     -p training-config="$(< training-config.yaml)" \
     -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
@@ -15,6 +15,6 @@ argo submit \
     -p prognostic-run-config="$(< prognostic-run.yaml)" \
     -p train-times="$(<  train.json)" \
     -p test-times="$(<  test.json)" \
-    -p public-report-output=gs://vcm-ml-scratch/noah/offline_ml_diags/$EXPERIMENT \
-    -p segment-count=1
+    -p public-report-output=gs://vcm-ml-public/offline_ml_diags/$EXPERIMENT \
+    -p segment-count=4
 
