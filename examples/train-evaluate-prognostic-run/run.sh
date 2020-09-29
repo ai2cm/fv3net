6df2d498613a9b68a1fade91001bb05639fce402
Noah D. Brenowitz <nbren12@gmail.com>
2020-09-28 18:13:11 -0700

    Don't predict microphysics
    job: train-diags-prog-vs7fk

--- examples/train-evaluate-prognostic-run/run.sh
+++ examples/train-evaluate-prognostic-run/run.sh
@@ -6,7 +6,7 @@ EXPERIMENT=2020-09-25-physics-on-free-debug
 
 argo submit \
     --from workflowtemplate/train-diags-prog \
-    -p image-tag=4cbe9d6f9df349afa8e376e094be5d1ff6a0c7c3 \
+    -p image-tag=d51feb8cbc0acf6bad6dea84c3bb4a3f0accdf8e \
     -p root=gs://vcm-ml-scratch/noah/$EXPERIMENT \
     -p train-test-data=gs://vcm-ml-archive/prognostic_runs/2020-09-25-physics-on-free/ \
     -p training-config="$(< training-config.yaml)" \
