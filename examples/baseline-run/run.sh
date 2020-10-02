3045b3cbbeb8ca40dd5024ab698a91a7fa78539d
Noah D. Brenowitz <nbren12@gmail.com>
2020-10-01 18:08:46 -0700

    Run baseline simulation with physics getters/setters
    argo: baseline-run-6v44s
    url: gs://vcm-ml-archive/prognostic_runs/2020-10-01-getters-for-DeepConv

--- examples/baseline-run/run.sh
+++ examples/baseline-run/run.sh
@@ -2,8 +2,10 @@
 
 set -e 
 
-output=gs://vcm-ml-archive/prognostic_runs/$(date +%F)-physics-on-free
+output=gs://vcm-ml-archive/prognostic_runs/$(date +%F)-getters-for-DeepConv
 gsutil cp diag_table $output/diag_table
 
 kubectl apply -k ../train-evaluate-prognostic-run/fv3net/workflows/argo
 argo submit argo.yaml -p diag_table=$output/diag_table -p output=$output
+
+echo "Output location: $output"
