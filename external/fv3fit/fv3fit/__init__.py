874df84bb64a6d475de05513f8ad18037a334f94
Jeremy McGibbon <jeremym@vulcan.com>
2020-10-08 16:03:51 -0700

    add RecurrentModel and ExternalModel to loadable model types

--- external/fv3fit/fv3fit/__init__.py
+++ external/fv3fit/fv3fit/__init__.py
@@ -2,4 +2,7 @@ from ._shared import Predictor, ArrayPacker
 from . import keras
 from . import sklearn
 
-PRODUCTION_MODEL_TYPES = {"sklearn": ["random_forest"], "keras": ["DenseModel"]}
+PRODUCTION_MODEL_TYPES = {
+    "sklearn": ["random_forest"],
+    "keras": ["DenseModel", "RecurrentModel", "ExternalModel"],
+}
--- external/fv3fit/fv3fit/keras/_models/__init__.py
+++ external/fv3fit/fv3fit/keras/_models/__init__.py
@@ -1,2 +1,4 @@
 from .models import DenseModel
 from .testing import DummyModel
+from .recurrent import RecurrentModel
+from .external import ExternalModel
