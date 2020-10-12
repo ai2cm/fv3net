d29506c61c746ec7ffbae39657dd2a4ed734ac6a
Anna Kwa <annak@vulcan.com>
2020-10-12 22:49:30 +0000

    fix diagnostics in prog config

--- experiment/test.sh
+++ experiment/test.sh
@@ -3,22 +3,22 @@ set -e
 
 
 IMAGE_TAG=42e3dbcc5995c4dce9c71c93ff60c489ba0245b7
-EXPERIMENT=2020-10-09-nn-random-seed-test
+EXPERIMENT=2020-10-12-nn-random-seed-test
 mkdir -p configs
 
 kubectl apply -k fv3net/workflows/argo
 
-for i in {0..3}
+for i in {0..0}
 do
-  sed '/random_seed/d' configs/train_config.yml > configs/test_altered_config_seed_$i.yml
-  echo "random_seed: $i" >> configs/test_altered_config_seed_$i.yml
+  sed '/random_seed/d' configs/train_config.yml > configs/test_config_seed_$i.yml
+  echo "random_seed: $i" >> configs/test_config_seed_$i.yml
   argo submit \
     --from workflowtemplate/train-diags-prog \
     -p image-tag=$IMAGE_TAG \
     -p root=gs://vcm-ml-scratch/annak/$EXPERIMENT/seed-$i \
     -p initial-condition="20160805.000000" \
     -p train-test-data='gs://vcm-ml-experiments/2020-09-24-tke-edmf-nudge-to-fine2' \
-    -p training-config="$(< configs/test_altered_config_seed_$i.yml)" \
+    -p training-config="$(< configs/test_config_seed_$i.yml)" \
     -p train-routine="keras" \
     -p train-times="$(< train_short.json)" \
     -p test-times="$(< test_short.json)" \
