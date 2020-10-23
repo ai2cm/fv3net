3a124e299a4c8d411e394306e296247654ebec51
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-10-23 16:56:24 +0000

    Lint shell script

--- workflows/prognostic_run_diags/entrypoint_diags.sh
+++ workflows/prognostic_run_diags/entrypoint_diags.sh
@@ -6,14 +6,14 @@ run=$1
 output=$2
 flags=$3
 
-cacheURL=gs://vcm-ml-archive/prognostic_run_diags
-cacheKey=`echo $run | cut -c6- | sed 's/\//-/g'`
+cacheKey=$(echo "$run" | cut -c6- | sed 's/\//-/g')
+cacheURL=gs://vcm-ml-archive/prognostic_run_diags/$cacheKey
 
 # check for existence of diagnostics and metrics in cache
-gsutil -q stat $cacheURL/$cacheKey/diags.nc
-diagsExitCode=`echo $?`
-gsutil -q stat $cacheURL/$cacheKey/metrics.json
-metricsExitCode=`echo $?`
+gsutil -q stat "$cacheURL/diags.nc"
+diagsExitCode=$?
+gsutil -q stat "$cacheURL/metrics.json"
+metricsExitCode=$?
 
 set -e
 
@@ -23,9 +23,9 @@ else
     echo "No prognostic run diagnostics detected in cache for given run. Computing diagnostics and adding to cache."	
     python save_prognostic_run_diags.py $flags $run diags.nc
     python metrics.py diags.nc > metrics.json
-    gsutil cp diags.nc $cacheURL/$cacheKey/diags.nc
-    gsutil cp metrics.json $cacheURL/$cacheKey/metrics.json	
+    gsutil cp diags.nc "$cacheURL/diags.nc"
+    gsutil cp metrics.json "$cacheURL/metrics.json"
 fi
 
-gsutil cp $cacheURL/$cacheKey/diags.nc $output/diags.nc	
-gsutil cp $cacheURL/$cacheKey/metrics.json $output/metrics.json
+gsutil cp "$cacheURL/diags.nc" "$output/diags.nc"
+gsutil cp "$cacheURL/metrics.json" "$output/metrics.json"
