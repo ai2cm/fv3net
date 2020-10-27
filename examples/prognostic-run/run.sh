2c10150f9cb0fff266365ab77841bebc621b1d3f
Noah D. Brenowitz <nbren12@gmail.com>
2020-10-26 18:20:35 -0700

    Full experiment

--- examples/prognostic-run/run.sh
+++ examples/prognostic-run/run.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-# adjust experiment name and replace "vcm-ml-scratch" with "vcm-ml-experiments"
-# under output after debugging configuration
-EXPERIMENT=fill_in_here
-
 argo submit \
     --from workflowtemplate/prognostic-run \
-    -p image-tag=4a8fc6aa3337d51967d5483695be582dd09561db \
-    -p output=gs://vcm-ml-scratch/$EXPERIMENT \
-    -p trained-ml=gs://vcm-ml-scratch/test-end-to-end-integration/integration-test-ffca74392a57/trained_model \
+    -p image-tag=dcee99f1378997d652995a3a653c01b3c638f0e7 \
+    -p output=gs://vcm-ml-experiments/noah/prognostic_runs/2020-10-26-triggered-regressor \
+    -p trained-ml=gs://vcm-ml-archive/noah/emulator/2020-10-16-triggered-regressor \
     -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
     -p initial-condition="20160805.000000" \
     -p config="$(< prognostic-run.yaml)" \
-    -p segment-count=1
+    -p segment-count=4
