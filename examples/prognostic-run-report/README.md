3970a08fb140ee3ea5dca9e97c46fa6c699cdbd3
Noah D Brenowitz <nbren12@gmail.com>
2020-10-27 09:37:13 -0700

    Add prognostic run report example (#68)
    * Add prognostic run report example
    
    * Add README
    
    * Change name to workflowtemplate

---
+++ examples/prognostic-run-report/README.md
@@ -0,0 +1,5 @@
+# Prognostic run report example
+
+See [documentation](https://github.com/VulcanClimateModeling/fv3net/tree/master/workflows/argo#prognostic-run-report).
+
+This requires the prognostic-run-diags workflow template to be installed in the cluster.
---
+++ examples/prognostic-run-report/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+name=$(date +%Ft%H%M%S)-$(openssl rand -hex 6)
+argo submit --from=workflowtemplate/prognostic-run-diags \
+    --name $name \
+    -p docker-image=us.gcr.io/vcm-ml/fv3net:9ca9d9bd4bf37b2b4ffcdc195dcae63580f8694e \
+    -p runs="$(< rundirs.json)" \
+    -p make-movies=true
