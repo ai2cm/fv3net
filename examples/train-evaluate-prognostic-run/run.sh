87d75f4ff3294d12113d0b97a1425f677195aa31
brianhenn <brianhenn@gmail.com>
2020-10-27 19:04:37 +0000

    train-diags-prog config set

--- examples/train-evaluate-prognostic-run/run.sh
+++ examples/train-evaluate-prognostic-run/run.sh
@@ -2,19 +2,26 @@
 
 set -e
 
-EXPERIMENT=2020-09-03-ignore-physics
+EXPERIMENT=2020-10-26-n2f-timescale-sensitivity
+TIMESCALES=(1, 3, 6, 10)
+RANDOM=$(openssl rand --hex 6)
 
-argo submit \
-    --from workflowtemplate/train-diags-prog \
-    -p image-tag=bdafdfb3ee3c4a7e15849b01aefc28185c69defc \
-    -p root=gs://vcm-ml-experiments/$EXPERIMENT \
-    -p train-test-data=gs://vcm-ml-experiments/2020-07-30-fine-res \
-    -p training-config="$(< training-config.yaml)" \
-    -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
-    -p initial-condition="20160805.000000" \
-    -p prognostic-run-config="$(< prognostic-run.yaml)" \
-    -p train-times="$(<  ../../train_short.json)" \
-    -p test-times="$(<  ../../test_short.json)" \
-    -p public-report-output=gs://vcm-ml-public/offline_ml_diags/$EXPERIMENT \
-    -p segment-count=1
+for TIMESCALE in ${TIMESCALES[@]}; do
+    echo "${TIMESCALE} hr timescale"
+    argo submit \
+        --from workflowtemplate/train-diags-prog \
+        -p image-tag="05b206107ed15178b97bc0dd3622e5918151b47f" \
+        -p root="gs://vcm-ml-experiments/$EXPERIMENT/timescale-${TIMESCALE}hr" \
+        -p train-test-data="gs://vcm-ml-experiments/${EXPERIMENT}/nudge-to-fine-runs/timescale-${TIMESCALE}hr" \
+        -p training-config="$(< training-config.yaml)" \
+        -p reference-restarts="gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts" \
+        -p initial-condition="20160805.000000" \
+        -p prognostic-run-config="$(< prognostic-run.yaml)" \
+        -p train-times="$(<  ../../train.json)" \
+        -p test-times="$(<  ../../test.json)" \
+        -p public-report-output="gs://vcm-ml-public/offline_ml_diags/$EXPERIMENT/timescale-${TIMESCALE}hr" \
+        -p segment-count=5 \
+        --name "${EXPERIMENT}-train-diags-prog-${RANDOM}"
+done ;
+        
 
