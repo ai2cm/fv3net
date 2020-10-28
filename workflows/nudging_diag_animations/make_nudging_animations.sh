2969980da6870394a7b03d40b25a069dfd96e641
brianhenn <brianhenn@gmail.com>
2020-10-28 05:46:21 +0000

    fix upload script

--- workflows/nudging_diag_animations/make_nudging_animations.sh
+++ workflows/nudging_diag_animations/make_nudging_animations.sh
@@ -7,5 +7,5 @@ REFERENCE_RESTART=$2
 REFERENCE_PHYSICS=$3
 OUTPUT=$4
 
-python make_nudging_animations.py $NUDGING_ROOT $REFERENCE_RESTART $REFERENCE_PHYSICS /tmp/animations
-gsutil cp /tmp/animations/*.mp4 $OUTPUT
+python make_nudging_animations.py $NUDGING_ROOT $REFERENCE_RESTART $REFERENCE_PHYSICS /tmp
+gsutil cp /tmp/*.mp4 $OUTPUT
