914145845ea4a38492ea5b98b4a3d3e3ccc5d265
Andre Perkins <frodre@gmail.com>
2020-10-28 22:28:31 +0000

    Add dense regressor w/ embedded classification

--- external/fv3fit/fv3fit/keras/_models/classifiers.py
+++ external/fv3fit/fv3fit/keras/_models/classifiers.py
@@ -1,10 +1,13 @@
+import logging
 import tensorflow as tf
 import xarray as xr
 import numpy as np
-from typing import Sequence, Optional, Any, Union
+from typing import Sequence, Optional, Any, Union, Mapping
 
 from .models import DenseModel
 from ._sequences import _XyArraySequence, _TargetToBool, _BalanceNegativeSkewBinary
+
+logger = logging.getLogger(__name__)
 
 
 class DenseClassifierModel(DenseModel):
@@ -82,3 +85,84 @@ class DenseClassifierModel(DenseModel):
                 max_queue_size=max_queue_size,
                 **fit_kwargs,
             )
+
+
+def get_feature_slices(packer):
+
+    slices = {}
+    start = 0
+    for pack_name in packer.pack_names:
+        n_features = packer.feature_counts[pack_name]
+        slices[pack_name] = slice(start, start + n_features)
+        start += n_features
+
+    return slices
+
+
+def _get_mask(x):
+
+    non_zero = tf.math.greater_equal(x, tf.zeros_like(x))
+    mask = tf.where(non_zero, x=tf.ones_like(x), y=tf.zeros_like(x))
+
+    return mask
+
+
+def separate_tensor_by_var(tensor, feature_dim_slices):
+
+    # assumes sample dim leading
+    sep = {
+        var_name: tensor[slice(None), feature_dim_slices[var_name]]
+        for var_name in feature_dim_slices
+    }
+
+    return sep
+
+
+class DenseWithClassifier(DenseModel):
+    def __init__(
+        self, *args, classifiers: Mapping[str, DenseClassifierModel] = None, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+
+        if classifiers is None:
+            self._classifiers = {}
+            logger.warning("DenseWithClassifier initiwalized without any classiefiers")
+        else:
+            self._classifiers = classifiers
+
+    def get_model(self, n_features_in: int, n_features_out: int) -> tf.keras.Model:
+
+        out_tensor_slices = get_feature_slices(self.y_packer)
+
+        inputs = tf.keras.Input(n_features_in)
+        x = self.X_scaler.normalize_layer(inputs)
+        for i in range(self._depth - 1):
+            x = tf.keras.layers.Dense(
+                self._width, activation=tf.keras.activations.relu
+            )(x)
+        x = tf.keras.layers.Dense(n_features_out)(x)
+        regr_outputs = self.y_scaler.denormalize_layer(x)
+
+        regr_outputs_by_var = separate_tensor_by_var(regr_outputs, out_tensor_slices)
+
+        to_combine = []
+        for var, out_tensor in regr_outputs_by_var.items():
+            if var in self._classifiers:
+                classified = self._classifiers[var]._model(inputs)
+                mask = tf.keras.layers.Lambda(_get_mask)(classified)
+                filtered = tf.keras.layers.Multiply()([out_tensor, mask])
+                to_combine.append(filtered)
+            else:
+                to_combine.append(out_tensor)
+
+        output = tf.concat(to_combine, 1)
+
+        model = tf.keras.Model(
+            inputs=inputs, outputs=output, name=self.__class__.__name__
+        )
+        model.compile(
+            optimizer=self._optimizer,
+            loss=self.loss,
+            metrics=[tf.keras.metrics.MeanAbsoluteError()],
+        )
+        return model
