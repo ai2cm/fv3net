1e0f4531583bc583f6831f6223fbc6aac3dff5ae
Anna Kwa <annak@vulcan.com>
2020-10-29 21:37:00 +0000

    add setup.py

---
+++ external/derived/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="derived",
+    version="0.1.0",
+    python_requires=">=3.7.0",
+    author="Anna Kwa",
+    author_email="annak@vulcan.com",
+    packages=find_packages(),
+    package_dir={"": "."},
+    package_data={},
+    install_requires=[
+        "fsspec>=0.7.4",
+        "xarray>=0.15.1",
+        "vcm",
+        "fv3gfs.util"
+    ],
+    dependency_links=["../vcm", "../fv3gfs-util"],
+)
