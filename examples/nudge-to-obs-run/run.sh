f6b38592f9d84f8f762d741d73dd8f0183b39ee3
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-10-31 01:08:00 +0000

    Update configs for 2-year n2o run

--- examples/nudge-to-obs-run/run.sh
+++ examples/nudge-to-obs-run/run.sh
@@ -4,17 +4,17 @@ set -e
 
 # adjust experiment name and replace "vcm-ml-scratch" with "vcm-ml-experiments"
 # under output-url after debugging configuration
-EXPERIMENT=fill_in_here
-IMAGE_TAG=4a8fc6aa3337d51967d5483695be582dd09561db
+EXPERIMENT=2020-10-30-nudge-to-obs-GRL-paper/nudge-to-obs-run
+IMAGE_TAG=12b6689ee53d1103ece4b8944ad8546861c2e1d6
 
-gsutil cp diag_table gs://vcm-ml-experiments/diag_tables/nudge_to_obs_5h/v1.0/diag_table
+gsutil cp diag_table gs://vcm-ml-experiments/diag_tables/nudge_to_obs_5h/v1.2/diag_table
 argo submit \
     --from workflowtemplate/nudge-to-obs \
     -p fv3net-image=us.gcr.io/vcm-ml/fv3net:${IMAGE_TAG} \
     -p post-process-image=us.gcr.io/vcm-ml/post_process_run:${IMAGE_TAG} \
     -p fv3gfs-image=us.gcr.io/vcm-ml/prognostic_run:${IMAGE_TAG} \
-    -p output-url=gs://vcm-ml-scratch/$EXPERIMENT \
+    -p output-url=gs://vcm-ml-experiments/$EXPERIMENT \
     -p nudging-config="$(< nudge-to-obs-run.yaml)" \
     -p cpu="24" \
     -p memory="20Gi" \
-    -p segment-count=1
+    -p segment-count=37
