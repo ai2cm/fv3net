0d2db836ec97c1d567f9820351adec2893054c1e
Anna Kwa <annak@vulcan.com>
2020-11-05 22:05:03 +0000

    add dQu, dQv to derived var register

--- external/vcm/vcm/derived_mapping.py
+++ external/vcm/vcm/derived_mapping.py
@@ -59,3 +59,45 @@ def cos_zenith_angle(self):
 def evaporation(self):
     lhf = self["latent_heat_flux"]
     return vcm.thermo.latent_heat_flux_to_evaporation(lhf)
+
+
+@DerivedMapping.register("dQu")
+def dQu(self):
+    # try/except is a placeholder for future PR to add keys to
+    # DerivedMapping. This is currently blocked because the FV3
+    # wrapper needs a function to get available field names.
+    try:
+        return self["dQu"]
+    except(KeyError):
+        wind_rotation_matrix = self.dataset(
+            [
+                "eastward_wind_u_coeff",
+                "eastward_wind_v_coeff",
+                "northward_wind_u_coeff",
+                "northward_wind_v_coeff",
+            ]
+        )
+        return vcm.cubedsphere.center_and_rotate_xy_winds(
+            wind_rotation_matrix, self["dQxwind"], self["dQywind"]
+        )[0]
+
+
+@DerivedMapping.register("dQv")
+def dQv(self):
+    # try/except is a placeholder for future PR to add keys to
+    # DerivedMapping. This is currently blocked because the FV3
+    # wrapper needs a function to get available field names.
+    try:
+        return self["dQv"]
+    except(KeyError):
+        wind_rotation_matrix = self.dataset(
+            [
+                "eastward_wind_u_coeff",
+                "eastward_wind_v_coeff",
+                "northward_wind_u_coeff",
+                "northward_wind_v_coeff",
+            ]
+        )
+        return vcm.cubedsphere.center_and_rotate_xy_winds(
+            wind_rotation_matrix, self["dQxwind"], self["dQywind"]
+        )[1]
