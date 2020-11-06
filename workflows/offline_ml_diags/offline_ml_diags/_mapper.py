1e1c268421eec4e1083261297ec7c7b5af3a26d1
Anna Kwa <annak@vulcan.com>
2020-11-06 22:29:39 +0000

    Use DerivedMapping in offline diags

--- workflows/offline_ml_diags/offline_ml_diags/_mapper.py
+++ workflows/offline_ml_diags/offline_ml_diags/_mapper.py
@@ -1,6 +1,6 @@
 from typing import Mapping, Union
 
-from vcm import safe, cast_to_datetime, cos_zenith_angle
+from vcm import safe, DerivedMapping
 import xarray as xr
 
 from fv3fit.sklearn import SklearnWrapper
@@ -21,29 +21,19 @@ class PredictionMapper(GeoMapper):
         wrapped_model: Predictor,
         z_dim: str = "z",
         rename_vars: Mapping[str, str] = None,
-        cos_z_var: str = None,
         grid: xr.Dataset = None,
     ):
         self._base_mapper = base_mapper
         self._model = wrapped_model
         self._z_dim = z_dim
-        self._cos_z_var = cos_z_var
         self._grid = grid
         self.rename_vars = rename_vars or {}
+        self._input_variables = self._model.input_variables
+        self._output_variables = self._model.output_variables
 
     def _predict(self, ds: xr.Dataset) -> xr.Dataset:
         output = self._model.predict_columnwise(ds, feature_dim=self._z_dim)
         return output.rename(self.rename_vars)
-
-    def _insert_cos_zenith_angle(self, time_key: str, ds: xr.Dataset) -> xr.Dataset:
-        time = cast_to_datetime(time_key)
-        if self._grid is not None:
-            cos_z = cos_zenith_angle(time, self._grid["lon"], self._grid["lat"])
-            return ds.assign(
-                {self._cos_z_var: (self._grid["lon"].dims, cos_z)}  # type: ignore
-            )
-        else:
-            raise ValueError()
 
     def _insert_prediction(self, ds: xr.Dataset, ds_pred: xr.Dataset) -> xr.Dataset:
         predicted_vars = ds_pred.data_vars
@@ -62,10 +52,12 @@ class PredictionMapper(GeoMapper):
 
     def __getitem__(self, key: str) -> xr.Dataset:
         ds = self._base_mapper[key]
-        if self._cos_z_var and self._grid:
-            ds = self._insert_cos_zenith_angle(key, ds)
-        ds_prediction = self._predict(ds)
-        return self._insert_prediction(ds, ds_prediction)
+        ds = ds.merge(self._grid)
+        derived_mapping = DerivedMapping(ds)
+        ds_derived = derived_mapping.dataset(
+            self._input_variables + self._output_variables)
+        ds_prediction = self._predict(ds_derived)
+        return self._insert_prediction(ds_derived, ds_prediction)
 
     def keys(self):
         return self._base_mapper.keys()
