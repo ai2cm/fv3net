9228dba355e45810433b1470d3b22b749c7b1538
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-11-06 21:00:30 +0000

    Add prognostic run to validate.sh

--- validate.sh
+++ validate.sh
@@ -5,7 +5,12 @@ set -x
 [[ -f ./kustomize ]] || \
     ./install_kustomize.sh 3.8.6
 
-kustomizations=("examples/train-evaluate-prognostic-run/" "examples/nudge-to-fine-run/" "examples/nudge-to-obs-run/")
+kustomizations=(
+    "examples/train-evaluate-prognostic-run/"
+    "examples/nudge-to-fine-run/"
+    "examples/nudge-to-obs-run/"
+    "examples/prognostic-run/"
+)
 
 for k in $kustomizations; do
     ./kustomize build $k
