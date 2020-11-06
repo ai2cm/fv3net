60e8249ce24ea4f7f836fba2eac604773b88bb8b
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-11-06 20:13:58 +0000

    Update default nudge-to-obs configuration

--- examples/nudge-to-obs-run/run.sh
+++ examples/nudge-to-obs-run/run.sh
@@ -5,16 +5,13 @@ set -e
 # adjust experiment name and replace "vcm-ml-scratch" with "vcm-ml-experiments"
 # under output-url after debugging configuration
 EXPERIMENT=fill_in_here
-IMAGE_TAG=4a8fc6aa3337d51967d5483695be582dd09561db
 
-gsutil cp diag_table gs://vcm-ml-experiments/diag_tables/nudge_to_obs_5h/v1.0/diag_table
+gsutil cp diag_table gs://vcm-ml-experiments/diag_tables/nudge_to_obs_3h/v1.0/diag_table
 argo submit \
     --from workflowtemplate/nudge-to-obs \
-    -p fv3net-image=us.gcr.io/vcm-ml/fv3net:${IMAGE_TAG} \
-    -p post-process-image=us.gcr.io/vcm-ml/post_process_run:${IMAGE_TAG} \
-    -p fv3gfs-image=us.gcr.io/vcm-ml/prognostic_run:${IMAGE_TAG} \
     -p output-url=gs://vcm-ml-scratch/$EXPERIMENT \
     -p nudging-config="$(< nudge-to-obs-run.yaml)" \
     -p cpu="24" \
     -p memory="20Gi" \
-    -p segment-count=1
+    -p segment-count=1 \
+    -p flags="--python-output-interval 5"
