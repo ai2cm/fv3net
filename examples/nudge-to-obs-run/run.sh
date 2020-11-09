e85fe10c7b02f60c487571937a3724db82c11181
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-11-09 15:42:45 +0000

    Do n2o run in 30-day segments

--- examples/nudge-to-obs-run/run.sh
+++ examples/nudge-to-obs-run/run.sh
@@ -4,7 +4,7 @@ set -e
 
 # adjust experiment name and replace "vcm-ml-scratch" with "vcm-ml-experiments"
 # under output-url after debugging configuration
-EXPERIMENT=2020-10-30-nudge-to-obs-GRL-paper/nudge-to-obs-run-8xdaily-diags
+EXPERIMENT=2020-10-30-nudge-to-obs-GRL-paper/nudge-to-obs-run-3hr-diags
 
 gsutil cp diag_table gs://vcm-ml-experiments/diag_tables/nudge_to_obs_3h/v1.1/diag_table
 argo submit \
@@ -13,5 +13,5 @@ argo submit \
     -p nudging-config="$(< nudge-to-obs-run.yaml)" \
     -p cpu="24" \
     -p memory="20Gi" \
-    -p segment-count="36" \
+    -p segment-count="24" \
     -p flags="--python-output-interval 5"
