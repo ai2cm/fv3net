948f048a319c615c403baa75f0e3eb789f1bf20b
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-11-09 16:50:35 +0000

    Add weather forecast runs

--- examples/prognostic-run/run.sh
+++ examples/prognostic-run/run.sh
@@ -2,11 +2,30 @@
 
 set -e
 
+INITIAL_MONTH=$1
+OUTPUT=gs://vcm-ml-experiments/2020-10-30-nudge-to-obs-GRL-paper/weather-forecasts/$INITIAL_CONDITION
+ML_MODEL=gs://vcm-ml-experiments/2020-10-30-nudge-to-obs-GRL-paper/rf-default/trained_model
+
+IC_TIMESTAMP="2016${INITIAL_MONTH}01.000000"
+IC_URL="gs://vcm-ml-raw/2020-11-05-GFS-month-start-initial-conditions-year-2016/2016${INITIAL_MONTH}0100"
+
+envsubst < "prognostic-run.yaml" > "prognostic-run-with-IC.yaml"
+
 argo submit \
     --from workflowtemplate/prognostic-run \
-    -p output=gs://vcm-ml-experiments/noah/prognostic_runs/2020-10-26-triggered-regressor \
-    -p trained-ml=gs://vcm-ml-archive/noah/emulator/2020-10-16-triggered-regressor \
+    -p output="${OUTPUT}-prognostic" \
+    -p trained-ml=$ML_MODEL \
     -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
-    -p initial-condition="20160805.000000" \
-    -p config="$(< prognostic-run.yaml)" \
-    -p segment-count=4
+    -p initial-condition=$IC_TIMESTAMP \
+    -p config="$(< prognostic-run-with-IC.yaml)"
+    -p flags="--nudge-to-observations"
+
+
+argo submit \
+    --from workflowtemplate/prognostic-run \
+    -p output="${OUTPUT}-baseline" \
+    -p trained-ml=$ML_MODEL \
+    -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
+    -p initial-condition=$IC_TIMESTAMP \
+    -p config="$(< prognostic-run-with-IC.yaml)" \
+    -p flags="--nudge-to-observations --diagnostic_ml"
