e5561617540334e7869de8ad219d1f5c7baa5f7e
Anna Kwa <annak@vulcan.com>
2020-11-10 19:54:56 +0000

    PR comments, and use 6 cores

--- experiment/run.sh
+++ experiment/run.sh
@@ -18,7 +18,7 @@ do
     argo submit \
         --from workflowtemplate/train-diags-prog \
         -p root=gs://vcm-ml-experiments/$EXPERIMENT/seed-$i \
-        -p train-test-data=gs://vcm-ml-experiments/2020-10-30-nudge-to-obs-GRL-paper/nudge-to-obs-run \
+        -p train-test-data=gs://vcm-ml-experiments/2020-10-30-nudge-to-obs-GRL-paper/nudge-to-obs-run-3hr-diags \
         -p training-config="$(< configs/training-config-seed-$i.yml)" \
         -p train-routine="keras" \
         -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
@@ -27,10 +27,10 @@ do
         -p train-times="$(<  ../train.json)" \
         -p test-times="$(<  ../test.json)" \
         -p public-report-output=gs://vcm-ml-public/offline_ml_diags/$EXPERIMENT/seed-$i \
-        -p segment-count=1 \
-        -p cpu-prog=24 \
-        -p memory-prog="25Gi" \
+        -p segment-count=3 \
+        -p cpu-prog=6 \
+        -p memory-prog="12Gi" \
         -p flags="--nudge-to-observations" \
         -p chunks="$(< chunks.yaml)" \
-        --name nudge-to-obs-nn-trials-seed-$i-test
+        --name nudge-to-obs-nn-trials-seed-$i
 done
--- experiment/run_test.sh
+++ experiment/run_test.sh
@@ -12,13 +12,14 @@ mkdir -p configs
 
 for i in {0..0}
 do
+
     sed '/random_seed/d' configs/base-training-config.yml > configs/training-config-seed-$i-test.yml
     echo "random_seed: $i" >> configs/training-config-seed-$i-test.yml
 
     argo submit \
         --from workflowtemplate/train-diags-prog \
         -p root=gs://vcm-ml-scratch/annak/$EXPERIMENT/seed-$i \
-        -p train-test-data=gs://vcm-ml-experiments/2020-10-30-nudge-to-obs-GRL-paper/nudge-to-obs-run \
+        -p train-test-data=gs://vcm-ml-experiments/2020-10-30-nudge-to-obs-GRL-paper/nudge-to-obs-run-3hr-diags \
         -p training-config="$(< configs/training-config-seed-$i-test.yml)" \
         -p train-routine="keras" \
         -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
@@ -27,9 +28,8 @@ do
         -p train-times="$(<  ../train_short.json)" \
         -p test-times="$(<  ../test_short.json)" \
         -p public-report-output=gs://vcm-ml-public/offline_ml_diags/$EXPERIMENT/seed-$i \
-        -p segment-count=2 \
-        -p cpu-prog=24 \
-        -p memory-prog="25Gi" \
+        -p cpu-prog=6 \
+        -p memory-prog="12Gi" \
         -p flags="--nudge-to-observations" \
         -p chunks="$(< chunks.yaml)" \
         --name nudge-to-obs-nn-trials-seed-$i-test
