7ce7097c171a55f4dd0a34a074c405b5b09c0cd7
Anna Kwa <annak@vulcan.com>
2020-11-10 00:04:47 +0000

    Use xr.merge for compat=override

--- workflows/offline_ml_diags/offline_ml_diags/_mapper.py
+++ workflows/offline_ml_diags/offline_ml_diags/_mapper.py
@@ -52,7 +52,8 @@ class PredictionMapper(GeoMapper):
 
     def __getitem__(self, key: str) -> xr.Dataset:
         ds = self._base_mapper[key]
-        ds = ds.merge(self._grid, compat="override").assign_coords(
+        # Prioritize dataset's land_sea_mask if grid values disagree
+        ds = xr.merge([ds, self._grid], compat="override").assign_coords(
             {"time": parse_datetime_from_str(key)}
         )
         derived_mapping = DerivedMapping(ds)
