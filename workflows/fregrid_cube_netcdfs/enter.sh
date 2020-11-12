866b0fdd941b56cd6d80cabdfa4b66400049ca1a
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-11-12 15:14:09 -0500

    Revive regridding workflow by downloading gridspec from new url
    The cubed sphere to lat/lon regridding workflow is currently broken because it depends on a tarball of the gridspec/mosaic data which was deleted. This PR modifies the regridding workflow to download the individual files (which were not deleted) instead of the tar. The files were moved to `vcm-ml-raw` because they are were provided by GFDL and are not easily reproducible with VCM code.
    
    Significant internal changes:
    - use gridspec/mosaic data from vcm-ml-raw
    
    I was able to successfully use the argo workflow with this change:
    ```
    (fv3net) OliWM@oliwm-vm:~/repos/explore/oliwm/2020-10-30-figures-for-nudge-to-obs-paper$ argo get regrid-the-input-data-hthtg
    Name:                regrid-the-input-data-hthtg
    Namespace:           default
    ServiceAccount:      default
    Status:              Succeeded
    Conditions:
     Completed           True
    Created:             Thu Nov 12 19:43:14 +0000 (3 minutes ago)
    Started:             Thu Nov 12 19:43:14 +0000 (3 minutes ago)
    Finished:            Thu Nov 12 19:44:17 +0000 (2 minutes ago)
    Duration:            1 minute 3 seconds
    ResourcesDuration:   1m22s*(1 cpu),1m22s*(100Mi memory)
    Parameters:
      source_prefix:     gs://vcm-ml-scratch/oliwm/test-regrid/baseline/diags_latlon_baseline
      output_bucket:     gs://vcm-ml-scratch/oliwm/test-regrid/baseline/diags_latlon_1deg.nc
      fields:            column_integrated_q1_time_mean_value,column_integrated_q2_time_mean_value,total_precip_time_mean_value,column_integrated_q1_time_mean_bias,column_integrated_q2_time_mean_bias,total_precip_time_mean_bias,land_sea_mask
      resolution:        C48
      extra_args:        --nlat 180 --nlon 360
    
    STEP                            TEMPLATE  PODNAME                      DURATION  MESSAGE
     ✔ regrid-the-input-data-hthtg  regrid    regrid-the-input-data-hthtg  1m
    ```

--- workflows/fregrid_cube_netcdfs/enter.sh
+++ workflows/fregrid_cube_netcdfs/enter.sh
@@ -2,6 +2,6 @@
 # Utility script for entering a docker image with proper google credentials
 # Used for developement purposes only
 
-credentials=$HOME/keys/noahb-vm.json
-bindMounts="-v $credentials:/creds.json -e GOOGLE_APPLICATION_CREDENTIALS=/creds.json  -v /home/noahb:/home/noahb -w $(pwd) -ti "
-docker run $bindMounts us.gcr.io/vcm-ml/fretools  bash
+credentials=/etc/key.json
+bindMounts="-v $credentials:/creds.json -e GOOGLE_APPLICATION_CREDENTIALS=/creds.json  -v /home/oliwm:/home/oliwm -w $(pwd) -ti "
+docker run $bindMounts --entrypoint /bin/bash us.gcr.io/vcm-ml/regrid_c3072_diag
