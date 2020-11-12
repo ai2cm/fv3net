3e986502f2973424bf8fb553167cc58ff55a04a3
Noah D. Brenowitz <nbren12@gmail.com>
2020-11-11 20:32:02 -0800

    Upload models to cloud

--- workflows/triggered_emulator/train_hybrid.sh
+++ workflows/triggered_emulator/train_hybrid.sh
@@ -3,9 +3,15 @@ set -e
 set -x
 
 DATA=/Users/noah/data/dev/2020-11-11-hybrid-data-resampled
+OUTPUT=gs://vcm-ml-archive/noah/emulator/$(date +%F)-triggered-hybrid
 CLASSIFIER=hybrid_classifier.pkl
 REGRESSOR=hybrid_regressor.pkl
 
-python download_hybrid.py "$DATA"
-python train_classifier.py "$DATA" "$CLASSIFIER"
-python train_regressor.py "$DATA" "$REGRESSOR"
+[[ -d "$DATA" ]] || python download_hybrid.py "$DATA"
+[[ -f "$CLASSIFIER" ]] || python train_classifier.py "$DATA" "$CLASSIFIER"
+[[ -f "$REGRESSOR" ]] || python train_regressor.py "$DATA" "$REGRESSOR"
+
+# upload model
+gsutil cp "$REGRESSOR" "$OUTPUT/regressor.pkl"
+gsutil cp "$CLASSIFIER" "$OUTPUT/classifier.pkl"
+gsutil cp hybrid-mapper.yaml "$OUTPUT/hybrid-mapper.yaml"
