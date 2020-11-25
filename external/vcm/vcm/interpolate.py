36f5aac2abc40fe79df5972bec93aa854f7bd489
Noah D. Brenowitz <nbren12@gmail.com>
2020-11-24 16:43:34 -0800

    Interpolate Datasets

--- external/vcm/vcm/interpolate.py
+++ external/vcm/vcm/interpolate.py
@@ -1,4 +1,4 @@
-from typing import Callable, Union, Optional
+from typing import Callable, Union, Optional, TypeVar
 import functools
 import metpy.interpolate
 import numpy as np
@@ -50,13 +50,15 @@ PRESSURE_GRID = xr.DataArray(
     dims="pressure",
 )
 
+T = TypeVar("T", xr.DataArray, xr.Dataset)
+
 
 def interpolate_to_pressure_levels(
-    field: xr.DataArray,
+    field: T,
     delp: xr.DataArray,
     levels: xr.DataArray = PRESSURE_GRID,
     dim: str = "pfull",
-) -> xr.DataArray:
+) -> T:
     """Regrid an atmospheric field to a fixed set of pressure levels
 
     Args:
@@ -75,8 +77,8 @@ def interpolate_to_pressure_levels(
 
 
 def interpolate_1d(
-    xp: xr.DataArray, x: xr.DataArray, field: xr.DataArray, dim: Optional[str] = None,
-) -> xr.DataArray:
+    xp: xr.DataArray, x: xr.DataArray, field: T, dim: Optional[str] = None,
+) -> T:
     """Interpolates data with any shape over a specified axis.
 
     Wraps metpy.interpolate.interplolate_1d
@@ -101,10 +103,22 @@ def interpolate_1d(
         https://unidata.github.io/MetPy/latest/api/generated/metpy.interpolate.interpolate_1d.html
 
     """
-    if xp.ndim == 1:
-        return _interpolate_1d_constant_output_levels(xp, x, field, dim)
-    else:
-        return _interpolate_1d_variable_output_levels(xp, x, field)
+    if isinstance(field, xr.Dataset):
+        data_vars = {}
+        for v in field:
+            if set(field[v].dims) >= set(x.dims):
+                data_vars[v] = interpolate_1d(xp, x, field[v], dim=dim)
+            else:
+                data_vars[v] = field[v]
+        return xr.Dataset(data_vars)
+    elif isinstance(field, xr.DataArray):
+        if xp.ndim == 1:
+            if dim is None:
+                raise ValueError(f"dim argument needed for 1D xp")
+            else:
+                return _interpolate_1d_constant_output_levels(xp, x, field, dim)
+        else:
+            return _interpolate_1d_variable_output_levels(xp, x, field)
 
 
 def _interpolate_1d_constant_output_levels(
@@ -140,7 +154,7 @@ def _interpolate_2d(xp: np.ndarray, x: np.ndarray, y: np.ndarray) -> np.ndarray:
 
 
 def _apply_2d(
-    func: Callable[[np.ndarray, np.ndarray, np.ndarray, int], np.ndarray],
+    func: Callable[[np.ndarray, np.ndarray, np.ndarray], np.ndarray],
     xp: np.ndarray,
     x: np.ndarray,
     y: np.ndarray,
@@ -239,9 +253,8 @@ def interpolate_unstructured(
     for key in coords:
         for dim in data[key].dims:
             spatial_dims.add(dim)
-    spatial_dims = list(spatial_dims)
 
-    stacked = data.stack({dim_name: spatial_dims})
+    stacked = data.stack({dim_name: list(spatial_dims)})
     order = list(coords)
     input_points = _coords_to_points(stacked, order)
     output_points = _coords_to_points(coords, order)
