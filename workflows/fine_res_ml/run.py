4c7bf5d983becd4d2cc219159be5e5894db338d8
Noah D. Brenowitz <nbren12@gmail.com>
2020-12-01 14:32:22 -0800

    Train ml

--- workflows/fine_res_ml/run.py
+++ workflows/fine_res_ml/run.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import download_data
+import tempfile
 import joblib
 import subprocess
 
@@ -9,10 +10,13 @@ cache = joblib.Memory(location="/home/noahb/joblib_cache/fine-res-ml", verbose=1
 
 
 @cache.cache
-def train(path: str, config_path: str, output: str):
-    subprocess.check_call(
-        [sys.executable, "-m", "fv3fit.sklearn", path, config_path, output]
-    )
+def train(path: str, config: str, output: str):
+    with tempfile.NamedTemporaryFile("w") as f:
+        f.write(config)
+        f.flush()
+        subprocess.check_call(
+            [sys.executable, "-m", "fv3fit.sklearn", path, f.name, output]
+        )
 
 
 fine_res = "gs://vcm-ml-experiments/2020-05-27-40day-fine-res-coarsening/"
@@ -23,4 +27,6 @@ config = "workflows/fine_res_ml/training.yaml"
 
 cache.cache(download_data.fine_res_to_zarr)(fine_res, local_fine_res)
 cache.cache(download_data.save_fine_res_to_zarr)(local_fine_res, local_ml_final)
-train(local_ml_final, config, trained)
+
+with open(config) as f:
+    train(local_ml_final, f.read(), trained)
