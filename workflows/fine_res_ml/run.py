035189c0fbc5d2d6d29defcc11c84cd3dcf9bbde
Noah D. Brenowitz <nbren12@gmail.com>
2020-12-02 13:20:07 -0800

    Add offline report to run.py

--- workflows/fine_res_ml/run.py
+++ workflows/fine_res_ml/run.py
@@ -33,11 +33,21 @@ def offline_diagnostics(data, config, model, output):
     )
 
 
+@cache.cache
+def offline_report(diagnostics, output):
+    subprocess.check_call(
+        [sys.executable, "-m", "offline_ml_diags.create_report", diagnostics, output]
+    )
+
+
 fine_res = "gs://vcm-ml-experiments/2020-05-27-40day-fine-res-coarsening/"
 local_fine_res = "/home/noahb/data/dev/2020-11-25-fine-res.zarr"
 local_ml_final = "/home/noahb/data/dev/2020-11-25-fine-res/ml.zarr"
 trained = "gs://vcm-ml-scratch/noah/trained-ml"
 offline_metrics = "gs://vcm-ml-scratch/noah/offline_diagnostics/2020-11-25-fine-res"
+offline_report_uri = (
+    "gs://vcm-ml-public/noah/offline_diagnostics_report/2020-11-25-fine-res"
+)
 config = "workflows/fine_res_ml/training.yaml"
 
 cache.cache(download_data.fine_res_to_zarr)(fine_res, local_fine_res)
@@ -48,3 +58,4 @@ with open(config) as f:
 
 
 offline_diagnostics(local_ml_final, config, trained, offline_metrics)
+offline_report(offline_metrics, offline_report_uri)
