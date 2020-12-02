2f0bcb082c209c1dbae5dc61a5d786d9640b5f2f
Noah D. Brenowitz <nbren12@gmail.com>
2020-12-02 15:06:18 -0800

    Export fv3post.clear_encoding

--- workflows/post_process_run/fv3post/__init__.py
+++ workflows/post_process_run/fv3post/__init__.py
@@ -1,5 +1,5 @@
 from .append import append_zarr_along_time, append_segment
-from .post_process import post_process
+from .post_process import post_process, clear_encoding
 from .consolidate_metadata import consolidate_metadata
 
 __version__ = "0.1.0"
