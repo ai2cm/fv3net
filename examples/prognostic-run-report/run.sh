338e4aad875723e366ec3742a4111ff447ea61ec
Noah D Brenowitz <nbren12@gmail.com>
2020-12-03 09:58:20 -0800

    Print link to report (#77)

--- examples/prognostic-run-report/run.sh
+++ examples/prognostic-run-report/run.sh
@@ -5,3 +5,5 @@ argo submit --from=workflowtemplate/prognostic-run-diags \
     -p docker-image=us.gcr.io/vcm-ml/fv3net:9ca9d9bd4bf37b2b4ffcdc195dcae63580f8694e \
     -p runs="$(< rundirs.json)" \
     -p make-movies=true
+
+echo "report generated at: https://storage.googleapis.com/vcm-ml-public/argo/$name/index.html"
