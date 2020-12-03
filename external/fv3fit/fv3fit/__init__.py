1f0c5f0f662a3a3f15c077dcabd413a39424c3e2
Noah D. Brenowitz <nbren12@gmail.com>
2020-12-03 14:44:43 -0800

    Use dump/load in offline ml diags

--- external/fv3fit/fv3fit/__init__.py
+++ external/fv3fit/fv3fit/__init__.py
@@ -2,5 +2,3 @@ from ._shared import Predictor
 from ._shared.io import dump, load
 from . import keras
 from . import sklearn
-
-PRODUCTION_MODEL_TYPES = {"sklearn": ["random_forest"], "keras": ["DenseModel"]}
