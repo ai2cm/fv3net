a809372fe6023cc93526eae5898f59bcb04b6e38
Noah D. Brenowitz <nbren12@gmail.com>
2020-12-04 19:39:49 -0800

    Type-check all of vcm

--- check_types.sh
+++ check_types.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 mypy --follow-imports silent \
-    external/vcm/vcm/cloud \
-    external/vcm/vcm/cubedsphere \
+    external/vcm/vcm/ \
     workflows/dataflow/fv3net/pipelines/restarts_to_zarr/ \
     workflows/prognostic_c48_run \
     workflows/prognostic_c48_run/tests/ \
     external/fv3fit/fv3fit \
     external/loaders/loaders/mappers/_local.py \
     external/loaders/loaders/batches/_sequences.py \
-    external/vcm/vcm/derived_mapping.py \
-    external/vcm/vcm/interpolate.py \
     workflows/offline_ml_diags/offline_ml_diags
