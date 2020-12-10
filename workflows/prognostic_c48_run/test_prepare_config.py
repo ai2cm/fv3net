bfb23ff59a16d9cdb56910058995ebb831721365
brianhenn <brianhenn@gmail.com>
2020-12-10 20:32:20 +0000

    started converting e2e n2f to prognostic_run workflow

--- workflows/prognostic_c48_run/test_prepare_config.py
+++ workflows/prognostic_c48_run/test_prepare_config.py
@@ -48,11 +48,14 @@ TIMESTAMPS = ["20160801.021500", "20160801.041500"]
     ["timestamps", "frequency_minutes", "expected"],
     [
         pytest.param(
-            TIMESTAMPS, None, {"kind": "selected", "times": TIMESTAMPS}, id="timestamps"
+            TIMESTAMPS,
+            15,
+            {"kind": "selected", "times": TIMESTAMPS},
+            id="both_provided_defaults_to_timestamps",
         ),
         pytest.param(None, 120, {"kind": "interval", "frequency": 7200}, id="2-hourly"),
         pytest.param(
-            None, None, {"kind": "interval", "frequency": 900}, id="default_15-minute"
+            None, 15, {"kind": "interval", "frequency": 900}, id="default_15-minute"
         ),
     ],
 )
