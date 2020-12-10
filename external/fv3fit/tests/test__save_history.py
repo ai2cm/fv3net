6aa491f9ead7c21c9263ac713b622a7f1cc49520
Anna Kwa <annak@vulcan.com>
2020-12-10 23:03:39 +0000

    linting

--- external/fv3fit/tests/test__save_history.py
+++ external/fv3fit/tests/test__save_history.py
@@ -5,13 +5,13 @@ from fv3fit.keras._save_history import _get_epoch_losses
 @pytest.mark.parametrize(
     "history, key, result",
     [
-        ({"loss": [[1., 2., 3.]]}, "loss", [2.]),
-        ({"val_loss": [[1., 2., 3.]]}, "val_loss", [3.]),
-        ({"loss": [[1., 2.], [3., 4.]]}, "loss", [1.5, 3.5]),
-        ({"val_loss": [[1., 2.], [3., 4.]]}, "val_loss", [2., 4.]),
-        ({"loss": [[1.], [3.]]}, "loss", [1., 3.]),
-        ({"loss": [[1., 2., 3.]]}, "val_loss", None),
-    ]
+        ({"loss": [[1.0, 2.0, 3.0]]}, "loss", [2.0]),
+        ({"val_loss": [[1.0, 2.0, 3.0]]}, "val_loss", [3.0]),
+        ({"loss": [[1.0, 2.0], [3.0, 4.0]]}, "loss", [1.5, 3.5]),
+        ({"val_loss": [[1.0, 2.0], [3.0, 4.0]]}, "val_loss", [2.0, 4.0]),
+        ({"loss": [[1.0], [3.0]]}, "loss", [1.0, 3.0]),
+        ({"loss": [[1.0, 2.0, 3.0]]}, "val_loss", None),
+    ],
 )
 def test__get_epoch_losses(history, key, result):
     assert _get_epoch_losses(history, key) == result
