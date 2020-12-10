bd981ee36d4f8327500e64d9b70ba45a35b0e5f6
Noah D. Brenowitz <nbren12@gmail.com>
2020-12-09 17:25:20 -0800

    Addres minor pr comments

--- workflows/offline_ml_diags/offline_ml_diags/compute_diags.py
+++ workflows/offline_ml_diags/offline_ml_diags/compute_diags.py
@@ -289,12 +289,8 @@ if __name__ == "__main__":
         pred_mapper, variables, timesteps=timesteps, **batch_kwargs,
     )
     batches = loaders.batches.diagnostic_batches_from_mapper(
-        pred_mapper,
-        variables,
-        timesteps=timesteps,
-        timesteps_per_batch=2,
-        **batch_kwargs,
-    )[:1]
+        pred_mapper, variables, timesteps=timesteps, **batch_kwargs,
+    )
 
     # compute diags
     ds_diagnostics, ds_diurnal, ds_scalar_metrics = _compute_diagnostics(
