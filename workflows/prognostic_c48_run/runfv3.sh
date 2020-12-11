d9d003f49dc043f5277abfd0e6b217d9423af4cb
Noah D. Brenowitz <nbren12@gmail.com>
2020-12-11 15:42:35 -0800

    Address reviews

--- workflows/prognostic_c48_run/runfv3.sh
+++ workflows/prognostic_c48_run/runfv3.sh
@@ -10,6 +10,8 @@ RUNFILE="$3"
 # directory writing stage finishes
 mkdir -p "$RUNDIR"
 
+
 write_run_directory "$CONFIG" "$RUNDIR"
 cd "$RUNDIR"
-mpirun -n 6 python3 "$RUNFILE" |& tee -a "$RUNDIR/logs.txt"
+NUM_PROC=$(yq '.namelist.fv_core_nml.layout | .[0] *.[1] * 6' "$CONFIG")
+mpirun -n "$NUM_PROC" python3 "$RUNFILE" |& tee -a "$RUNDIR/logs.txt"
