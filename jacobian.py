13a8c517561180a59ebdf67dcca65f31e771b219
Noah D. Brenowitz <nbren12@gmail.com>
2020-12-14 10:57:45 -0800

    refactor into externally plottable notebook

--- jacobian.py
+++ jacobian.py
@@ -4,6 +4,7 @@ import xarray as xr
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import os
 
 def unpack_matrix(x_packer, y_packer, J):
     jacobian_dict = {}
@@ -32,41 +33,43 @@ def jacobian(model, mean: xr.Dataset):
 
 
 # unstable
-url = "gs://vcm-ml-experiments/2020-12-03-sensitivity-to-amount-of-input-data/neural-network-128-timesteps-seed-0/trained_model"
+# url = "gs://vcm-ml-experiments/2020-12-03-sensitivity-to-amount-of-input-data/neural-network-128-timesteps-seed-0/trained_model"
 
 # stable
-# url = "gs://vcm-ml-experiments/2020-12-03-sensitivity-to-amount-of-input-data/neural-network-128-timesteps-1-epochs-seed-0/trained_model"
+url = "gs://vcm-ml-experiments/2020-12-03-sensitivity-to-amount-of-input-data/neural-network-128-timesteps-1-epochs-seed-0/trained_model"
 
-model =  fv3fit.load(url)
-nfeat = sum(model.X_packer.feature_counts.values())
+def plot_jacobian(model_url):
 
-mean = model.X_packer.to_dataset(model.X_scaler.mean[np.newaxis, :])
-jacobian_dict = jacobian(model, mean)
+    model =  fv3fit.load(model_url)
+    nfeat = sum(model.X_packer.feature_counts.values())
 
-variables_3d = [var_ for var_ in mean if 'z' in mean[var_].dims]
-variables_2d = [var_ for var_ in mean if 'z' not in mean[var_].dims]
-        
-fig, axs = plt.subplots(len(variables_3d), len(model.output_variables), figsize=(12,12))
-for i, in_name in enumerate(variables_3d):
-    for j, out_name in enumerate(model.y_packer.pack_names):
-        pane = jacobian_dict[(in_name, out_name)]
-        im = axs[i, j].imshow(pane)
-        axs[i, j].set_ylabel(f'in ({in_name})')
-        axs[i, j].set_xlabel(f'out ({out_name})')
-        axs[i, j].xaxis.tick_top()
-        axs[i, j].xaxis.set_label_position('top') 
-        plt.colorbar(im, ax=axs[i,j])
-        
-plt.tight_layout()
+    mean = model.X_packer.to_dataset(model.X_scaler.mean[np.newaxis, :])
+    jacobian_dict = jacobian(model, mean)
 
-fig, axs = plt.subplots(len(variables_2d), len(model.output_variables), figsize=(12,12))
-for i, in_name in enumerate(variables_2d):
-    for j, out_name in enumerate(model.y_packer.pack_names):
-        pane = jacobian_dict[(in_name, out_name)]
-        axs[i, j].plot(pane.ravel(), np.arange(pane.size))
-        axs[i, j].set_xlabel(out_name)
-        axs[i, j].set_title(f'change in {in_name}')
-        axs[i ,j].set_ylabel('vertical level')
+    variables_3d = [var_ for var_ in mean if 'z' in mean[var_].dims]
+    variables_2d = [var_ for var_ in mean if 'z' not in mean[var_].dims]
+            
+    fig, axs = plt.subplots(len(variables_3d), len(model.output_variables), figsize=(12,12), constrained_layout=True)
+    for i, in_name in enumerate(variables_3d):
+        for j, out_name in enumerate(model.y_packer.pack_names):
+            pane = jacobian_dict[(in_name, out_name)]
+            im = axs[i, j].imshow(pane)
+            axs[i, j].set_ylabel(f'in ({in_name})')
+            axs[i, j].set_xlabel(f'out ({out_name})')
+            axs[i, j].xaxis.tick_top()
+            axs[i, j].xaxis.set_label_position('top') 
+            plt.colorbar(im, ax=axs[i,j])
 
-plt.tight_layout()
-plt.show()
+    fig.suptitle(url)
+
+    fig, axs = plt.subplots(len(variables_2d), len(model.output_variables), figsize=(12,12), constrained_layout=True)
+    for i, in_name in enumerate(variables_2d):
+        for j, out_name in enumerate(model.y_packer.pack_names):
+            pane = jacobian_dict[(in_name, out_name)]
+            axs[i, j].plot(pane.ravel(), np.arange(pane.size))
+            axs[i, j].set_xlabel(out_name)
+            axs[i, j].set_title(f'change in {in_name}')
+            axs[i ,j].set_ylabel('vertical level')
+
+    fig.suptitle(url)
+    plt.show()
