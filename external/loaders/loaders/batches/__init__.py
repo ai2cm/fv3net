927341121d78fcdc43776abcbeb1ca266e76d700
Andre Perkins <frodre@gmail.com>
2020-12-17 01:44:51 +0000

    Add loader for serialized data from call_py_fort

--- external/loaders/loaders/batches/__init__.py
+++ external/loaders/loaders/batches/__init__.py
@@ -2,6 +2,7 @@ from ._batch import (
     batches_from_geodata,
     batches_from_mapper,
     batches_from_serialized,
+    batches_from_serialized_callpyfort,
     diagnostic_batches_from_geodata,
     diagnostic_batches_from_mapper,
 )
