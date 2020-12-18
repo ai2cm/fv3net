ed299b737e0f0ebbef8b9275730f2ac9c219f1d5
Andre Perkins <frodre@gmail.com>
2020-12-18 17:59:03 +0000

    Bring back monitor changes from training branch

--- external/emulation_slim/emulation_slim/monitor.py
+++ external/emulation_slim/emulation_slim/monitor.py
@@ -2,6 +2,7 @@ import logging
 import os
 import cftime
 import f90nml
+import yaml
 import numpy as np
 from mpi4py import MPI
 
@@ -11,10 +12,19 @@ from .debug import print_errors
 
 logger = logging.getLogger(__name__)
 
+# TODO: Switch from env_var config to something compatible with ARGO
 NML_PATH = os.environ.get("INPUT_NML_PATH")
 namelist = f90nml.read(NML_PATH)
 logger.info(f"Loaded namelist for ZarrMonitor from {NML_PATH}")
 
+VAR_META_PATH = os.environ.get("VARIABLE_METADATA_YAML")
+try:
+    with open(str(VAR_META_PATH), "r") as f:
+        variable_metadata = yaml.safe_load(f)
+        logger.info(f"Loaded variable metadata from: {VAR_META_PATH}")
+except FileNotFoundError:
+    variable_metadata = {}
+    logger.info(f"No metadata found at: {VAR_META_PATH}")
 
 partitioner = CubedSpherePartitioner.from_namelist(namelist)
 DUMP_PATH = os.environ.get("STATE_DUMP_PATH")
@@ -46,6 +56,21 @@ def _adjust_vert_plus_one(data, dims):
     return dims
 
 
+def _get_attrs(key):
+    if key in variable_metadata:
+        var_key = key
+    elif key.split("_")[0] in variable_metadata:
+        # var_input_... style
+        var_key = key.split("_")[0]
+        logger.debug(f"Reduced key to {var_key} for metadata mapping")
+    else:
+        var_key = None
+        logger.debug(f"No metadata found for {key}... skipping")
+        return {}
+
+    return dict(**variable_metadata[var_key])
+
+
 def _convert_to_quantites(state):
 
     quantities = {}
@@ -54,7 +79,11 @@ def _convert_to_quantites(state):
         dims = DIMS_MAP[data.ndim]
         if "lev" in dims:
             dims = _adjust_vert_plus_one(data, dims)
-        quantities[key] = Quantity(data, dims, "units")
+        attrs = _get_attrs(key)
+        units = attrs.pop("units", "units")
+        quantities[key] = Quantity(data, dims, units)
+        # Access to private member could break TODO: Quantity kwarg for attrs?
+        quantities[key]._attrs.update(attrs)
 
     return quantities
 
