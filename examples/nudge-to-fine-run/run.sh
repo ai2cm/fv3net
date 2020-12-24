7db222cc3b218be7b9343c9a768ddc064f295eff
brianhenn <brianhenn@gmail.com>
2020-12-24 00:53:04 +0000

    physics-on baseline with tke edmf, 12-23

--- examples/nudge-to-fine-run/run.sh
+++ examples/nudge-to-fine-run/run.sh
@@ -3,15 +3,15 @@
 set -e
 
 EXPERIMENT=2020-12-17-n2f-moisture-conservation
-NSEGMENTS=20
+NSEGMENTS=19
 RANDOM=$(openssl rand --hex 6)
 
 argo submit --from workflowtemplate/prognostic-run \
-    -p initial-condition="20160801.001500" \
-    -p config="$(< nudge-to-fine-config.yaml)" \
+    -p initial-condition="20160801.010000" \
+    -p config="$(< baseline-config.yaml)" \
     -p reference-restarts="gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts" \
-    -p flags="--output-frequency 120" \
-    -p output="gs://vcm-ml-experiments/${EXPERIMENT}/moisture-conserving/nudge-to-fine-run" \
+    -p flags="--output-frequency 60" \
+    -p output="gs://vcm-ml-experiments/${EXPERIMENT}/baseline/prognostic-run" \
     -p segment-count="${NSEGMENTS}" \
     -p chunks="$(< chunks.yaml)" \
     --name "${EXPERIMENT}-nudge-to-fine-${RANDOM}"
