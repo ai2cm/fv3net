fdc7a760e9f6ccef46e54fe41c3052b79ed2b5c2
brianhenn <brianhenn@gmail.com>
2020-12-24 00:50:45 +0000

    report generation

--- examples/prognostic-run-report/run.sh
+++ examples/prognostic-run-report/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-name=$(date +%Ft%H%M%S)-$(openssl rand -hex 6)
+name=2020-12-17-n2f-moisture-conservation-$(openssl rand -hex 6)
 argo submit --from=workflowtemplate/prognostic-run-diags \
     --name $name \
-    -p docker-image=us.gcr.io/vcm-ml/fv3net:9ca9d9bd4bf37b2b4ffcdc195dcae63580f8694e \
     -p runs="$(< rundirs.json)" \
-    -p make-movies=true
+    -p make-movies=false
 
 echo "report generated at: https://storage.googleapis.com/vcm-ml-public/argo/$name/index.html"
