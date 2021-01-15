867779ebb81d45264d96f2ebdcf856942f485178
Noah D. Brenowitz <nbren12@gmail.com>
2021-01-14 16:09:34 -0800

    Refactor NudgingStepper to runtime.steppers

---
+++ workflows/prognostic_c48_run/runtime/steppers/nudging.py
@@ -0,0 +1,129 @@
+import functools
+from typing import (
+    Any,
+    Mapping,
+    List,
+)
+
+from runtime.steppers.base import (
+    Stepper,
+    State,
+    Diagnostics,
+    apply,
+    precipitation_sum,
+)
+
+from runtime.diagnostics.machine_learning import compute_nudging_diagnostics
+
+from runtime.nudging import (
+    nudging_timescales_from_dict,
+    setup_get_reference_state,
+    get_nudging_tendency,
+    set_state_sst_to_reference,
+)
+
+from runtime.names import (
+    DELP,
+    TOTAL_PRECIP,
+    PRECIP_RATE,
+    AREA,
+)
+
+
+SST_NAME = "ocean_surface_temperature"
+TSFC_NAME = "surface_temperature"
+MASK_NAME = "land_sea_mask"
+
+
+class NudgingStepper(Stepper):
+    """Stepper for nudging
+    """
+
+    def __init__(
+        self,
+        fv3gfs: Any,
+        comm: Any,
+        config: Mapping,
+        timestep: float,
+        states_to_output: Any,
+    ):
+
+        self._states_to_output = states_to_output
+
+        self._fv3gfs = fv3gfs
+        self._comm = comm
+        self.rank: int = comm.rank
+        self._timestep: float = timestep
+
+        self._nudging_timescales = nudging_timescales_from_dict(
+            config["nudging"]["timescale_hours"]
+        )
+        self._get_reference_state = setup_get_reference_state(
+            config,
+            self.nudging_variables + [SST_NAME, TSFC_NAME],
+            self._comm,
+            self._fv3gfs.get_tracer_metadata(),
+        )
+        self._get_nudging_tendency = functools.partial(
+            get_nudging_tendency, nudging_timescales=self._nudging_timescales,
+        )
+        self._tendencies_to_apply_to_dycore_state: State = {}
+
+    @property
+    def nudging_variables(self) -> List[str]:
+        return list(self._nudging_timescales)
+
+    def _compute_python_tendency(self) -> Diagnostics:
+
+        self._log_debug("Computing nudging tendencies")
+        variables: List[str] = self.nudging_variables + [
+            SST_NAME,
+            TSFC_NAME,
+            MASK_NAME,
+        ]
+        state: State = {name: self._state[name] for name in variables}
+        reference = self._get_reference_state(self._state.time)
+        set_state_sst_to_reference(state, reference)
+        self._tendencies_to_apply_to_dycore_state = self._get_nudging_tendency(
+            state, reference
+        )
+
+        return {
+            f"{key}_reference": reference_state
+            for key, reference_state in reference.items()
+        }
+
+    def _apply_python_to_dycore_state(self) -> Diagnostics:
+
+        diagnostics: Diagnostics = {}
+
+        variables: List[str] = self.nudging_variables + [
+            TOTAL_PRECIP,
+            PRECIP_RATE,
+            DELP,
+        ]
+        self._log_debug(f"Getting state variables: {variables}")
+        state: State = {name: self._state[name] for name in variables}
+        tendency: State = self._tendencies_to_apply_to_dycore_state
+
+        diagnostics.update(compute_nudging_diagnostics(state, tendency))
+        updated_state: State = apply(state, tendency, dt=self._timestep)
+        updated_state[TOTAL_PRECIP] = precipitation_sum(
+            state[TOTAL_PRECIP],
+            diagnostics["net_moistening_due_to_nudging"],
+            self._timestep,
+        )
+
+        self._log_debug("Setting Fortran State")
+        self._state.update(updated_state)
+
+        diagnostics.update({name: self._state[name] for name in self._states_to_output})
+
+        return {
+            "area": self._state[AREA],
+            "cnvprcp_after_python": self._fv3gfs.get_diagnostic_by_name(
+                "cnvprcp"
+            ).data_array,
+            "total_precip": updated_state[TOTAL_PRECIP],
+            **diagnostics,
+        }
