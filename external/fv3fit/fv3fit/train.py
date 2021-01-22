4e033191232feb5ea86b75f4a462b024d92dce0e
Anna Kwa <annak@vulcan.com>
2021-01-22 22:18:37 +0000

    consolidate get_model related functions

--- external/fv3fit/fv3fit/train.py
+++ external/fv3fit/fv3fit/train.py
@@ -1,5 +1,5 @@
 import argparse
-from importlib import import_module
+import inspect
 import loaders
 import logging
 import os
@@ -12,28 +12,29 @@ from ._shared import (
     load_data_sequence,
     ModelTrainingConfig,
     io,
+    Estimator
 )
 from .keras._training import get_regularizer, get_optimizer, set_random_seed
 from .keras._validation_data import validation_dataset
+import fv3fit.keras._models as keras_models
 
 
 KERAS_CHECKPOINT_PATH = "model_checkpoints"
+KERAS_MODEL_TYPES = [m[0] for m in inspect.getmembers(keras_models, inspect.isclass)]
+SKLEARN_MODEL_TYPES = ["sklearn", "rf", "random_forest", "sklearn_random_forest"]
+ROUTINE_LOOKUP = {
+    **{model: "keras" for model in KERAS_MODEL_TYPES},
+    **{model: "sklearn" for model in SKLEARN_MODEL_TYPES},
+}
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "routine",
-        type=str,
-        help="Training routine to use. Choices are 'keras' or 'sklearn'",
+        "data_path", nargs="*", type=str, help="Location of training data"
     )
     parser.add_argument(
-        "train_data_path", nargs="*", type=str, help="Location of training data"
-    )
-    parser.add_argument(
-        "train_config_file",
-        type=str,
-        help="Local path for training configuration yaml file",
+        "config_file", type=str, help="Local path for training configuration yaml file",
     )
     parser.add_argument(
         "output_data_path", type=str, help="Location to save config and trained model."
@@ -60,27 +61,39 @@ def parse_args():
     return parser.parse_args()
 
 
-def _get_model_kwargs(routine: str, config: ModelTrainingConfig) -> dict:
-    # returns the args for the appropriate training routine's get_model
+def _get_model(config: ModelTrainingConfig) -> Estimator:
+    routine = ROUTINE_LOOKUP[config.model_type]
     if routine == "sklearn":
-        return {"train_config": config}
+        from fv3fit.sklearn import get_model
+        return get_model(
+            model_type=config.model_type,
+            input_variables=config.input_variables,
+            output_variables=config.output_variables,
+            scaler_type=config.scaler_type,
+            scaler_kwargs=config.scaler_kwargs,
+            **config.hyperparameters
+        )
     elif routine == "keras":
-        return {
-            "model_type": config.model_type,
-            "sample_dim_name": loaders.SAMPLE_DIM_NAME,
-            "input_variables": config.input_variables,
-            "output_variables": config.output_variables,
-            "optimizer": get_optimizer(config.hyperparameters),
-            "kernel_regularizer": get_regularizer(config.hyperparameters),
-            "checkpoint_path": (
-                os.path.join(args.output_data_path, KERAS_CHECKPOINT_PATH)
-                if config.save_model_checkpoints
-                else None
-            ),
+        from fv3fit.keras import get_model
+        fit_kwargs = _keras_fit_kwargs(config)
+        checkpoint_path = (
+            os.path.join(args.output_data_path, KERAS_CHECKPOINT_PATH)
+            if config.save_model_checkpoints
+            else None
+        )
+        return get_model(
+            model_type=config.model_type,
+            sample_dim_name=loaders.SAMPLE_DIM_NAME,
+            input_variables=config.input_variables,
+            output_variables=config.output_variables,
+            optimizer=get_optimizer(config.hyperparameters),
+            kernel_regularizer=get_regularizer(config.hyperparameters),
+            checkpoint_path=checkpoint_path,
+            fit_kwargs=fit_kwargs,
             **config.hyperparameters,
-        }
+        )
     else:
-        raise ValueError("arg for 'routine' should be either 'sklearn' or 'keras'.")
+        raise NotImplementedError(f"Model type {config.model_type} is not implemented.")
 
 
 def _keras_fit_kwargs(config: ModelTrainingConfig) -> dict:
@@ -91,11 +104,10 @@ def _keras_fit_kwargs(config: ModelTrainingConfig) -> dict:
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     args = parse_args()
     data_path = parse_data_path(args)
-    train_config = load_model_training_config(
-        args.train_config_file, args.train_data_path
-    )
+    train_config = load_model_training_config(args.config_file, args.data_path)
 
     if args.timesteps_file:
         with open(args.timesteps_file, "r") as f:
@@ -108,21 +120,12 @@ if __name__ == "__main__":
         train_config.validation_timesteps = val_timesteps
 
     save_config_output(args.output_data_path, train_config)
-
-    logging.basicConfig(level=logging.INFO)
-
-    if args.routine == "keras":
-        set_random_seed(train_config.random_seed)
-        fit_kwargs = _keras_fit_kwargs(train_config)
-    else:
-        fit_kwargs = {}
+    set_random_seed(train_config.random_seed)
 
     batches = load_data_sequence(data_path, train_config)
     if args.local_download_path:
         batches = batches.local(args.local_download_path)  # type: ignore
 
-    get_model = getattr(import_module(f"fv3fit.{args.routine}"), "get_model")
-    model_kwargs = _get_model_kwargs(args.routine, train_config)
-    model = get_model(**model_kwargs)
-    model.fit(batches, **fit_kwargs)
+    model = _get_model(train_config)
+    model.fit(batches)
     io.dump(model, args.output_data_path)
