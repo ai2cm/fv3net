775ac0458cef7bfe1f86c6ac3435ef99be360f8c
Andre Perkins <frodre@gmail.com>
2021-01-30 02:11:32 +0000

    Checkpoint on _batch and _utils refactor w/ some test updates

---
+++ external/loaders/loaders/mappers/_emulation.py
@@ -0,0 +1,48 @@
+import logging
+import os
+import fsspec
+import xarray as xr
+from typing import Sequence
+
+from ._xarray import XarrayMapper
+
+logger = logging.getLogger(__name__)
+
+
+def open_phys_emu_training(
+    url: str,
+    init_times: Sequence[str],
+    consolidated: bool = True,
+):
+    """
+    Load training data for the all-physics emulation.  Combines specified initialization
+    time runs together into a single dataset.
+    
+    Args:
+        url (str): path to a nudge-to-obs output directory, remote or local
+        init_times: list of initialization times to combine into a single dataset
+        consolidated (bool): whether zarrs to open have consolidated metadata
+        
+    Returns:
+        mapper to dataset containing nudging physics tendencies
+            and model state data
+        
+    """
+    logger.info(f"Loading emulation training from: {url}")
+    logger.info(f"From times: {init_times}")
+
+    # state_after_timestep currently constists of state after "step_dynamics"
+    dataset_names = ["physics_tendencies.zarr", "state_after_timestep.zarr"]
+    loaded_time_ds = []
+    for init_t in init_times:
+        loaded = []
+        for src in dataset_names:
+            mapper = fsspec.get_mapper(os.path.join(url, init_t, src))
+            ds = xr.open_zarr(mapper, consolidated=consolidated)
+            loaded.append(ds)
+        loaded_time_ds.append(xr.merge(loaded))
+
+    # TODO: Doesn't seem to care about overlapping times with different data
+    full_ds = xr.merge(loaded_time_ds, dim="time")
+
+    return XarrayMapper(full_ds)
--- external/loaders/tests/test__utils.py
+++ external/loaders/tests/test__utils.py
@@ -4,7 +4,9 @@ import xarray as xr
 from loaders._utils import (
     shuffled,
     _get_chunk_indices,
-    stack_dropnan_shuffle,
+    drop_nan,
+    stack,
+    preserve_samples_per_batch,
     nonderived_variables,
     _needs_grid_data,
 )
@@ -50,12 +52,11 @@ def test_gridded_dataset(request):
 @pytest.mark.parametrize(
     "test_gridded_dataset", [(0, 1, 10, 10), (0, 10, 10, 10)], indirect=True,
 )
-def test_stack_dropnan_shuffle_dims(test_gridded_dataset):
-    ds_grid = test_gridded_dataset
-    rs = np.random.RandomState(seed=0)
-    ds_train = stack_dropnan_shuffle(random_state=rs, ds=ds_grid)
+def test_stack_dims(test_gridded_dataset):
+    ds_train = stack(test_gridded_dataset)
     assert set(ds_train.dims) == {"sample", "z"}
-    assert len(ds_train["z"]) == len(ds_grid.z)
+    assert len(ds_train["z"]) == len(test_gridded_dataset.z)
+    assert ds_train["var"].dims[0] == "sample"
 
 
 @pytest.mark.parametrize(
@@ -63,20 +64,19 @@ def test_stack_dropnan_shuffle_dims(test_gridded_dataset):
     [((0, 2, 10, 10), 100), ((10, 2, 10, 10), 90), ((110, 2, 10, 10), 0)],
     indirect=["test_gridded_dataset"],
 )
-def test_stack_dropnan_shuffle_samples(test_gridded_dataset, num_finite_samples):
-    ds_grid = test_gridded_dataset
+def test_dropnan_samples(test_gridded_dataset, num_finite_samples):
+    ds_grid = stack(test_gridded_dataset)
     nan_mask_2d = ~np.isnan(
         ds_grid["var"].sum("z", skipna=False)
     )  # mask if any z coord has nan
     flattened = ds_grid["var"].where(nan_mask_2d).values.flatten()
     finite_samples = flattened[~np.isnan(flattened)]
-    rs = np.random.RandomState(seed=0)
 
     if num_finite_samples == 0:
         with pytest.raises(ValueError):
-            ds_train = stack_dropnan_shuffle(random_state=rs, ds=ds_grid)
+            ds_train = drop_nan(ds_grid)
     else:
-        ds_train = stack_dropnan_shuffle(random_state=rs, ds=ds_grid)
+        ds_train = drop_nan(ds_grid)
         assert len(ds_train["sample"]) == num_finite_samples
         assert set(ds_train["var"].values.flatten()) == set(finite_samples)
 
