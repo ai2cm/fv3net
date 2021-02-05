f197a8af83a005322407859f2c7523ad1fd288cf
Anna Kwa <annak@vulcan.com>
2021-02-05 20:59:46 +0000

    keep some of the offline diags tests

---
+++ tests/training/test_fineres_source.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TRAINING_DATA=gs://vcm-ml-experiments/2020-06-02-fine-res/fine_res_budget/
+OUTPUT=gs://vcm-ml-scratch/annak/2020-05-22/sklearn_train/
+gsutil -m rm -r $OUTPUT
+python -m fv3fit.sklearn \
+    $TRAINING_DATA \
+    tests/training/test_training_regression/train_sklearn_model_fineres_source.yml  \
+    $OUTPUT
