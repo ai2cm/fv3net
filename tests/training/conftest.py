2f39055daf31d1241635417ced979f3770839815
Anna Kwa <annak@vulcan.com>
2021-02-11 16:20:26 -0800

    Delete fvnet/tests aside from integration and large file check (#976)
    * delete fvnet/tests except from integration and large file check
    * add full test of `offline_ml_diags.compute_diags` main function to replace the previous tests removed from `fv3net/tests`

--- tests/training/conftest.py
+++
@@ -1,9 +0,0 @@
-from synth import (  # noqa: F401
-    dataset_fixtures_dir,
-    data_source_name,
-    nudging_dataset_path,
-    fine_res_dataset_path,
-    data_source_path,
-    C48_SHiELD_diags_dataset_path,
-    grid_dataset,
-)
--- workflows/offline_ml_diags/offline_ml_diags/_plot_jacobian.py
+++ workflows/offline_ml_diags/offline_ml_diags/_plot_jacobian.py
@@ -21,38 +21,39 @@ def plot_jacobian(model: fv3fit.keras._models.DenseModel, output_dir: str):
     variables_3d = [var_ for var_ in inputs if jacobian_dict.sizes[var_] > 1]
     variables_2d = [var_ for var_ in inputs if jacobian_dict.sizes[var_] == 1]
 
-    fig, axs = plt.subplots(len(variables_3d), len(outputs), figsize=(12, 12),)
-
-    for i, in_name in enumerate(variables_3d):
-        for j, out_name in enumerate(outputs):
-            logging.debug(f"{in_name}_{out_name}")
-            pane = jacobian_dict[(in_name, out_name)]
-            im = pane.rename(f"{out_name}_from_{in_name}").plot.imshow(
-                x=out_name,
-                y=in_name,
-                ax=axs[i, j],
-                yincrease=False,
-                xincrease=False,
-                add_colorbar=False,
-            )
-            axs[i, j].set_ylabel(f"in ({in_name})")
-            axs[i, j].set_xlabel(f"out ({out_name})")
-            axs[i, j].xaxis.tick_top()
-            axs[i, j].xaxis.set_label_position("top")
-            plt.colorbar(im, ax=axs[i, j])
-    plt.tight_layout()
-    with fsspec.open(os.path.join(output_dir, MATRIX_NAME), "wb") as f:
-        fig.savefig(f)
-    fig, axs = plt.subplots(
-        len(variables_2d), len(outputs), figsize=(12, 12), squeeze=False
-    )
-    for i, in_name in enumerate(variables_2d):
-        for j, out_name in enumerate(outputs):
-            pane = np.asarray(jacobian_dict[(in_name, out_name)])
-            axs[i, j].plot(pane.ravel(), np.arange(pane.size))
-            axs[i, j].set_xlabel(out_name)
-            axs[i, j].set_title(f"change in {in_name}")
-            axs[i, j].set_ylabel("vertical level")
-    plt.tight_layout()
-    with fsspec.open(os.path.join(output_dir, LINE_NAME), "wb") as f:
-        fig.savefig(f)
+    if len(variables_3d) > 0:
+        fig, axs = plt.subplots(len(variables_3d), len(outputs), figsize=(12, 12),)
+        for i, in_name in enumerate(variables_3d):
+            for j, out_name in enumerate(outputs):
+                logging.debug(f"{in_name}_{out_name}")
+                pane = jacobian_dict[(in_name, out_name)]
+                im = pane.rename(f"{out_name}_from_{in_name}").plot.imshow(
+                    x=out_name,
+                    y=in_name,
+                    ax=axs[i, j],
+                    yincrease=False,
+                    xincrease=False,
+                    add_colorbar=False,
+                )
+                axs[i, j].set_ylabel(f"in ({in_name})")
+                axs[i, j].set_xlabel(f"out ({out_name})")
+                axs[i, j].xaxis.tick_top()
+                axs[i, j].xaxis.set_label_position("top")
+                plt.colorbar(im, ax=axs[i, j])
+        plt.tight_layout()
+        with fsspec.open(os.path.join(output_dir, MATRIX_NAME), "wb") as f:
+            fig.savefig(f)
+    if len(variables_2d) > 0:
+        fig, axs = plt.subplots(
+            len(variables_2d), len(outputs), figsize=(12, 12), squeeze=False
+        )
+        for i, in_name in enumerate(variables_2d):
+            for j, out_name in enumerate(outputs):
+                pane = np.asarray(jacobian_dict[(in_name, out_name)])
+                axs[i, j].plot(pane.ravel(), np.arange(pane.size))
+                axs[i, j].set_xlabel(out_name)
+                axs[i, j].set_title(f"change in {in_name}")
+                axs[i, j].set_ylabel("vertical level")
+        plt.tight_layout()
+        with fsspec.open(os.path.join(output_dir, LINE_NAME), "wb") as f:
+            fig.savefig(f)
