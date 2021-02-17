02e77ede0de9054ea92147dab47d5e60c27c95bf
Noah D. Brenowitz <nbren12@gmail.com>
2021-02-17 14:40:41 -0800

    Add packaging information

---
+++ workflows/prognostic_run_diags/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+install_requirements = [
+    "xarray",
+    "holoviews",
+    "fsspec",
+    "gcsfs",
+    "bokeh",
+    "h5netcdf",
+    "cftime",
+    "intake",
+    "vcm",
+]
+
+setup(
+    author="Vulcan Technologies, LLC",
+    author_email="noahb@vulcan.com",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="Prognostic run diagnostics",
+    install_requires=install_requirements,
+    license="BSD license",
+    include_package_data=True,
+    name="fv3net-diagnostics-prog-run",
+    packages=find_packages(),
+    version="0.1.0",
+)
