af8482184c26af973be31ae3d7012971a97f5668
Noah D. Brenowitz <nbren12@gmail.com>
2021-02-19 15:30:26 -0800

    Lint

--- workflows/prognostic_c48_run/docs/conf.py
+++ workflows/prognostic_c48_run/docs/conf.py
@@ -27,11 +27,7 @@ author = "Noah D. Brenowitz"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.graphviz"
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.graphviz"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
