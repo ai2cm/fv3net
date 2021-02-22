ba29e76467ad19919ad995fc93f5409edaa8c407
Jeremy McGibbon <jeremym@vulcan.com>
2021-02-22 09:35:59 -0800

    add load_training_config back to root level

--- external/fv3fit/fv3fit/__init__.py
+++ external/fv3fit/fv3fit/__init__.py
@@ -1,6 +1,6 @@
 from ._shared.predictor import Predictor, Estimator
 from ._shared.io import dump, load
-from ._shared.config import ModelTrainingConfig
+from ._shared.config import ModelTrainingConfig, load_training_config
 from . import keras
 from . import sklearn
 
