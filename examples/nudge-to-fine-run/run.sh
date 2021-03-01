9f6b4cda7c114f8b1b34cd914595ee29711bfa7b
Oliver Watt-Meyer <oliwm@vulcan.com>
2021-03-01 09:43:09 -0800

    Add 'default' diagnostics to examples, fix n2f (#84)
    * Make example n2f use prognostic-run argo workflow
    * Add 'default' diagnostics to VCM examples
    * Add default diagnostics for n2f case
    * Consistent frequency for reference_state.zarr

--- examples/nudge-to-fine-run/run.sh
+++ examples/nudge-to-fine-run/run.sh
@@ -3,12 +3,12 @@
 set -e
 
 EXPERIMENT=2020-10-26-n2f-timescale-sensitivity
-NSEGMENTS=1
 RANDOM=$(openssl rand --hex 6)
 
-argo submit --from workflowtemplate/nudging \
-    -p nudging-config="$(< nudging-config.yaml)" \
-    -p segment-count="${NSEGMENTS}" \
-    -p output-url="gs://vcm-ml-scratch/brianh/${EXPERIMENT}-test" \
-    -p times="$(< output-times-test.json)" \
+argo submit --from workflowtemplate/prognostic-run \
+    -p output="gs://vcm-ml-scratch/oliwm/${EXPERIMENT}-test" \
+    -p config="$(< nudging-config.yaml)" \
+    -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
+    -p initial-condition="20160805.000000" \
+    -p segment-count="1" \
     --name "${EXPERIMENT}-nudge-to-fine-${RANDOM}"
