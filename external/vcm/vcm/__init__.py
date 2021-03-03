646ae4395378fe41fca155760b1f7eb62ab856d3
brianhenn <brianhenn@gmail.com>
2021-03-03 06:17:48 +0000

    refactor to vcm

--- external/vcm/vcm/__init__.py
+++ external/vcm/vcm/__init__.py
@@ -39,6 +39,7 @@ from .xarray_loaders import open_tiles, open_delayed, open_remote_nc, dump_nc
 from .sampling import train_test_split_sample
 from .derived_mapping import DerivedMapping
 from . import metadata
+from .standardize_diagnostics import standardize_gfsphysics_diagnostics
 
 
 __all__ = [item for item in dir() if not item.startswith("_")]
--- workflows/prognostic_run_diags/fv3net/diagnostics/prognostic_run/load_diagnostic_data.py
+++ workflows/prognostic_run_diags/fv3net/diagnostics/prognostic_run/load_diagnostic_data.py
@@ -1,9 +1,8 @@
 import intake
 import logging
-import warnings
 import os
 import xarray as xr
-from typing import List, Iterable, Mapping, Set, Sequence
+from typing import List, Mapping, Sequence
 
 import fsspec
 import vcm
@@ -13,141 +12,8 @@ from fv3net.diagnostics.prognostic_run.constants import DiagArg
 
 logger = logging.getLogger(__name__)
 
-# desired name as keys with set containing sources to rename
-# TODO: could this be tied to the registry?
-DIM_RENAME_INVERSE_MAP = {
-    "x": {"grid_xt", "grid_xt_coarse"},
-    "y": {"grid_yt", "grid_yt_coarse"},
-    "tile": {"rank"},
-    "xb": {"grid_x", "grid_x_coarse", "x_interface"},
-    "yb": {"grid_y", "grid_y_coarse", "y_interface"},
-}
-VARNAME_SUFFIX_TO_REMOVE = ["_coarse"]
-_DIAG_OUTPUT_LOADERS = []
-MASK_VARNAME = "SLMSKsfc"
+
 GRID_ENTRIES = {48: "grid/c48", 96: "grid/c96"}
-
-
-def _adjust_tile_range(ds: xr.Dataset) -> xr.Dataset:
-
-    if "tile" in ds:
-        tiles = ds.tile
-
-        if tiles.isel(tile=-1) == 6:
-            ds = ds.assign_coords({"tile": tiles - 1})
-
-    return ds
-
-
-def _rename_dims(
-    ds: xr.Dataset, rename_inverse: Mapping[str, Set[str]] = DIM_RENAME_INVERSE_MAP
-) -> xr.Dataset:
-
-    varname_target_registry = {}
-    for target_name, source_names in rename_inverse.items():
-        varname_target_registry.update({name: target_name for name in source_names})
-
-    vars_to_rename = {
-        var: varname_target_registry[var]
-        for var in ds.dims
-        if var in varname_target_registry
-    }
-    ds = ds.rename(vars_to_rename)
-    return ds
-
-
-def _set_calendar_to_julian(ds, time_coord="time"):
-    if time_coord in ds.coords:
-        ds[time_coord].attrs["calendar"] = "julian"
-    return ds
-
-
-def _round_to_nearest_second(time: xr.DataArray) -> xr.DataArray:
-    return time.dt.round("1S")
-
-
-def _round_time_coord(ds, time_coord="time"):
-
-    if time_coord in ds.coords:
-        new_times = _round_to_nearest_second(ds[time_coord])
-        ds = ds.assign_coords({time_coord: new_times})
-    else:
-        logger.debug(
-            "Round time operation called on dataset missing a time coordinate."
-        )
-
-    return ds
-
-
-def _set_missing_attrs(ds):
-
-    for var in ds:
-        da = ds[var]
-
-        # True for some prognostic zarrs
-        if "description" in da.attrs and "long_name" not in da.attrs:
-            da.attrs["long_name"] = da.attrs["description"]
-
-        if "long_name" not in da.attrs:
-            da.attrs["long_name"] = var
-
-        if "units" not in da.attrs:
-            da.attrs["units"] = "unspecified"
-    return ds
-
-
-def _remove_name_suffix(ds):
-    for target in VARNAME_SUFFIX_TO_REMOVE:
-        replace_names = {
-            vname: vname.replace(target, "")
-            for vname in ds.data_vars
-            if target in vname
-        }
-
-        warn_on_overwrite(ds.data_vars.keys(), replace_names.values())
-        ds = ds.rename(replace_names)
-    return ds
-
-
-def warn_on_overwrite(old: Iterable, new: Iterable):
-    """
-    Warn if new data keys will overwrite names (e.g., in a xr.Dataset)
-    via an overlap with old keys or from duplication in new keys.
-
-    Args:
-        old: Original keys to check against
-        new: Incoming keys to check for duplicates or existence in old
-    """
-    duplicates = {item for item in new if list(new).count(item) > 1}
-    overlap = set(old) & set(new)
-    overwrites = duplicates | overlap
-    if len(overwrites) > 0:
-        warnings.warn(
-            UserWarning(
-                f"Overwriting keys detected. Overlap: {overlap}"
-                f"  Duplicates: {duplicates}"
-            )
-        )
-
-
-def standardize_gfsphysics_diagnostics(ds):
-
-    for func in [
-        _set_calendar_to_julian,
-        xr.decode_cf,
-        _adjust_tile_range,
-        _rename_dims,
-        _round_time_coord,
-        _remove_name_suffix,
-        _set_missing_attrs,
-    ]:
-        ds = func(ds)
-
-    return ds
-
-
-def _open_tiles(path):
-    return xr.open_mfdataset(path + ".tile?.nc", concat_dim="tile", combine="nested")
 
 
 def load_verification(catalog_keys: List[str], catalog: intake.Catalog,) -> xr.Dataset:
@@ -166,7 +32,7 @@ def load_verification(catalog_keys: List[str], catalog: intake.Catalog,) -> xr.D
     verif_data = []
     for dataset_key in catalog_keys:
         ds = catalog[dataset_key].to_dask()
-        ds = standardize_gfsphysics_diagnostics(ds)
+        ds = vcm.standardize_gfsphysics_diagnostics(ds)
         verif_data.append(ds)
 
     return xr.merge(verif_data, join="outer")
@@ -176,7 +42,7 @@ def _load_standardized(path):
     logger.info(f"Loading and standardizing {path}")
     m = fsspec.get_mapper(path)
     ds = xr.open_zarr(m, consolidated=True, decode_times=False)
-    return standardize_gfsphysics_diagnostics(ds)
+    return vcm.standardize_gfsphysics_diagnostics(ds)
 
 
 def _load_prognostic_run_physics_output(url):
@@ -229,8 +95,10 @@ def load_dycore(
 
     # open grid
     logger.info("Opening Grid Spec")
-    grid_c48 = standardize_gfsphysics_diagnostics(catalog["grid/c48"].to_dask())
-    ls_mask = standardize_gfsphysics_diagnostics(catalog["landseamask/c48"].to_dask())
+    grid_c48 = vcm.standardize_gfsphysics_diagnostics(catalog["grid/c48"].to_dask())
+    ls_mask = vcm.standardize_gfsphysics_diagnostics(
+        catalog["landseamask/c48"].to_dask()
+    )
     grid_c48 = xr.merge([grid_c48, ls_mask])
 
     # open verification
@@ -267,8 +135,10 @@ def load_physics(
 
     # open grid
     logger.info("Opening Grid Spec")
-    grid_c48 = standardize_gfsphysics_diagnostics(catalog["grid/c48"].to_dask())
-    ls_mask = standardize_gfsphysics_diagnostics(catalog["landseamask/c48"].to_dask())
+    grid_c48 = vcm.standardize_gfsphysics_diagnostics(catalog["grid/c48"].to_dask())
+    ls_mask = vcm.standardize_gfsphysics_diagnostics(
+        catalog["landseamask/c48"].to_dask()
+    )
     grid_c48 = xr.merge([grid_c48, ls_mask])
 
     # open verification
