f6db5643d1ed6624ee8c82ba0b26bd3ba2cce125
Jeremy McGibbon <jeremym@vulcan.com>
2021-03-04 17:20:19 -0800

    add simple ensemble model to fv3fit

---
+++ external/fv3fit/fv3fit/_shared/models.py
@@ -0,0 +1,37 @@
+from typing import Iterable
+import yaml
+import os
+import xarray as xr
+from . import io
+from .predictor import Predictor
+
+
+@io.register("ensemble")
+class EnsembleModel(Predictor):
+
+    _CONFIG_FILENAME = "ensemble_model.yaml"
+
+    def __init__(self, models: Iterable[Predictor], reduction: str):
+        self._models = models
+        if reduction.lower() not in ("mean", "median"):
+            raise NotImplementedError(
+                f"only supported reductions are mean and median, got {reduction}"
+            )
+        self._reduction = reduction
+
+    def predict(self, X: xr.Dataset) -> xr.Dataset:
+        """Predict an output xarray dataset from an input xarray dataset."""
+        outputs = [m.predict(X) for m in self._models]
+        ds = xr.concat(outputs, dim="member")
+        if self._reduction == "median":
+            return ds.median(dim="member")
+        else:
+            return ds.mean(dim="member")
+
+    def load(cls, path: str) -> object:
+        """Load a serialized model from a directory."""
+        with open(os.path.join(path, cls._CONFIG_FILENAME), "r") as f:
+            config = yaml.safe_load(f)
+        models = [io.load(path) for path in config["models"]]
+        return cls(models)
+
