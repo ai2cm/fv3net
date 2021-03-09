92d2c9092085f203e45e62c475eb17cd2f765771
Anna Kwa <annak@vulcan.com>
2021-03-09 23:06:45 +0000

    update examples for train-diags-prog template

--- examples/train-evaluate-prognostic-run/run.sh
+++ examples/train-evaluate-prognostic-run/run.sh
@@ -5,15 +5,16 @@ set -e
 EXPERIMENT=2020-test
 
 argo submit \
-    --from workflowtemplate/train-diags-prog \
+    --from workflowtemplate/train-diags-prog-multiple-models \
     -p root=gs://vcm-ml-scratch/$EXPERIMENT \
     -p train-test-data=gs://vcm-ml-experiments/2020-07-30-fine-res \
-    -p training-config="$(< training-config.yaml)" \
+    -p training-configs="$( yq . training-config.yaml )" \
     -p reference-restarts=gs://vcm-ml-experiments/2020-06-02-fine-res/coarsen_restarts \
     -p initial-condition="20160805.000000" \
     -p prognostic-run-config="$(< prognostic-run.yaml)" \
     -p train-times="$(<  ../../train_short.json)" \
     -p test-times="$(<  ../../test_short.json)" \
     -p public-report-output=gs://vcm-ml-public/offline_ml_diags/$EXPERIMENT \
-    -p segment-count=1
+    -p segment-count=1 \
+    --name 2021-03-07-test
 
