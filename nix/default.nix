4da240ae7939b88197d01c79d10c26d37e870646
Noah D. Brenowitz <nbren12@gmail.com>
2021-03-09 19:22:49 -0800

    Figured out how to build fv3gfs-wrapper
    gfortran.cc.lib is the key

--- nix/default.nix
+++ nix/default.nix
@@ -1,20 +1,73 @@
 # Copied from https://nixos.org/nixos/nix-pills/callpackage-design-pattern.html
 let
-  nixpkgs = import <nixpkgs> {};
-  allPkgs = nixpkgs // pkgs;
-  callPackage = path: overrides:
-    let f = import path;
-    in f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
-  packageOverrides = with nixpkgs.python3Packages; callPackage ../python-packages.nix { };
-  python = nixpkgs.python3.override {
-    packageOverrides = packageOverrides;
+  # nixpkgs = import <nixpkgs> {};
+  # allPkgs = nixpkgs // pkgs;
+  # callPackage = path: overrides:
+  #   let f = import path;
+  #   in f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
+  packageOverrides = mpi: fv3: self: super: rec {
+    wrapper = super.callPackage ./wrapper.nix {inherit fv3;};
+    mpi4py = super.mpi4py.override {inherit mpi;};
+    cftime = super.cftime.overrideAttrs (oldAttrs: {
+        pname = "cftime";
+        version="1.2.1";
+        src = super.fetchPypi {
+          pname = "cftime";
+          version="1.2.1";
+          sha256 = "ab5d5076f7d3e699758a244ada7c66da96bae36e22b9e351ce0ececc36f0a57f";
+        };
+      });
+    fv3gfs-util = super.buildPythonPackage {
+      version = "0.1.0";
+      pname = "fv3gfs-wrapper";
+      src = ../external/fv3gfs-util;
+      propagatedBuildInputs = [
+        super.fsspec
+        super.xarray
+        super.zarr
+        super.typing-extensions
+        cftime
+      ];
+
+    };
   };
-  py = python.withPackages (ps: [ ps.pytest ps.pyyaml ps.fv3config ]);
-  pkgs = with nixpkgs; {
-    fms = callPackage ./fms { };
-    esmf = callPackage ./esmf { };
-    nceplibs = callPackage ./nceplibs { };
-    fv3 = callPackage ./fv3 { };
-    python = py;
+  overlay = self: super: with nixpkgs; rec {
+    # ensure that everything uses mpich for mpi
+    fms = super.callPackage ./fms { };
+    esmf = super.callPackage ./esmf { };
+    nceplibs = super.callPackage ./nceplibs { };
+    fv3 = super.callPackage ./fv3 { };
+    python3 = super.python3.override {
+      packageOverrides = packageOverrides mpich fv3;
+    };
+    py = python.withPackages (ps: [ ps.pytest ps.pyyaml ps.wrapper]);
+    wrapper = python.pkgs.wrapper;
   };
-in pkgs
+  nixpkgs = import <nixpkgs> {overlays = [overlay];};
+  mypy = nixpkgs.python3.withPackages (ps: with ps; [pkgconfig jinja2 cython numpy mpi4py setuptools]);
+  shell = with nixpkgs; mkShell {
+  buildInputs = [
+          mypy
+          pkg-config
+          fv3
+          fms
+          esmf
+          nceplibs
+          netcdf
+          netcdffortran
+          lapack
+          blas
+          gfortran 
+          # see this issue: https://discourse.nixos.org/t/building-shared-libraries-with-fortran/11876/2
+          gfortran.cc.lib
+          mpich
+    ];
+
+    MPI="mpich";
+    FC="gfortran";
+    inherit fms;
+    CC="${gfortran}/bin/gcc";
+    inherit gfortran;
+    gfc="${gfortran.cc}";
+};
+in nixpkgs.python3Packages.wrapper
---
+++ nix/wrapper.nix
@@ -0,0 +1,62 @@
+{buildPythonPackage, fv3, cython, jinja2, pkgconfig, numpy, pkg-config, gfortran
+  , fms
+  , esmf
+  , nceplibs
+  , netcdf
+  , netcdffortran
+  , lapack
+  , blas
+  , mpi4py
+  , pyyaml
+  , fv3gfs-util
+}:
+buildPythonPackage rec {
+      version = "0.1.0";
+      pname = "fv3gfs-wrapper";
+
+      src = ../external/fv3gfs-wrapper;
+      checkInputs = [];
+
+      # environmental variables needed for the wrapper
+      PKG_CONFIG_PATH="${fv3}/lib/pkgconfig";
+      MPI = "mpich";
+
+      buildInputs = [
+          fms
+          esmf
+          nceplibs
+          netcdf
+          netcdffortran
+          lapack
+          blas
+          mpi4py.mpi
+          # this is key: https://discourse.nixos.org/t/building-shared-libraries-with-fortran/11876/2
+          gfortran.cc.lib
+          gfortran
+      ];
+
+      nativeBuildInputs =  [
+          pkg-config
+          pkgconfig
+          jinja2
+          cython
+          fv3
+          gfortran
+          fv3gfs-util
+      ];
+
+
+      preBuild = ''
+      echo "RUNNING"
+      make -C lib
+    #   export LDFLAGS="-L${fms}/lib -L${netcdf}/lib -L${netcdffortran}/lib -L${lapack}/lib -L${blas}/lib -L${esmf}/lib -L${mpi4py.mpi}/lib"
+      export LDFLAGS="-shared"
+      export LDSHARED=ld
+      '';
+
+      propagatedBuildInputs = [
+          mpi4py
+          numpy
+          pyyaml
+      ];
+  }
