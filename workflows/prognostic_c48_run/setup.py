c7314831910f9f3b42f0ac54a82bc40e2a8ec754
Oliver Watt-Meyer <oliwm@vulcan.com>
2021-03-18 18:10:23 +0000

    Require fv3config>=v0.7.1

--- workflows/prognostic_c48_run/setup.py
+++ workflows/prognostic_c48_run/setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-requirements = ["fv3fit", "vcm", "jsonschema>=3.2", "dacite >= 1.6", "fv3config>=0.7.0"]
+requirements = ["fv3fit", "vcm", "jsonschema>=3.2", "dacite >= 1.6", "fv3config>=0.7.1"]
 
 test_requirements = ["pytest"]
 
