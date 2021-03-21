c7541598a8604af02bf7f650009547d1b5837171
brianhenn <brianhenn@gmail.com>
2021-03-21 06:57:20 +0000

    specify z dim in interpolating prognostic output

--- workflows/prognostic_run_diags/fv3net/diagnostics/prognostic_run/load_diagnostic_data.py
+++ workflows/prognostic_run_diags/fv3net/diagnostics/prognostic_run/load_diagnostic_data.py
@@ -124,7 +124,9 @@ def load_3d(
         pressure_vars = [var for var in ds.data_vars if "z" in ds[var].dims]
         for var in pressure_vars:
             ds_interp[var] = vcm.interpolate_to_pressure_levels(
-                field=ds[var], delp=ds["pressure_thickness_of_atmospheric_layer"]
+                field=ds[var],
+                delp=ds["pressure_thickness_of_atmospheric_layer"],
+                dim="z",
             )
 
         # open verification
