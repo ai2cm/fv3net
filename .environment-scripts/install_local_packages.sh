65f551e1fd057d952c93e502afb5d98673f8c726
Jeremy McGibbon <jeremym@vulcan.com>
2021-03-31 14:40:06 -0700

    force a rebuild of vcm when making environment/clean build dir

--- .environment-scripts/install_local_packages.sh
+++ .environment-scripts/install_local_packages.sh
@@ -4,6 +4,11 @@ CONDA_ENV=$1
 
 source activate $CONDA_ENV
 
+# we want to force a rebuild in case numpy version changes
+# this doesn't rebuild automatically when dependencies change version
+rm external/vcm/vcm/mappm.*.so
+rm -r external/vcm/build
+
 local_packages_to_install=( 
   external/vcm
   external/loaders
