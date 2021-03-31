2b46d558520ebeba2e9869f339866fd280d5a291
Noah D. Brenowitz <nbren12@gmail.com>
2021-03-31 16:14:25 -0700

    extract data path logic into helper function

--- external/loaders/loaders/mappers/_hybrid.py
+++ external/loaders/loaders/mappers/_hybrid.py
@@ -2,7 +2,10 @@ from typing import Mapping, List, TypeVar
 import xarray as xr
 
 from ._base import GeoMapper
-from ._nudged import open_merged_nudged_full_tendencies, open_nudged_to_obs_prognostic
+from ._nudged import (
+    open_merged_nudged_full_tendencies,
+    open_nudged_to_obs_prognostic,
+)
 from ._fine_resolution_budget import (
     FineResolutionSources,
     open_fine_res_apparent_sources,
@@ -122,9 +125,16 @@ def _open_fine_resolution_nudging_hybrid(
     Returns:
         a mapper
     """
+    nudging, fine_res = _inject_data_paths(data_paths, nudging, fine_res)
+    nudged = open_merged_nudged_full_tendencies(**nudging)
+    offset_seconds = fine_res.pop("offset_seconds", 450)
+    fine_res = open_fine_res_apparent_sources(offset_seconds=offset_seconds, **fine_res)
+    return mapper(nudged, fine_res, **kwargs)
+
+
+def _inject_data_paths(data_paths, nudging, fine_res):
     nudging = nudging or {}
     fine_res = fine_res or {}
-    offset_seconds = fine_res.pop("offset_seconds", 450)
     if isinstance(data_paths, (tuple, List)) and len(data_paths) == 2:
         nudging["url"] = data_paths[0]
         fine_res["fine_res_url"] = data_paths[1]
@@ -136,10 +146,7 @@ def _open_fine_resolution_nudging_hybrid(
                 "i) data_paths list arg of len 2, or ii) keys in nudging kwargs and "
                 "fine res kwargs."
             )
-
-    nudged = open_merged_nudged_full_tendencies(**nudging)
-    fine_res = open_fine_res_apparent_sources(offset_seconds=offset_seconds, **fine_res)
-    return mapper(nudged, fine_res, **kwargs)
+    return nudging, fine_res
 
 
 def open_fine_resolution_nudging_hybrid(
