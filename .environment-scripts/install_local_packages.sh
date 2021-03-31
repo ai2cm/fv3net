5d453e47c6b12d1cb5648602b33a4bf9bc1d47f4
Jeremy McGibbon <jeremym@vulcan.com>
2021-03-31 14:57:03 -0700

    Update .environment-scripts/install_local_packages.sh

--- .environment-scripts/install_local_packages.sh
+++ .environment-scripts/install_local_packages.sh
@@ -6,8 +6,8 @@ source activate $CONDA_ENV
 
 # we want to force a rebuild in case numpy version changes
 # this doesn't rebuild automatically when dependencies change version
-rm external/vcm/vcm/mappm.*.so
-rm -r external/vcm/build
+rm -f external/vcm/vcm/mappm.*.so
+rm -rf external/vcm/build
 
 local_packages_to_install=( 
   external/vcm
