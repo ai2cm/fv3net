9bfdc1660ec9b9b421dda17e7485ef0ba6e05edc
Noah D. Brenowitz <nbren12@gmail.com>
2021-04-01 13:55:08 -0700

    Fix referene to load_schema

--- external/synth/synth/_nudging.py
+++ external/synth/synth/_nudging.py
@@ -4,5 +4,5 @@ from . import schemas
 
 def generate_nudging(outdir: str):
     ranges = {"pressure_thickness_of_atmospheric_layer": Range(0.99, 1.01)}
-    directory_schema = schemas.load_schema_directory("nudge_to_fine")
+    directory_schema = schemas.load_directory_schema_from_library("nudge_to_fine")
     write_directory_schema(outdir, directory_schema, ranges)
