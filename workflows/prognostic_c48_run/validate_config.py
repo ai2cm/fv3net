539015e832e1b0897d8277c13b834532020c7236
Oliver Watt-Meyer <oliwm@vulcan.com>
2021-04-06 00:56:13 +0000

    Lint and fix typing

--- workflows/prognostic_c48_run/validate_config.py
+++ workflows/prognostic_c48_run/validate_config.py
@@ -1,10 +1,8 @@
 from datetime import datetime, timedelta
-import os
-from typing import Mapping, Union
+from typing import Any, Dict, Union
 
 import dacite
 import fv3config
-import fsspec
 from runtime.config import UserConfig, DiagnosticFileConfig
 from runtime.diagnostics.fortran import FortranFileConfig
 
@@ -13,14 +11,17 @@ class ConfigValidationError(ValueError):
     pass
 
 
-def validate_chunks(config_dict: Mapping):
+def validate_chunks(config_dict: Dict[str, Any]):
     user_config = dacite.from_dict(UserConfig, config_dict)
     run_duration = fv3config.get_run_duration(config_dict)
     initial_time = datetime(*config_dict["namelist"]["coupler_nml"]["current_date"])
     timestep = timedelta(seconds=config_dict["namelist"]["coupler_nml"]["dt_atmos"])
 
-    for diag_file_config in user_config.diagnostics + user_config.fortran_diagnostics:
+    for diag_file_config in user_config.diagnostics:
         _validate_time_chunks(diag_file_config, initial_time, timestep, run_duration)
+
+    for fortran_file_config in user_config.fortran_diagnostics:
+        _validate_time_chunks(fortran_file_config, initial_time, timestep, run_duration)
 
 
 def _validate_time_chunks(
@@ -35,7 +36,7 @@ def _validate_time_chunks(
         time_container = diag_file_config.times.time_container(initial_time)
         num_output_timesteps = sum(
             time_container.indicator(t) is not None for t in all_times
-        )
+        )  # type: ignore
         if num_output_timesteps % diag_file_config.chunks["time"] != 0:
             raise ConfigValidationError(
                 "Time dimension size must be a multiple of time chunk size. This was "
