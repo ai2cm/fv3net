ai2cm/fv3net

commit 92f22e7f8a5ca14b3f0affba32b09d2dfc8f7bd5

pre commit hooks (#1160)
* Install pre-commit

The licenses for the added dependencies are on the pre-approved
list:

    appnope - BSD
    cfgv - MIT
    identify - MIT
    nodeenv - BSD
    pre-commit - MIT

* add pre-commit configuration

this configurations replaces the custom make lint target,
and large-file script

* fix typo in circle ci config

* remove dependency on check-file-size

.pre-commit-config.yaml
@@ -1,0 +1,26 @@
+repos:
+-   repo: https://github.com/pre-commit/pre-commit-hooks
+    rev: v3.4.0
+    hooks:
+    - id: check-added-large-files
+      args: [--enforce-all, --maxkb=250]
+      exclude: "\
+      workflows/fine_res_budget/tests/diag.json|\
+      external/vcm/tests/test_data/test_data.tar"
+-   repo: https://github.com/psf/black
+    rev: 19.10b0
+    hooks:
+    - id: black
+      args: [--check, --diff]
+-   repo: https://gitlab.com/pycqa/flake8
+    rev: 3.7.9
+    hooks:
+    - id: flake8
+      name: flake8
+      language_version: python3
+      exclude: "__init__.py"
+    - id: flake8
+      name: flake8 __init__.py files
+      files: "__init__.py"
+      # ignore unused import error in __init__.py files
+      args: ["--ignore=F401,E203"]

Makefile
@@ -173,20 +173,15 @@ create_environment:
 # Linting
 ############################################################
 
-PYTHON_FILES = $(shell git ls-files | grep -e 'py$$' | grep -v -e '__init__.py')
-PYTHON_INIT_FILES = $(shell git ls-files | grep '__init__.py')
-
-check_file_size:
-	./tests/check_for_large_files.sh
+setup-hooks:
+	pip install -c constraints.txt pre-commit
+	pre-commit install
 
 typecheck:
 	./check_types.sh
 
-lint: check_file_size
-	black --diff --check $(PYTHON_FILES) $(PYTHON_INIT_FILES)
-	flake8 $(PYTHON_FILES)
-	# ignore unused import error in __init__.py files
-	flake8 --ignore=F401 E203 $(PYTHON_INIT_FILES)
+lint:
+	pre-commit run --all-files
 	@echo "LINTING SUCCESSFUL"
 
 reformat:

.circleci/config.yml
@@ -132,7 +132,7 @@ jobs:
       - image: circleci/python:3.7
     steps:
       - checkout
-      - run: sudo pip install black==19.10b0 flake8==3.7.9
+      - run: sudo pip install -c constraints.txt pre-commit
       - run: make lint
 
   integration_tests:

tests/check_for_large_files.sh
@@ -1,35 +1,0 @@
-#!/bin/bash
-
-whiteList="\
-external/vcm/tests/test_data/test_data.tar \
-workflows/fine_res_budget/tests/gfsphysics.json \
-workflows/fine_res_budget/tests/diag.json \
-workflows/fine_res_budget/tests/restart.json \
-workflows/prognostic_c48_run/tests/input_data/inputs_4x4.nc
-"
-
-thresholdKb=250
-
-ret=0
-
-while IFS= read -r -d $'\0' file
-do
-    size=$(du -k "$file" | awk -F' ' '{print $1}')
-
-    [[ -f "$file" ]] || continue
-
-    if [[ "$size" -ge "$thresholdKb" ]]
-    then
-        # check if present in whitelist
-        echo "$whiteList" | grep -w "$file" > /dev/null
-        inWhiteList=$?
-        if [[ $inWhiteList -ne 0 ]]
-        then
-            echo "Large file found: \"$file\" is $size kb"
-            ret=1
-        fi
-    fi
-
-done < <(git ls-files -z)
-
-exit $ret

commit 0dd937fa4473e884e3870bfcafac990a6e93c241

Reformat in pre-commit checks
92f22e7f8a5ca14b3f0affba32b09d2dfc8f7bd5 broke the reformat makefile rule.
Also, now that some time has passed, I think we can make the reformatting
happen during the commit hook. This will make it easier to reformat the code
without changing directories to the root.

.pre-commit-config.yaml
@@ -12,7 +12,6 @@ repos:
     rev: 19.10b0
     hooks:
     - id: black
-      args: [--check, --diff]
 -   repo: https://gitlab.com/pycqa/flake8
     rev: 3.7.9
     hooks:

Makefile
@@ -188,7 +188,7 @@ lint:
 	@echo "LINTING SUCCESSFUL"
 
 reformat:
-	black $(PYTHON_FILES) $(PYTHON_INIT_FILES)
+	pre-commit run --all-files black
 
 #################################################################################
 # Self Documenting Commands                                                     #