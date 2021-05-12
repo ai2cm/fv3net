50778a7af95ea9eaf9a79d157362c7788386d3b4
Noah D. Brenowitz <nbren12@gmail.com>
2021-05-11 20:58:24 -0700

    generate offline report

--- workflows/train_hybrid/train.sh
+++ workflows/train_hybrid/train.sh
@@ -2,6 +2,7 @@
 
 TRAINED_ML=/Users/noah/data/gs/vcm-ml-archive/noahb/hybrid-fine-res/trained_ml
 OFFLINE_DIAGS=/Users/noah/data/gs/vcm-ml-archive/noahb/hybrid-fine-res/offline_diags
+OFFLINE_REPORTS=/Users/noah/data/gs/vcm-ml-archive/noahb/hybrid-fine-res/offline_reports
 
 function train {
     config="$1"
@@ -17,20 +18,27 @@ function train {
 }
 
 function evaluate {
-    python -m offline_ml_diags.compute_diags \
-        "$1" \
-        "$2" \
-        --timesteps-file test.json
+    if ! [ -d "$2" ]; then
+        python -m offline_ml_diags.compute_diags \
+            "$1" \
+            "$2" \
+            --timesteps-file test.json
+    fi
 }
 
+function offlineReport {
+    if ! [ -d "$2" ]; then
+        python -m offline_ml_diags.create_report "$1" "$2"
+    fi
+}
 
-# train training.yml $TRAINED_ML/full_atmos_input
-# train training-lower.yml $TRAINED_ML/lower
+train training.yml $TRAINED_ML/full_atmos_input
+train training-lower.yml $TRAINED_ML/lower
 
 mkdir -p $OFFLINE_DIAGS
 for model in full_atmos_input lower
 do
-    if ! [ -d $OFFLINE_DIAGS/$model ]; then
-        evaluate $TRAINED_ML/$model $OFFLINE_DIAGS/$model
-    fi
+    evaluate $TRAINED_ML/$model $OFFLINE_DIAGS/$model
+    offlineReport $OFFLINE_DIAGS/$model $OFFLINE_REPORTS/$model
+    echo "Offline report: $OFFLINE_REPORTS/$model/index.html"
 done
