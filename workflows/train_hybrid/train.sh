c2869f3275b7d2b78fd4b173f7ce365bea4e7702
Noah D. Brenowitz <nbren12@gmail.com>
2021-05-11 17:00:31 -0700

    train and evaluate a lower atmospheric model

--- workflows/train_hybrid/train.sh
+++ workflows/train_hybrid/train.sh
@@ -1,5 +1,36 @@
-python -m fv3fit.train \
-    --timesteps-file train.json \
-    /Users/noah/data/gs/vcm-ml-archive/noahb/hybrid-fine-res/2021-05-05-hybrid-training.zarr \
-    training.yml \
-    /Users/noah/data/gs/vcm-ml-archive/noahb/hybrid-fine-res/trained_ml/full_atmos_input
+#!/bin/bash
+
+TRAINED_ML=/Users/noah/data/gs/vcm-ml-archive/noahb/hybrid-fine-res/trained_ml
+OFFLINE_DIAGS=/Users/noah/data/gs/vcm-ml-archive/noahb/hybrid-fine-res/offline_diags
+
+function train {
+    config="$1"
+    out="$2"
+
+    if ! [[ -d $out ]]; then
+        python -m fv3fit.train \
+            --timesteps-file train.json \
+            /Users/noah/data/gs/vcm-ml-archive/noahb/hybrid-fine-res/2021-05-05-hybrid-training.zarr \
+            "$config" \
+            "$out"
+    fi
+}
+
+function evaluate {
+    python -m offline_ml_diags.compute_diags \
+        "$1" \
+        "$2" \
+        --timesteps-file test.json
+}
+
+
+# train training.yml $TRAINED_ML/full_atmos_input
+# train training-lower.yml $TRAINED_ML/lower
+
+mkdir -p $OFFLINE_DIAGS
+for model in full_atmos_input lower
+do
+    if ! [ -d $OFFLINE_DIAGS/$model ]; then
+        evaluate $TRAINED_ML/$model $OFFLINE_DIAGS/$model
+    fi
+done
