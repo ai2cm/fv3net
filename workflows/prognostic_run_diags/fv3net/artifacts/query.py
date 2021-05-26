c3c0de51f9b6f69664d44df2937259b250d58bef
Noah D. Brenowitz <nbren12@gmail.com>
2021-05-25 23:24:45 -0700

    add artifacts CLI for querying buckets
    gsutil is the main tool we use for querying our experiments. This approach is
    somewhat laborious tightly couples our queries to data storage particulars.
    
    The argo workflows save data in a structured format, that is somewhat less
    human readable than before.
    
    * new CLI `artifacts` which can easily query artifacts

---
+++ workflows/prognostic_run_diags/fv3net/artifacts/query.py
@@ -0,0 +1,142 @@
+import argparse
+import asyncio
+import dataclasses
+import datetime
+import os
+import pathlib
+
+import fsspec
+
+import gcsfs
+
+
+@dataclasses.dataclass
+class Step:
+    fs: fsspec.AbstractFileSystem
+    path: pathlib.Path
+    date_created: datetime.date
+    project: str
+    tag: str
+    step: str
+
+
+async def flat_gather(promises):
+    out = await asyncio.gather(*promises)
+    return sum(out, [])
+
+
+async def _get_runs_with_tag(f, tag, date, project):
+    out = []
+    for step in await _list(f, tag):
+        path = pathlib.Path(step)
+        if path.name in ["fv3gfs_run", "fv3gfs_run_diagnostics"]:
+            out.append(
+                Step(
+                    f,
+                    path,
+                    date,
+                    pathlib.Path(project).name,
+                    pathlib.Path(tag).name,
+                    path.name,
+                )
+            )
+    return out
+
+
+async def _get_runs_in_date(f, path_with_date, project):
+    path = pathlib.Path(path_with_date)
+
+    try:
+        date = datetime.date.fromisoformat(path.name)
+    except ValueError:
+        return []
+
+    return await flat_gather(
+        [
+            _get_runs_with_tag(f, tag, date, project)
+            for tag in await _list(f, path.as_posix())
+        ]
+    )
+
+
+async def _get_runs(f, bucket, projects):
+
+    top_level = [os.path.join(bucket, project) for project in set(projects)]
+
+    files = []
+
+    for project in top_level:
+        total = await flat_gather(
+            [_get_runs_in_date(f, obj, project) for obj in await _list(f, project)]
+        )
+
+        files.extend(total)
+    return files
+
+
+async def _list(f: fsspec.AbstractFileSystem, path):
+    try:
+        return await f._ls(path)
+    except AttributeError:
+        return f.ls(path)
+
+
+async def _close_session(f):
+    try:
+        await f.session.close()
+    except AttributeError:
+        pass
+
+
+def get_artifacts(bucket, projects):
+    loop = asyncio.get_event_loop()
+    if bucket.startswith("gs://"):
+        f = gcsfs.GCSFileSystem(asynchronous=True)
+    else:
+        f = fsspec.filesystem("file")
+
+    ans = loop.run_until_complete(_get_runs(f, bucket, projects or ["default"]))
+    loop.run_until_complete(_close_session(f))
+    return ans
+
+
+def list(args):
+    for run in get_artifacts(args.bucket, args.project):
+        if (run.step in args.step or not args.step) and (
+            run.tag == args.tag or not args.tag
+        ):
+            print(
+                run.project,
+                run.date_created,
+                run.tag,
+                run.step,
+                "gs://" + run.path.as_posix(),
+            )
+
+
+def register_parser(parser):
+    parser.add_argument(
+        "step", nargs="*", help="One of fv3gfs_run, fv3gfs_run_diagnostics"
+    )
+    parser.add_argument(
+        "-b",
+        "--bucket",
+        help="Bucket. Default: gs://vcm-ml-experiments",
+        default="gs://vcm-ml-experiments",
+    )
+    parser.add_argument(
+        "-p",
+        "--project",
+        help="Include project in list. Can be passed multiple times. Default: default",
+        action="append",
+        default=[],
+    )
+    parser.add_argument("-t", "--tag", help="Experiment tag", default=None)
+    parser.set_defaults(func=list)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Query the experiments buckets.")
+    register_parser(parser)
+    args = parser.parse_args()
+    args.func(args)
---
+++ workflows/prognostic_run_diags/fv3net/artifacts/tests/test_query.py
@@ -0,0 +1,25 @@
+import sys
+from fv3net.artifacts.query import get_artifacts, Step, main
+import pathlib
+
+import pytest
+
+
+local_experiment_root = (
+    (pathlib.Path(__file__).parent / "experiment-root").absolute().as_posix()
+)
+
+
+@pytest.mark.parametrize("exp_root", [local_experiment_root, "gs://vcm-ml-experiments"])
+def test_get_runs(exp_root):
+    steps = get_artifacts(exp_root, ["default"])
+
+    assert steps
+    for step in steps:
+        assert isinstance(step, Step)
+
+
+def test_cli(regtest, monkeypatch):
+    monkeypatch.setattr(sys, "argv", ["artifacts", "--bucket", local_experiment_root])
+    with regtest:
+        main()
