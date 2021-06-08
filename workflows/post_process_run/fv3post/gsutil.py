88f6a6ef58406724b5528c84bdb3b28c87f355ac
Spencer Clark <spencerkclark@gmail.com>
2021-06-08 22:08:37 +0000

    Add retry logic for upload_dir

--- workflows/post_process_run/fv3post/gsutil.py
+++ workflows/post_process_run/fv3post/gsutil.py
@@ -1,6 +1,12 @@
 import os
 import subprocess
 
+import backoff
+
+
+class GSUtilResumableUploadException(Exception):
+    pass
+
 
 def authenticate():
     try:
@@ -13,8 +19,17 @@ def authenticate():
         )
 
 
+@backoff.on_exception(backoff.expo, GSUtilResumableUploadException, max_tries=3)
 def upload_dir(d, dest):
-    subprocess.check_call(["gsutil", "-m", "rsync", "-r", "-e", d, dest])
+    try:
+        subprocess.check_output(
+            ["gsutil", "-m", "rsync", "-r", "-e", d, dest], stderr=subprocess.STDOUT
+        )
+    except subprocess.CalledProcessError as e:
+        if "ResumableUploadException" in e.output:
+            raise GSUtilResumableUploadException()
+        else:
+            raise e
 
 
 def download_directory(dir_, dest):
---
+++ workflows/post_process_run/tests/test_gsutil.py
@@ -0,0 +1,31 @@
+import subprocess
+
+import pytest
+
+from subprocess import CalledProcessError
+from unittest.mock import MagicMock
+from fv3post.gsutil import upload_dir, GSUtilResumableUploadException
+
+
+UPLOAD_EXCEPTION = CalledProcessError(-1, None, output="ResumableUploadException")
+OTHER_EXCEPTION = CalledProcessError(-1, None, output="Other")
+
+
+@pytest.mark.parametrize(
+    ("side_effect", "exception", "call_count"),
+    [
+        (UPLOAD_EXCEPTION, GSUtilResumableUploadException, 3),
+        (OTHER_EXCEPTION, CalledProcessError, 1),
+        (None, None, 1),
+        ([UPLOAD_EXCEPTION, None], None, 2),
+    ],
+)
+def test_upload_dir(monkeypatch, side_effect, exception, call_count):
+    mock = MagicMock(side_effect=side_effect)
+    monkeypatch.setattr(subprocess, "check_output", mock)
+    if exception is not None:
+        with pytest.raises(exception):
+            upload_dir("foo", "bar")
+    else:
+        upload_dir("foo", "bar")
+    assert mock.call_count == call_count
