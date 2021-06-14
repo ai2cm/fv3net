f032eda58342180dc3d73770bdfa64e15820c572
Noah D. Brenowitz <nbren12@gmail.com>
2021-06-14 16:11:05 -0700

    Remove gcsfs subtree and update (#1258)
    The latest releases of gcsfs include the bug commits we contributed, so we no
    longer need the subtree
    
    * Revert "gcsfs updates (#1191)"
    
    This reverts commit edc9ae121baa41dafd03e00a98d4861ca3525300.
    
    * Revert "gcsfs: install as a subtree"
    
    This reverts commit dd610280df78e5d2b2351cb95dd0395963154541.
    
    * update gcsfs to 2021.6.0
    
    * update dataflow job gcsfs version to 2021.6.0

--- external/gcsfs/gcsfs/tests/test_credentials.py
+++
@@ -1,9 +0,0 @@
-from gcsfs.credentials import GoogleCredentials
-from gcsfs.tests.utils import my_vcr
-
-
-@my_vcr.use_cassette(match=["all"])
-def test_GoogleCredentials_None():
-    credentials = GoogleCredentials(project=None, token=None, access="read_only")
-    headers = {}
-    credentials.apply(headers)
