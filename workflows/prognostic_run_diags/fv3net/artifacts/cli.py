6e39997fbb491be877d65b45ec3f974bd750f4c8
Oliver Watt-Meyer <oliwm@vulcan.com>
2021-06-24 23:14:28 +0000

    Refactor artifacts CLI to new cli.py module

---
+++ workflows/prognostic_run_diags/fv3net/artifacts/cli.py
@@ -0,0 +1,19 @@
+import argparse
+from fv3net.artifacts import query
+from fv3net.artifacts import report_search
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Query available experiment and report output."
+    )
+    subparsers = parser.add_subparsers(required=True, dest="command")
+    query.register_parser(subparsers)
+    report_search.register_parser(subparsers)
+    return parser
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    args.func(args)
