fix sign error in density and relative humidity computation

workflows/prognostic_c48_run/runtime/emulator/thermo.py
@@ -29,11 +29,11 @@ def specific_humidity_from_rh(
 
 
 def density(delp, delz):
-    return delp / delz / _GRAVITY
+    return tf.abs(delp / delz / _GRAVITY)
 
 
 def pressure_thickness(rho, delz):
-    return rho * delz * _GRAVITY
+    return tf.abs(rho * delz * _GRAVITY)
 
 
 class ThermoBasis: