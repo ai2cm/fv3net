032beffdace9f4bf9bb859084e29be282e2529ea
Oliver Watt-Meyer <oliwm@vulcan.com>
2021-06-29 20:13:04 +0000

    Handle missing diagnostics in metrics.py

--- workflows/prognostic_run_diags/fv3net/diagnostics/prognostic_run/metrics.py
+++ workflows/prognostic_run_diags/fv3net/diagnostics/prognostic_run/metrics.py
@@ -30,7 +30,7 @@ def grab_diag(ds, name):
             replace_dict[var] = var[: -len(match)]
 
     if len(replace_dict) == 0:
-        raise ValueError(f"No diagnostics with name {name} found.")
+        return xr.Dataset()
 
     return ds[list(replace_dict.keys())].rename(replace_dict)
 
@@ -98,7 +98,8 @@ for day in [3, 5]:
     @add_to_metrics(f"rmse_{day}day")
     def rmse_on_day(diags, day=day):
         rms_global = grab_diag(diags, "rms_global").drop(GRID_VARS, errors="ignore")
-
+        if len(rms_global) == 0:
+            return xr.Dataset()
         rms_global_daily = rms_global.resample(time="1D").mean()
 
         try:
@@ -113,6 +114,8 @@ for day in [3, 5]:
 @add_to_metrics("rmse_days_3to7_avg")
 def rmse_days_3to7_avg(diags):
     rms_global = grab_diag(diags, "rms_global").drop(GRID_VARS, errors="ignore")
+    if len(rms_global) == 0:
+        return xr.Dataset()
     rms_global_daily = rms_global.resample(time="1D").mean()
     if rms_global_daily.sizes["time"] >= 7:
         rmse_days_3to7_avg = rms_global_daily.isel(time=slice(2, 7)).mean("time")
@@ -128,6 +131,8 @@ def drift_3day(diags):
     averages = grab_diag(diags, "spatial_mean_dycore_global").drop(
         GRID_VARS, errors="ignore"
     )
+    if len(averages) == 0:
+        return xr.Dataset()
 
     daily = averages.resample(time="1D").mean()
 
@@ -147,6 +152,8 @@ for mask_type in ["global", "land", "sea"]:
     @add_to_metrics(f"time_and_{mask_type}_mean_value")
     def time_and_global_mean_value(diags, mask_type=mask_type):
         time_mean_value = grab_diag(diags, "time_mean_value")
+        if len(time_mean_value) == 0:
+            return xr.Dataset()
         masked_area = _mask_array(mask_type, diags["area"], diags["land_sea_mask"])
         time_and_global_mean_value = weighted_mean(
             time_mean_value, masked_area, HORIZONTAL_DIMS
@@ -160,6 +167,8 @@ for mask_type in ["global", "land", "sea"]:
     @add_to_metrics(f"time_and_{mask_type}_mean_bias")
     def time_and_domain_mean_bias(diags, mask_type=mask_type):
         time_mean_bias = grab_diag(diags, f"time_mean_bias")
+        if len(time_mean_bias) == 0:
+            return xr.Dataset()
         masked_area = _mask_array(mask_type, diags["area"], diags["land_sea_mask"])
         time_and_domain_mean_bias = weighted_mean(
             time_mean_bias, masked_area, HORIZONTAL_DIMS
@@ -173,6 +182,8 @@ for mask_type, suffix in zip(["global", "land", "sea"], ["", "_land", "_sea"]):
     @add_to_metrics(f"rmse_of_time_mean{suffix}")
     def rmse_time_mean(diags, mask_type=mask_type):
         time_mean_bias = grab_diag(diags, f"time_mean_bias")
+        if len(time_mean_bias) == 0:
+            return xr.Dataset()
         masked_area = _mask_array(mask_type, diags["area"], diags["land_sea_mask"])
         rms_of_time_mean_bias = np.sqrt(
             weighted_mean(time_mean_bias ** 2, masked_area, HORIZONTAL_DIMS)
@@ -186,6 +197,8 @@ for percentile in PERCENTILES:
     @add_to_metrics(f"percentile_{percentile}")
     def percentile_metric(diags, percentile=percentile):
         histogram = grab_diag(diags, "histogram")
+        if len(histogram) == 0:
+            return xr.Dataset()
         percentiles = xr.Dataset()
         data_vars = [v for v in histogram.data_vars if not v.endswith("bin_width")]
         for varname in data_vars:
