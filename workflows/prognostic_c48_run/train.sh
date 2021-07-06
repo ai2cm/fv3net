41acf57b1896078d048d1a19b9b4b16911785717
Noah D. Brenowiz <nbren12@gmail.com>
2021-07-06 12:47:47 -0700

    Download fv3net in training script
    Updating the prognostic run images for minor changes in pure python
    source code is slow.
    
    Download the fv3net commit pointed to be the environmental variable SHA
    before training.

--- workflows/prognostic_c48_run/train.sh
+++ workflows/prognostic_c48_run/train.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+# Environmental variables used: SHA
 
 set -e
-echo "Pulling data with dvc" > /dev/stderr
-dvc config cache.type hardlink,symlink
-dvc pull --verbose data/training data/validation
-echo "Starting Training" > /dev/stderr
-./train.py $@
+
+function cleanUp {
+    echo "Cleaning up $DIR"
+    rm -r "$DIR"
+}
+
+DIR=$(mktemp -d)
+echo "Preparing run in $DIR" > /dev/stderr
+git clone https://github.com/VulcanClimateModeling/fv3net "$DIR"
+trap  cleanUp EXIT
+(
+    cd "$DIR"
+    git checkout "$SHA"
+    cd workflows/prognostic_c48_run
+    echo "Pulling data with dvc" > /dev/stderr
+    dvc cache dir /mnt/dvc
+    dvc config cache.type hardlink,symlink
+    dvc pull --verbose data/training data/validation
+    echo "Starting Training" > /dev/stderr
+    ./train.py $@
+)
