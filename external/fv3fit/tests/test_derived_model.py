3d3ce3df2e894a21ddce919ab3c200176c7e9a67
Anna Kwa <annak@vulcan.com>
2021-07-07 22:24:45 +0000

    fix var name in test

--- external/fv3fit/tests/test_derived_model.py
+++ external/fv3fit/tests/test_derived_model.py
@@ -24,7 +24,7 @@ def test_derived_prediction():
     derived_model = DerivedModel(
         base_model,
         additional_input_variables=["surface_diffused_shortwave_albedo"],
-        derived_output_variables=["net_downward_shortwave_sfc_flux_derived"],
+        derived_output_variables=["net_shortwave_sfc_flux_derived"],
     )
     ds_in = xr.Dataset(
         data_vars={
@@ -35,14 +35,14 @@ def test_derived_prediction():
         }
     )
     prediction = derived_model.predict(ds_in.stack(sample=["x", "y"]))
-    assert "net_downward_shortwave_sfc_flux_derived" in prediction
+    assert "net_shortwave_sfc_flux_derived" in prediction
 
 
 def test_derived_alert_to_missing_additional_input():
     derived_model = DerivedModel(
         base_model,
         additional_input_variables=["surface_diffused_shortwave_albedo"],
-        derived_output_variables=["net_downward_shortwave_sfc_flux_derived"],
+        derived_output_variables=["net_shortwave_sfc_flux_derived"],
     )
     ds_in = xr.Dataset(
         data_vars={"input": xr.DataArray(np.zeros([3, 3, 5]), dims=["x", "y", "z"])}
