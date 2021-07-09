1abd3f858ea7f4d925091d8ba5f1cf87eaeb6264
Andre Perkins <frodre@gmail.com>
2021-07-09 00:46:17 +0000

    Test loading functions (batched ds not working yet)

--- external/fv3fit/fv3fit/emulation/data/load.py
+++ external/fv3fit/fv3fit/emulation/data/load.py
@@ -1,18 +1,15 @@
 import logging
 import os
-import numpy as np
 import tensorflow as tf
-import xarray as xr
-from toolz import compose_left
-from typing import Any, Callable, List, Optional, Sequence, Union
+from typing import Callable, List,  Sequence
 
-from vcm import open_remote_nc, get_fs
+from vcm import get_fs
 
 
 logger = logging.getLogger(__name__)
 
 
-def get_nc_files(path) -> List:
+def get_nc_files(path: str) -> List[str]:
 
     fs = get_fs(path)
     files = list(fs.glob(os.path.join(path, "*.nc")))
@@ -20,22 +17,15 @@ def get_nc_files(path) -> List:
     return files
 
 
-def _get_generator_constructor(batched_source, processing_func):
+def batched_to_tf_dataset(
+    batched_source: Sequence,
+    transform: Callable,
+) -> tf.data.Dataset:
 
     def get_generator():
         for batch in batched_source:
-            output = processing_func(batch)
+            output = transform(batch)
             yield tf.data.Dataset.from_tensor_slices(output)
-
-    return get_generator
-
-
-def batched_to_tf_dataset(
-    batched_source: Sequence,
-    processing_func: Callable[[Any], Union[tf.Tensor, np.ndarray]],
-) -> tf.data.Dataset:
-
-    get_generator = _get_generator_constructor(batched_source, processing_func)
 
     peeked = next(get_generator())
     signature = tf.data.DatasetSpec.from_value(peeked)
@@ -48,10 +38,3 @@ def batched_to_tf_dataset(
     tf_ds = tf_ds.prefetch(tf.data.AUTOTUNE).interleave(lambda x: x)
 
     return tf_ds
-
-
-def nc_dir_to_tf_dataset(path, preprocessing_func, num_parallel_calls=None):
-
-    files: List = get_nc_files(path)
-    preproc = compose_left(*[open_netcdf, preprocessing_func])
-    return batched_to_tf_dataset(files, preproc, num_parallel_calls=num_parallel_calls)
---
+++ external/fv3fit/tests/emulation/test_data_load.py
@@ -0,0 +1,49 @@
+import os
+import pytest
+import tempfile
+import numpy as np
+import tensorflow as tf
+import xarray as xr
+
+from fv3fit.emulation.data import load
+
+@pytest.fixture
+def xr_dataset():
+    return xr.Dataset(
+        {
+            "air_temperature": xr.DataArray(
+                data=np.arange(30).reshape(10, 3), dims=["sample", "z"]
+            ),
+            "specific_humidity": xr.DataArray(
+                data=np.arange(30, 60).reshape(10, 3), dims=["sample", "z"]
+            ),
+        }
+    )
+
+
+def test_get_nc_files(xr_dataset):
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        num_files = 3
+        paths = [os.path.join(tmpdir, f"file{i}.nc") for i in range(num_files)]
+        for path in paths:
+            xr_dataset.to_netcdf(path)
+
+        result_files = load.get_nc_files(tmpdir)
+        assert len(result_files) == num_files
+        for path in paths:
+            assert path in result_files
+
+
+def test_batched_to_tf_dataset(xr_dataset):
+
+    batches = [np.arange(30).reshape(10, 3)]*3
+
+    def transform(batch):
+        return batch*2
+
+    tf_ds = load.batched_to_tf_dataset(batches, transform)
+    assert isinstance(tf_ds, tf.data.Dataset)
+
+    result = next(tf_ds.batch(10).as_numpy_iterator())
+    np.testing.assert_equal(result, batches[0]*2)
