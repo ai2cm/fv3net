ai2cm/fv3net

commit a5805ad895ba95bceb50d53ef5d6322b85f8070a

prog run: implement run-segment in python  (#1291)
This is part of a long-term plan to reimplement runfv3 in python. Moving
more logic into python will make it easier to extend segmented run
features.

workflows/prognostic_c48_run/runtime/segmented_run/run.py
@@ -1,0 +1,53 @@
+import sys
+import subprocess
+import fv3config
+import os
+import glob
+import yaml
+import shutil
+import contextlib
+
+
+@contextlib.contextmanager
+def cwd(path):
+    cwd = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(cwd)
+
+
+def find(path: str):
+    return glob.glob(os.path.join(path, "**"), recursive=True)
+
+
+def run_segment(config: dict, rundir: str, runfile: str):
+    fv3config.write_run_directory(config, rundir)
+    shutil.copy(runfile, os.path.join(rundir, "runfile.py"))
+
+    with cwd(rundir):
+        manifest = find(".")
+        with open("preexisting-files.txt", "w") as f:
+            for path in manifest:
+                print(path, file=f)
+
+        x, y = config["namelist"]["fv_core_nml"]["layout"]
+        nprocs = x * y * 6
+        with open("logs.txt", "w") as f:
+            subprocess.check_call(
+                [
+                    "mpirun",
+                    "-n",
+                    str(nprocs),
+                    sys.executable,
+                    os.path.abspath(runfile),
+                ],
+                stdout=f,
+                stderr=f,
+            )
+
+
+def main():
+    config_path, rundir, runfile = sys.argv[1:]
+    with open(config_path) as f:
+        config = yaml.safe_load(f)
+    run_segment(config, rundir, runfile)

workflows/prognostic_c48_run/runfv3
@@ -124,28 +124,6 @@ function appendSegment {
 }
 
 
-function runSegment {
-    fv3config="$1"
-    rundir="$2"
-    runfile="$3"
-
-    if [[ $# -ne 3 ]]
-    then
-        usage
-        exit 1
-    fi
-
-    write_run_directory "$fv3config" "$rundir"
-    mkdir -p "$rundir"
-    cp "$runfile" "$rundir/runfile.py"
-    NUM_PROC=$(yq '.namelist.fv_core_nml.layout | .[0] *.[1] * 6' "$fv3config")
-    (
-        cd "$rundir"
-        find . > preexisting_files.txt
-        mpirun -n "$NUM_PROC" python3 -m mpi4py "runfile.py" |& tee -a "logs.txt"
-    )
-}
-
 function verifyUrlEmpty {
     url="$1"
     set +e

workflows/prognostic_c48_run/setup.py
@@ -22,6 +22,9 @@ setup(
     tests_require=test_requirements,
     extras_require={"docs": ["sphinx >=3.2", "sphinx-rtd-theme>=0.5.0"]},
     entry_points={
-        "console_scripts": ["segmented-run-create=runtime.segmented_run.cli:create"]
+        "console_scripts": [
+            "segmented-run-create=runtime.segmented_run.cli:create",
+            "runSegment=runtime.segmented_run.run:main",
+        ]
     },
 )

commit f482c5d42fc9fe40b3b3490733a298ae39367dce

Fix a typo in "preexisting_files" (#1295)
Found another bug in master that was introduced by a5805ad.

workflows/prognostic_c48_run/runtime/segmented_run/run.py
@@ -26,7 +26,7 @@ def run_segment(config: dict, rundir: str, runfile: str):
 
     with cwd(rundir):
         manifest = find(".")
-        with open("preexisting-files.txt", "w") as f:
+        with open("preexisting_files.txt", "w") as f:
             for path in manifest:
                 print(path, file=f)
 