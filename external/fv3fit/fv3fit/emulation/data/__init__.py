848eaeb97de22741c28b6518a2ecb635bf7dc424
Andre Perkins <frodre@gmail.com>
2021-07-12 23:57:08 +0000

    Move get_nc_files to io.py

--- external/fv3fit/fv3fit/emulation/data/__init__.py
+++ external/fv3fit/fv3fit/emulation/data/__init__.py
@@ -1,3 +1,4 @@
 from .config import TransformConfig
 from . import transforms
-from .load import get_nc_files, batched_to_tf_dataset
+from .load import batched_to_tf_dataset
+from .io import get_nc_files
--- external/fv3fit/fv3fit/emulation/data/load.py
+++ external/fv3fit/fv3fit/emulation/data/load.py
@@ -1,26 +1,9 @@
 import logging
-import os
 import tensorflow as tf
-from typing import Callable, List, Sequence
-
-from vcm import get_fs
+from typing import Callable, Sequence
 
 
 logger = logging.getLogger(__name__)
-
-
-def get_nc_files(path: str) -> List[str]:
-    """
-    Get a list of netCDF files from a remote/local directory
-
-    Args:
-        path: Local or remote gcs path to netCDF directory
-    """
-
-    fs = get_fs(path)
-    files = list(fs.glob(os.path.join(path, "*.nc")))
-
-    return files
 
 
 def batched_to_tf_dataset(
