f8ec14876add7d9f1798c3ee3abb3fd25add0231
Noah D. Brenowiz <nbren12@gmail.com>
2021-07-16 17:16:35 -0700

    prog run: use runfv3 in regression tests
    There is limited coverage of many fv3post and runfv3 operations outside
    of the end to end test. This makes iterating on errors in this routines
    challenging.
    
    Use runfv3 in the prognostic run image tests to test these additional
    integration.

--- workflows/prognostic_c48_run/tests/test_regression.py
+++ workflows/prognostic_c48_run/tests/test_regression.py
@@ -345,7 +345,8 @@ def run_native(config, rundir):
     with tempfile.NamedTemporaryFile("w") as f:
         yaml.safe_dump(config, f)
         fv3_script = Path(__file__).parent.parent.joinpath("runfv3").as_posix()
-        subprocess.check_call([fv3_script, "run-native", f.name, str(rundir)])
+        subprocess.check_call([fv3_script, "create", rundir, f.name])
+        subprocess.check_call([fv3_script, "append", rundir])
 
 
 def assets_from_initial_condition_dir(dir_: str):
@@ -516,27 +517,32 @@ def completed_rundir(configuration, tmpdir_factory):
     else:
         raise NotImplementedError()
 
-    rundir = tmpdir_factory.mktemp("rundir")
+    rundir = tmpdir_factory.mktemp("rundir").join("subdir")
     run_native(config, str(rundir))
     return rundir
 
 
-def test_fv3run_checksum_restarts(completed_rundir, regtest):
+@pytest.fixture()
+def completed_segment(completed_rundir):
+    return completed_rundir.join("artifacts").join("20160801.000000")
+
+
+def test_fv3run_checksum_restarts(completed_segment, regtest):
     """Please do not add more test cases here as this test slows image build time.
     Additional Predictor model types and configurations should be tested against
     the base class in the fv3fit test suite.
     """
-    fv_core = completed_rundir.join("RESTART").join("fv_core.res.tile1.nc")
+    fv_core = completed_segment.join("RESTART").join("fv_core.res.tile1.nc")
     print(fv_core.computehash(), file=regtest)
 
 
 @pytest.mark.parametrize("path", [LOG_PATH, STATISTICS_PATH, PROFILES_PATH])
-def test_fv3run_logs_present(completed_rundir, path):
-    assert completed_rundir.join(path).exists()
+def test_fv3run_logs_present(completed_segment, path):
+    assert completed_segment.join(path).exists()
 
 
-def test_chunks_present(completed_rundir):
-    assert completed_rundir.join(CHUNKS_PATH).exists()
+def test_chunks_present(completed_segment):
+    assert completed_segment.join(CHUNKS_PATH).exists()
 
 
 def test_fv3run_diagnostic_outputs_check_variables(regtest, completed_rundir):
@@ -556,11 +562,11 @@ def test_fv3run_diagnostic_outputs_schema(regtest, completed_rundir):
     diagnostics.info(regtest)
 
 
-def test_fv3run_python_mass_conserving(completed_rundir, configuration):
+def test_fv3run_python_mass_conserving(completed_segment, configuration):
     if configuration == ConfigEnum.nudging:
         pytest.skip()
 
-    path = str(completed_rundir.join(STATISTICS_PATH))
+    path = str(completed_segment.join(STATISTICS_PATH))
 
     # read python mass conservation info
     with open(path) as f:
@@ -579,11 +585,11 @@ def test_fv3run_python_mass_conserving(completed_rundir, configuration):
         )
 
 
-def test_fv3run_vertical_profile_statistics(completed_rundir, configuration):
+def test_fv3run_vertical_profile_statistics(completed_segment, configuration):
     if configuration == ConfigEnum.nudging:
         # no specific humidity limiter for nudging run
         pytest.skip()
-    path = str(completed_rundir.join(PROFILES_PATH))
+    path = str(completed_segment.join(PROFILES_PATH))
     npz = yaml.safe_load(default_fv3config)["namelist"]["fv_core_nml"]["npz"]
     with open(path) as f:
         lines = f.readlines()
