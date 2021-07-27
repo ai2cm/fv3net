1badcdaf999a26e3e24f12576ffb15b152f67dad
Noah D. Brenowiz <nbren12@gmail.com>
2021-07-27 12:17:43 -0700

    update test.sh for new training CLI

--- workflows/prognostic_c48_run/test.sh
+++ workflows/prognostic_c48_run/test.sh
@@ -1,7 +1,13 @@
 # TODO delete this dev script
 set -e
 
-pytest runtime/emulator
-guild run --tag dev -y epochs=1 nfiles=1 problem=single-level lr=0.0001 wandb_logger=False
-guild run --tag dev -y epochs=1 nfiles=1 problem=all lr=0.0001 wandb_logger=False
-guild run --tag dev -y epochs=1 nfiles=1 problem=rh lr=10.0 wandb_logger=False scale=1.0
+python3 ./train.py \
+--nfiles 2 \
+--epochs=2 \
+--levels=25,50,75 --multi-output --relative-humidity --extra-variables=cos_zenith_angle,surface_pressure --rh-weight=10000 --q-weight=0 --lr=0.01 --momentum=0.9  --batch-size=512 --num-hidden=512 --num-hidden-layers=3
+
+
+python3 ./train.py \
+--nfiles 2 \
+--epochs=2 \
+--levels=25,50,75 --level 50 --variable 3 --extra-variables=cos_zenith_angle,surface_pressure --lr=10 --momentum=0.9  --batch-size=512 --num-hidden=512 --num-hidden-layers=3
