bd9f0adfa8eda5646c42de4095d1fc812d67d698
W. Andre Perkins <frodre@gmail.com>
2021-07-28 16:00:23 -0700

    Fix emulation function "get_nc_files" (#1311)
    There was a bug i noticed when using nc_dir_to_tf_dataset that stems from a problem with get_nc_files.
    
    If provided with a path to a GCS directory of netCDF paths, the get_nc_files function used the glob method of a filesystem object, which would strip the protocol (gs://) from the list of files. The removal of this information breaks the loading of the remote netCDF downstream because we a) don't provide initiated filesystem objects (gets messy fast) and b) without any protocol info in the path get_remote_nc thinks the file is local. I adjusted get_nc_files to keep the path if it detects a GCS protocol for now.
    
    * Add protocol preservation for globbed paths
    
    * Update Mock name to be more descriptive
    
    * Add missing kwarg to get_nc_files docstring
    
    * Linting
    
    * Remove network mark
    
    * Fix fv3gfs-fortran submodule
    
    * remove pytest import

--- external/fv3fit/fv3fit/emulation/data/io.py
+++ external/fv3fit/fv3fit/emulation/data/io.py
@@ -1,18 +1,30 @@
 import os
-from typing import List
+import fsspec
+from typing import List, Optional
 
 from vcm import get_fs
 
 
-def get_nc_files(path: str) -> List[str]:
+def get_nc_files(
+    path: str, fs: Optional[fsspec.AbstractFileSystem] = None
+) -> List[str]:
     """
     Get a list of netCDF files from a remote/local directory
 
     Args:
         path: Local or remote gcs path to netCDF directory
+        fs: Filesystem object to use for the glob operation
+            searching for netCDFs in the path
     """
 
-    fs = get_fs(path)
+    if fs is None:
+        fs = get_fs(path)
+
     files = list(fs.glob(os.path.join(path, "*.nc")))
+
+    # we want to preserve information about the remote protocol
+    # so any downstream operations can glean that info from the paths
+    if "gs" in fs.protocol:
+        files = ["gs://" + f for f in files]
 
     return files
