ai2cm/fv3net

commit d7f0d75cf26ba9fc03bb79c9cfaec11f600eff10

refactor to fv3fit.emulator.layers

workflows/prognostic_c48_run/runtime/emulator/emulator.py
@@ -13,7 +13,11 @@ from runtime.emulator.thermo import (
     SpecificHumidityBasis,
     ThermoBasis,
 )
-from runtime.emulator.layers import NormLayer, ScalarNormLayer, UnNormLayer
+from fv3fit.emulation.layers.normalization import (
+    MaxFeatureStdDenormLayer,
+    StandardNormLayer,
+    StandardDenormLayer,
+)
 import logging
 import json
 
@@ -372,7 +376,7 @@ class UVTQSimple(tf.keras.layers.Layer):
     def __init__(self, u_size, v_size, t_size, q_size):
         super(UVTQSimple, self).__init__()
         self.scalers_fitted = False
-        self.norm = NormLayer(name="norm")
+        self.norm = StandardNormLayer(name="norm")
         self.linear = tf.keras.layers.Dense(256, name="lin")
         self.relu = tf.keras.layers.ReLU()
         self.out_u = tf.keras.layers.Dense(u_size, name="out_u")
@@ -380,7 +384,7 @@ class UVTQSimple(tf.keras.layers.Layer):
         self.out_t = tf.keras.layers.Dense(t_size, name="out_t")
         self.out_q = tf.keras.layers.Dense(q_size, name="out_q")
 
-        self.scalers = [UnNormLayer(name=f"out_{i}") for i in range(4)]
+        self.scalers = [MaxFeatureStdDenormLayer(name=f"out_{i}") for i in range(4)]
 
     def _fit_input_scaler(self, args: Sequence[tf.Variable]):
         args = [atleast_2d(arg) for arg in args]
@@ -451,8 +455,8 @@ class ScalarMLP(tf.keras.layers.Layer):
         self.var_level = var_level
 
         # input and output normalizations
-        self.norm = NormLayer(name="norm")
-        self.output_scaler = ScalarNormLayer(name="output_scalar")
+        self.norm = StandardNormLayer(name="norm")
+        self.output_scaler = StandardDenormLayer(name="output_scalar")
 
         # model architecture
         self.sequential.add(self.norm)

workflows/prognostic_c48_run/runtime/emulator/layers.py
@@ -1,78 +1,0 @@
-import tensorflow as tf
-
-
-class NormLayer(tf.keras.layers.Layer):
-    def __init__(self, epsilon: float = 1e-7, name=None):
-        super(NormLayer, self).__init__(name=name)
-        self.epsilon = epsilon
-
-    def build(self, in_shape):
-        self.mean = self.add_weight(
-            "mean", shape=[in_shape[-1]], dtype=tf.float32, trainable=False
-        )
-        self.sigma = self.add_weight(
-            "sigma", shape=[in_shape[-1]], dtype=tf.float32, trainable=False
-        )
-        self.fitted = False
-
-    def fit(self, tensor):
-        self(tensor)
-        self.mean.assign(tf.cast(tf.reduce_mean(tensor, axis=0), tf.float32))
-        self.sigma.assign(
-            tf.cast(
-                tf.sqrt(tf.reduce_mean((tensor - self.mean) ** 2, axis=0)), tf.float32,
-            )
-        )
-
-    def call(self, tensor):
-        return (tensor - self.mean) / (self.sigma + self.epsilon)
-
-
-class ScalarNormLayer(NormLayer):
-    """UnNormalize a vector using a scalar mean and standard deviation
-
-
-    """
-
-    def __init__(self, name=None):
-        super(ScalarNormLayer, self).__init__(name=name)
-
-    def build(self, in_shape):
-        self.mean = self.add_weight(
-            "mean", shape=[in_shape[-1]], dtype=tf.float32, trainable=False
-        )
-        self.sigma = self.add_weight(
-            "sigma", shape=[], dtype=tf.float32, trainable=False
-        )
-
-    def fit(self, tensor):
-        self(tensor)
-        self.mean.assign(tf.cast(tf.reduce_mean(tensor, axis=0), tf.float32))
-        self.sigma.assign(
-            tf.cast(tf.sqrt(tf.reduce_mean((tensor - self.mean) ** 2)), tf.float32,)
-        )
-
-    def call(self, tensor):
-        return tensor * self.sigma + self.mean
-
-
-class UnNormLayer(tf.keras.layers.Layer):
-    def __init__(self, name=None):
-        super(UnNormLayer, self).__init__(name=name)
-        self.norm = NormLayer()
-
-    @property
-    def fitted(self):
-        return self.norm.fitted
-
-    def fit(self, x):
-        self.norm.fit(x)
-
-    def call(self, tensor):
-        try:
-            self.norm.mean
-        except AttributeError:
-            # need to initialize the weights
-            self.norm(tensor)
-
-        return tensor * self.norm.sigma + self.norm.mean

workflows/prognostic_c48_run/runtime/emulator/test_layers.py
@@ -1,60 +1,0 @@
-import numpy as np
-import tensorflow as tf
-from runtime.emulator.layers import (
-    NormLayer,
-    UnNormLayer,
-    ScalarNormLayer,
-)
-from hypothesis import given
-from hypothesis.strategies import integers
-
-def test_NormLayer():
-    u = tf.Variable([[0.0], [1.0]], dtype=tf.float32)
-    layer = NormLayer()
-    layer.fit(u)
-    norm = layer(u)
-    expected = np.array([[-1.0], [1.0]])
-    np.testing.assert_allclose(expected, norm, rtol=1e-6)
-
-
-def test_NormLayer_no_trainable_variables():
-    u = tf.Variable([[0.0], [1.0]], dtype=tf.float32)
-    layer = NormLayer()
-    layer(u)
-
-    assert [] == layer.trainable_variables
-
-
-def test_NormLayer_gradient_works():
-    u = tf.Variable([[0.0, 0.0], [1.0, 2.0]], dtype=tf.float32)
-    layer = NormLayer()
-    layer(u)
-
-    with tf.GradientTape() as tape:
-        y = layer(u)
-    (g,) = tape.gradient(y, [u])
-    expected = 1 / (layer.sigma + layer.epsilon)
-    np.testing.assert_array_almost_equal(expected, g[0, :])
-
-
-@given(integers())
-def test_ScalarNormLayer(seed):
-    tf.random.set_seed(seed)
-    u = tf.random.uniform([2, 3])
-    norm = NormLayer()
-    norm.fit(u)
-
-    unnorm = UnNormLayer()
-    unnorm.fit(u)
-    round_tripped = unnorm(norm(u))
-    np.testing.assert_almost_equal(u.numpy(), round_tripped.numpy())
-
-
-def test_scalar_norm_layer():
-    input = np.array([[1, 2], [-1, -2]], dtype=np.float32)
-    expected = input * np.sqrt(10 / 4)
-
-    norm = ScalarNormLayer()
-    norm.fit(input)
-
-    np.testing.assert_allclose(norm(input).numpy(), expected)

commit f1ac3064a5bb746fec2436c66abeb68c53f0c6c0

Fix scaling related performance regression from d7f0d75
Using the MaxStd scaler doesn' work well. Partially revert the changes
in that commit.

workflows/prognostic_c48_run/runtime/emulator/emulator.py
@@ -21,7 +21,6 @@ from runtime.emulator.thermo import (
 )
 from runtime.emulator.models import V1QCModel, atleast_2d
 from fv3fit.emulation.layers.normalization import (
-    MaxFeatureStdDenormLayer,
     StandardNormLayer,
     StandardDenormLayer,
 )
@@ -377,7 +376,7 @@ class UVTQSimple(tf.keras.layers.Layer):
         self.out_t = tf.keras.layers.Dense(t_size, name="out_t")
         self.out_q = tf.keras.layers.Dense(q_size, name="out_q")
 
-        self.scalers = [MaxFeatureStdDenormLayer(name=f"out_{i}") for i in range(4)]
+        self.scalers = [StandardDenormLayer(name=f"out_{i}") for i in range(4)]
 
     def _fit_input_scaler(self, args: Sequence[tf.Variable]):
         args = [atleast_2d(arg) for arg in args]

workflows/prognostic_c48_run/runtime/emulator/layers.py
@@ -1,0 +1,30 @@
+import tensorflow as tf
+from fv3fit.emulation.layers.normalization import StandardNormLayer as NormLayer
+
+
+class ScalarNormLayer(NormLayer):
+    """UnNormalize a vector using a scalar mean and standard deviation
+
+
+    """
+
+    def __init__(self, name=None):
+        super(ScalarNormLayer, self).__init__(name=name)
+
+    def build(self, in_shape):
+        self.mean = self.add_weight(
+            "mean", shape=[in_shape[-1]], dtype=tf.float32, trainable=False
+        )
+        self.sigma = self.add_weight(
+            "sigma", shape=[], dtype=tf.float32, trainable=False
+        )
+
+    def fit(self, tensor):
+        self(tensor)
+        self.mean.assign(tf.cast(tf.reduce_mean(tensor, axis=0), tf.float32))
+        self.sigma.assign(
+            tf.cast(tf.sqrt(tf.reduce_mean((tensor - self.mean) ** 2)), tf.float32,)
+        )
+
+    def call(self, tensor):
+        return tensor * self.sigma + self.mean