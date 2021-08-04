2165a8f10782ced8b23f9e4359c09d8f095a3adf
Oliver Watt-Meyer <oliwm@vulcan.com>
2021-08-04 23:10:16 +0000

    Fix bug in XarrayMapper

--- external/loaders/tests/test__xarray.py
+++ external/loaders/tests/test__xarray.py
@@ -2,6 +2,7 @@ import doctest
 import loaders.mappers._xarray
 import xarray as xr
 import numpy as np
+import pytest
 import cftime
 
 # ensure that imported path exists
@@ -12,15 +13,20 @@ def test_xarray_wrapper_doctests():
     doctest.testmod(loaders.mappers._xarray, raise_on_error=True)
 
 
-def test_open_zarr(tmpdir):
+@pytest.mark.parametrize(
+    "time_dim_name", ["another_name", "time"],
+)
+def test_open_zarr(tmpdir, time_dim_name):
     time = cftime.DatetimeJulian(2020, 1, 1)
     time_str = "20200101.000000"
     ds = xr.Dataset(
-        {"a": (["time", "tile", "z", "y", "x"], np.ones((1, 2, 3, 4, 5)))},
-        coords={"time": [time]},
+        {"a": ([time_dim_name, "tile", "z", "y", "x"], np.ones((1, 2, 3, 4, 5)))},
+        coords={time_dim_name: [time]},
     )
     ds.to_zarr(str(tmpdir), consolidated=True)
 
-    mapper = open_zarr(str(tmpdir))
+    mapper = open_zarr(str(tmpdir), dim=time_dim_name)
     assert isinstance(mapper, XarrayMapper)
-    xr.testing.assert_equal(mapper[time_str], ds.isel(time=0))
+    print(ds.isel({time_dim_name: 0}))
+    print(mapper)
+    xr.testing.assert_equal(mapper[time_str], ds.isel({time_dim_name: 0}))
