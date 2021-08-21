2381fb9ad6980acb049065174dc21b199b997556
Andre Perkins <frodre@gmail.com>
2021-08-21 01:59:44 +0000

    Switched to wrapped model w/ negative fixer

--- external/emulation_training/emulation_training/emulate.py
+++ external/emulation_training/emulation_training/emulate.py
@@ -16,7 +16,7 @@ DT_SEC = None
 
 
 @print_errors
-def _load_environmt_vars_into_global():
+def _load_environment_vars_into_global():
 
     global TF_MODEL_PATH
     global NML_PATH
@@ -56,101 +56,9 @@ def _load_tf_model() -> tf.keras.Model:
     return model
 
 
-_load_environmt_vars_into_global()
+_load_environment_vars_into_global()
 NML = _load_nml()
 MODEL = _load_tf_model()
-
-
-def _split_tendency_state_update_predictions(predictions):
-
-    tendency_updates = {
-        name: data
-        for name, data in predictions.items()
-        if "tendency_of_" in name
-    }
-
-    state_update_keys = set(predictions.keys()) - set(tendency_updates.keys())
-    state_updates = {
-        name: predictions[name]
-        for name in state_update_keys
-    }
-
-    logger.info(f"Predicted tendencies: {tendency_updates.keys()}")
-    logger.info(f"Predicted states: {state_update_keys}")
-
-    return tendency_updates, state_updates
-
-
-def _get_state_variable_name(name: str) -> str:
-    prefix = "tendency_of_"
-    suffix = "_due_to_"
-
-    if not name.startswith(prefix) and suffix not in name:
-        raise ValueError(f"Received unknown tendency name format {name}")
-
-    state_name = name[len(prefix):name.index(suffix)]
-    return state_name
-
-
-def _tendencies_to_state_update(state, tendency_outputs):
-
-    levs = NML["fv_core_nml"]["npz"]
-    state_updates = {}
-    for tend_name, tendencies in tendency_outputs.items():
-        state_name = _get_state_variable_name(tend_name)
-        logger.debug(f"Updating state {state_name} with tendencies {tend_name}")
-
-        in_name = state_name.replace("_output", "_input")
-        input_state_var = state[in_name].T
-        tendencies = _maybe_expand_field(levs, tendencies)
-        updated_state_var = input_state_var + tendencies * DT_SEC
-        state_updates[state_name] = updated_state_var
-
-    return state_updates
-
-
-def _combine_tend_state_updates(tend_state, direct_state):
-
-    duplicates = set(tend_state).intersection(direct_state)
-    if duplicates:
-        raise ValueError(
-            "State update from predictions ambiguous with overlapping"
-            f" state keys {duplicates}"
-        )
-    
-    combined = dict(direct_state)
-    combined.update(tend_state)
-
-    return combined
-
-
-def _maybe_expand_field(target_lev, field):
-
-    if field.shape[-1] < target_lev:
-        expanded_data = np.zeros((field.shape[0], target_lev))
-        expanded_data[:, :field.shape[-1]] = field
-        field = expanded_data
-    
-    return field
-    
-
-
-# def _expand_dataset_to_full_vertical(state_predictions):
-#     """
-#     Hard-coded expansion for now assumes levels
-#     cut off from top and all predictions are 2D
-#     """
-#     levs = NML["fv_core_nml"]["npz"]
-
-#     full_levels = {}
-#     for name, data in state_predictions.items():
-#         full_levels[name] = _maybe_expand_field(levs, data)
-    
-#     logger.debug(f"Expanding feature dims for: {full_levels.keys()}")
-#     new_state = dict(state_predictions)
-#     new_state.update(full_levels)
-
-#     return new_state
 
 
 def _adjusted_model_inputs(state):
@@ -166,21 +74,19 @@ def _adjusted_model_inputs(state):
     return adjusted
 
 
-def _get_state_updates(state, predictions):
-    
-    tendencies, updated_state = \
-        _split_tendency_state_update_predictions(predictions)
-    
-    tend_states = _tendencies_to_state_update(state, tendencies)
-    combined = _combine_tend_state_updates(tend_states, updated_state)
-    transposed = {name: data.T for name, data in combined.items()}
-    
-    logger.info(f"Predicted states from model: {transposed.keys()}")
+def _fix_negative(state):
+    check_fields = [
+        "specific_humidity_output",
+        "cloud_water_mixing_ratio_output"
+    ]
 
-    #add back in tendencies for debugging
-    transposed.update(tendencies)
-
-    return transposed
+    for name, field in state.items():
+        if name in check_fields:
+            neg_locs = field < 0
+            num_neg = neg_locs.sum()
+            if num_neg > 0:
+                logger.info(f"Fixing {num_neg} negative values to 0 in {name}")
+                field[field < 0] = 0.0
 
 
 # TODO: quite confusing to have to keep track of transposes
@@ -191,12 +97,14 @@ def microphysics(state):
     inputs = [data for data in adjusted_in_state.values()]
     outputs = MODEL.predict(inputs)
     named_outputs = {
-        name: output
+        name: output.T # transposed adjust
         for name, output in zip(MODEL.output_names, outputs)
     }
 
-    new_state = _get_state_updates(state, named_outputs)
+    # new_state = _get_state_updates(state, named_outputs)
 
+    new_state = named_outputs
+    _fix_negative(new_state)
     overwrites = set(state).intersection(new_state)
     logger.info(f"Overwritting existing state fields: {overwrites}")
     diag_uphys = {
