f218ad0970648f1aed033f66cf44e429e122d968
Noah D. Brenowiz <nbren12@gmail.com>
2021-08-24 15:31:15 -0700

    Add FeatureMeanDenormLayer to fv3fit
    This type of normalization gives better results than FeatureMax standard
    scaling.

--- workflows/prognostic_c48_run/runtime/emulator/layers.py
+++ workflows/prognostic_c48_run/runtime/emulator/layers.py
@@ -1,30 +1,4 @@
-import tensorflow as tf
-from fv3fit.emulation.layers.normalization import StandardNormLayer as NormLayer
+from fv3fit.emulation.layers.normalization import FeatureMeanDenormLayer
 
 
-class ScalarNormLayer(NormLayer):
-    """UnNormalize a vector using a scalar mean and standard deviation
-
-
-    """
-
-    def __init__(self, name=None):
-        super(ScalarNormLayer, self).__init__(name=name)
-
-    def build(self, in_shape):
-        self.mean = self.add_weight(
-            "mean", shape=[in_shape[-1]], dtype=tf.float32, trainable=False
-        )
-        self.sigma = self.add_weight(
-            "sigma", shape=[], dtype=tf.float32, trainable=False
-        )
-
-    def fit(self, tensor):
-        self(tensor)
-        self.mean.assign(tf.cast(tf.reduce_mean(tensor, axis=0), tf.float32))
-        self.sigma.assign(
-            tf.cast(tf.sqrt(tf.reduce_mean((tensor - self.mean) ** 2)), tf.float32,)
-        )
-
-    def call(self, tensor):
-        return tensor * self.sigma + self.mean
+ScalarNormLayer = FeatureMeanDenormLayer
