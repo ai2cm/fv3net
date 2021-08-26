4efdfe80acacff600220890d7c47a3df33d056e3
Noah D. Brenowiz <nbren12@gmail.com>
2021-08-26 09:27:19 -0700

    delete data tests in runtime
    These are replaced by the fv3fit tests andre requested.

--- workflows/prognostic_c48_run/runtime/emulator/test_data.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from fv3fit.emulation.data import netcdf_url_to_dataset
-
-
-def test_netcdf_url_to_dataset():
-    d = netcdf_url_to_dataset("data/training", variables=["specific_humidity"])
-    m = next(iter(d))
-    assert isinstance(m["specific_humidity"], tf.Tensor)
