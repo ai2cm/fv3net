4b590f9bad21fb3cb11d6921bf4caa86b35a35a3
Andre Perkins <frodre@gmail.com>
2021-09-10 19:01:11 +0000

    Online training

--- external/emulation_training/emulation_training/emulate.py
+++ external/emulation_training/emulation_training/emulate.py
@@ -1,10 +1,9 @@
 import f90nml
 import logging
-import numpy as np
 import os
 import tensorflow as tf
 
-from .debug import print_errors, dump_state
+from .debug import print_errors
 from ._filesystem import get_dir
 
 logger = logging.getLogger(__name__)
@@ -13,6 +12,9 @@ logger = logging.getLogger(__name__)
 TF_MODEL_PATH = None
 NML_PATH = None
 DT_SEC = None
+ONLINE_TRAIN = None
+LOSSES = None
+USE_PHYS_COUNTER = 0
 
 
 @print_errors
@@ -20,10 +22,17 @@ def _load_environment_vars_into_global():
 
     global TF_MODEL_PATH
     global NML_PATH
+    global ONLINE_TRAIN
 
     cwd = os.getcwd()
     TF_MODEL_PATH = os.environ["TF_MODEL_PATH"]
     NML_PATH = os.path.join(cwd, "input.nml")
+
+    try:
+        ONLINE_TRAIN = bool(os.environ["ONLINE_TRAIN"])
+    except KeyError:
+        logger.debug("Environment var ONLINE_TRAIN not set. Defaulting to False.")
+        ONLINE_TRAIN = False
 
 
 @print_errors
@@ -40,8 +49,33 @@ def _load_nml():
 class NormalizedMSE(tf.keras.losses.MeanSquaredError):
     """Temporary dummy"""
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, sample, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.mean = tf.cast(tf.reduce_mean(sample, axis=0), tf.float32)
+        self.sigma = tf.cast(tf.sqrt(tf.reduce_mean((sample - self.mean) ** 2)), tf.float32,)
+
+    def _normalize(self, data):
+        return (data - self.mean) / self.sigma
+
+    def call(self, y_true, y_pred):
+        return super().call(self._normalize(y_true), self._normalize(y_pred))
+
+
+class DummyMSE(tf.keras.losses.MeanSquaredError):
+
+    def call(self, y_true, y_pred):
+        return 0.0
+
+
+@print_errors
+def _initialize_model_for_train(y):
+
+    global LOSSES
+
+    LOSSES = [NormalizedMSE(field) for field in y] + [DummyMSE()]
+    weights = [1.0 for l in LOSSES]
+    weights[-2] = 1 / 100
+    MODEL.compile(loss=LOSSES, optimizer="adam", loss_weights=weights)
 
 
 @print_errors
@@ -49,7 +83,7 @@ def _load_tf_model() -> tf.keras.Model:
     logger.debug(f"Loading keras model: {TF_MODEL_PATH}")
     with get_dir(TF_MODEL_PATH) as local_model_path:
         model = tf.keras.models.load_model(
-            local_model_path, 
+            local_model_path,
             custom_objects={"NormalizedMSE": NormalizedMSE}
         )
 
@@ -60,51 +94,45 @@ _load_environment_vars_into_global()
 NML = _load_nml()
 MODEL = _load_tf_model()
 
+@print_errors
+def _train(X, y):
 
-def _adjusted_model_inputs(state):
+    if LOSSES is None:
+        _initialize_model_for_train(y[:-1])
 
-    adjusted = {}
-    for i, name in enumerate(MODEL.input_names):
-        data = state[name].T
-        layer = MODEL.inputs[i]
-        if layer.shape[-1] < data.shape[-1]:
-            data = data[:, :layer.shape[-1]]
-        adjusted[name] = data
-    
-    return adjusted
+    MODEL.fit(
+        X,
+        y,
+        batch_size=64,
+        epochs=25,
+        verbose=2,
+    )
 
-
-def _fix_negative(state):
-    check_fields = [
-        "specific_humidity_output",
-        "cloud_water_mixing_ratio_output"
-    ]
-
-    for name, field in state.items():
-        if name in check_fields:
-            neg_locs = field < 0
-            num_neg = neg_locs.sum()
-            if num_neg > 0:
-                logger.info(f"Fixing {num_neg} negative values to 0 in {name}")
-                field[field < 0] = 0.0
-
-
-# TODO: quite confusing to have to keep track of transposes
-#       for going from fortran -> state to numpy state
 @print_errors
 def microphysics(state):
-    adjusted_in_state = _adjusted_model_inputs(state)
-    inputs = [data for data in adjusted_in_state.values()]
-    outputs = MODEL.predict(inputs)
-    named_outputs = {
-        name: output.T # transposed adjust
-        for name, output in zip(MODEL.output_names, outputs)
-    }
+    global USE_PHYS_COUNTER
 
-    # new_state = _get_state_updates(state, named_outputs)
+    inputs = [state[name].T for name in MODEL.input_names]
+
+    if ONLINE_TRAIN:
+        train_outputs = [
+            state[name].T
+            for name in MODEL.output_names
+            if name != "total_water"
+        ]
+        train_outputs.append(train_outputs[-1])  # dummy for total water not included in training
+        _train(inputs, train_outputs)
+    
+    if USE_PHYS_COUNTER == 0 or  USE_PHYS_COUNTER % 4 != 0:
+        predictions = MODEL.predict(inputs)
+        named_outputs = {
+            name: output.T # transposed adjust
+            for name, output in zip(MODEL.output_names, predictions)
+        }
+    else:
+        named_outputs = {}
 
     new_state = named_outputs
-    _fix_negative(new_state)
     overwrites = set(state).intersection(new_state)
     logger.info(f"Overwritting existing state fields: {overwrites}")
     diag_uphys = {
@@ -113,4 +141,5 @@ def microphysics(state):
     }
     state.update(new_state)
     state.update(diag_uphys)
-    # dump_state(state)
+
+    USE_PHYS_COUNTER += 1
