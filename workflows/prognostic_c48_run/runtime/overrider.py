a76c31535fa4048b6cf805474ddc7d6f95d28373
Oliver Watt-Meyer <oliverwatt@gmail.com>
2021-09-20 14:53:52 -0700

    Use zarr.LRUStoreCache

--- workflows/prognostic_c48_run/runtime/overrider.py
+++ workflows/prognostic_c48_run/runtime/overrider.py
@@ -1,11 +1,11 @@
 import dataclasses
-import functools
 import logging
 from typing import Hashable, Mapping, MutableMapping, Set
 
 import cftime
 import fsspec
 import xarray as xr
+import zarr
 
 import fv3gfs.util
 from runtime.monitor import Monitor
@@ -46,15 +46,14 @@ class OverriderAdapter:
         if self.communicator.rank == 0:
             logger.debug(f"Opening tendency overriding dataset from: {self.config.url}")
         tile = self.communicator.partitioner.tile_index(self.communicator.rank)
-        mapper = fsspec.get_mapper(self.config.url)
+        mapper = zarr.LRUStoreCache(fsspec.get_mapper(self.config.url), 128 * 2 ** 20)
         ds = xr.open_zarr(mapper, consolidated=True)
-        ds = ds[list(self.config.variables.values())].isel(tile=tile)
-        self._tendency_ds = DatasetCachedByChunk(ds, "time")
+        self._tendency_ds = ds[list(self.config.variables.values())].isel(tile=tile)
 
     def _open_tendencies_dataset(self, time: cftime.DatetimeJulian) -> xr.Dataset:
         ds = xr.Dataset()
         if self.communicator.tile.rank == 0:
-            ds = self._tendency_ds.load(time).drop_vars("time")
+            ds = self._tendency_ds.sel(time=time).drop_vars("time").load()
         tendencies = self.communicator.tile.scatter_state(ds_to_quantity_state(ds))
         return quantity_state_to_ds(tendencies)
 
@@ -98,33 +97,3 @@ class OverriderAdapter:
 
         step.__name__ = func.__name__
         return step
-
-
-class DatasetCachedByChunk:
-    def __init__(self, ds: xr.Dataset, dim: str):
-        """Wrapper to cache the most recently loaded chunks of an xarray dataset.
-        
-        Args:
-            ds: A chunked xarray dataset.
-            dim: Aimension along which different chunks are being accessed.
-
-        Example:
-            >>> cached_ds = DatasetCachedByChunk(ds, "time")
-            >>> cached_ds.load("2016-08-01T00:00:00")  # slow
-            >>> cached_ds.load("2016-08-01T00:15:00")  # fast if on same chunk
-        """
-        self._ds = ds
-        self._dim = dim
-        self._chunksize = ds.chunks[dim][0]
-        self._index = ds.get_index(dim)
-
-    def load(self, key):
-        """Load key from the given dimension of dataset."""
-        idx = self._index.get_loc(key)
-        chunknum, chunk_idx = divmod(idx, self._chunksize)
-        return self._load_chunk(chunknum).isel({self._dim: chunk_idx})
-
-    @functools.lru_cache(2)
-    def _load_chunk(self, i):
-        chunk_slice = slice(i * self._chunksize, (i + 1) * self._chunksize)
-        return self._ds.isel({self._dim: chunk_slice}).load()
--- workflows/prognostic_c48_run/tests/test_overrider.py
+++ workflows/prognostic_c48_run/tests/test_overrider.py
@@ -7,16 +7,7 @@ import xarray as xr
 import fv3gfs.util
 from fv3gfs.util.testing import DummyComm
 
-from runtime.overrider import DatasetCachedByChunk, OverriderAdapter, OverriderConfig
-
-da = xr.DataArray(
-    np.arange(11 * 4 * 6).reshape((11, 4, 6)),
-    dims=["time", "x", "y"],
-    coords={"time": np.arange(2000, 2011)},
-).chunk(
-    {"time": 5, "x": 4, "y": 4}  # type: ignore
-)
-ds = xr.Dataset({"var": da})
+from runtime.overrider import OverriderAdapter, OverriderConfig
 
 
 class MockDerivedState:
@@ -55,15 +46,6 @@ def _get_dummy_comm():
         DummyComm(0, 6, {}),
         fv3gfs.util.CubedSpherePartitioner(fv3gfs.util.TilePartitioner((1, 1))),
     )
-
-
-def test_dataset_caching():
-    cached = DatasetCachedByChunk(ds, "time")
-    ds_year_2000 = cached.load(2000)
-    xr.testing.assert_identical(ds_year_2000, ds.sel(time=2000))
-    assert cached._load_chunk.cache_info().hits == 0
-    cached.load(2001)
-    assert cached._load_chunk.cache_info().hits == 1
 
 
 def test_overrider(state, tmpdir, regtest):
