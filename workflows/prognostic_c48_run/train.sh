f8c330b54c5e300ec1696ff644c3c081a1079e01
Noah D. Brenowitz <nbren12@gmail.com>
2021-10-01 12:58:50 -0700

    Pull training data to correct path
    it used to be in separate folders, but now data/training points to
    data/raw.

--- workflows/prognostic_c48_run/train.sh
+++ workflows/prognostic_c48_run/train.sh
@@ -3,6 +3,6 @@
 set -e
 echo "Pulling data with dvc" > /dev/stderr
 dvc config cache.type hardlink,symlink
-dvc pull --verbose data/training data/validation
+dvc pull --verbose data/raw
 echo "Starting Training" > /dev/stderr
 python -m fv3fit.train_emulator $@
