0f5df9129c67414c29c614e93aa46f75d332ca87
W. Andre Perkins <frodre@gmail.com>
2021-10-04 18:15:10 -0700

    Microphysics emulation model utilities (#1370)
    This PR adds microphysics keras model creation to `fv3fit. It can reproduce the tendency model types investigated by @jacnugent over the summer, but includes all the convenience tooling for directly outputting incremented fields from those predictions.
    
    Added public API:
    
    MicrophysicsConfig in fv3fit.emulation.models: model configuration/builder
    Many general use keras layers for model building in fv3fit.emulation.layers:
    IncrementStateLayer: Convenience layer for incrementing an input field tensor with a tendency
    IncrementedFieldOutput: A layer that forces learned outputs to be tendency-based by incrementing an input field with denormalized model output.
    Allows for retrieval of tendency outputs as well
    FieldInput: Input layer that normalizes a field and optionally subselects along the feature dim
    FieldOutput: Direct prediction of an output field using dense output w/ linear activation and denormalization.
    CombineInputs: Concatenate input tensors along a dimension with the option to add a new dimension prior to combining
    RNNBlock: A simple RNN architecture layer with optional connected dense layers following
    MLPBlock: A simple dense network block with 0-many hidden layers
    Significant internal changes:
    
    TransformConfig in fv3fit.emulation.data now includes a DerivedMapping transform before conversion to arrays/tensors
    
    * Add utility layers for microphysics models
    
    * Utility layers test
    
    * Add tendency derived mappings for microphysics
    
    * Add derived mapping into transform pipeline
    
    * Linting
    
    * Update to master fv3gfs
    
    * Output layers
    
    * Start of microphysics models
    
    * model incremental adjustmet
    
    * Reorganization for model builder
    
    * Build works
    
    * Layer tests
    
    * Finished model tests and docstrings, added optional tend outputs
    
    * Remove pass-thru layer, using Lambdas
    
    * Linting
    
    * Fix use of normalize key for output initialization of model
    
    * Add norm/denorm checks
    
    * Add docstrings
    
    * Add linear block and redo combine init
    
    * Fix denormalize kwarg in output layer tests
    
    * Clean up outputs function in model config
    
    * Fix derived mapping variables bug
    
    * Fix improper normalization for residual layer
    
    * Add positive value enforce to outputs
    
    * Linting
    
    * Use Architecture params for dataclass
    
    * Remove unused method
    
    * Linting and mypy
    
    * Fix model config tests
    
    * Update Residual to not subclass FieldOutput
    
    * linting
    
    * Switch to use  for new transform
    
    * Add timestep to TransformConfig
    
    * Change IncrementLayer call signature to have just two arguments
    
    * Update ResidualOutput layer to IncrementedFieldOutput
    
    * Switch ArchitectureParams method 'instance' to 'build'
    
    * ArchitectureParams -> ArchitectureCOnfig and update description
    
    * Update doctsring descriptions in the model
    
    * Remove input combination from Architecture layers
    
    * Move input combining into model build step
    
    * Linting
    
    * Reorganize layers into general emulation layers module
    
    * Fix import references
    
    * Linting
    
    * Move util layer test into test_fields
    
    * Fix derived transform test
    
    * Update RNNBlock docstring
    
    * address review suggestions
    
    * Move microphysics to -> models
    
    * Reorganize microphysics folder into models module
    
    * Make tests reflect new emulation module structure
    
    * Linting

---
+++ external/fv3fit/fv3fit/emulation/layers/architecture.py
@@ -0,0 +1,113 @@
+from typing import Optional
+import tensorflow as tf
+
+
+class CombineInputs(tf.keras.layers.Layer):
+    """Input tensor stacking with option to add a dimension for RNNs"""
+
+    def __init__(
+        self, combine_axis: int, *args, expand_axis: Optional[int] = None, **kwargs
+    ):
+        """
+        Args:
+            combine_axis: Axis to concatenate tensors along.  Note that if expand_axis
+                is specified, it is applied before concatenation.  E.g., combine_axis=1
+                and expand_axis=1 will concatenate along the newly created dimension.
+            expand_axis: New axis to add to the input tensors
+        """
+        super().__init__(*args, **kwargs)
+
+        self.combine_axis = combine_axis
+        self.expand_axis = expand_axis
+
+    def call(self, inputs):
+
+        if self.expand_axis is not None:
+            inputs = [
+                tf.expand_dims(tensor, axis=self.expand_axis) for tensor in inputs
+            ]
+
+        return tf.concat(inputs, axis=self.combine_axis)
+
+
+class RNNBlock(tf.keras.layers.Layer):
+    """
+    RNN connected to an MLP for prediction
+
+    Layer call expects a 3-D input tensor (sample, z, variable),
+    which recurses backwards (top-to-bottom for the physics-param data)
+    by default over the z dimension.
+    """
+
+    def __init__(
+        self,
+        *args,
+        channels: int = 256,
+        dense_width: int = 256,
+        dense_depth: int = 1,
+        activation: str = "relu",
+        go_backwards: bool = True,
+        **kwargs,
+    ):
+        """
+        Args:
+            channels: width of RNN layer
+            dense_width: width of MLP layer, only used if dense_depth > 0
+            dense_depth: number of MLPlayers connected to RNN output, set to
+                0 to disable
+            activation: activation function to use for RNN and MLP
+            go_backwards: whether to recurse in the reverse direction
+                over the dim 1.  If using wrapper outputs, TOA starts
+                at 0, should be false
+        """
+        super().__init__(*args, **kwargs)
+
+        self.rnn = tf.keras.layers.SimpleRNN(
+            channels, activation=activation, go_backwards=go_backwards
+        )
+        self.dense = MLPBlock(width=dense_width, depth=dense_depth)
+
+    def call(self, input):
+        """
+        Args:
+            input: a 3-d tensor with dims (sample, per_var_features, variables)
+        """
+
+        rnn_out = self.rnn(input)
+
+        output = self.dense(rnn_out)
+
+        return output
+
+
+class MLPBlock(tf.keras.layers.Layer):
+    """MLP layer for basic NN predictions"""
+
+    def __init__(
+        self,
+        *args,
+        width: int = 256,
+        depth: int = 2,
+        activation: str = "relu",
+        **kwargs,
+    ):
+        """
+        Args:
+            width: width of MLP layer, only used if dense_depth > 0
+            depth: number of MLPlayers, set to 0 to disable
+            activation: activation function to use for RNN and MLP
+        """
+        super().__init__(*args, **kwargs)
+
+        self.dense = [
+            tf.keras.layers.Dense(width, activation=activation) for i in range(depth)
+        ]
+
+    def call(self, input):
+
+        outputs = input
+
+        for i in range(len(self.dense)):
+            outputs = self.dense[i](outputs)
+
+        return outputs
---
+++ external/fv3fit/tests/emulation/layers/test_normalization.py
@@ -0,0 +1,83 @@
+import pytest
+import numpy as np
+import tensorflow as tf
+
+from fv3fit.emulation import layers
+
+_all_layers = [
+    layers.StandardNormLayer,
+    layers.StandardDenormLayer,
+    layers.MaxFeatureStdNormLayer,
+    layers.MaxFeatureStdNormLayer,
+    layers.MeanFeatureStdNormLayer,
+    layers.MeanFeatureStdDenormLayer,
+]
+
+
+def _get_tensor():
+    """
+    Tensor with 2 features (columns)
+    and 2 samples (rows)
+    """
+
+    return tf.Variable([[0.0, 0.0], [1.0, 2.0]], dtype=tf.float32)
+
+
+@pytest.mark.parametrize(
+    "norm_cls, denorm_cls, expected",
+    [
+        (
+            layers.StandardNormLayer,
+            layers.StandardDenormLayer,
+            [[-1.0, -1.0], [1.0, 1.0]],
+        ),
+        (
+            layers.MaxFeatureStdNormLayer,
+            layers.MaxFeatureStdDenormLayer,
+            [[-0.5, -1.0], [0.5, 1.0]],
+        ),
+    ],
+)
+def test_normalize_layers(norm_cls, denorm_cls, expected):
+    tensor = _get_tensor()
+    norm_layer = norm_cls()
+    denorm_layer = denorm_cls()
+    norm_layer.fit(tensor)
+    denorm_layer.fit(tensor)
+
+    norm = norm_layer(tensor)
+    expected = np.array(expected)
+    np.testing.assert_allclose(norm, expected, rtol=1e-6)
+    denorm = denorm_layer(norm)
+    np.testing.assert_allclose(denorm, tensor, rtol=1e-6, atol=1e-6)
+
+
+@pytest.mark.parametrize("layer_cls", _all_layers)
+def test_layers_no_trainable_variables(layer_cls):
+    tensor = _get_tensor()
+    layer = layer_cls()
+    layer(tensor)
+
+    assert [] == layer.trainable_variables
+
+
+def test_standard_layers_gradient_works_epsilon():
+    tensor = _get_tensor()
+    norm_layer = layers.StandardNormLayer()
+
+    with tf.GradientTape(persistent=True) as tape:
+        y = norm_layer(tensor)
+
+    g = tape.gradient(y, tensor)
+    expected = 1 / (norm_layer.sigma + norm_layer.epsilon)
+    np.testing.assert_array_almost_equal(expected, g[0, :])
+
+
+@pytest.mark.parametrize("layer_cls", _all_layers)
+def test_fit_layers_are_fitted(layer_cls):
+    tensor = _get_tensor()
+    layer = layer_cls()
+
+    assert not layer.fitted
+    layer.fit(tensor)
+    assert layer.fitted
