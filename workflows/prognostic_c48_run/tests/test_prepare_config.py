43440ddf7c38eaf6d6104f1a363793aaa109c3b0
Oliver Watt-Meyer <oliverwatt@gmail.com>
2021-10-12 15:47:00 -0700

    Fix prepare config tests and add fine-res example

--- workflows/prognostic_c48_run/tests/test_prepare_config.py
+++ workflows/prognostic_c48_run/tests/test_prepare_config.py
@@ -15,6 +15,7 @@ import pytest
         pytest.param(["examples/nudge_to_fine_config.yml"], id="n2f"),
         pytest.param(["examples/nudge_to_obs_config.yml"], id="n2o"),
         pytest.param(["examples/emulator.yml"], id="emulator"),
+        pytest.param(["examples/fine_res_ml.yml"], id="fine-res-ml"),
     ],
 )
 def test_prepare_ml_config_regression(regtest, argv):
