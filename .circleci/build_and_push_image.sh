ai2cm/fv3net

commit f3a49e6833df3ac53b0a1efbc84528f1cec39e5c

Run prognostic run tests outside of dockerfile (#1408)
Run prognostic runs tests outside of the dockerfile, so that we can
1. catch breaking changes to external state
2. use built images to debug failing tests. Currently there is no clean way to enter a development environment equivalent to the testing environment.

Added public API:
- new makefile targets `make test_image_%`. `push_image` now depends on these.

.circleci/build_and_push_image.sh
@@ -12,7 +12,6 @@ then
 fi
 
 apt-get install -y make jq
-make build_image_$IMAGE
 
 
 if [[ "$CIRCLE_BRANCH" == "master" ]]

Makefile
@@ -14,15 +14,30 @@ IMAGES = fv3net post_process_run prognostic_run
 .PHONY: build_images push_image run_integration_tests image_name_explicit
 ############################################################
 # Docker Image Management
+# 
+# There are three phases
+# build -> image test -> push
+# 
 ############################################################
 # pattern rule for building docker images
 build_image_%:
 	tools/docker_build_cached.sh us.gcr.io/vcm-ml/$*:$(CACHE_TAG) \
 		-f docker/$*/Dockerfile -t $(REGISTRY)/$*:$(VERSION) .
 
+push_image_%: image_test_%
+	docker push $(REGISTRY)/$*:$(VERSION)
+
+pull_image_%:
+	docker pull $(REGISTRY)/$*:$(VERSION)
+
+image_test_%: build_image_%
+	echo "No tests specified"
+
 build_images: $(addprefix build_image_, $(IMAGES))
 push_images: $(addprefix push_image_, $(IMAGES))
 
+
+# Prognostic run
 build_image_prognostic_run:
 	tools/docker_build_cached.sh us.gcr.io/vcm-ml/prognostic_run:$(CACHE_TAG) \
 		-f docker/prognostic_run/Dockerfile -t $(REGISTRY)/prognostic_run:$(VERSION) \
@@ -32,15 +47,18 @@ build_image_prognostic_run:
 		-f docker/prognostic_run/Dockerfile -t $(REGISTRY)/notebook:$(VERSION) \
 		--target notebook .
 
-push_image_prognostic_run: build_image_prognostic_run
+image_test_prognostic_run: build_image_prognostic_run
+	docker run \
+		--rm \
+		-v ${GOOGLE_APPLICATION_CREDENTIALS}:/tmp/key.json \
+		-e GOOGLE_APPLICATION_CREDENTIALS=/tmp/key.json \
+		-w /fv3net/workflows/prognostic_c48_run \
+		$(REGISTRY)/prognostic_run:$(VERSION) pytest
+
+push_image_prognostic_run: image_test_prognostic_run
 	docker push $(REGISTRY)/prognostic_run:$(VERSION)
 	docker push $(REGISTRY)/notebook:$(VERSION)
 
-push_image_%: build_image_%
-	docker push $(REGISTRY)/$*:$(VERSION)
-
-pull_image_%:
-	docker pull $(REGISTRY)/$*:$(VERSION)
 
 ############################################################
 # Documentation (rules match "deploy_docs_%")
@@ -82,9 +100,6 @@ deploy_local:
 ############################################################
 run_integration_tests:
 	./tests/end_to_end_integration/run_test.sh $(REGISTRY) $(VERSION)
-
-test_prognostic_run:
-	docker run prognostic_run pytest
 
 test_prognostic_run_report:
 	bash workflows/diagnostics/tests/prognostic/test_integration.sh

docker/prognostic_run/Dockerfile
@@ -134,20 +134,10 @@ WORKDIR /fv3net/workflows/prognostic_c48_run
 CMD ["bash"]
 
 ###############################################################################
-#  prognostic-run tests
-###############################################################################
-FROM bld as test
-
-ENV GOOGLE_APPLICATION_CREDENTIALS /tmp/key.json
-RUN --mount=type=secret,id=gcp,dst=/tmp/key.json \
-    ["bash", "-o", "pipefail", "-c", "pytest | tee /tmp/test-results.txt"]
-
-###############################################################################
 #  prognostic-run image
 ###############################################################################
 FROM bld AS prognostic-run
 # Copy results from test stage to ensure docker buildkit executes it
-COPY --from=test /tmp/test-results.txt .
 
 # build the docs
 RUN apt-get install -y graphviz && pip3 install sphinx sphinx_rtd_theme

commit bbdfd41e89b11948c5657bac45a78826e4062556

Fix doc deployment (#1412)
f3a49e6 broke the documentation deployment.

This was previously relying the image build invoked at the beginning of the build_and_push_image.sh script.

.circleci/build_and_push_image.sh
@@ -18,7 +18,7 @@ if [[ "$CIRCLE_BRANCH" == "master" ]]
 then
     echo "pushing untagged images as 'latest'"
     make push_image_$IMAGE VERSION=latest
-    make deploy_docs_$IMAGE
+    make deploy_docs_$IMAGE VERSION=latest
 fi
 
 if [[ -n "$CIRCLE_TAG" ]]