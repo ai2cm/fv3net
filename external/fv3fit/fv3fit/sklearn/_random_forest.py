59310f239e9222ab30bbf52cb45db4f3f51cb8ef
Oliver Watt-Meyer <oliwm@vulcan.com>
2021-10-18 14:17:08 -0700

    Refactor ArrayPacker to use xr.to_stacked_array (#1415)
    Refactor ArrayPacker to use the pack and unpack functions
    which use the xarray to_stacked_array and to_unstacked_dataset
    functions. This is mostly deleting a bunch of code and modifying
    the serialization of the ArrayPacker.
    
    Refactored public API:
    - The serialization format of ArrayPacker has changed in a non backwards compatible way
    - renamed packed-keras to packed-keras-v2
    
    Significant internal changes:
    - use pack and unpack instead of custom to_array and to_dataset functions

--- external/fv3fit/fv3fit/sklearn/_random_forest.py
+++ external/fv3fit/fv3fit/sklearn/_random_forest.py
@@ -4,7 +4,6 @@ import io
 import numpy as np
 from copy import copy
 import xarray as xr
-import pandas as pd
 import fsspec
 import joblib
 from .._shared import (
@@ -13,6 +12,8 @@ from .._shared import (
     Predictor,
     get_scaler,
     register_training_function,
+    multiindex_to_tuple,
+    tuple_to_multiindex,
 )
 from .._shared.config import RandomForestHyperparameters
 from .. import _shared
@@ -29,15 +30,6 @@ import sklearn.ensemble
 from typing import Optional, Iterable, Sequence
 import yaml
 from vcm import safe
-
-
-def _multiindex_to_tuple(index: pd.MultiIndex) -> tuple:
-    return list(index.names), list(index.to_list())
-
-
-def _tuple_to_multiindex(d: tuple) -> pd.MultiIndex:
-    names, list_ = d
-    return pd.MultiIndex.from_tuples(list_, names=names)
 
 
 def _parse_metadata_backward_compatible(metadata: dict) -> tuple:
@@ -310,7 +302,7 @@ class SklearnWrapper(Predictor):
         metadata = {
             "input_variables": self.input_variables,
             "output_variables": self.output_variables,
-            "output_features": _multiindex_to_tuple(self.output_features_),
+            "output_features": multiindex_to_tuple(self.output_features_),
         }
 
         mapper[self._METADATA_NAME] = yaml.safe_dump(metadata).encode("UTF-8")
@@ -334,7 +326,7 @@ class SklearnWrapper(Predictor):
             output_variables,
             output_features_tuple,
         ) = _parse_metadata_backward_compatible(metadata)
-        output_features_ = _tuple_to_multiindex(output_features_tuple)
+        output_features_ = tuple_to_multiindex(output_features_tuple)
 
         obj = cls(input_variables, output_variables, model)
         obj.target_scaler = scaler_obj
