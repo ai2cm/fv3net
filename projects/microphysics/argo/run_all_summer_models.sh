8c134c2113270b62c4eca7695f48ae58f7fb5fa7
Noah D. Brenowiz <nbren12@gmail.com>
2021-11-12 15:01:38 -0800

    add image-tag parameter to avoid circular commit nonsense

--- projects/microphysics/argo/run_all_summer_models.sh
+++ projects/microphysics/argo/run_all_summer_models.sh
@@ -5,6 +5,7 @@ set -x
 
 BASE_TAG="prognostic"
 MODEL_BASE="gs://vcm-ml-experiments/2021-10-14-microphsyics-emulation-paper/models"
+IMAGE="811d4a88b8b8f29d727f2d61f46f96b770c8ab47"
 
 run_style=("offline" "online")
 qc_tends=("all-tends-limited" "direct-qc-limited")
@@ -17,6 +18,7 @@ for flag in ${run_style[@]}; do
             MODEL="${MODEL_BASE}/${tend}/${tend}-${arch}/model.tf"
             argo submit prog-run-and-eval.yaml \
                 -p tag=$TAG \
+		        -p image_tag=$IMAGE \
                 -p baseline_tag="baseline" \
                 -p config="$(< configs/default.yaml)" \
                 -p tf_model=$MODEL \
