482f4fa51b3860dc4be5b8a0a821c445deba1a54
Andre Perkins <frodre@gmail.com>
2021-11-12 15:09:16 +0000

    Summer model runs

--- projects/microphysics/argo/run_all_summer_models.sh
+++ projects/microphysics/argo/run_all_summer_models.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-BASE_TAG="prognostic-offline"
+BASE_TAG="prognostic"
 RANDOM=$(openssl rand --hex 4)
 MODEL_BASE="gs://vcm-ml-experiments/2021-10-14-microphsyics-emulation-paper/models"
 
@@ -14,11 +14,12 @@ archictectures=("dense", "rnn")
 for flag in ${run_style[@]}; do
     for tend in ${qc_tends[@]}; do
         for arch in ${archictectures[@]}; do
-            TAG=${BASE_TAG}-${tend}-${arch}-${RANDOM}
+            TAG=${BASE_TAG}-${flag}-${tend}-${arch}-${RANDOM}
             MODEL=${MODEL_BASE}/${tend}/${tend}-${arch}/model.tf
             argo submit prog-run-and-eval.yaml \
                 -p tag=$TAG \
-                -p config=$(< default.yaml) \
+                -p baseline_tag="baseline" \
+                -p config="$(< configs/default.yaml)" \
                 -p tf_model=$MODEL \
                 -p on_off_flag="--${flag}"
         done
