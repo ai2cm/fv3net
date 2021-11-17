fd0b42e8fe804ef6ff372243ec4fb40d1a5d7735
Andre Perkins <frodre@gmail.com>
2021-11-17 21:30:04 +0000

    Training run scripts

--- projects/microphysics/create_training/run_single.sh
+++ projects/microphysics/create_training/run_single.sh
@@ -4,27 +4,16 @@ set -e
 
 PROG_YAML=$1
 MONTH=$2
-PROJECT=$3
+OUTPUT_FREQUENCY=$3
 
-RANDOM="$(openssl rand --hex 4)"
 IC_TIMESTAMP="2016${MONTH}01.000000"
-export IC_URL="gs://vcm-ml-raw/2020-11-05-GFS-month-start-initial-conditions-year-2016/2016${MONTH}0100"
-NC_OUTPUT_FREQ_SEC=18000
+IC_URL="gs://vcm-ml-raw/2020-11-05-GFS-month-start-initial-conditions-year-2016/2016${MONTH}0100"
 
-envsubst < $PROG_YAML > "prognostic-run-with-IC.yaml"
+# envsubst < $PROG_YAML > "prognostic-run-with-IC.yaml"
 
 # submit prognostic run forecasts
-argo submit \
-    --from workflowtemplate/prognostic-run \
-    --name create-emu-micro-data-${MONTH}-${RANDOM} \
-    -p reference-restarts=null \
-    -p initial-condition=$IC_TIMESTAMP \
-    -p config="$(< prognostic-run-with-IC.yaml)" \
-    -p memory="10Gi" \
-    -p cpu="6" \
-    -p project=${PROJECT} \
-    -p tag="init-${IC_TIMESTAMP}" \
-    -p online-diags="false" \
-    -p output_freq_sec=${NC_OUTPUT_FREQ_SEC}
-
-rm prognostic-run-with-IC.yaml
+argo submit argo.yaml \
+    -p initial_condition=$IC_URL \
+    -p config="$(< ${PROG_YAML})" \
+    -p tag="create-training-${IC_TIMESTAMP}" \
+    -p output_frequency=${OUTPUT_FREQUENCY}
