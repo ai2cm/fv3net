66a97197224d1cb62570713c07d72e71d97f39bb
Andre Perkins <frodre@gmail.com>
2021-11-17 23:22:21 +0000

    Add output_frequency configuration and switch to IC templating

--- projects/microphysics/create_training/run_single.sh
+++ projects/microphysics/create_training/run_single.sh
@@ -3,17 +3,20 @@
 set -e
 
 PROG_YAML=$1
-MONTH=$2
+# to replace current_date in template
+export MONTH=$2
 OUTPUT_FREQUENCY=$3
 
 IC_TIMESTAMP="2016${MONTH}01.000000"
-IC_URL="gs://vcm-ml-raw/2020-11-05-GFS-month-start-initial-conditions-year-2016/2016${MONTH}0100"
+# to replace initial_conditions in template
+export IC_URL="gs://vcm-ml-raw/2020-11-05-GFS-month-start-initial-conditions-year-2016/2016${MONTH}0100"
 
-# envsubst < $PROG_YAML > "prognostic-run-with-IC.yaml"
+envsubst < $PROG_YAML > "prognostic-run-with-IC.yaml"
 
 # submit prognostic run forecasts
 argo submit argo.yaml \
-    -p initial_condition=$IC_URL \
-    -p config="$(< ${PROG_YAML})" \
+    -p config="$(< prognostic-run-with-IC.yaml)" \
     -p tag="create-training-${IC_TIMESTAMP}" \
     -p output_frequency=${OUTPUT_FREQUENCY}
+
+rm prognostic-run-with-IC.yaml
