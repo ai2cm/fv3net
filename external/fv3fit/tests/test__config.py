44d5c7302049c41ace95f9d41108458fe74fd268
Anna Kwa <annak@allenai.org>
2021-11-18 22:35:44 +0000

    Replace DenseModel training func in tests with dense

--- external/fv3fit/tests/test__config.py
+++ external/fv3fit/tests/test__config.py
@@ -18,7 +18,7 @@ import pytest
 @pytest.mark.parametrize("hyperparameters", [{}])
 def test_dense_training_config_uses_optimizer_config(hyperparameters):
     config_dict = {
-        "model_type": "DenseModel",
+        "model_type": "dense",
         "input_variables": [],
         "output_variables": [],
         "hyperparameters": hyperparameters,
@@ -35,7 +35,7 @@ def test_safe_dump_training_config():
     """
     # TODO: extend this test to run not just for Dense, but for all registered models
     config = TrainingConfig(
-        model_type="DenseModel",  # an arbitrary model type
+        model_type="dense",  # an arbitrary model type
         hyperparameters=DenseHyperparameters(
             input_variables=["a"], output_variables=["b"],
         ),
@@ -68,7 +68,7 @@ def test_safe_dump_training_config():
 )
 def test__load_config_catches_errors_with_strict_checking(hyperparameters, passes):
     config_dict = {
-        "model_type": "DenseModel",
+        "model_type": "dense",
         "input_variables": [],
         "output_variables": [],
         "hyperparameters": hyperparameters,
