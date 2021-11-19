6bdf211b1f529a9701d70a76d2144d24abf35c68
Anna Kwa <annak@allenai.org>
2021-11-19 11:36:52 -0800

    Fix bug when zero padding larger than clipped feature dim length

--- external/fv3fit/fv3fit/keras/_models/shared/clip.py
+++ external/fv3fit/fv3fit/keras/_models/shared/clip.py
@@ -2,7 +2,7 @@ import dataclasses
 import numpy as np
 import tensorflow as tf
 from typing import Mapping, Hashable, Sequence
-from  fv3fit._shared.config import SliceConfig
+from fv3fit._shared.config import SliceConfig
 
 
 ClipDims = Mapping[Hashable, Mapping[str, SliceConfig]]
@@ -13,7 +13,7 @@ class ClipConfig:
     clip: ClipDims = dataclasses.field(default_factory=dict)
 
     def single_feature_slice(self, name: str) -> SliceConfig:
-        self._check_only_feature_dim_is_clipped(name)   
+        self._check_only_feature_dim_is_clipped(name)
         variable_slice_config = list(self.clip[name].values())[0]
         return variable_slice_config
 
@@ -26,22 +26,26 @@ class ClipConfig:
         stop = variable_slice_config.stop or original_1d_layer_size
         n_removed_from_start = start
         n_removed_from_end = original_1d_layer_size - stop
-        return (n_removed_from_start, n_removed_from_end) 
+        return (n_removed_from_start, n_removed_from_end)
 
     def clip_layer_along_feature_dim(self, layer, name):
         if name in self.clip:
-            self._check_only_feature_dim_is_clipped(name)   
+            self._check_only_feature_dim_is_clipped(name)
             variable_slice_config = list(self.clip[name].values())[0]
             if variable_slice_config.step is not None:
                 raise NotImplementedError(
-                    "Stepped slice is not implented for clipping a 1D layer along the feature dim."
+                    "Stepped slice is not implented for clipping a "
+                    "1D layer along the feature dim."
                 )
-            feature_start, feature_stop = variable_slice_config.start, variable_slice_config.stop
+            feature_start, feature_stop = (
+                variable_slice_config.start,
+                variable_slice_config.stop,
+            )
             feature_start = feature_start or 0
             feature_stop = feature_stop or layer.shape[-1]
 
             slice_start = [0 for l in layer.shape[:-1]] + [feature_start]
-            slice_size = [-1 for l in layer.shape[:-1]] + [feature_stop-feature_start]
+            slice_size = [-1 for l in layer.shape[:-1]] + [feature_stop - feature_start]
 
             return tf.slice(layer, slice_start, slice_size)
 
@@ -54,9 +58,12 @@ class ClipConfig:
                 f"Variable {name} has >1 dimension to clip along. "
                 "This method clip_layer_along_feature_dim assumes that only "
                 "the feature dim is specified in the clip config."
-                )     
+            )
 
-def zero_pad_output_feature_dim(layer: tf.Tensor, n_removed_from_start, n_removed_from_end) -> tf.Tensor:
+
+def zero_pad_output_feature_dim(
+    layer: tf.Tensor, n_removed_from_start, n_removed_from_end
+) -> tf.Tensor:
     # Restores a clipped output dim to its original feature dimension size by replacing
     # clipped slices with zeros
 
@@ -70,33 +77,39 @@ def zero_pad_output_feature_dim(layer: tf.Tensor, n_removed_from_start, n_remove
     if n_removed_from_end > 0:
         pad_end = _zero_slice_with_placeholder_dim(layer, n_removed_from_end)
         concat_layers.append(pad_end)
-    
+
     return tf.concat(concat_layers, axis=-1)
 
 
 def zero_fill_clipped_layers(
-        config: ClipConfig, 
-        clipped_layers: Sequence[tf.Tensor], 
-        variable_names: Sequence[str],
-        original_feature_dim_sizes: Sequence[int],
+    config: ClipConfig,
+    clipped_layers: Sequence[tf.Tensor],
+    variable_names: Sequence[str],
+    original_feature_dim_sizes: Sequence[int],
 ) -> Sequence[tf.Tensor]:
     # Takes layers and restores those that were clipped to their
     # original feature dim sizes by replacing clipped levels with zeroes.
     outputs = []
-    for layer, name, original_size in zip(clipped_layers, variable_names, original_feature_dim_sizes):
+    for layer, name, original_size in zip(
+        clipped_layers, variable_names, original_feature_dim_sizes
+    ):
         if name in config.clip:
-            n_removed_from_start, n_removed_from_end = config.removed_sizes(original_size, name)
-            outputs.append(zero_pad_output_feature_dim(layer, n_removed_from_start, n_removed_from_end))
+            n_removed_from_start, n_removed_from_end = config.removed_sizes(
+                original_size, name
+            )
+            outputs.append(
+                zero_pad_output_feature_dim(
+                    layer, n_removed_from_start, n_removed_from_end
+                )
+            )
         else:
             outputs.append(layer)
     return outputs
-    
+
 
 def clip_layers(
-        config: ClipConfig, 
-        layers: Sequence[tf.Tensor], 
-        variable_names: Sequence[str]
-        ) -> Sequence[tf.Tensor]:
+    config: ClipConfig, layers: Sequence[tf.Tensor], variable_names: Sequence[str]
+) -> Sequence[tf.Tensor]:
     # Takes layers and applies clipping to those that have entries in the ClipConfig.
     # The layer_names input is the list of variable names corresponding
     # to each layer in layers.
@@ -111,9 +124,7 @@ def clip_layers(
 
 
 def clip_arrays(
-        config: ClipConfig, 
-        arrays: Sequence[np.ndarray],
-        variable_names: Sequence[str],
+    config: ClipConfig, arrays: Sequence[np.ndarray], variable_names: Sequence[str],
 ) -> Sequence[np.ndarray]:
     # Takes np arrays and applies clipping to those that have entries
     # in the ClipConfig. The variable_names input is the list of variable
@@ -125,16 +136,28 @@ def clip_arrays(
     for array, name in zip(arrays, variable_names):
         if name in config.clip:
             slice = config.single_feature_slice(name)
-            outputs.append(array.take(indices=range(slice.start, slice.stop), axis=-1))
+            outputs.append(
+                array.take(
+                    indices=range(slice.start, slice.stop), axis=-1  # type: ignore
+                )
+            )
         else:
             outputs.append(array)
     return outputs
 
 
 def _zero_slice_with_placeholder_dim(layer: tf.Tensor, n_zero_levels: int) -> tf.Tensor:
-    # hacky way of getting zero padding tensor with same first placeholder dim as input
-    # but different feature dim size
+    # Hacky way of getting zero padding tensor with same first placeholder dim as input
+    # but different feature dim size. 
+    # The slice size cannot be larger than the original layer size, 
     layer_copy = tf.zeros_like(layer)
+    
+    # if padding needed is longer than clipped feature dim size, need to
+    # expand the copy layer to allow for this when slicing
+    n_template_levels = layer_copy.shape[-1]
+    if n_zero_levels > n_template_levels:
+        n_copies_needed = n_zero_levels // n_template_levels + 1
+        layer_copy = tf.concat([layer_copy for i in range(n_copies_needed)], axis=-1)
     slice_start = [0 for l in layer.shape]
     slice_size = [-1 for l in layer.shape[:-1]] + [n_zero_levels]
     return tf.slice(layer_copy, slice_start, slice_size)
