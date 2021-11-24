154dfb2bd4f6dd21665ef7588b7fd172219a4c1d
Oliver Watt-Meyer <oliwm@vulcan.com>
2021-11-24 13:33:17 -0800

    Keep units for dynamics diff; don't require for QuantityState (#1517)
    * Keep units when computing dynamics difference sources
    
    * Dont require units when converted dataset to QuantityState
    
    * Better name for test file

--- workflows/prognostic_c48_run/tests/test_conversions.py
+++ workflows/prognostic_c48_run/tests/test_conversions.py
@@ -7,8 +7,13 @@ da = xr.DataArray(np.ones((3, 4, 4)), dims=["tile", "x", "y"])
 ds = xr.Dataset(
     {"temp": da.assign_attrs(units="K"), "sphum": da.assign_attrs(units="kg/kg")}
 )
+ds_no_units = xr.Dataset({"temp": da, "sphum": da})
 
 
 def test_ds_to_quantity_round_trip():
     round_tripped_ds = quantity_state_to_dataset(dataset_to_quantity_state(ds))
     xr.testing.assert_identical(ds, round_tripped_ds)
+
+
+def test_ds_to_quantity_no_units():
+    dataset_to_quantity_state(ds_no_units)
