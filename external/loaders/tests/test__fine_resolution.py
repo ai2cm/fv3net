cdeed72b807e91c41eb187dc7f2013f9540069e0
Noah D. Brenowiz <nbren12@gmail.com>
2021-11-23 16:15:04 -0800

    Add dynamics difference defined appparent source

--- external/loaders/tests/test__fine_resolution.py
+++ external/loaders/tests/test__fine_resolution.py
@@ -1,7 +1,11 @@
-import xarray as xr
 import numpy as np
 import pytest
-from loaders.mappers._fine_res import _extend_lower
+import xarray as xr
+from loaders.mappers._fine_res import (
+    Approach,
+    _extend_lower,
+    compute_budget,
+)
 
 
 @pytest.mark.parametrize(
@@ -29,3 +33,51 @@ def test__extend_lower(nz, nx, vertical_dim, n_levels, error):
             )
             for i in range(n_levels)
         ]
+
+
+@pytest.mark.parametrize("include_temperature_nudging", [True, False])
+@pytest.mark.parametrize("approach", list(Approach))
+def test_compute_budget(approach, include_temperature_nudging):
+
+    if approach == Approach.apparent_sources_plus_dynamics_differences:
+        pytest.skip()
+
+    one = xr.DataArray(np.ones((5, 1, 1)), dims=["z", "y", "x"])
+    ds = xr.Dataset()
+    for name in [
+        "delp",
+        "T",
+        "dq3dt_deep_conv_coarse",
+        "dq3dt_mp_coarse",
+        "dq3dt_pbl_coarse",
+        "dq3dt_shal_conv_coarse",
+        "dt3dt_deep_conv_coarse",
+        "dt3dt_lw_coarse",
+        "dt3dt_mp_coarse",
+        "dt3dt_ogwd_coarse",
+        "dt3dt_pbl_coarse",
+        "dt3dt_shal_conv_coarse",
+        "dt3dt_sw_coarse",
+        "eddy_flux_vulcan_omega_sphum",
+        "eddy_flux_vulcan_omega_temp",
+        "exposed_area",
+        "qv_dt_fv_sat_adj_coarse",
+        "qv_dt_phys_coarse",
+        "sphum",
+        "sphum_storage",
+        "sphum_vulcan_omega_coarse",
+        "t_dt_fv_sat_adj_coarse",
+        "t_dt_nudge_coarse",
+        "t_dt_phys_coarse",
+        "vulcan_omega_coarse",
+        "T_vulcan_omega_coarse",
+        "T_storage",
+        "air_temperature_tendency_due_to_nudging",
+        "specific_humidity_tendency_due_to_nudging",
+        "tendency_of_air_temperature_due_to_dynamics",
+        "tendency_of_specific_humidity_due_to_dynamics",
+    ]:
+        ds[name] = one
+
+    out = compute_budget(ds, approach, include_temperature_nudging)
+    assert {"dQ1", "dQ2"} <= set(out)
