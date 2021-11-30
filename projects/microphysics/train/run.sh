e054fd48f9c5a406a02fe6925c59fe0fe0e3d6c7
Noah D. Brenowiz <nbren12@gmail.com>
2021-11-30 11:12:01 -0800

    Delete experiment launching layers
    it is clearer if all the experiments are in one place

--- projects/microphysics/train/run.sh
+++ projects/microphysics/train/run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
-config_file=$1
+for config in all-tendency-limited direct-cloud-limited; do
+    for model_type in rnn linear dense; do
+        model_name="${config}-${model_type}"
+        config_file="${config}.yaml"
+        out_url=$(artifacts resolve-url vcm-ml-experiments microphysics-emulation "${model_name}")
 
-argo submit argo.yaml \
-    -p training-config="$(< $config_file)"
+        argo submit argo.yaml \
+            -p training-config="$(base64 --wrap 0 $config_file)"\
+            -p flags="--model.architecture.name ${model_type} --wandb_model_name ${model_name} --out_url ${out_url}"
+
+    done
+done
