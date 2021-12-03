9d9ebc7849a3e7e8f87847f4f4eec6a824cf54cf
Noah D. Brenowiz <nbren12@gmail.com>
2021-12-03 13:54:48 -0800

    add Field docstring

--- external/fv3fit/fv3fit/emulation/models/microphysics.py
+++ external/fv3fit/fv3fit/emulation/models/microphysics.py
@@ -159,6 +159,28 @@ class MicrophysicsConfig:
 
 @dataclasses.dataclass(frozen=True)
 class Field:
+    """Configuration describing a prognostic field
+
+    A field with no ``output_name`` can be interpreted as an ML input only.
+    A field with no ``input_name`` is a diagnostic.
+
+    Attributes:
+        output_name: the name of the variable representing a field after being
+            modified by the an emulator
+        input_name: the name of the output variable
+        tendency_name: the name to call the output-input difference by
+        selection: how to subset the feature-space of the field
+    
+    Example:
+
+        >>> air_temperature = Field(
+        ...     output_name="air_temperature_after_step",
+        ...     input_name="air_temperature_before_step",
+        ...     tendency_name="tendency_of_air_temperature_due_to_step",
+        ...)
+
+    """
+
     output_name: str = ""
     input_name: str = ""
     residual: bool = True
@@ -169,6 +191,10 @@ class Field:
 
 @dataclasses.dataclass(frozen=True)
 class ZhaoCarrFields:
+    """Relationship between names and the physical input/outputs of the
+    Zhao-carr microphysics
+    """
+
     cloud_water: Field = Field(
         "cloud_water_mixing_ratio_after_precpd",
         "cloud_water_mixing_ratio_input",
@@ -188,6 +214,18 @@ class ZhaoCarrFields:
 
 @dataclasses.dataclass
 class ConservativeWaterConfig:
+    """Builds a model that diagnoses surface precipitation based on the total
+    water sink predicted by another ML model
+
+
+    The model returned by .build will include this surface precipitation
+    under the output name ``fields.surface_precipitation``.
+
+    Attributes:
+        extra_input_variables: extra inputs beyond cloud water, specific
+            humidity, and air temperature to pass to the ML architecture.
+    """
+
     fields: ZhaoCarrFields = ZhaoCarrFields()
     architecture: ArchitectureConfig = dataclasses.field(
         default_factory=lambda: ArchitectureConfig(name="linear")
@@ -255,7 +293,7 @@ class ConservativeWaterConfig:
     def name(self):
         return f"conservative-microphysics-emulator-{self.architecture.name}"
 
-    def build(self, data) -> tf.keras.Model:
+    def build(self, data: Mapping[str, tf.Tensor]) -> tf.keras.Model:
         model = self._build_base_model(data)
         return _assoc_conservative_precipitation(model, self.fields)
 
