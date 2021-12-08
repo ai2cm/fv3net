48228bc9c9d031a00cf72a8eeb7df7528b326583
Andre Perkins <frodre@gmail.com>
2021-12-08 23:30:04 +0000

    Run default RNN fix experiments

--- projects/microphysics/train/run.sh
+++ projects/microphysics/train/run.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 if [[ "$1" == "--test" ]]; then
-    extra_flags="--nfiles 2 --nfiles_valid 2"
+    extra_flags="--nfiles 2 --nfiles_valid 2 --epochs 10"
     bucket="vcm-ml-scratch"
 else
     bucket="vcm-ml-experiments"
@@ -13,13 +13,14 @@ fi
 group="$(openssl rand -hex 3)"
 
 for config in all-tendency-limited direct-cloud-limited; do
-    for model_type in rnn linear dense; do
+    for model_type in rnn-v1 rnn-v1-shared-weights; do
         model_name="${config}-${model_type}"
         config_file="${config}.yaml"
         out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}-${group}")
 
         argo submit argo.yaml \
             --name "${model_name}-${group}" \
+            -p wandb-run-group="rnn-fix-experiments" \
             -p training-config="$(base64 --wrap 0 $config_file)"\
             -p flags="--model.architecture.name ${model_type} --out_url ${out_url} ${extra_flags}"
 
