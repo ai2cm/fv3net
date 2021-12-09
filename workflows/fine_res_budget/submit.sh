3d3b4dea405e0d29446a5cbfaa17a4f5547305d9
Noah D. Brenowitz <nbren12@gmail.com>
2021-12-08 16:46:44 -0800

    Re-submit fine res coarsening (#1538)
    @mcgibbon fixed some bugs in this workflow so it needs to be re-run.
    
    I was unable to run as is without first fixing some version bugs in the setup.py. This workflow requires the `region` argument of `to_zarr` which was not in `xarray==0.15.0`. I just hacked the new versions into the `setup.py` since it will take much effort to hook this into our version locking sytem.
    
    I submitted the job locally for similar reasons and it ran succesfully:
    https://console.cloud.google.com/dataflow/jobs/us-central1/2021-12-07_21_06_53-2096919572283850840?project=vcm-ml
    
    See `submit.sh` for the new output path: gs://vcm-ml-experiments/default/2021-12-07/2020-05-27-40-day-X-SHiELD-simulation-v2/fine-res-budget.zarr

---
+++ workflows/fine_res_budget/submit.sh
@@ -0,0 +1,21 @@
+C384_ATMOS="gs://vcm-ml-raw/2020-05-27-40-day-X-SHiELD-simulation-C384-diagnostics/atmos_15min_coarse_ave.zarr"
+C384_RESTARTS="gs://vcm-ml-experiments/2020-06-02-fine-res/2020-05-27-40-day-X-SHiELD-simulation-C384-restart-files.zarr"
+OUTPUT="gs://vcm-ml-experiments/default/2021-12-07/2020-05-27-40-day-X-SHiELD-simulation-v2/fine-res-budget.zarr"
+
+cd ../dataflow
+
+./dataflow.sh submit  \
+    -m budget \
+    $C384_ATMOS \
+    $C384_RESTARTS \
+    gs://vcm-ml-raw/2020-05-27-40-day-X-SHiELD-simulation-C384-diagnostics/gfsphysics_15min_coarse.zarr/ \
+    gs://vcm-ml-raw/2020-11-10-C3072-to-C384-exposed-area.zarr \
+    $OUTPUT \
+    --job_name find-res-budget-$(openssl rand -hex 6) \
+    --project vcm-ml \
+    --region us-central1 \
+    --runner DataFlow \
+    --temp_location gs://vcm-ml-scratch/tmp_dataflow \
+    --autoscaling_algorithm=NONE \
+    --num_workers 64 \
+    --worker_machine_type n1-highmem-2
