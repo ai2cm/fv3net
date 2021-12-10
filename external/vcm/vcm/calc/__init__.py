ed0f7e737a908105531d1ca20591b0e26a636a0a
Jeremy McGibbon <mcgibbon@uw.edu>
2021-12-10 15:32:36 -0800

    remove some unused vcm functions

--- external/vcm/vcm/calc/__init__.py
+++ external/vcm/vcm/calc/__init__.py
@@ -1,6 +1,5 @@
-from .calc import apparent_heating, apparent_source, local_time, weighted_average
+from .calc import local_time, weighted_average
 from .metrics import r2_score
-from .q_terms import compute_Q_terms
 from ._zenith_angle import cos_zenith_angle
 
 __all__ = [
