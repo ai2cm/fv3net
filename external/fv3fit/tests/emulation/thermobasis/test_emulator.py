aa8bde6d78ea21583741f77d84c13c1c9556e09b
Anna Kwa <annak@allenai.org>
2021-12-14 09:42:48 -0800

    set deadline=None for flaky test

--- external/fv3fit/tests/emulation/thermobasis/test_emulator.py
+++ external/fv3fit/tests/emulation/thermobasis/test_emulator.py
@@ -17,7 +17,7 @@ from fv3fit.emulation.thermobasis.loss import (
 )
 from utils import _get_argsin
 import pytest
-from hypothesis import given
+from hypothesis import given, settings
 from hypothesis.strategies import lists, integers
 
 
@@ -152,6 +152,7 @@ def test_Config_register_parser(args, loss_cls):
 
 
 @given(lists(integers(min_value=0, max_value=100)))
+@settings(deadline=None)
 def test_Config_multi_output_levels(levels):
     str_levels = ",".join(str(s) for s in levels)
     parser = argparse.ArgumentParser()
