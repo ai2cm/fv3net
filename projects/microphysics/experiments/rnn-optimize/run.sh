60f62d0faaee814ae91ca7e99d6eead032771bef
Andre Perkins <frodre@gmail.com>
2021-12-22 22:25:21 +0000

    Switch optimize run to training argo

--- projects/microphysics/experiments/rnn-optimize/run.sh
+++ projects/microphysics/experiments/rnn-optimize/run.sh
@@ -6,32 +6,32 @@ set -o pipefail
 if [[ "$1" == "--test" ]]; then
     extra_flags="--nfiles 2 --nfiles_valid 2 --epochs 5"
     bucket="vcm-ml-scratch"
+    run_group="testing"
 else
     bucket="vcm-ml-experiments"
+    run_group="rnn-optimize-tests"
 fi
 
 group="$(openssl rand -hex 3)"
-tag=":acc03550cc1b9148b806f11e01f9ff01b1e2f87a"
+tag=":65382f5cd62c112f3e3cd2b7bc77e85d0aaa20b1"
+argofile="../../train/argo.yaml"
 
 submit () {
     model_name=$1
     flags=$2
 
     if [[ "$3" == "--gpu" ]]; then
-        argofile="argo_gpu.yaml"
-        tag=":cuda-base-v2"
-    else
-        argofile="../../train/argo.yaml"
+        gpu_param="-p gpu-train=true"
     fi
 
     out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}-${group}")
     argo submit ${argofile} \
         --name "${model_name}-${group}" \
-        -p wandb-run-group="rnn-optimize-tests" \
+        -p wandb-run-group=${run_group} \
         -p tag="${tag}" \
         -p training-config="$(base64 --wrap 0 rnn-train.yaml)" \
         -p flags="--out_url ${out_url} ${flags} ${extra_flags}" \
-        ${node}
+        ${gpu_param}
 }
 
 # LR
@@ -44,4 +44,4 @@ submit () {
 
 # GPU node w/ batch size
 submit rnn-v1-optimize-gpu-batch-512 "--loss.optimizer.kwargs.learning_rate 0.0002 --batch_size 512" "--gpu"
-submit rnn-v1-optimize-gpu-batch-1024 "--loss.optimizer.kwargs.learning_rate 0.00028 --batch_size 1024" "--gpu"
+# submit rnn-v1-optimize-gpu-batch-1024 "--loss.optimizer.kwargs.learning_rate 0.00028 --batch_size 1024" "--gpu"
