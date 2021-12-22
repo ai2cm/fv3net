4476a8f50bb2e856cfa6d165c0e84abcd44df6e3
Andre Perkins <frodre@gmail.com>
2021-12-22 23:42:05 +0000

    Fix learning rate schedule from yaml

--- projects/microphysics/experiments/rnn-optimize/run.sh
+++ projects/microphysics/experiments/rnn-optimize/run.sh
@@ -13,14 +13,15 @@ else
 fi
 
 group="$(openssl rand -hex 3)"
-tag=":65382f5cd62c112f3e3cd2b7bc77e85d0aaa20b1"
+tag=":c493e95fe3b0f2df24002d95601550eb62383ede"
 argofile="../../train/argo.yaml"
 
 submit () {
     model_name=$1
-    flags=$2
+    config=$2
+    flags=$3
 
-    if [[ "$3" == "--gpu" ]]; then
+    if [[ "$4" == "--gpu" ]]; then
         gpu_param="-p gpu-train=true"
     fi
 
@@ -29,19 +30,24 @@ submit () {
         --name "${model_name}-${group}" \
         -p wandb-run-group=${run_group} \
         -p tag="${tag}" \
-        -p training-config="$(base64 --wrap 0 rnn-train.yaml)" \
+        -p training-config="$(base64 --wrap 0 ${config})" \
         -p flags="--out_url ${out_url} ${flags} ${extra_flags}" \
         ${gpu_param}
 }
 
 # LR
-# submit rnn-v1-optimize-lr-001 "--loss.optimizer.kwargs.learning_rate 0.001"
+# submit rnn-v1-optimize-lr-001 rnn-train.yaml "--loss.optimizer.kwargs.learning_rate 0.001"
 
 # batch_size
-# submit rnn-v1-optimize-batch-256 "--loss.optimizer.kwargs.learning_rate 0.00014 --batch_size 512"
-# submit rnn-v1-optimize-batch-512 "--loss.optimizer.kwargs.learning_rate 0.0002 --batch_size 512"
-# submit rnn-v1-optimize-batch-1024 "--loss.optimizer.kwargs.learning_rate 0.00028 --batch_size 1024"
+# submit rnn-v1-optimize-batch-256 rnn-train.yaml "--loss.optimizer.kwargs.learning_rate 0.00014 --batch_size 512"
+# submit rnn-v1-optimize-batch-512 rnn-train.yaml "--loss.optimizer.kwargs.learning_rate 0.0002 --batch_size 512"
+# submit rnn-v1-optimize-batch-1024 rnn-train.yaml "--loss.optimizer.kwargs.learning_rate 0.00028 --batch_size 1024"
 
 # GPU node w/ batch size
-submit rnn-v1-optimize-gpu-batch-512 "--loss.optimizer.kwargs.learning_rate 0.0002 --batch_size 512" "--gpu"
-# submit rnn-v1-optimize-gpu-batch-1024 "--loss.optimizer.kwargs.learning_rate 0.00028 --batch_size 1024" "--gpu"
+# submit rnn-v1-optimize-gpu-batch-512 rnn-train.yaml "--loss.optimizer.kwargs.learning_rate 0.0002 --batch_size 512" "--gpu"
+# submit rnn-v1-optimize-gpu-batch-1024 rnn-train.yaml "--loss.optimizer.kwargs.learning_rate 0.00028 --batch_size 1024" "--gpu"
+
+# GPU node w/ learning rate schedule
+submit rnn-v1-optimize-gpu-batch-1024-lr-sched rnn-train-lr-schedule.yaml "--batch_size 1024" "--gpu"
+# submit rnn-v1-optimize-gpu-batch-2048-lr-sched rnn-train-lr-schedule.yaml "--batch_size 1024 --loss.optimizer.learning_rate.initial_learning_rate 0.0004" "--gpu"
+
