7354d5a7cd185a125c9bb2d2667456640b134a9d
Andre Perkins <frodre@gmail.com>
2021-12-23 01:19:11 +0000

    Update tag

--- projects/microphysics/experiments/rnn-antarctic/run.sh
+++ projects/microphysics/experiments/rnn-antarctic/run.sh
@@ -11,20 +11,20 @@ else
 fi
 
 group="$(openssl rand -hex 3)"
-tag=":cuda-base-v2"
+tag=":4476a8f50bb2e856cfa6d165c0e84abcd44df6e3"
 
 submit () {
     model_name=$1
     flags=$2
 
     out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}-${group}")
-    argo submit ../rnn-optimize/argo_gpu.yaml \
+    argo submit ../../train/argo.yaml \
         --name "${model_name}-${group}" \
         -p wandb-run-group="antarctic-training-experiment" \
         -p tag="${tag}" \
         -p training-config="$(base64 --wrap 0 rnn-v1-shared-weights.yaml)" \
         -p flags="--out_url ${out_url} ${flags} ${extra_flags}" \
-        ${node}
+        -p gpu-train="true"
 }
 
 submit direct-qc-rnn-v1-shared-weights-antarctic-only
