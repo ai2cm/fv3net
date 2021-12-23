bed535ec125dc69cbbae7c545494277caf0e4bb3
Andre Perkins <frodre@gmail.com>
2021-12-23 01:45:07 +0000

    Latest image

--- projects/microphysics/experiments/rnn-antarctic/offline.sh
+++ projects/microphysics/experiments/rnn-antarctic/offline.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 argo submit ../../argo/prog-run-and-eval.yaml \
     -p tag="direct-qc-rnn-antarctic-only-86560b" \
-    -p image_tag="acc03550cc1b9148b806f11e01f9ff01b1e2f87a" \
+    -p image_tag="ada69b948dcb3eee8416c91e2c1efcc099cac5bf" \
     -p baseline_tag="baseline-short" \
     -p config="$(base64 --wrap 0  ../../configs/default_short.yaml)" \
     -p tf_model="gs://vcm-ml-experiments/microphysics-emulation/2021-12-22/direct-qc-rnn-v1-shared-weights-antarctic-only-86560b/model.tf"
