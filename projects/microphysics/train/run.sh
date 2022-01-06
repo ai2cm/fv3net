5d4404ace3c34b626de7e7c5e26098f7daf0f841
Noah D. Brenowiz <nbren12@gmail.com>
2022-01-06 11:34:17 -0800

    experiment with last_gscond inputs
    * goal: improve log(qc) skill at individual columns. cloud-free cells need
      this information to predict cloud
    * argo job: log-cloud-dense-9b3e1a

--- projects/microphysics/train/run.sh
+++ projects/microphysics/train/run.sh
@@ -14,13 +14,10 @@ group="$(openssl rand -hex 3)"
 config=log-cloud
 config_file="${config}.yaml"
 
-for qcweight in 0.0 0.1 1.0 10.0 ; do
-    model_name="${config}-residual-qcweight${qcweight}-${group}"
-    out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}")
+model_name="${config}-gscond-inputs-${group}"
+out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}")
 
-    argo submit argo.yaml \
-        --name "${model_name}" \
-        -p training-config="$(base64 --wrap 0 $config_file)" \
-        -p flags="--loss.weights.cloud_water_mixing_ratio_after_precpd ${qcweight} \
-        --out_url ${out_url} ${extra_flags}" | tee -a submitted-jobs.yaml
-done
+argo submit argo.yaml \
+    --name "${model_name}" \
+    -p training-config="$(base64 --wrap 0 $config_file)" \
+    -p flags="--out_url ${out_url} ${extra_flags}" | tee -a submitted-jobs.txt
