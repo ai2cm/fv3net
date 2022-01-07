9c2d184ed57031365c52484978b49dcd10414bc1
Noah D. Brenowitz <nbren12@gmail.com>
2022-01-07 15:51:38 -0800

    Improve training data section
    * remove some extraneous figures
    * compare histograms of cloud water before and after

--- projects/microphysics/docs/training-data.md
+++ projects/microphysics/docs/training-data.md
@@ -1,115 +1,12 @@
----
-jupytext:
-  cell_metadata_filter: -all
-  formats: md:myst,ipynb
-  main_language: python
-  text_representation:
-    extension: .md
-    format_name: myst
-    format_version: 0.13
-    jupytext_version: 1.11.5
-kernelspec:
-  display_name: fv3net
-  language: python
-  name: fv3net
----
-
-
 # Training Data
 
-The training data is generated from several simulations with the FV3GFS atmospheric model
+The training data is generated from several simulations with the FV3GFS atmospheric model.
 
-```{code-cell} ipython3
-from fv3fit.emulation.zhao_carr_fields import Field, ZhaoCarrFields
-from fv3fit.train_microphysics import nc_dir_to_tf_dataset, TrainConfig
-import matplotlib.pyplot as plt
-from cycler import cycler
-import tensorflow as tf
+{numref}`log-qc-before` shows the histogram of cloud water before and after the Zhao-carr Microphysics.
+Trace amounts of cloud water with concentrations less than $10^{-22}$ kg/kg occur frequently in the input and output.
+The zhao-carr scheme seems to substantially reduce the clouds for the larger band.
 
-wong_palette = [
-    "#000000",
-    "#E69F00",
-    "#56B4E9",
-    "#009E73",
-    "#F0E442",
-    "#0072B2",
-    "#D55E00",
-    "#CC79A7",
-]
-
-
-plt.rcParams["axes.prop_cycle"] = cycler("color", wong_palette)
-```
-
-```{code-cell} ipython3
-# open data
-config = TrainConfig.from_yaml_path(
-    "gs://vcm-ml-experiments/microphysics-emulation/2022-01-04/"
-    "log-cloud-dense-9b3e1a/config.yaml"
-)
-
-train_ds = nc_dir_to_tf_dataset(
-    config.train_url, config.get_dataset_convertor(), nfiles=config.nfiles
-)
-train_set = next(iter(train_ds.batch(40000)))
-train_set = config.get_transform().forward(train_set)
-
-# open model
-model = tf.keras.models.load_model(config.out_url + "/model.tf")
-```
-
-The following conditional averaged plots show that a residual model works well for specific humidity and air temperature, but much less so for cloud water mixing ratio ($q_c$).
-The cloud water mixing ratio likely depends strongly on the relative humidity tendency by the non-grid-scale condensation processes.
-
-```{code-cell} ipython3
-predictions = model(train_set)
-
-fields = ZhaoCarrFields()
-qc = fields.cloud_water
-
-import numpy as np
-def conditional(y, x, bins=100):
-    """condtional pdf p(y|x)"""
-    y = np.asarray(y).ravel()
-    x = np.asarray(x).ravel()
-    f, xe, ye = np.histogram2d(x, y, bins=bins)
-    conditional_pdf = f / f.sum(0)
-    
-    # Add a gridspec with two rows and two columns and a ratio of 2 to 7 between
-    # the size of the marginal axes and the main axes in both directions.
-    # Also adjust the subplot parameters for a square plot.
-    fig = plt.figure()
-    gs = fig.add_gridspec(2, 1,  height_ratios=(7, 2),
-                          left=0.1, right=0.9, bottom=0.1, top=0.9,
-                          wspace=0.05, hspace=0.05)
-
-    ax = fig.add_subplot(gs[0, 0])
-    ax_hist = fig.add_subplot(gs[1, 0], sharex=ax)
-    
-    ax.pcolormesh(xe, ye, conditional_pdf, cmap='bone_r')
-    lim = [x.min(), x.max()]
-    ax.plot(lim, lim, color='green', lw=2)
-    
-    ax_hist.hist(x, bins=bins)
-    return ax, ax_hist
-
-
-def conditional_field(train_set, qc: Field):
-    ax, ax_hist = conditional(train_set[qc.output_name], train_set[qc.input_name], bins=30)
-    ax_hist.set_xlabel(qc.input_name)
-    ax.set_ylabel(qc.output_name)
-    ax.set_title(f"{qc.output_name} | {qc.input_name}")
-
-plt.figure()
-conditional_field(train_set, fields.air_temperature)
-
-plt.figure()
-conditional_field(train_set, fields.cloud_water)
-
-plt.figure()
-conditional_field(train_set, fields.specific_humidity)
-
-log_qc = Field("log_cloud_output", "log_cloud_input")
-plt.figure()
-conditional_field(train_set, log_qc)
+```{glue:figure} qc-before-after
+:name: log-qc-before
+Cloud water histograms before and after the Zhao-Carr microphysics. The total water change is {glue:text}`qc-before-after-total-percent:.2f` %.
 ```
---
+++ projects/microphysics/docs/training_data_figures.py
@@ -0,0 +1,159 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: fv3net
+#     language: python
+#     name: fv3net
+# ---
+
+# %% [markdown]
+# # Training Data
+#
+# The training data is generated from several simulations with the FV3GFS
+# atmospheric model
+
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+from cycler import cycler
+from fv3fit.emulation.zhao_carr_fields import Field, ZhaoCarrFields
+from fv3fit.train_microphysics import TrainConfig, nc_dir_to_tf_dataset
+from myst_nb import glue
+
+wong_palette = [
+    "#000000",
+    "#E69F00",
+    "#56B4E9",
+    "#009E73",
+    "#F0E442",
+    "#0072B2",
+    "#D55E00",
+    "#CC79A7",
+]
+
+
+plt.rcParams["axes.prop_cycle"] = cycler("color", wong_palette)
+
+# %%
+# open data
+config = TrainConfig.from_yaml_path(
+    "gs://vcm-ml-experiments/microphysics-emulation/2022-01-04/"
+    "log-cloud-dense-9b3e1a/config.yaml"
+)
+
+train_ds = nc_dir_to_tf_dataset(
+    config.train_url, config.get_dataset_convertor(), nfiles=config.nfiles
+)
+train_set = next(iter(train_ds.batch(40000)))
+train_set = config.get_transform().forward(train_set)
+
+
+# %% [markdown]
+# The following conditional averaged plots show that a residual model works well
+# for specific humidity and air temperature, but much less so for cloud water
+# mixing ratio ($q_c$).
+# The cloud water mixing ratio likely depends strongly on the relative humidity
+# tendency by the non-grid-scale condensation processes.
+
+# %%
+fields = ZhaoCarrFields()
+qc = fields.cloud_water
+
+
+def conditional(y, x, bins=100):
+    """condtional pdf p(y|x)"""
+    y = np.asarray(y).ravel()
+    x = np.asarray(x).ravel()
+    f, xe, ye = np.histogram2d(x, y, bins=bins)
+    conditional_pdf = f / f.sum(0)
+
+    # Add a gridspec with two rows and two columns and a ratio of 2 to 7 between
+    # the size of the marginal axes and the main axes in both directions.
+    # Also adjust the subplot parameters for a square plot.
+    fig = plt.figure()
+    gs = fig.add_gridspec(
+        2,
+        1,
+        height_ratios=(7, 2),
+        left=0.1,
+        right=0.9,
+        bottom=0.1,
+        top=0.9,
+        wspace=0.05,
+        hspace=0.05,
+    )
+
+    ax = fig.add_subplot(gs[0, 0])
+    ax_hist = fig.add_subplot(gs[1, 0], sharex=ax)
+
+    ax.pcolormesh(xe, ye, conditional_pdf, cmap="bone_r")
+    lim = [x.min(), x.max()]
+    ax.plot(lim, lim, color="green", lw=2)
+
+    ax_hist.hist(x, bins=bins)
+    return ax, ax_hist
+
+
+def conditional_field(train_set, qc: Field):
+    ax, ax_hist = conditional(
+        train_set[qc.output_name], train_set[qc.input_name], bins=30
+    )
+    ax_hist.set_xlabel(qc.input_name)
+    ax.set_ylabel(qc.output_name)
+    ax.set_title(f"{qc.output_name} | {qc.input_name}")
+
+
+plt.figure()
+conditional_field(train_set, fields.air_temperature)
+
+plt.figure()
+conditional_field(train_set, fields.cloud_water)
+
+plt.figure()
+conditional_field(train_set, fields.specific_humidity)
+
+log_qc = Field("log_cloud_output", "log_cloud_input")
+plt.figure()
+conditional_field(train_set, log_qc)
+
+
+# %%
+def plot_log_cloud_before_after(train_set, log=True):
+
+    if log:
+        bins = 10.0 ** np.arange(-40, 10, 1)
+        scale = "log"
+    else:
+        bins = 100
+        scale = "linear"
+
+    pred = train_set["cloud_water_mixing_ratio_after_precpd"].numpy()
+    truth = train_set["cloud_water_mixing_ratio_input"].numpy()
+    _, ax = plt.subplots()
+    ax.hist(truth.ravel(), bins, histtype="step", label="before")
+    ax.hist(pred.ravel(), bins, histtype="step", label="after")
+    ax.set_ylabel("count")
+    ax.set_xlabel(r"$q_c$ output (kg/kg)")
+    ax.legend()
+
+    ax.set_xscale(scale)
+    return ax
+
+
+plot_log_cloud_before_after(train_set)
+glue("qc-before-after", plt.gcf())
+
+after = train_set["cloud_water_mixing_ratio_after_precpd"].numpy()
+before = train_set["cloud_water_mixing_ratio_input"].numpy()
+delp = train_set["pressure_thickness_of_atmospheric_layer"].numpy()
+before = np.mean(np.sum(delp * before / 9.81, -1))
+after = np.mean(np.sum(delp * after / 9.81, -1))
+total_change = after - before
+glue("qc-before-after-total-percent", 100 * total_change / before)
