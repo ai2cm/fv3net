fix another bug: latitude was in radians

external/emulation/emulation/_emulate/microphysics.py
@@ -131,7 +131,7 @@ class MicrophysicsHook:
 
         lat_range = (-50, 50)
         logging.info(f"masking emulator predictions outside latitudes: {lat_range}")
-        inputs["latitude"] = state["latitude"].reshape((-1, 1))
+        inputs["latitude"] = np.rad2deg(state["latitude"].reshape((-1, 1)))
         lat_mask = mask.is_outside_lat_range(inputs, lat_range=lat_range)
         outputs = {name: np.atleast_2d(state[name]).T for name in predictions}
         predictions = mask.where(lat_mask, outputs, predictions)