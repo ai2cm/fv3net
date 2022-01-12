fix bug in masking code

external/emulation/emulation/_emulate/microphysics.py
@@ -133,11 +133,7 @@ class MicrophysicsHook:
         logging.info(f"masking emulator predictions outside latitudes: {lat_range}")
         inputs["latitude"] = state["latitude"].reshape((-1, 1))
         lat_mask = mask.is_outside_lat_range(inputs, lat_range=lat_range)
-        outputs = {
-            name: np.atleast_2d(state[name]).T
-            for name in predictions
-            if (name in state) and (state[name])
-        }
+        outputs = {name: np.atleast_2d(state[name]).T for name in predictions}
         predictions = mask.where(lat_mask, outputs, predictions)
 
         # tranpose back to FV3 conventions