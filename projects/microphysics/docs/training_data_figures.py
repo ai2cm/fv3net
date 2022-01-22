87d9f5a664a20d2466b92c331b49aecce58c840a
Noah D. Brenowitz <nbren12@gmail.com>
2022-01-21 18:27:14 -0800

    Compare the scaling methods

--- projects/microphysics/docs/training_data_figures.py
+++ projects/microphysics/docs/training_data_figures.py
@@ -1,7 +1,8 @@
 # ---
+# flake8: noqa
 # jupyter:
 #   jupytext:
-#     cell_metadata_filter: -all
+#     cell_metadata_filter: title,-all
 #     text_representation:
 #       extension: .py
 #       format_name: percent
@@ -24,6 +25,7 @@
 import subprocess
 
 import matplotlib.pyplot as plt
+import matplotlib.colors
 import numpy as np
 import pandas as pd
 import scipy.interpolate
@@ -72,12 +74,16 @@ fields = ZhaoCarrFields()
 qc = fields.cloud_water
 
 
-def conditional(y, x, bins=100):
+def conditional(y, x, bins=100, plot_1_1=True, norm=matplotlib.colors.LogNorm()):
     """condtional pdf p(y|x)"""
     y = np.asarray(y).ravel()
     x = np.asarray(x).ravel()
+
+    mask = ~(np.isnan(x) | np.isnan(y))
+    y = y[mask]
+    x = x[mask]
     f, xe, ye = np.histogram2d(x, y, bins=bins)
-    conditional_pdf = f / f.sum(0)
+    conditional_pdf = f / f.sum(1)
 
     # Add a gridspec with two rows and two columns and a ratio of 2 to 7 between
     # the size of the marginal axes and the main axes in both directions.
@@ -85,8 +91,9 @@ def conditional(y, x, bins=100):
     fig = plt.figure()
     gs = fig.add_gridspec(
         2,
-        1,
+        2,
         height_ratios=(7, 2),
+        width_ratios=(10, 0.25),
         left=0.1,
         right=0.9,
         bottom=0.1,
@@ -97,10 +104,13 @@ def conditional(y, x, bins=100):
 
     ax = fig.add_subplot(gs[0, 0])
     ax_hist = fig.add_subplot(gs[1, 0], sharex=ax)
+    ax_cbar = fig.add_subplot(gs[0, 1])
 
-    ax.pcolormesh(xe, ye, conditional_pdf, cmap="bone_r")
-    lim = [x.min(), x.max()]
-    ax.plot(lim, lim, color="green", lw=2)
+    im = ax.pcolormesh(xe, ye, conditional_pdf.T, cmap="bone_r", norm=norm)
+    plt.colorbar(im, cax=ax_cbar)
+    if plot_1_1:
+        lim = [x.min(), x.max()]
+        ax.plot(lim, lim, color="green", lw=2)
 
     ax_hist.hist(x, bins=bins)
     return ax, ax_hist
@@ -113,6 +123,14 @@ def conditional_field(train_set, qc: Field):
     ax_hist.set_xlabel(qc.input_name)
     ax.set_ylabel(qc.output_name)
     ax.set_title(f"{qc.output_name} | {qc.input_name}")
+
+
+train_set["log_cloud_input"] = np.log10(
+    train_set[fields.cloud_water.input_name] + 1e-30
+)
+train_set["log_cloud_output"] = np.log10(
+    train_set[fields.cloud_water.output_name] + 1e-30
+)
 
 
 plt.figure()
@@ -196,7 +214,6 @@ df.plot(
 temp = df.temp.tolist()
 qc = df.qc.tolist()
 print("# 99.9%-tile cloud conditioned on temperature")
-print(f"# computed in {__file__}")
 sha = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
 print(f"# git-rev: {sha}")
 print("temp = ", temp)
@@ -210,3 +227,94 @@ df.plot(
     xlabel="Temperature (K)",
     logy=True,
 )
+# %%
+
+
+def compare_conditional(df, x, y):
+    ax, ax_hist = conditional(
+        df[y], df[T], norm=matplotlib.colors.LogNorm(), plot_1_1=False
+    )
+    ax.set_title(y)
+    ax.set_ylabel(y)
+    ax_hist.set_xlabel(x)
+
+
+df = pd.DataFrame(
+    {key: np.ravel(array) for key, array in train_set.items() if array.shape[1] != 1}
+)
+
+qc = "cloud_water_mixing_ratio_after_precpd"
+T = "air_temperature_input"
+
+df["qv difference"] = (
+    df["specific_humidity_after_precpd"] - df["specific_humidity_input"]
+)
+df["temp difference"] = df["air_temperature_after_precpd"] - df["air_temperature_input"]
+df["qc difference"] = (
+    df["cloud_water_mixing_ratio_after_precpd"] - df["cloud_water_mixing_ratio_input"]
+)
+
+df["log_10(qc+1e-45)"] = np.log10(df[qc] + 1e-45)
+df["log_10(qc+1e-10)"] = np.log10(df[qc] + 1e-10)
+
+
+for field in [
+    "qv difference",
+    "temp difference",
+    "qc difference",
+    "cloud_water_mixing_ratio_after_precpd",
+    "log_10(qc+1e-10)",
+    "log_10(qc+1e-45)",
+]:
+    compare_conditional(df, T, field)
+
+
+# %% [markdown]
+#
+# The variances of the "difference" variables all depend strongly on temperature. The cloud water for amounts > $10^{-10}$ also has some temperature dependence, but does not vary over as many orders of magnitude.
+
+# %% [markdown]
+# # Compare scaling method
+#
+# See overleaf. The metric is $a^{-1} / x$ where $a^{-1}$ is the typical magnitude in the transformed spaced.
+
+# %% [markdown]
+# This is a very large expected multiplier on the error magnitude
+
+# %%
+def boxplot_scalings(df, field, diff_field, scale=None, diff_scale=None):
+
+    scale = scale or (field + "_sig")
+    diff_scale = diff_scale or (diff_field + "_sig")
+    output = {}
+    output["t_dep"] = df[scale] / df[field]
+    output["d_t_dep"] = df[diff_scale] / df[field]
+    output["scale"] = df[field].std() / df[field]
+    output["d_scale"] = df[diff_field].std() / df[field]
+    plt.boxplot(output.values(), labels=output.keys())
+
+
+bins = np.arange(170, 320, 2.5)
+t_bins = pd.cut(df["air_temperature_input"], bins)
+df_m = df.assign(t_bins=t_bins)
+scale = df_m.groupby(t_bins).std()
+df_m = df_m.merge(scale, left_on="t_bins", right_index=True, suffixes=("", "_sig"))
+
+# %%
+boxplot_scalings(df_m, "air_temperature_after_precpd", "temp difference")
+plt.grid()
+plt.yscale("log")
+
+# %%
+boxplot_scalings(df_m, "specific_humidity_after_precpd", "qv difference")
+plt.grid()
+plt.yscale("log")
+
+# %%
+boxplot_scalings(
+    df_m[df_m["cloud_water_mixing_ratio_after_precpd"] > 1e-10],
+    "cloud_water_mixing_ratio_after_precpd",
+    "qc difference",
+)
+plt.yscale("log")
+plt.grid()
