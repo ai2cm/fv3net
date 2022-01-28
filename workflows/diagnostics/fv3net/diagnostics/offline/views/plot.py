304d83b1e856cfd768f90821988445384b7ed1d4
Anna Kwa <annak@allenai.org>
2022-01-28 11:54:53 -0800

    consistent color legend in profile var plot and diurnal cycle plot

--- workflows/diagnostics/fv3net/diagnostics/offline/views/plot.py
+++ workflows/diagnostics/fv3net/diagnostics/offline/views/plot.py
@@ -106,9 +106,8 @@ def plot_profile_var(
     derivation_dim: str = "derivation",
     domain_dim: str = "domain",
     dataset_dim: str = "dataset",
-    derivation_plot_coords: Sequence[str] = ("target", "predict"),
+    derivation_plot_coords: Sequence[str] = ("predict", "target"),
     xlim: Sequence[float] = None,
-    xticks: Union[Sequence[float], np.ndarray] = None,
 ):
     if dataset_dim in ds[var].dims:
         ds[var] = ds[var].mean("dataset")
