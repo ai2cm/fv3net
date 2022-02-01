4e5493f3350542503ea817f51dac71e280bd0da6
Noah D. Brenowiz <nbren12@gmail.com>
2022-02-01 13:27:44 -0800

    Submit three different learning rate experiments
    Use weight of 1 for all variables for simplicity
    
    Name:                rnn-alltdep-2658c1-lr0.0001
      flags:             --out_url gs://vcm-ml-experiments/microphysics-emulation/2022-02-01/rnn-alltdep-2658c1-lr0.0001  --loss.optimizer.kwargs.learning_rate 0.0001
    
    Name:                rnn-alltdep-2658c1-lr0.001
      flags:             --out_url gs://vcm-ml-experiments/microphysics-emulation/2022-02-01/rnn-alltdep-2658c1-lr0.001  --loss.optimizer.kwargs.learning_rate 0.001
    
    Name:                rnn-alltdep-2658c1-lr0.01
      flags:             --out_url gs://vcm-ml-experiments/microphysics-emulation/2022-02-01/rnn-alltdep-2658c1-lr0.01  --loss.optimizer.kwargs.learning_rate 0.01

--- projects/microphysics/train/run.sh
+++ projects/microphysics/train/run.sh
@@ -14,10 +14,12 @@ group="$(openssl rand -hex 3)"
 config=rnn
 config_file="${config}.yaml"
 
-model_name="rnn-gscond-alltdep-${group}"
-out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}")
-
-argo submit argo.yaml \
-    --name "${model_name}" \
-    -p training-config="$(base64 --wrap 0 $config_file)" \
-    -p flags="--out_url ${out_url} ${extra_flags}" | tee -a submitted-jobs.txt
+for lr in 0.0001 0.001 0.01
+do
+    model_name="rnn-alltdep-${group}-lr${lr}"
+    out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}")
+    argo submit argo.yaml \
+        --name "${model_name}" \
+        -p training-config="$(base64 --wrap 0 $config_file)" \
+        -p flags="--out_url ${out_url} ${extra_flags} --loss.optimizer.kwargs.learning_rate $lr" | tee -a submitted-jobs.txt
+done
