e027b2899cdca70aa73260429f530fc78182e3c9
Noah D. Brenowitz <nbren12@gmail.com>
2022-02-07 11:14:55 -0800

    Improve performance of duration calculation

--- projects/microphysics/download_wandb/4_prog_run_stats.py
+++ projects/microphysics/download_wandb/4_prog_run_stats.py
@@ -2,8 +2,34 @@ import sqlite3
 import wandb
 import os
 import logging
-import tqdm
 import time_to_crash
+import joblib
+
+
+def process(row):
+    con = sqlite3.connect("example.db")
+    cur1 = con.cursor()
+    id_, wandb_id = row
+    run = run_from_id(wandb_id)
+    try:
+        rundir = get_rundir_from_prog_run(run)
+        duration = time_to_crash.get_duration(rundir)
+        cur1.execute(
+            """
+            INSERT INTO progsummary (run_id, location, duration_seconds)
+            VALUES (?, ?, ?)
+            """,
+            (id_, rundir, duration.total_seconds()),
+        )
+        logging.info(f"{id_} processed.")
+    except Exception:
+        logging.error(f"{id_} not processed.")
+        pass
+    con.commit()
+    con.close()
+
+
+logging.basicConfig(level=logging.INFO)
 
 project = "microphysics-emulation"
 entity = "ai2cm"
@@ -27,29 +53,12 @@ def get_rundir_from_prog_run(run):
 con = sqlite3.connect("example.db")
 
 cur = con.cursor()
-count = list(cur.execute("select count(*) from prognostic_runs"))[0][0]
-print(count)
-
-cur = con.cursor()
 out = cur.execute("select id, wandb_id from prognostic_runs")
-for row in tqdm.tqdm(out, total=count):
-    id_, wandb_id = row
-    run = run_from_id(wandb_id)
-    try:
-        rundir = get_rundir_from_prog_run(run)
-        duration = time_to_crash.get_duration(rundir)
-        cur1 = con.cursor()
-        cur1.execute(
-            """
-            INSERT INTO progsummary (run_id, location, duration_seconds)
-            VALUES (?, ?, ?)
-            """,
-            (id_, rundir, duration.total_seconds()),
-        )
-        logging.info(f"{id_} processed.")
-    except Exception:
-        logging.error(f"{id_} not processed.")
-        pass
+
+joblib.Parallel(n_jobs=32, backend="threading")(
+    joblib.delayed(process)(row) for row in list(out)
+)
+
 
 con.commit()
 con.close()
