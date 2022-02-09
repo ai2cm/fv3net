6a6730e7c257fd721f281f10b37305d1fbc9fd4c
Andre Perkins <frodre@gmail.com>
2022-02-09 22:45:11 +0000

    Antarctic configurations

---
+++ projects/microphysics/antarctic/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [[ "$1" == "--test" ]]; then
+    extra_flags="--nfiles 2 --nfiles_valid 2 --epochs 5"
+    bucket="vcm-ml-scratch"
+else
+    bucket="vcm-ml-experiments"
+fi
+
+group="$(openssl rand -hex 3)"
+config="rnn-cloudtdep"
+
+# for config in rnn-cloudtdep dense-cloudtdep
+for config in rnn-cloudtdep
+do
+# for antarctic_only in true false
+for antarctic_only in true
+do
+    config_file="${config}.yaml"
+    model_name="zcemu-${config}-${group}-antarctic-${antarctic_only}"
+    out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}")
+    flags="--out_url ${out_url} ${extra_flags} \
+    --transform.antarctic_only ${antarctic_only} \
+    "
+
+    argo submit ../train/argo.yaml \
+        --name "${model_name}" \
+        -p training-config="$(base64 --wrap 0 $config_file)" \
+        -p wandb-run-group="antarctic-experiments-feb-2022" \
+        -p flags="$flags" | tee -a submitted-jobs.txt
+done
+done
