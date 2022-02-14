50827a45d74754d0cb2167cf692562abf778cc0a
Anna Kwa <annak@allenai.org>
2022-02-14 12:46:46 -0800

    Use dataclass for RF feature importances

--- external/fv3fit/fv3fit/_shared/input_sensitivity.py
+++ external/fv3fit/fv3fit/_shared/input_sensitivity.py
@@ -1,13 +1,34 @@
 import numpy as np
-from typing import Optional, Mapping
+from typing import Optional, Mapping, Sequence
 from dataclasses import dataclass
 
 
-RandomForestInputSensitivity = Mapping[str, Mapping[str, np.ndarray]]
+# Jacobian format: {y_name: {x_name: dy/dx}}
 JacobianInputSensitivity = Mapping[str, Mapping[str, np.ndarray]]
 
 
 @dataclass
+class RandomForestInputSensitivity:
+    """
+    Attributes:
+        name: input feature name
+        mean_importances: average value of feature importance across ensemble
+        std_importances: standard deviation of feature importance across ensemble
+        indices: for input features of length >1 along feature dimension, the indices
+            corresponding to position along feature dimension. Has length of 1 with
+            nan index for scalar features.
+    """
+
+    name: str
+    mean_importances: Sequence[float]
+    std_importances: Sequence[float]
+    indices: Sequence[int]
+
+
+RandomForestInputSensitivities = Sequence[RandomForestInputSensitivity]
+
+
+@dataclass
 class InputSensitivity:
-    rf_feature_importances: Optional[RandomForestInputSensitivity] = None
+    rf_feature_importances: Optional[RandomForestInputSensitivities] = None
     jacobians: Optional[JacobianInputSensitivity] = None
