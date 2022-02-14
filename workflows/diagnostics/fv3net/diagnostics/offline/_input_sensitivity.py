42c96ffbc02a4a02e4c138f4651f94768c94fd5b
Anna Kwa <annak@allenai.org>
2022-02-14 14:05:38 -0800

    use RandomForestInputSensitivity dataclass

--- workflows/diagnostics/fv3net/diagnostics/offline/_input_sensitivity.py
+++ workflows/diagnostics/fv3net/diagnostics/offline/_input_sensitivity.py
@@ -49,11 +49,16 @@ def plot_input_sensitivity(model: fv3fit.Predictor, sample: xr.Dataset):
 
 def _plot_rf_feature_importance(rf_input_sensitivity):
     vector_features, scalar_features = {}, {}
-    for k, v in rf_input_sensitivity.items():
-        if len(v["indices"]) > 1:
-            vector_features[k] = v
+    for feature in rf_input_sensitivity:
+        info = {
+            "indices": feature.indices,
+            "mean_importances": feature.mean_importances,
+            "std_importances": feature.std_importances,
+        }
+        if len(feature.indices) > 1:
+            vector_features[feature.name] = info
         else:
-            scalar_features[k] = v
+            scalar_features[feature.name] = info
     n_panels = (
         len(vector_features) + 1 if len(scalar_features) > 0 else len(vector_features)
     )
