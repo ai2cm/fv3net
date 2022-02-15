9fd0b64a3ad69ec78315d22e9a7918a59f5e505e
Jeremy McGibbon <mcgibbon@uw.edu>
2022-02-15 09:55:09 -0800

    add logging to loaders.batches.save

--- external/loaders/loaders/batches/save.py
+++ external/loaders/loaders/batches/save.py
@@ -2,6 +2,11 @@ import argparse
 from loaders._config import BatchesLoader
 import yaml
 import os.path
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -28,13 +33,25 @@ def main(data_config: str, output_path: str):
     with open(data_config, "r") as f:
         config = yaml.safe_load(f)
     loader = BatchesLoader.from_dict(config)
+    logger.info("configuration loaded, creating batches object")
+    print(1)
     batches = loader.load_batches()
+    n_batches = len(batches)
+    logger.info(f"batches object created, saving {n_batches} batches")
     for i, batch in enumerate(batches):
         out_filename = os.path.join(output_path, f"{i:05}.nc")
+        logger.info(f"saving batch {i}")
         batch.to_netcdf(out_filename, engine="h5netcdf")
 
 
 if __name__ == "__main__":
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(
+        logging.Formatter("%(name)s %(asctime)s: %(module)s/L%(lineno)d %(message)s")
+    )
+    handler.setLevel(logging.INFO)
+    logging.basicConfig(handlers=[handler], level=logging.INFO)
+
     parser = get_parser()
     args = parser.parse_args()
     main(data_config=args.data_config, output_path=args.output_path)
