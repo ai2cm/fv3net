1290421e627001297fdfe2e33bc60ec2a0404807
Noah D. Brenowitz <nbren12@gmail.com>
2022-02-16 15:38:21 -0800

    fv3fit: filter small values when fitting scaled-transform (#1641)
    The temperature scaling is current fit including samples that are identically zero. This causes unstable training (See microphysics emulation slides).
    
    * Add option `filter_magnitude` to remove these samples from the fit
    * refactor differencing to new object for improved composability

--- external/fv3fit/fv3fit/emulation/transforms/transforms.py
+++ external/fv3fit/fv3fit/emulation/transforms/transforms.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Callable, List
+from typing import Callable, List, Set
 
 import tensorflow as tf
 from typing_extensions import Protocol
@@ -12,6 +12,41 @@ class TensorTransform(Protocol):
 
     def backward(self, y: TensorDict) -> TensorDict:
         pass
+
+
+@dataclasses.dataclass
+class Difference(TensorTransform):
+    """A difference variable::
+
+        to = after - before
+
+    Notes:
+        This class is its own factory (i.e. includes the .build and
+        .backwards_names methods). This is only possible because it doesn't
+        depend on data and can be represented directly in yaml.
+
+    """
+
+    to: str
+    before: str
+    after: str
+
+    def backward_names(self, requested_names: Set[str]) -> Set[str]:
+        new_names = {self.before, self.after} if self.to in requested_names else set()
+        return requested_names.union(new_names)
+
+    def build(self, sample: TensorDict) -> TensorTransform:
+        return self
+
+    def forward(self, x: TensorDict) -> TensorDict:
+        x = {**x}
+        x[self.to] = x[self.after] - x[self.before]
+        return x
+
+    def backward(self, y: TensorDict) -> TensorDict:
+        y = {**y}
+        y[self.after] = y[self.before] + y[self.to]
+        return y
 
 
 @dataclasses.dataclass
@@ -57,16 +92,14 @@ class ConditionallyScaledTransform(TensorTransform):
     def __init__(
         self,
         to: str,
-        input_name: str,
-        output_name: str,
+        source: str,
         on: str,
         scale: Callable[[tf.Tensor], tf.Tensor],
         center: Callable[[tf.Tensor], tf.Tensor],
         min_scale: float = 0.0,
     ) -> None:
         self.to = to
-        self.input_name = input_name
-        self.output_name = output_name
+        self.source = source
         self.on = on
         self.scale = scale
         self.center = center
@@ -77,17 +110,15 @@ class ConditionallyScaledTransform(TensorTransform):
 
     def forward(self, x: TensorDict) -> TensorDict:
         out = {**x}
-        out[self.to] = (
-            x[self.output_name] - x[self.input_name] - self.center(x[self.on])
-        ) / self._limited_scale(x[self.on])
+        out[self.to] = (x[self.source] - self.center(x[self.on])) / self._limited_scale(
+            x[self.on]
+        )
         return out
 
     def backward(self, y: TensorDict) -> TensorDict:
         out = {**y}
-        out[self.output_name] = (
-            y[self.to] * self._limited_scale(y[self.on])
-            + y[self.input_name]
-            + self.center(y[self.on])
+        out[self.source] = y[self.to] * self._limited_scale(y[self.on]) + self.center(
+            y[self.on]
         )
         return out
 
