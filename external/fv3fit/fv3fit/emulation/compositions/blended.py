ee9393cd255431a56a73cfef8b1d2e2c95928dfd
Andre Perkins <frodre@gmail.com>
2022-02-18 20:05:16 +0000

    Clarify variable names

--- external/fv3fit/fv3fit/emulation/compositions/blended.py
+++ external/fv3fit/fv3fit/emulation/compositions/blended.py
@@ -7,8 +7,8 @@ TensorDict = Mapping[str, tf.Tensor]
 
 
 def blended_model(
-    model_a: tf.keras.Model,
-    model_b: tf.keras.Model,
+    model_for_true: tf.keras.Model,
+    model_for_false: tf.keras.Model,
     mask_func: Callable[[TensorDict], tf.Tensor],
     inputs: Mapping[str, tf.keras.Input],
 ) -> tf.keras.Model:
@@ -20,35 +20,37 @@ def blended_model(
     dimension from the mask, when singleton feature fields exist.
 
     Args:
-        model_a: model to use at points where mask == True
-        model_b: model to use at points where mask == False
+        model_for_true: model to use at points where mask == True
+        model_for_false: model to use at points where mask == False
         mask_func: masking function determining model output selection.  the
-            function uses the model inputs and should return a mask tensor
-            with [sample, feature] or [sample, 1] dimensions
+            function uses the blended model's inputs and should return a mask
+            tensor with [sample, feature] or [sample, 1] dimensions
         inputs: input tensors for the model, assumed to be
             cover all necessary inputs for model_a and model_b
     """
 
     # avoid name conflicts in blended model
-    model_a._name = "ModelA"
-    model_b._name = "ModelB"
+    model_for_true._name = "model_for_true"
+    model_for_false._name = "model_for_false"
 
-    model_a_out = ensure_dict_output(model_a)(inputs)
-    model_b_out = ensure_dict_output(model_b)(inputs)
+    model_for_true_out = ensure_dict_output(model_for_true)(inputs)
+    model_for_false_out = ensure_dict_output(model_for_false)(inputs)
 
-    overlap = set(model_a_out) & set(model_b_out)
+    overlap = set(model_for_true_out) & set(model_for_false_out)
 
     mask = tf.cast(mask_func(inputs), tf.bool)
 
     new_outputs = {}
     for key in overlap:
-        new_outputs[key] = tf.where(mask, x=model_a_out[key], y=model_b_out[key])
+        new_outputs[key] = tf.where(
+            mask, x=model_for_true_out[key], y=model_for_false_out[key]
+        )
 
     new_outputs.update(
-        {key: a_out for key, a_out in model_a_out.items() if key not in overlap}
+        {key: a_out for key, a_out in model_for_true_out.items() if key not in overlap}
     )
     new_outputs.update(
-        {key: b_out for key, b_out in model_b_out.items() if key not in overlap}
+        {key: b_out for key, b_out in model_for_false_out.items() if key not in overlap}
     )
 
     return tf.keras.Model(inputs=inputs, outputs=new_outputs)
