fa5ed1f8dba6b11415714a4cf117870f73915ab3
brianhenn <brianhenn@allenai.org>
2022-03-01 21:15:55 +0000

    loosen assumptions on grid metadata in offline diags

--- workflows/diagnostics/fv3net/diagnostics/offline/_helpers.py
+++ workflows/diagnostics/fv3net/diagnostics/offline/_helpers.py
@@ -79,7 +79,9 @@ def load_grid_info(res: str = "c48"):
     wind_rotation = catalog[f"wind_rotation/{res}"].read()
     land_sea_mask = catalog[f"landseamask/{res}"].read()
     grid_info = xr.merge([grid, wind_rotation, land_sea_mask])
-    return safe.get_variables(grid_info, GRID_INFO_VARS).drop("tile")
+    return safe.get_variables(grid_info, GRID_INFO_VARS).drop_vars(
+        "tile", errors="ignore"
+    )
 
 
 def open_diagnostics_outputs(
