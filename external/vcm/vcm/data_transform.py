2daa8ce618e695b8cf99ef3eaf117790aaaa781f
Oliver Watt-Meyer <oliverwatt@gmail.com>
2022-03-17 16:03:08 -0700

    Use func.__name__ to simplify registering

--- external/vcm/vcm/data_transform.py
+++ external/vcm/vcm/data_transform.py
@@ -14,11 +14,9 @@ TransformName = Literal[
 
 @curry
 def register(
-    name: str,
-    inputs: Sequence[str],
-    outputs: Sequence[str],
-    func: Callable[..., xr.Dataset],
+    inputs: Sequence[str], outputs: Sequence[str], func: Callable[..., xr.Dataset],
 ):
+    name = func.__name__
     if name in DATA_TRANSFORM_REGISTRY:
         raise ValueError(f"Function {name} has already been added to registry.")
     DATA_TRANSFORM_REGISTRY[name] = {
@@ -29,25 +27,25 @@ def register(
     return func
 
 
-@register("qm_from_q1_q2", ["Q1", "Q2"], ["Qm"])
+@register(["Q1", "Q2"], ["Qm"])
 def qm_from_q1_q2(ds):
     ds["Qm"] = vcm.moist_static_energy_tendency(ds["Q1"], ds["Q2"])
     return ds
 
 
-@register("q1_from_qm_q2", ["Qm", "Q2"], ["Q1"])
+@register(["Qm", "Q2"], ["Q1"])
 def q1_from_qm_q2(ds):
     ds["Q1"] = vcm.temperature_tendency(ds["Qm"], ds["Q2"])
     return ds
 
 
-@register("q1_from_dQ1_pQ1", ["dQ1", "pQ1"], ["Q1"])
+@register(["dQ1", "pQ1"], ["Q1"])
 def q1_from_dQ1_pQ1(ds):
     ds["Q1"] = ds["dQ1"] + ds["pQ1"]
     return ds
 
 
-@register("q2_from_dQ2_pQ2", ["dQ2", "pQ2"], ["Q2"])
+@register(["dQ2", "pQ2"], ["Q2"])
 def q2_from_dQ2_pQ2(ds):
     ds["Q2"] = ds["dQ2"] + ds["pQ2"]
     return ds
