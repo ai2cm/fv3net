28388c9ad17325f2605de6e7d49a83e2a4aa1b41
Noah D. Brenowiz <nbren12@gmail.com>
2022-03-17 11:47:12 -0700

    resubmit with correct bucket name
    Name:                cycle-trained-limited-3e4503
    Namespace:           default
    ServiceAccount:      default
    Status:              Pending
    Created:             Thu Mar 17 11:46:43 -0700 (1 second ago)
    Parameters:
      flags:             --out_url gs://vcm-ml-experiments/microphysics-emulation/2022-03-17/cycle-trained-limited-3e4503 --train_url gs://vcm-ml-experiments/microphysics-emulation/2022-03-17/online-12hr-cycle-v3-online/artifacts/20160611.000000/netcdf_output --test_url gs://vcm-ml-experiments/microphysics-emulation/2022-03-17/online-12hr-cycle-v3-online/artifacts/20160611.000000/netcdf_output
      tag:               :b0cbb9b7fe01f7dd62ca90312970906fc38daed1
      wandb-run-group:
      gpu-train:         false
    Name:                cycle-trained-rnn-3e4503
    Namespace:           default
    ServiceAccount:      default
    Status:              Pending
    Created:             Thu Mar 17 11:46:44 -0700 (now)
    Parameters:
      flags:             --out_url gs://vcm-ml-experiments/microphysics-emulation/2022-03-17/cycle-trained-rnn-3e4503 --train_url gs://vcm-ml-experiments/microphysics-emulation/2022-03-17/online-12hr-cycle-v3-online/artifacts/20160611.000000/netcdf_output --test_url gs://vcm-ml-experiments/microphysics-emulation/2022-03-17/online-12hr-cycle-v3-online/artifacts/20160611.000000/netcdf_output
      tag:               :b0cbb9b7fe01f7dd62ca90312970906fc38daed1
      wandb-run-group:
      gpu-train:         false

--- projects/microphysics/train/run.sh
+++ projects/microphysics/train/run.sh
@@ -9,7 +9,7 @@ for config in limited rnn
 do
     model_name=cycle-trained-${config}
     config_file=${config}.yaml
-    out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}-${group}")
+    out_url=$(artifacts resolve-url vcm-ml-experiments microphysics-emulation "${model_name}-${group}")
     data_url=gs://vcm-ml-experiments/microphysics-emulation/2022-03-17/online-12hr-cycle-v3-online/artifacts/20160611.000000/netcdf_output
     argo submit argo.yaml \
         --name "${model_name}-${group}" \
