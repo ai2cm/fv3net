3b76b380178cd8b1de24c12970cae826c043e218
Oliver Watt-Meyer <oliverwatt@gmail.com>
2022-03-18 12:07:10 -0700

    Add flux-form data transforms

--- external/vcm/tests/test_data_transform.py
+++ external/vcm/tests/test_data_transform.py
@@ -1,10 +1,57 @@
+import numpy as np
 import xarray as xr
 import dacite
-from vcm.data_transform import DATA_TRANSFORM_REGISTRY
+from vcm.data_transform import (
+    DATA_TRANSFORM_REGISTRY,
+    DLW_SFC,
+    DSW_SFC,
+    DSW_TOA,
+    ULW_SFC,
+    ULW_TOA,
+    USW_SFC,
+    USW_TOA,
+    LHF,
+    SHF,
+    PRECIP,
+    COL_T_NUDGE,
+)
 import vcm
 import pytest
 
-ARRAY = xr.DataArray([0, 1, 2], dims=["x"])
+# to construct datasets that operate on all transforms, specify which
+# variables are 2D here. All others are assumed to be 3D.
+VARIABLES_2D = {
+    DLW_SFC,
+    DSW_SFC,
+    DSW_TOA,
+    ULW_SFC,
+    ULW_TOA,
+    USW_SFC,
+    USW_TOA,
+    LHF,
+    SHF,
+    PRECIP,
+    COL_T_NUDGE,
+    "implied_downward_radiative_flux_at_surface",
+    "implied_surface_precipitation_rate",
+}
+
+
+def get_3d_array():
+    return xr.DataArray(np.random.random_sample((6, 4, 4)), dims=["z", "y", "x"])
+
+
+def get_2d_array():
+    return xr.DataArray(np.random.random_sample((4, 4)), dims=["y", "x"])
+
+
+def get_dataset(names):
+    return xr.Dataset(
+        {
+            name: get_2d_array() if name in VARIABLES_2D else get_3d_array()
+            for name in names
+        }
+    )
 
 
 def test_all_registered_transforms_are_added_to_data_transform_name_type():
@@ -18,24 +65,23 @@ def test_all_registered_transforms_are_added_to_data_transform_name_type():
             ) from error
 
 
-def test_all_transform_functions():
-    for key in DATA_TRANSFORM_REGISTRY:
-        input_variables = DATA_TRANSFORM_REGISTRY[key].inputs
-        data = xr.Dataset({name: ARRAY for name in input_variables})
-        out = DATA_TRANSFORM_REGISTRY[key].func(data)
-        for output_name in DATA_TRANSFORM_REGISTRY[key].outputs:
-            assert output_name in out
+@pytest.mark.parametrize("key", list(DATA_TRANSFORM_REGISTRY))
+def test_transform_correctly_specify_inputs_and_outputs(key):
+    data = get_dataset(DATA_TRANSFORM_REGISTRY[key].inputs)
+    out = DATA_TRANSFORM_REGISTRY[key].func(data)
+    for output_name in DATA_TRANSFORM_REGISTRY[key].outputs:
+        assert output_name in out
 
 
 def test_data_transform():
-    data = xr.Dataset({"Q1": ARRAY, "Q2": ARRAY})
+    data = get_dataset(["Q1", "Q2"])
     transform = vcm.DataTransform(name="Qm_from_Q1_Q2")
     out = transform.apply(data)
     assert "Qm" in out
 
 
 def test_chained_data_transform():
-    data = xr.Dataset({"dQ1": ARRAY, "pQ1": ARRAY, "Q2": ARRAY})
+    data = get_dataset(["dQ1", "pQ1", "Q2"])
     data["dQ1"] = data.pQ1
     config_dict = {
         "transforms": [{"name": "Q1_from_dQ1_pQ1"}, {"name": "Qm_from_Q1_Q2"}]
@@ -74,3 +120,20 @@ def test_chained_transform_inputs_outputs(
     transform = dacite.from_dict(vcm.ChainedDataTransform, config_dict)
     assert transform.input_variables == expected_inputs
     assert transform.output_variables == expected_outputs
+
+
+@pytest.mark.parametrize(
+    "name, kwargs",
+    [
+        ("Qm", {"rectify_downward_radiative_flux": False}),
+        ("Q2", {"rectify_surface_precipitation_rate": False}),
+    ],
+)
+def test_flux_transform_round_trip(name, kwargs):
+    forward_name = f"{name}_flux_from_{name}_tendency"
+    backward_name = f"{name}_tendency_from_{name}_flux"
+    data = get_dataset(DATA_TRANSFORM_REGISTRY[forward_name].inputs)
+    data_with_flux = DATA_TRANSFORM_REGISTRY[forward_name].func(data, **kwargs)
+    data_with_flux = data_with_flux.drop_vars([name])
+    round_tripped = DATA_TRANSFORM_REGISTRY[backward_name].func(data_with_flux)
+    xr.testing.assert_allclose(data[name], round_tripped[name])
--- external/vcm/vcm/data_transform.py
+++ external/vcm/vcm/data_transform.py
@@ -21,6 +21,10 @@ TransformName = Literal[
     "Qm_from_Q1_Q2_temperature_dependent",
     "Q1_from_dQ1_pQ1",
     "Q2_from_dQ2_pQ2",
+    "Qm_flux_from_Qm_tendency",
+    "Q2_flux_from_Q2_tendency",
+    "Qm_tendency_from_Qm_flux",
+    "Q2_tendency_from_Q2_flux",
 ]
 
 
@@ -74,6 +78,127 @@ def Q1_from_dQ1_pQ1(ds):
 @register(["dQ2", "pQ2"], ["Q2"])
 def Q2_from_dQ2_pQ2(ds):
     ds["Q2"] = ds["dQ2"] + ds["pQ2"]
+    return ds
+
+
+DELP = "pressure_thickness_of_atmospheric_layer"
+DLW_SFC = "total_sky_downward_longwave_flux_at_surface"
+DSW_SFC = "total_sky_downward_shortwave_flux_at_surface"
+DSW_TOA = "total_sky_downward_shortwave_flux_at_top_of_atmosphere"
+ULW_SFC = "total_sky_upward_longwave_flux_at_surface"
+ULW_TOA = "total_sky_upward_longwave_flux_at_top_of_atmosphere"
+USW_SFC = "total_sky_upward_shortwave_flux_at_surface"
+USW_TOA = "total_sky_upward_shortwave_flux_at_top_of_atmosphere"
+COL_T_NUDGE = "storage_of_internal_energy_path_due_to_fine_res_temperature_nudging"
+LHF = "latent_heat_flux"
+SHF = "sensible_heat_flux"
+PRECIP = "surface_precipitation_rate"
+
+
+@register(
+    [
+        "Qm",
+        DELP,
+        DLW_SFC,
+        DSW_SFC,
+        DSW_TOA,
+        ULW_SFC,
+        ULW_TOA,
+        USW_SFC,
+        USW_TOA,
+        LHF,
+        SHF,
+        COL_T_NUDGE,
+    ],
+    ["Qm_flux", "implied_downward_radiative_flux_at_surface"],
+)
+def Qm_flux_from_Qm_tendency(
+    ds, rectify_downward_radiative_flux=True, include_temperature_nudging=True,
+):
+    Qm_flux = -vcm.mass_cumsum(ds["Qm"], ds[DELP], dim="z")
+    toa_rad_flux = ds[DSW_TOA] - ds[USW_TOA] - ds[ULW_TOA]
+    upward_surface_mse_flux = ds[LHF] + ds[SHF] + ds[USW_SFC] + ds[ULW_SFC]
+
+    # add TOA boundary condition
+    Qm_flux = Qm_flux.pad(z=(1, 0), constant_values=0.0)
+    Qm_flux += toa_rad_flux
+    if include_temperature_nudging:
+        Qm_flux += ds[COL_T_NUDGE]
+
+    # compute downward flux at surface
+    downward_sfc_Qm_flux = Qm_flux.isel(z=-1) + upward_surface_mse_flux
+    if rectify_downward_radiative_flux:
+        downward_sfc_Qm_flux = downward_sfc_Qm_flux.where(downward_sfc_Qm_flux >= 0, 0)
+
+    # remove bottom flux level from net fluxes
+    Qm_flux = Qm_flux.isel(z=slice(None, -1))
+
+    ds["Qm_flux"] = Qm_flux.chunk({"z": Qm_flux.sizes["z"]})
+    ds["implied_downward_radiative_flux_at_surface"] = downward_sfc_Qm_flux
+    return ds
+
+
+@register(
+    ["Q2", DELP, LHF, PRECIP], ["Q2_flux", "implied_surface_precipitation_rate"],
+)
+def Q2_flux_from_Q2_tendency(
+    ds, rectify_surface_precipitation_rate=True,
+):
+    Q2_flux = -vcm.mass_cumsum(ds["Q2"], ds[DELP], dim="z")
+    evaporation = vcm.latent_heat_flux_to_evaporation(ds[LHF])
+
+    # pad at top for TOA flux (boundary condition for Q2_flux=0)
+    Q2_flux = Q2_flux.pad(z=(1, 0), constant_values=0.0)
+
+    # compute downward flux at surface
+    downward_sfc_Q2_flux = Q2_flux.isel(z=-1) + evaporation
+    if rectify_surface_precipitation_rate:
+        downward_sfc_Q2_flux = downward_sfc_Q2_flux.where(downward_sfc_Q2_flux >= 0, 0)
+
+    # remove bottom flux level from net fluxes
+    Q2_flux = Q2_flux.isel(z=slice(None, -1))
+
+    ds["Q2_flux"] = Q2_flux.chunk({"z": Q2_flux.sizes["z"]})
+    ds["implied_surface_precipitation_rate"] = downward_sfc_Q2_flux
+    return ds
+
+
+@register(
+    [
+        "Qm_flux",
+        "implied_downward_radiative_flux_at_surface",
+        DELP,
+        ULW_SFC,
+        USW_SFC,
+        LHF,
+        SHF,
+    ],
+    ["Qm"],
+)
+def Qm_tendency_from_Qm_flux(ds):
+    upward_surface_mse_flux = ds[LHF] + ds[SHF] + ds[USW_SFC] + ds[ULW_SFC]
+    net_surface_mse_flux = (
+        ds["implied_downward_radiative_flux_at_surface"] - upward_surface_mse_flux
+    )
+    Qm_flux = xr.concat([ds["Qm_flux"], net_surface_mse_flux], dim="z")
+    Qm_flux = Qm_flux.chunk({"z": Qm_flux.sizes["z"]})
+    ds["Qm"] = -vcm.mass_divergence(
+        Qm_flux, ds[DELP], dim_center="z", dim_interface="z"
+    )
+    return ds
+
+
+@register(
+    ["Q2_flux", "implied_surface_precipitation_rate", DELP, LHF], ["Q2"],
+)
+def Q2_tendency_from_Q2_flux(ds):
+    evaporation = vcm.latent_heat_flux_to_evaporation(ds[LHF])
+    net_surface_q2_flux = ds["implied_surface_precipitation_rate"] - evaporation
+    Q2_flux = xr.concat([ds["Q2_flux"], net_surface_q2_flux], dim="z")
+    Q2_flux = Q2_flux.chunk({"z": Q2_flux.sizes["z"]})
+    ds["Q2"] = -vcm.mass_divergence(
+        Q2_flux, ds[DELP], dim_center="z", dim_interface="z"
+    )
     return ds
 
 
