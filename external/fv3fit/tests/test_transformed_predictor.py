3a933d2cb7fffd812bb3ef1be39647cec388a932
Oliver Watt-Meyer <oliverwatt@gmail.com>
2022-03-19 13:02:22 -0700

    Dont output extra vars in TransformedPredictor.predict

--- external/fv3fit/tests/test_transformed_predictor.py
+++ external/fv3fit/tests/test_transformed_predictor.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import numpy as np
 import fv3fit
 import vcm
 
@@ -30,6 +31,28 @@ def test_transformed_prediction_additional_required_input():
     )
     output = transformed_model.predict(input_dataset)
     assert "Qm" in output
+    assert "Q2" not in output
+
+
+def test_transformed_prediction_when_inputs_contain_an_output():
+    # this case can arise in offline diags
+    base = fv3fit.testing.ConstantOutputPredictor(["input"], ["Q1"])
+    base.set_outputs(Q1=np.array([5, 6, 7]))
+    transformed_model = fv3fit.TransformedPredictor(base, transforms)
+    input_dataset = xr.Dataset(
+        {
+            "input": xr.DataArray([0, 1, 2]),
+            "Q2": xr.DataArray([0, 1, 2], dims=["z"]),
+            "Qm": xr.DataArray([3, 4, 5], dims=["z"]),
+        }
+    )
+    base_output = base.predict(input_dataset)
+    merged_output = {"Q1": base_output["Q1"], "Q2": input_dataset["Q2"]}
+    expected_Qm = transforms[0].apply(merged_output)["Qm"]
+    transformed_model_output = transformed_model.predict(input_dataset)
+    assert "Qm" in transformed_model_output
+    assert "Q2" not in transformed_model_output
+    xr.testing.assert_allclose(expected_Qm, transformed_model_output["Qm"])
 
 
 def test_transformed_predictor_inputs_outputs():
