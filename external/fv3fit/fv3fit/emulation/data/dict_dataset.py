0fb639a821bc925ded418a83ba105e988578eb9e
Andre Perkins <frodre@gmail.com>
2022-03-22 22:02:28 +0000

    Use cached netcdf opening routine

--- external/fv3fit/fv3fit/emulation/data/dict_dataset.py
+++ external/fv3fit/fv3fit/emulation/data/dict_dataset.py
@@ -1,25 +1,29 @@
+import logging
 from typing import Sequence
 import tensorflow as tf
+
 import vcm
 from fv3fit.emulation.data.io import get_nc_files
+from .transforms import open_netcdf_dataset
 
 __all__ = ["netcdf_url_to_dataset"]
+logger = logging.getLogger(__name__)
 
 
-def read_variables_as_dict(fs, url, variables):
+def read_variables_as_dict(url, variables):
     sig = (tf.float32,) * len(variables)
     # tf.py_function can only wrap functions which output tuples of tensors, not
     # dicts
     outputs = tf.py_function(
-        lambda url: read_variables_greedily_as_tuple(fs, url, variables), [url], sig
+        lambda url: read_variables_greedily_as_tuple(url, variables), [url], sig
     )
     return dict(zip(variables, outputs))
 
 
-def read_variables_greedily_as_tuple(fs, url, variables):
+def read_variables_greedily_as_tuple(url, variables):
     url = url.numpy().decode()
-    print(f"opening {url}")
-    ds = vcm.DerivedMapping(vcm.open_remote_nc(fs, url))
+    logger.debug(f"opening {url}")
+    ds = open_netcdf_dataset(url)
     return tuple([tf.convert_to_tensor(ds[v], dtype=tf.float32) for v in variables])
 
 
@@ -43,7 +47,7 @@ def netcdf_url_to_dataset(
     d = tf.data.Dataset.from_tensor_slices(sorted(files))
     if shuffle:
         d = d.shuffle(100_000)
-    return d.map(lambda url: read_variables_as_dict(fs, url, variables))
+    return d.map(lambda url: read_variables_as_dict(url, variables))
 
 
 def load_samples(train_dataset, n_train):
