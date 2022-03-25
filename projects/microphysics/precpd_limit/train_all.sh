585643e661d02a4504261b2ad713bbf76c274bef
Andre Perkins <frodre@gmail.com>
2022-03-25 23:55:41 +0000

    Initial experiments

---
+++ projects/microphysics/precpd_limit/train_all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [[ "$1" == "--test" ]]; then
+    extra_flags="--nfiles 10 --nfiles_valid 10 --epochs 2"
+    bucket="vcm-ml-scratch"
+else
+    bucket="vcm-ml-experiments"
+fi
+
+group="$(openssl rand -hex 3)"
+
+
+for config in rnn rnn-limited-qc
+do
+    config_file="${config}.yaml"
+    model_name="dqc-precpd-limiter-${config}-v1-${group}"
+    out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}")
+    flags="--out_url ${out_url} ${extra_flags}"
+
+    argo submit ../train/argo.yaml \
+        --name "${model_name}" \
+        -p training-config="$(base64 --wrap 0 $config_file)" \
+        -p flags="$flags" \
+        -p wandb-run-group="qc-precpd-limiter-mar2022"
+done
