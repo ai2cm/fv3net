31577083d05b8e1e2c5609d2517b3c8dc8e25e9b
Andre Perkins <frodre@gmail.com>
2022-03-29 21:05:50 +0000

    New training experiment

--- projects/microphysics/precpd_limit/train_all.sh
+++ projects/microphysics/precpd_limit/train_all.sh
@@ -14,18 +14,18 @@ group="$(openssl rand -hex 3)"
 
 
 # for config in rnn-control
-for config in rnn-limited-qc
+for config in rnn-control rnn-limited-qc rnn
 do
     config_file="${config}.yaml"
-    # model_name="dqc-precpd-limiter-${config}-v2-adjust-lr-lossvar-${group}"
-    # out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}")
-    model_name=dqc-precpd-limiter-rnn-limited-qc-v1-f0c7fa
-    out_url=gs://vcm-ml-experiments/microphysics-emulation/2022-03-25/dqc-precpd-limiter-rnn-limited-qc-v1-f0c7fa
+    model_name="dqc-precpd-limiter-${config}-${group}"
+    out_url=$(artifacts resolve-url "$bucket" microphysics-emulation "${model_name}")
+    # model_name=dqc-precpd-limiter-rnn-limited-qc-v1-f0c7fa
+    # out_url=gs://vcm-ml-experiments/microphysics-emulation/2022-03-25/dqc-precpd-limiter-rnn-limited-qc-v1-f0c7fa
     flags="--out_url ${out_url} ${extra_flags}"
 
     argo submit ../train/argo.yaml \
         --name "${model_name}" \
         -p training-config="$(base64 --wrap 0 $config_file)" \
         -p flags="$flags" \
-        -p wandb-run-group="qc-precpd-limiter-mar2022"
+        -p wandb-run-group="qc-precpd-limiter-v2-mar2022"
 done
