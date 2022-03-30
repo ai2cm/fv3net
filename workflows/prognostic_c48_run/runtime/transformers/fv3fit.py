f434fa64eb271aa4e263d0a38130e5447db4800f
Oliver Watt-Meyer <oliverwatt@gmail.com>
2022-03-30 14:18:14 -0700

    Dont maintain backwards compatibility

--- workflows/prognostic_c48_run/runtime/transformers/fv3fit.py
+++ workflows/prognostic_c48_run/runtime/transformers/fv3fit.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Mapping, Iterable, Hashable, Sequence, Union
+from typing import Mapping, Iterable, Hashable, Sequence
 
 import xarray as xr
 import fv3fit
@@ -14,7 +14,7 @@ __all__ = ["Config", "Adapter"]
 class Config:
     """
     Attributes:
-        url: Path to a model to-be-loaded, or sequence of paths.
+        url: Sequence of paths to models that can be loaded with fv3fit.load.
         variables: Mapping from state names to name of corresponding tendency predicted
             by model. For example: {"air_temperature": "dQ1"}.
         limit_negative_humidity: if True, rescale tendencies to not allow specific
@@ -22,7 +22,7 @@ class Config:
         online: if True, the ML predictions will be applied to model state.
     """
 
-    url: Union[str, Sequence[str]]
+    url: Sequence[str]
     variables: Mapping[str, str]
     limit_negative_humidity: bool = True
     online: bool = True
@@ -34,11 +34,8 @@ class Adapter:
     timestep: float
 
     def __post_init__(self: "Adapter"):
-        if isinstance(self.config.url, str):
-            self.model = fv3fit.load(self.config.url)
-        else:
-            models = [fv3fit.load(url) for url in self.config.url]
-            self.model = MultiModelAdapter(models)
+        models = [fv3fit.load(url) for url in self.config.url]
+        self.model = MultiModelAdapter(models)  # type: ignore
 
     def predict(self, inputs: State) -> State:
         tendencies = self.model.predict(xr.Dataset(inputs))
