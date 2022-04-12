e39b32092ee77d21b32a46ba72602bcfae3b3db3
Noah D. Brenowiz <nbren12@gmail.com>
2022-04-11 21:59:03 -0700

    submit gscond only experiment

--- projects/microphysics/experiments/prog-run.py
+++ projects/microphysics/experiments/prog-run.py
@@ -6,8 +6,14 @@ from end_to_end import PrognosticJob, load_yaml, submit_jobs  # noqa: E402
 
 MODEL = "gs://vcm-ml-experiments/microphysics-emulation/2022-03-02/limit-tests-all-loss-rnn-7ef273/model.tf"  # noqa: E501
 config = load_yaml("../configs/default.yaml")
-config["zhao_carr_emulation"] = {"model": {"path": MODEL}}
-job = PrognosticJob(name=f"squash-control", image_tag="latest", config=config)
+
+# set gscond only model
+config["zhao_carr_emulation"] = {"gscond": {"path": MODEL}}
+config["namelist"]["gfs_physics_nml"]["emulate_gscond_only"] = True
+
+job = PrognosticJob(
+    name=f"squash-control-gscond-only", image_tag="latest", config=config
+)
 
 jobs = [job]
 submit_jobs(jobs, f"squash")
