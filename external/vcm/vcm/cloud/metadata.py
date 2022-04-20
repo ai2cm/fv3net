8d5ac0d11434c7d383a6f77144739c9f85f36aef
Anna Kwa <annak@allenai.org>
2022-04-20 22:53:43 +0000

    add step metadata dataclass

---
+++ external/vcm/vcm/cloud/metadata.py
@@ -0,0 +1,15 @@
+import dataclasses
+import json
+from typing import Mapping, List
+
+
+@dataclasses.dataclass
+class StepMetadata:
+    job_type: str
+    commit: str
+    url: str
+    dependencies: Mapping[str, str] = dataclasses.field(default_factory=dict)
+    args: List[str] = dataclasses.field(default_factory=list)
+
+    def print(self):
+        print(json.dumps(dataclasses.asdict(self)))
