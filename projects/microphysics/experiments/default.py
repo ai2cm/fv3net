cb5e9ac2696e3c46b88a01743a984dd079957c20
Noah D. Brenowiz <nbren12@gmail.com>
2022-04-19 17:10:01 -0700

    Default launches every training config

--- projects/microphysics/experiments/default.py
+++ projects/microphysics/experiments/default.py
@@ -1,21 +1,26 @@
 import sys
+import glob
+import os
 
 sys.path.insert(0, "../argo")
 
 from end_to_end import EndToEndJob, load_yaml, submit_jobs  # noqa: E402
 
 
-def _get_job(config_name: str, revision):
-    config = load_yaml(f"../train/{config_name}.yaml")
+def _get_job(config: str, revision):
+    config_name = os.path.splitext(os.path.basename(config))[0]
+    config = load_yaml(config)
     return EndToEndJob(
         name=f"{config_name}-{revision[:6]}-v1",
         fv3fit_image_tag=revision,
         image_tag=revision,
         ml_config=config,
-        prog_config=load_yaml("../configs/default_short.yaml"),
+        prog_config=load_yaml("../configs/default.yaml"),
     )
 
 
+configs = glob.glob("../train/*.yaml")
+
 revision = sys.argv[1] if len(sys.argv) > 1 else "latest"
-jobs = [_get_job(config, revision) for config in ["limited", "rnn"]]
+jobs = [_get_job(config, revision) for config in configs]
 submit_jobs(jobs, f"{revision}-end-to-end")
