6cfd23c0dbcc31ac685ff0f1d0fbce10c2526627
Anna Kwa <annak@allenai.org>
2022-04-21 21:13:05 +0000

    add step metadata to prog run diags

--- workflows/diagnostics/fv3net/diagnostics/prognostic_run/compute.py
+++ workflows/diagnostics/fv3net/diagnostics/prognostic_run/compute.py
@@ -18,6 +18,7 @@ import sys
 import datetime
 import intake
 import numpy as np
+import os
 import xarray as xr
 from dask.diagnostics import ProgressBar
 import fsspec
@@ -47,6 +48,8 @@ from .constants import (
 )
 from fv3net.diagnostics._shared.registry import Registry
 from fv3net.diagnostics._shared import transform
+from fv3net.artifacts.metadata import StepMetadata
+
 
 import logging
 
@@ -525,6 +528,17 @@ def get_verification(args, catalog, join_2d="outer"):
         return load_diags.CatalogSimulation(args.verification, catalog, join_2d=join_2d)
 
 
+def _print_step_metadata_json(args):
+    info = StepMetadata(
+        job_type="prognostic_run_diags",
+        commit=os.environ.get("COMMIT_SHA"),
+        url=args.output,
+        dependencies={"prognostic_run": args.url},
+        args=sys.argv[1:],
+    )
+    info.print_json()
+
+
 def main(args):
 
     logging.basicConfig(level=logging.INFO)
@@ -557,3 +571,5 @@ def main(args):
     logger.info(f"Saving data to {args.output}")
     with fsspec.open(args.output, "wb") as f:
         vcm.dump_nc(diags, f)
+
+    _print_step_metadata_json(args)
