e70c4a76249803148953f0b0b27de27f463cf884
W. Andre Perkins <frodre@gmail.com>
2022-05-10 15:39:02 -0700

    Fix the e2e emulation run (#1819)
    This PR fixes a few minor things with the E2E emulation workflow
    
    reverts changes to the prognostic config introduced in Generate emulation training data v4 #1788
    Address requester pays issue with prognostic run step
    Ensure the prognostic run, piggy-back diags, and prognostic eval appear in in the same wandb run group
    Adds a test mode for faster checks on the E2E job in default.py
    
    * Short test for cron job
    
    * Fix emulation prognostic config changes
    
    * Add test optional argument for shorter training
    
    * Add requester pays envvar to run_model step
    
    * Make default experiment group easier to search
    
    * Switch prognostic evaluation run group for easy wandb grouping
    
    * Fix tag for length limit

--- projects/microphysics/experiments/default.py
+++ projects/microphysics/experiments/default.py
@@ -8,15 +8,24 @@ sys.path.insert(0, "../argo")
 from end_to_end import EndToEndJob, load_yaml, submit_jobs  # noqa: E402
 
 
-def _get_job(config: str, revision, suffix):
+def _get_job(config: str, revision: str, suffix: str, test: bool):
     config_name = os.path.splitext(os.path.basename(config))[0]
     config = load_yaml(config)
+
+    if test:
+        config["nfiles"] = 50
+        config["epochs"] = 3
+        config["nfiles_valid"] = 10
+        run_config = "default_short"
+    else:
+        run_config = "default"
+
     return EndToEndJob(
         name=f"{config_name}-{revision[:6]}-{suffix}",
         fv3fit_image_tag=revision,
         image_tag=revision,
         ml_config=config,
-        prog_config=load_yaml("../configs/default.yaml"),
+        prog_config=load_yaml(f"../configs/{run_config}.yaml"),
     )
 
 
@@ -24,7 +33,9 @@ configs = glob.glob("../train/*.yaml")
 parser = argparse.ArgumentParser()
 parser.add_argument("--revision", default="latest")
 parser.add_argument("--suffix", default="v1")
+parser.add_argument("--test", action="store_true")
 
 args = parser.parse_args()
-jobs = [_get_job(config, args.revision, args.suffix) for config in configs]
-submit_jobs(jobs, f"{args.revision}-end-to-end")
+jobs = [_get_job(config, args.revision, args.suffix, args.test) for config in configs]
+test_str = "test-" if args.test else ""
+submit_jobs(jobs, f"emu-end-to-end-{test_str}{args.revision[:6]}")
