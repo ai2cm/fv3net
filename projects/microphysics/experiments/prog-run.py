a309eb976b78f96b69008d2f8fdba2a584394de6
Andre Perkins <frodre@gmail.com>
2022-05-13 23:01:34 +0000

    Set up prog run with upper 5 level masked

--- projects/microphysics/experiments/prog-run.py
+++ projects/microphysics/experiments/prog-run.py
@@ -12,11 +12,16 @@ from end_to_end import (
 MODEL = "gs://vcm-ml-experiments/microphysics-emulation/2022-05-12/gscond-only-tscale-dense-local-41b1c1-v1/model.tf"  # noqa: E501
 config = load_yaml("../configs/default.yaml")
 
+# Stop emulator application on top 5 model levels
+upper_5_levels = dict(start=74, stop=None)
+mask_levels = {
+    "specific_humdity_after_gscond": upper_5_levels,
+    "air_temperature_after_gscond": upper_5_levels,
+}
+
+model_kwargs = dict(gscond_cloud_conservative=True, mask_emulator_levels=mask_levels,)
 config = set_prognostic_emulation_model(
-    config,
-    MODEL,
-    gscond_only=True,
-    emu_model_kwargs=dict(gscond_cloud_conservative=True),
+    config, MODEL, gscond_only=True, emu_model_kwargs=model_kwargs,
 )
 
 job = PrognosticJob(
