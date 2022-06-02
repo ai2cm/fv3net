8cc97c55f52885549da7116f475ca56ae788823d
Andre Perkins <frodre@gmail.com>
2022-06-02 22:36:18 +0000

    Add test for LevelMask

--- external/emulation/tests/test_mask.py
+++ external/emulation/tests/test_mask.py
@@ -1,4 +1,7 @@
-from emulation.masks import RangeMask, compose_masks
+import pytest
+import numpy as np
+
+from emulation.masks import RangeMask, LevelMask, compose_masks
 
 
 def test_RangeMask():
@@ -12,3 +15,16 @@ def test_compose_masks_no_action():
     mask = compose_masks([])
     out = {"a": 1}
     assert mask({}, out) == out
+
+
+@pytest.mark.parametrize("start, stop", [(2, 3), (2, 5), (None, 2), (None, None)])
+def test_LevelMask(start, stop):
+    mask = LevelMask("foo", start, stop)
+    ones = np.ones((2, 4))
+    zeros = ones * 0
+    result = mask(state={"foo": zeros}, emulator={"foo": ones})
+
+    sl_ = slice(start, stop)
+    num_fortran_elements = zeros[:, sl_].size
+    np.testing.assert_array_equal(result["foo"][:, sl_], zeros[:, sl_])
+    assert np.sum(result["foo"]) == result["foo"].size - num_fortran_elements
