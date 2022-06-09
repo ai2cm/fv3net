37f8801935c9b51faf1045e010c56cc971cb7234
Jeremy McGibbon <mcgibbon@uw.edu>
2022-06-09 00:19:21 +0000

    fix the report label

--- .circleci/update_coverage_description.sh
+++ .circleci/update_coverage_description.sh
@@ -2,6 +2,8 @@
 
 set -e -x
 
+report_type=$1
+
 if [[ -f htmlcov/index.html ]]; then
     workflow=$CIRCLE_WORKFLOW_ID
     pr_number=${CIRCLE_PULL_REQUEST##*/}
@@ -9,9 +11,9 @@ if [[ -f htmlcov/index.html ]]; then
     coverage_label=$(grep -oP '(?<=<span class="pc_cov">).+(?=<\/span>)' htmlcov/index.html)
 
     if [[ "$body" == *"${workflow}:"* ]]; then
-        body=$(echo "$body" | sed -e "s/${workflow}: [.*](https:\/\/.*\/index.html)/${workflow}: [${coverage_label}](https:\/\/output.circle-artifacts.com\/output\/job\/${CIRCLE_WORKFLOW_JOB_ID}\/artifacts\/0\/tmp\/coverage\/htmlcov-$CIRCLE_WORKFLOW_ID\/index.html)/g")
+        body=$(echo "$body" | sed -e "s/${report_type}: [.*](https:\/\/.*\/index.html)/${workflow}: [${coverage_label}](https:\/\/output.circle-artifacts.com\/output\/job\/${CIRCLE_WORKFLOW_JOB_ID}\/artifacts\/0\/tmp\/coverage\/htmlcov-${report_type}\/index.html)/g")
     else
-        body=$(echo "$body" | sed -e "s/Coverage reports (updated automatically):/Coverage reports (updated automatically):\n- ${workflow}: [${coverage_label}](https:\/\/output.circle-artifacts.com\/output\/job\/${CIRCLE_WORKFLOW_JOB_ID}\/artifacts\/0\/tmp\/coverage\/htmlcov-$CIRCLE_WORKFLOW_ID\/index.html)/g")
+        body=$(echo "$body" | sed -e "s/Coverage reports (updated automatically):/Coverage reports (updated automatically):\n- ${workflow}: [${coverage_label}](https:\/\/output.circle-artifacts.com\/output\/job\/${CIRCLE_WORKFLOW_JOB_ID}\/artifacts\/0\/tmp\/coverage\/htmlcov-${report_type}\/index.html)/g")
     fi
 
     gh pr edit --body "$body"
