4a5fd309a4dba9946ac1731eadbb2f5b11300a3c
Noah D. Brenowiz <nbren12@gmail.com>
2022-06-09 15:54:49 -0700

    TrainingJob(name='gscond-classifier-v1', fv3fit_image_tag='1a2610a55a889855f5c1f93028f83cf08c1d2bf7', bucket='vcm-ml-experiments', project='microphysics-emulation') to monitor experiments: argo list -lexperiment=classifier

---
+++ projects/microphysics/experiments/classifer.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.path.insert(0, "../argo")
+
+from end_to_end import TrainingJob, load_yaml, submit_jobs  # noqa: E402
+
+
+def _get_job():
+    config = load_yaml(f"../train/gscond-classifier.yaml")
+    # Training loss problems are visible after 5 epochs
+    config["epochs"] = 50
+    return TrainingJob(
+        name=f"gscond-classifier-v1", fv3fit_image_tag=revision, config=config,
+    )
+
+
+revision = "1a2610a55a889855f5c1f93028f83cf08c1d2bf7"
+submit_jobs([_get_job()], f"classifier")
