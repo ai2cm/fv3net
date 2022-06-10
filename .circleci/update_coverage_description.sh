59b57bceffc23c267d2fa29d2ef32674bee38adf
Jeremy McGibbon <mcgibbon@uw.edu>
2022-06-10 16:31:17 +0000

    use hopefully-more-stable command to get PR body

--- .circleci/update_coverage_description.sh
+++ .circleci/update_coverage_description.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
-set -e -x
+set -e
 
 report_type=$1
 
 if [[ -f htmlcov/index.html ]]; then
     pr_number=${CIRCLE_PULL_REQUEST##*/}
-    body=$(curl https://api.github.com/repos/ai2cm/fv3net/pulls/${pr_number} | jq '.body' | tail -c +2 | head -c -2 | sed -e 's/\\r\\n/\r\n/g' | sed -e 's/\\r/\r/g')
-    x=`tail -n 1 <(echo "$body")`
-    if ! [ "$x" == "" ]; then
-        # no newline at end of body, probably removed by curl, so add one
-        body="${body}\n"
-    fi
+    body=$(gh pr view | sed -e '1,13d')
     coverage_label=$(grep -oP '(?<=<span class="pc_cov">).+(?=<\/span>)' htmlcov/index.html)
 
     if [[ "$body" == *"${report_type}:"* ]]; then
@@ -19,7 +14,7 @@ if [[ -f htmlcov/index.html ]]; then
     else
         body=$(echo "$body" | sed -e "s/Coverage reports (updated automatically):/Coverage reports (updated automatically):\r\n- ${report_type}: [${coverage_label}](https:\/\/output.circle-artifacts.com\/output\/job\/${CIRCLE_WORKFLOW_JOB_ID}\/artifacts\/0\/tmp\/coverage\/htmlcov-${report_type}\/index.html)/g")
     fi
-
+    echo "$body"
     gh pr edit --body "$body"
 else
     echo "no coverage report found"
