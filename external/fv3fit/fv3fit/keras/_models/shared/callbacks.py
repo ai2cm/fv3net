ec9bb74c305a46f8c2dbdd02c95a69f0afaed0d7
Anna Kwa <annak@allenai.org>
2022-06-28 21:26:05 +0000

    add subclass for saving at intervals

--- external/fv3fit/fv3fit/keras/_models/shared/callbacks.py
+++ external/fv3fit/fv3fit/keras/_models/shared/callbacks.py
@@ -24,3 +24,38 @@ class CallbackConfig:
                     f"callback {self.name} is not in the Keras library nor the list "
                     f"of usable third party callbacks {list(THIRD_PARTY_CALLBACKS)}"
                 )
+
+
+class EpochModelCheckpoint(tf.keras.callbacks.ModelCheckpoint):
+    def __init__(
+        self,
+        filepath,
+        interval=1,
+        monitor="val_loss",
+        verbose=0,
+        save_best_only=False,
+        save_weights_only=False,
+        mode="auto",
+        options=None,
+        **kwargs,
+    ):
+        super(EpochModelCheckpoint, self).__init__(
+            filepath,
+            monitor,
+            verbose,
+            save_best_only,
+            save_weights_only,
+            mode,
+            "epoch",
+            options,
+        )
+        self.epochs_since_last_save = 0
+        self.interval = interval
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.epochs_since_last_save += 1
+        if self.epochs_since_last_save % self.interval == 0:
+            self._save_model(epoch=epoch, batch=None, logs=logs)
+
+    def on_train_batch_end(self, batch, logs=None):
+        pass
