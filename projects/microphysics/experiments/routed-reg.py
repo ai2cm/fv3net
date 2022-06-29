87233eec086fe39602e62b8872443613e8c56a51
Noah D. Brenowiz <nbren12@gmail.com>
2022-06-28 18:57:47 -0700

    Submit gscond-routed-reg-v3 training
    TrainingJob(name='gscond-routed-reg-v3', fv3fit_image_tag='8cdac54391290cf256af089be941917165e4705f', bucket='vcm-ml-experiments', project='microphysics-emulation')
    to monitor experiments: argo list -lexperiment=routed-reg

--- projects/microphysics/experiments/routed-reg.py
+++ projects/microphysics/experiments/routed-reg.py
@@ -8,10 +8,9 @@ from end_to_end import TrainingJob, load_yaml, submit_jobs  # noqa: E402
 def _get_job():
     config = load_yaml(f"../train/gscond-only-routed.yaml")
     config["epochs"] = 25
-    return TrainingJob(
-        name=f"gscond-routed-reg-v2", fv3fit_image_tag=revision, config=config,
-    )
+    return TrainingJob(name=name, fv3fit_image_tag=revision, config=config,)
 
 
-revision = "d51594567be2297b141d89458dded5942cf53440"
+name = "gscond-routed-reg-v3"
+revision = "8cdac54391290cf256af089be941917165e4705f"
 submit_jobs([_get_job()], f"routed-reg")
