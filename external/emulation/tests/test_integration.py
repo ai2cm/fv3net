1617128e77ba679272f1520f2063024381ab0622
Noah D. Brenowiz <nbren12@gmail.com>
2022-07-01 11:43:39 -0700

    Fix input signature error
    There are two differences of concrete functions compared to tf models
    1. they cannot be passed dict with keys not specified in the saved signature
    1. cannot cast dtype appropriately
    
    Morever, 1D inputs should be increased to 2D
    2. All scalar inputs should be up-cast (potentially just an issue with the test)

--- external/emulation/tests/test_integration.py
+++ external/emulation/tests/test_integration.py
@@ -3,6 +3,9 @@ CI.
 """
 import pytest
 import emulation.config
+from fv3fit.emulation.data.io import download_cached
+import xarray
+import numpy as np
 
 
 @pytest.mark.xfail
@@ -11,4 +14,9 @@ def test_combined_classifier():
         path="gs://vcm-ml-experiments/microphysics-emulation/2022-06-28/gscond-routed-reg-v3/model.tf",  # noqa
         classifier_path="gs://vcm-ml-experiments/microphysics-emulation/2022-06-09/gscond-classifier-v1/model.tf",  # noqa
     )
-    assert config.build()
+    nc_data = "gs://vcm-ml-experiments/microphysics-emulation/2022-04-18/microphysics-training-data-v4/test/state_20160203.014500_0.nc"  # noqa
+    path = download_cached(nc_data)
+    ds = xarray.open_dataset(path)
+    state = {key: np.asarray(v).astype(np.float64).T for key, v in ds.items()}
+    model = config.build()
+    model.microphysics(state)
