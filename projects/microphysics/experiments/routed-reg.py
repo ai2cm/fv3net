191a44585263536fb50d770501ae800b3c69250f
Noah D. Brenowiz <nbren12@gmail.com>
2022-07-02 15:54:52 -0700

    Submit gscond-routed-reg-v4

--- projects/microphysics/experiments/routed-reg.py
+++ projects/microphysics/experiments/routed-reg.py
@@ -11,6 +11,6 @@ def _get_job():
     return TrainingJob(name=name, fv3fit_image_tag=revision, config=config,)
 
 
-name = "gscond-routed-reg-v3"
-revision = "8cdac54391290cf256af089be941917165e4705f"
+name = "gscond-routed-reg-v4"
+revision = "5003e52966b17f64be2da40f0dde1d70c95088a7"
 submit_jobs([_get_job()], f"routed-reg")
