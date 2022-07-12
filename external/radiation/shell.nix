54003a77b55b11475b36565af734a004457b0db7
Noah D. Brenowiz <nbren12@gmail.com>
2022-07-12 15:34:37 -0700

    Add nix-shell and update README
    nix makes environment management very easy

---
+++ external/radiation/shell.nix
@@ -0,0 +1,25 @@
+# shell.nix
+let
+  pkgs = import (builtins.fetchGit {
+    # Descriptive name to make the store path easier to identify
+    name = "ai2cm-packages";
+    url = "git@github.com:VulcanClimateModeling/packages.git";
+    ref = "master";
+    # SHA of the commit of pkgs to use. This effectively pins all packages to
+    # the versions specified in that commit.
+    # rev = "b3194fd82e98c288d04ca6fdd1091b484371b26c";
+    rev = "7f6bd8ccb8217fc6012aa287d8e61474ee4d46e3";
+  }) { };
+in pkgs.mkShell {
+  buildInputs = [
+    pkgs.python38Packages.gt4py-dev
+    pkgs.python38Packages.xarray
+    pkgs.python38Packages.numpy
+    pkgs.python38Packages.netcdf4
+    pkgs.python38Packages.ipython
+
+  ];
+  shellHook = ''
+    export PYTHONPATH=${pkgs.serialbox}/python:$PYTHONPATH
+  '';
+}
