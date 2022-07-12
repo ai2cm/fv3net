3a9527045219b5776deca523b98228bc2a400493
Clayton <clayton.h.sanford@gmail.com>
2022-07-12 14:28:09 -0700

    lint

--- external/fv3fit/tests/test_out_of_sample.py
+++ external/fv3fit/tests/test_out_of_sample.py
@@ -8,16 +8,20 @@ import xarray as xr
 from fv3fit.tfdataset import tfdataset_from_batches
 from fv3fit.sklearn._random_forest import train_random_forest
 
-from tests.training.test_train import get_dataset, get_uniform_sample_func, regression_model_type, train_identity_model, unstack_test_dataset
-
-@pytest.mark.parametrize(
-    "base_value, novelty_cutoff, output",
-    [(1, -1, 0), (1, 1, 1)]
+from tests.training.test_train import (
+    get_dataset,
+    get_uniform_sample_func,
+    unstack_test_dataset,
 )
+
+
+@pytest.mark.parametrize("base_value, novelty_cutoff, output", [(1, -1, 0), (1, 1, 1)])
 def test_out_of_sample_model(base_value, novelty_cutoff, output):
     input_variables = ["input"]
     output_variables = ["output"]
-    base_model = fv3fit.testing.ConstantOutputPredictor(input_variables, output_variables)
+    base_model = fv3fit.testing.ConstantOutputPredictor(
+        input_variables, output_variables
+    )
     base_model.set_outputs(output=base_value)
     novelty_detector = fv3fit.testing.ConstantOutputNoveltyDetector(input_variables)
     oosModel = OutOfSampleModel(base_model, novelty_detector, novelty_cutoff)
@@ -31,24 +35,25 @@ def test_out_of_sample_model(base_value, novelty_cutoff, output):
     np.testing.assert_almost_equal(ds_out["output"].values, output)
 
 
-@pytest.mark.parametrize(
-    "base_value, novelty_cutoff, output",
-    [(1, -1, 0), (1, 1, 1)]
-)
+@pytest.mark.parametrize("base_value, novelty_cutoff, output", [(1, -1, 0), (1, 1, 1)])
 def test_out_of_sample_model_different_inputs(base_value, novelty_cutoff, output):
     base_input_variables = ["shared_input", "base_input"]
     novelty_input_variables = ["shared_input", "novelty_input"]
     output_variables = ["output"]
-    base_model = fv3fit.testing.ConstantOutputPredictor(base_input_variables, output_variables)
+    base_model = fv3fit.testing.ConstantOutputPredictor(
+        base_input_variables, output_variables
+    )
     base_model.set_outputs(output=base_value)
-    novelty_detector = fv3fit.testing.ConstantOutputNoveltyDetector(novelty_input_variables)
+    novelty_detector = fv3fit.testing.ConstantOutputNoveltyDetector(
+        novelty_input_variables
+    )
     oosModel = OutOfSampleModel(base_model, novelty_detector, novelty_cutoff)
 
     ds_in = xr.Dataset(
-        data_vars= {
+        data_vars={
             "shared_input": xr.DataArray(np.zeros([3, 3, 5]), dims=["x", "y", "z"],),
             "base_input": xr.DataArray(np.ones([3, 3]), dims=["x", "y"],),
-            "novelty_input": xr.DataArray(np.ones([3, 3, 5]), dims=["x", "y", "z"],)
+            "novelty_input": xr.DataArray(np.ones([3, 3, 5]), dims=["x", "y", "z"],),
         }
     )
     ds_out = oosModel.predict(ds_in)
@@ -72,7 +77,7 @@ def test_out_of_sample_identity_same_output_when_in_sample():
     train_tfdataset = tfdataset_from_batches([train_dataset for _ in range(10)])
     val_tfdataset = tfdataset_from_batches([test_dataset])
     test_dataset = unstack_test_dataset(test_dataset)
-    
+
     base_model = train_random_forest(hyperparameters, train_tfdataset, val_tfdataset)
 
     novelty_detector = fv3fit.testing.ConstantOutputNoveltyDetector(input_variables)
@@ -96,13 +101,12 @@ def test_out_of_sample_dump_and_load_default_maintains_prediction():
     input_variables, output_variables, train_dataset = get_dataset(
         regression_model_type, sample_func
     )
-    _, _, test_dataset = get_dataset(regression_model_type, sample_func
-    )
+    _, _, test_dataset = get_dataset(regression_model_type, sample_func)
     hyperparameters = RandomForestHyperparameters(input_variables, output_variables)
     train_tfdataset = tfdataset_from_batches([train_dataset for _ in range(10)])
     val_tfdataset = tfdataset_from_batches([test_dataset])
     test_dataset = unstack_test_dataset(test_dataset)
-    
+
     base_model = train_random_forest(hyperparameters, train_tfdataset, val_tfdataset)
     novelty_detector = fv3fit.testing.ConstantOutputNoveltyDetector(input_variables)
     original_model = OutOfSampleModel(base_model, novelty_detector, 1)
