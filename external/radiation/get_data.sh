8fad14f7413f204ecfafec540c849998da4aab8b
brianhenn <brianhenn@allenai.org>
2022-07-20 21:22:30 +0000

    updated get data script to run cleanly

--- external/radiation/get_data.sh
+++ external/radiation/get_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p fortran  
 if [ ! -z "$IS_DOCKER" ] ; then
     echo "This script cannot be run in the Docker image"
@@ -20,7 +22,7 @@ else
     fi
 
     if [ -z "$(ls -A ./fortran/data/LW)" ]; then
-        gsutil cp -r gs://vcm-fv3gfs-serialized-regression-data/physics/fv3gfs-fortran-output/lwrad/* ./fortran/data/LW/.
+        gsutil -m cp -r gs://vcm-fv3gfs-serialized-regression-data/physics/fv3gfs-fortran-output/lwrad/* ./fortran/data/LW/.
         cd ./fortran/data/LW
         tar -xzvf data.tar.gz
         cd $MYHOME
@@ -29,7 +31,7 @@ else
     fi
 
     if [ -z "$(ls -A ./fortran/data/SW)" ]; then
-        gsutil cp -r gs://vcm-fv3gfs-serialized-regression-data/physics/fv3gfs-fortran-output/swrad/* ./fortran/data/SW/.
+        gsutil -m cp -r gs://vcm-fv3gfs-serialized-regression-data/physics/fv3gfs-fortran-output/swrad/* ./fortran/data/SW/.
         cd ./fortran/data/SW
         tar -xzvf data.tar.gz
         cd $MYHOME
@@ -51,23 +53,21 @@ else
     fi
 
     if [ ! -d "./fortran/radlw/dump" ]; then
-        cd ./fortran/radlw
-        mkdir dump
+        mkdir -p ./fortran/radlw/dump
         cd $MYHOME
     else
         echo "LW standalone output directory already exists"
     fi
 
     if [ ! -d "./fortran/radsw/dump" ]; then
-        cd ./fortran/radsw
-        mkdir dump
+        mkdir -p ./fortran/radsw/dump
         cd $MYHOME
     else
         echo "SW standalone output directory already exists"
     fi
 
     if [ -z "$(ls -A ./fortran/radlw/dump)" ]; then
-        gsutil cp gs://vcm-fv3gfs-serialized-regression-data/physics/standalone-output/lwrad/* ./fortran/radlw/dump/.
+        gsutil -m cp gs://vcm-fv3gfs-serialized-regression-data/physics/standalone-output/lwrad/* ./fortran/radlw/dump/.
         cd ./fortran/radlw/dump
         tar -xzvf data.tar.gz
         cd $MYHOME
@@ -76,7 +76,7 @@ else
     fi
 
     if [ -z "$(ls -A ./fortran/radsw/dump)" ]; then
-        gsutil cp gs://vcm-fv3gfs-serialized-regression-data/physics/standalone-output/swrad/* ./fortran/radsw/dump/.
+        gsutil -m cp gs://vcm-fv3gfs-serialized-regression-data/physics/standalone-output/swrad/* ./fortran/radsw/dump/.
         cd ./fortran/radsw/dump
         tar -xzvf data.tar.gz
         cd $MYHOME
@@ -102,7 +102,7 @@ else
     fi
 
     if [ -z "$(ls -A ./python/forcing)" ]; then
-	    gsutil cp gs://vcm-fv3gfs-serialized-regression-data/physics/forcing/* ./python/forcing/.
+	    gsutil -m cp gs://vcm-fv3gfs-serialized-regression-data/physics/forcing/* ./python/forcing/.
 	    cd ./python/forcing
 	    tar -xzvf data.tar.gz
         cd $MYHOME
