8bb3a0767475026eed4686f225d1107f04821f5b
Andre Perkins <frodre@gmail.com>
2022-07-20 20:39:22 +0000

    Add torch to the requirements.in

--- projects/full_model_emulation/install_local_env.sh
+++ projects/full_model_emulation/install_local_env.sh
@@ -4,7 +4,7 @@ set -e -x
 
 ENV_NAME=torch
 
-# PROJECT_NAME=${ENV_NAME} make -C ../../ create_environment
+PROJECT_NAME=${ENV_NAME} make -C ../../ create_environment
 source activate ${ENV_NAME}
 # conda install mkl=2022.0.1 $ optional for intel-based computers
-conda install pytorch=1.12.0 cudatoolkit=11.3 -c pytorch
+pip install -c ../../constraints.txt -r requirements.in
