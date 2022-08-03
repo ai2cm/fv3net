95ec4317056d98db18431668737c6b11e56efa05
Noah D. Brenowiz <nbren12@gmail.com>
2022-08-03 16:17:38 -0700

    Document filter calling conventions

--- external/fv3fit/fv3fit/emulation/zhao_carr/filters.py
+++ external/fv3fit/fv3fit/emulation/zhao_carr/filters.py
@@ -1,4 +1,10 @@
+"""Filters to be used with unbatched tensorflow datasets
+
+These should be callables of a single column of data (i.e. no batch dimension)
+it must return a scalar boolean.
+"""
 from typing import Set
+import tensorflow as tf
 from fv3fit.emulation.types import TensorDict
 import dataclasses
 import numpy as np
@@ -12,7 +18,7 @@ class HighAntarctic:
     def input_variables(self) -> Set[str]:
         return {"latitude", "surface_air_pressure"}
 
-    def __call__(self, elm: TensorDict):
-        lat = elm["latitude"] < np.deg2rad(-60)
-        ps = elm["surface_air_pressure"] < 700e2
+    def __call__(self, element: TensorDict) -> tf.Tensor:
+        lat = element["latitude"] < np.deg2rad(-60)
+        ps = element["surface_air_pressure"] < 700e2
         return (ps & lat)[0]
