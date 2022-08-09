9df73573ed5136fd1cef67277c83808fb77d2e92
YakelynRJ <yakelynr@uw.edu>
2022-08-09 23:40:58 +0000

    output full path filename

--- external/radiation/python/getdata.py
+++ external/radiation/python/getdata.py
@@ -334,10 +334,10 @@ def astronomy(forcing_dir, isolar, tile_number):
 
     if tile_number == 0:
         if isolar == 1:  # noaa ann-tile_numberan tsi in absolute scale
-            solar_file = "solarconstant_noaa_a0.nc"
-
-            if os.path.isfile(os.path.join(forcing_dir, solar_file)):
-                data = xr.open_dataset(os.path.join(forcing_dir, solar_file))
+            file_name = "solarconstant_noaa_a0.nc"
+            solar_file = os.path.join(forcing_dir, file_name)
+            if os.path.isfile(solar_file):
+                data = xr.open_dataset(solar_file)
             else:
                 warnings.warn(f'Requested solar data file "{solar_file}" not found!',)
                 raise FileNotFoundError(
@@ -345,9 +345,10 @@ def astronomy(forcing_dir, isolar, tile_number):
                 )
 
         elif isolar == 2:  # noaa ann-tile_numberan tsi in tim scale
-            solar_file = "solarconstant_noaa_an.nc"
-            if os.path.isfile(os.path.join(forcing_dir, solar_file)):
-                data = xr.open_dataset(os.path.join(forcing_dir, solar_file))
+            file_name = "solarconstant_noaa_an.nc"
+            solar_file = os.path.join(forcing_dir, file_name)
+            if os.path.isfile(solar_file):
+                data = xr.open_dataset(solar_file)
             else:
                 warnings.warn(f'Requested solar data file "{solar_file}" not found!',)
                 raise FileNotFoundError(
@@ -355,9 +356,10 @@ def astronomy(forcing_dir, isolar, tile_number):
                 )
 
         elif isolar == 3:  # cmip5 ann-tile_numberan tsi in tim scale
-            solar_file = "solarconstant_cmip_an.nc"
-            if os.path.isfile(os.path.join(forcing_dir, solar_file)):
-                data = xr.open_dataset(os.path.join(forcing_dir, solar_file))
+            file_name = "solarconstant_cmip_an.nc"
+            solar_file = os.path.join(forcing_dir, file_name)
+            if os.path.isfile(solar_file):
+                data = xr.open_dataset(solar_file)
             else:
                 warnings.warn(f'Requested solar data file "{solar_file}" not found!',)
                 raise FileNotFoundError(
@@ -365,9 +367,10 @@ def astronomy(forcing_dir, isolar, tile_number):
                 )
 
         elif isolar == 4:  # cmip5 mon-tile_numberan tsi in tim scale
-            solar_file = "solarconstant_cmip_mn.nc"
-            if os.path.isfile(os.path.join(forcing_dir, solar_file)):
-                data = xr.open_dataset(os.path.join(forcing_dir, solar_file))
+            file_name = "solarconstant_cmip_mn.nc"
+            solar_file = os.path.join(forcing_dir, file_name)
+            if os.path.isfile(solar_file):
+                data = xr.open_dataset(solar_file)
             else:
                 warnings.warn(f'Requested solar data file "{solar_file}" not found!',)
                 raise FileNotFoundError(
