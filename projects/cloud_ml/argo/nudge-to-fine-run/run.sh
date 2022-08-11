0e93a16eebf9573802275c25e8f9ff83c135761c
brianhenn <brianhenn@allenai.org>
2022-08-11 22:14:55 +0000

    short nudged and free runs with everything at 15 minutes, 2022-08-11

--- projects/cloud_ml/argo/nudge-to-fine-run/run.sh
+++ projects/cloud_ml/argo/nudge-to-fine-run/run.sh
@@ -8,13 +8,13 @@ PROJECT=cloud-ml
 
 EXPERIMENT="cloud-ml-training-data"
 TRIAL="trial-0"
-TAG=${EXPERIMENT}
-NAME="${TAG}-nudge-to-fine-run"
+TAG=${EXPERIMENT}-${CONFIG}
+NAME="${TAG}"
 
 argo submit --from workflowtemplate/prognostic-run \
     -p project=${PROJECT} \
     -p tag=${TAG} \
     -p config="$(< ${CONFIG}-config.yaml)" \
-    -p segment-count="2" \
+    -p segment-count="5" \
     --name "${NAME}" \
     --labels "project=${PROJECT},experiment=${EXPERIMENT},trial=${TRIAL}"
