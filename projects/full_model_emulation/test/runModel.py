d28691af12d75c761ef3393b04dad4bf466e99bd
Ebrahim Nabizadeh <eb.nabizade@gmail.com>
2022-08-17 05:29:55 +0000

    KS model

--- projects/full_model_emulation/test/runModel.py
+++ projects/full_model_emulation/test/runModel.py
@@ -22,7 +22,6 @@ from KSutils import *
 import wandb
 from fv3net.artifacts.resolve_url import resolve_url
 from vcm import get_fs
-from torch_geometric.nn.conv import MessagePassing
 from tqdm import trange
 import pandas as pd
 from graph_weather import GraphWeatherForecaster
@@ -303,7 +302,7 @@ elif res==5:
 
 lat_lons=np.swapaxes(lat_lons,0,1)
 lat_lons=lat_lons*180/np.pi
-
+lat_lons=lat_lons.float().to(device)
 
 Zmean = 5765.8457  # Z500mean=5765.8457,
 Zstd = 90.79599  # Z500std=90.79599
@@ -320,7 +319,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 loss = nn.MSELoss()
 
-model = GraphWeatherForecaster(lat_lons,resolution=2, feature_dim=7, output_dim=2, node_dim=64, num_blocks=6,hidden_dim_processor_node=64,hidden_dim_processor_edge=64,hidden_layers_processor_node=2,hidden_layers_processor_edge=2,hidden_dim_decoder=64,hidden_layers_decoder=2,norm_type="LayerNorm").to(device)
+model = GraphWeatherForecaster(lat_lons,resolution=resolution, feature_dim=feature_dim, output_dim=output_dim, node_dim=node_dim, num_blocks=num_blocks,hidden_dim_processor_node=hidden_dim_processor_node,hidden_dim_processor_edge=hidden_dim_processor_edge,hidden_layers_processor_node=hidden_layers_processor_node,hidden_layers_processor_edge=hidden_layers_processor_edge,hidden_dim_decoder=hidden_dim_decoder,hidden_layers_decoder=hidden_layers_decoder,norm_type=norm_type).to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.7)
