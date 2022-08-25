1a98f5e013fed82ee3cc4afc140b9c7ef2951130
Ebrahim Nabizadeh <eb.nabizade@gmail.com>
2022-08-25 21:46:04 +0000

    module's name changed

--- external/fv3fit/fv3fit/pytorch/graph/__init__.py
+++ external/fv3fit/fv3fit/pytorch/graph/__init__.py
@@ -1,3 +1,3 @@
-from .UNet_GraphNet import GraphUNet, UNetGraphNetworkConfig, CubedSphereGraphOperation
+from .unet import GraphUNet, UNetGraphNetworkConfig, CubedSphereGraphOperation
 from .train import train_graph_model, TrainingLoopConfig, GraphHyperparameters
 from .graph_builder import build_dgl_graph, build_graph
