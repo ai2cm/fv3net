bc66274bdf4756726982e4640b32d14ee5f20f51
Andre Perkins <frodre@gmail.com>
2022-08-26 03:30:17 +0000

    Fix dims for classification state variables

--- external/emulation/emulation/_monitor/monitor.py
+++ external/emulation/emulation/_monitor/monitor.py
@@ -17,13 +17,9 @@ from emulation._time import translate_time
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 TIME_FMT = "%Y%m%d.%H%M%S"
-DIMS_MAP = {
-    1: ["sample"],
-    2: ["sample", "z"],
-}
+DIMS_MAP = {1: ["sample"], 2: ["sample", "z"], 3: ["sample", "z", "category"]}
 
 
 @dataclasses.dataclass
