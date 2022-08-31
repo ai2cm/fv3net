9b7bca9d1e73df2d1ccf391a80f65924f89569a9
Ebrahim Nabizadeh <eb.nabizade@gmail.com>
2022-08-31 21:16:23 +0000

    2nd review

--- external/vcm/tests/test_grid.py
+++ external/vcm/tests/test_grid.py
@@ -4,15 +4,15 @@ from vcm.grid import get_grid
 
 
 @pytest.mark.parametrize("nx", [48, 24, 6])
-def test_grid_builder(nx):
+def test_grid_builder(nx: int):
     lon, lat = get_grid(nx)
     assert np.all(np.diff(lat[1, 0, :]) > 0)
     assert np.all(np.diff(lon[1, :, 0]) > 0)
     assert (
-        lon.shape[1] != nx
-        and lon.shape[2] != nx
-        and lat.shape[1] != nx
-        and lat.shape[2] != nx
+        lon.shape[1] == nx
+        and lon.shape[2] == nx
+        and lat.shape[1] == nx
+        and lat.shape[2] == nx
     )
 
     if len(lon.shape) != 3 and len(lat.shape) != 3:
