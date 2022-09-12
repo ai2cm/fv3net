da8fb0bf2ba5e7a99ac10b0f5002cdab1b9c218c
Spencer Clark <spencerkclark@gmail.com>
2022-09-12 20:21:33 +0000

    Parametrize local packages for fv3net install script

--- .environment-scripts/install_fv3net_python_dependencies.sh
+++ .environment-scripts/install_fv3net_python_dependencies.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -e
 
-ROOT_DIRECTORY=$1
+REQUIREMENTS_PATH=$1
+LOCAL_DEPENDENCIES=${@:2}
 
-cd $ROOT_DIRECTORY
-pip install --no-cache-dir -r docker/prognostic_run/requirements.txt
-pip install --no-cache-dir --no-dependencies -e external/vcm
-pip install --no-cache-dir --no-dependencies -e external/loaders
-pip install --no-cache-dir --no-dependencies -e external/fv3fit
-pip install --no-cache-dir --no-dependencies -e external/fv3kube
-pip install --no-cache-dir --no-dependencies -e workflows/post_process_run
-pip install --no-cache-dir --no-dependencies -e workflows/prognostic_c48_run
-pip install --no-cache-dir --no-dependencies -e external/emulation
-pip install --no-cache-dir --no-dependencies -e external/radiation
+pip install --no-cache-dir -r $REQUIREMENTS_PATH
+for dependency in $LOCAL_DEPENDENCIES
+do
+    pip install --no-cache-dir --no-dependencies -e $dependency
+done
