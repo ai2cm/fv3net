bdbacf23654906bbd9dd2927cf18989aa396d435
Spencer Clark <spencerkclark@gmail.com>
2022-09-14 19:32:38 +0000

    Add generic activate script

---
+++ .environment-scripts/activate_environment.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+PLATFORM=$1
+SCRIPTS=$2
+FV3NET_DIR=$3
+CONDA_ENV=$4
+
+ACTIVATE_CONDA_ENVIRONMENT=$SCRIPTS/activate_conda_environment.sh
+if [ -f $ACTIVATE_CONDA_ENVIRONMENT];
+then
+    source $ACTIVATE_CONDA_ENVIRONMENT
+fi
+
+# these are needed for "click" to work
+export LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
+
+# Override microphysics emulation
+export VAR_META_PATH=$FV3NET_DIR/external/emulation/microphysics_parameter_metadata.yaml
+export OUTPUT_FREQ_SEC=18000
+
+# Add emulation project scripts
+chmod +x $FV3NET_DIR/projects/microphysics/scripts/*
+export PATH=$FV3NET_DIR/projects/microphysics/scripts:${PATH}
+
+export PYTHONPATH=$FV3NET_DIR/workflows/prognostic_c48_run:$FV3NET_DIR/external/fv3fit:$FV3NET_DIR/external/emulation:$FV3NET_DIR/external/vcm:/fv3net/external/artifacts:$FV3NET_DIR/external/loaders:$FV3NET_DIR/external/fv3kube:$FV3NET_DIR/workflows/post_process_run:$FV3NET_DIR/external/radiation:${PYTHONPATH}
--- .environment-scripts/gaea/activate_conda_environment.sh
+++ .environment-scripts/gaea/activate_conda_environment.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+CONDA_ENV=$1
+
 # We don't use module load python/3.9 since that pollutes our PATH
 CONDA_PATH=/ncrc/sw/gaea-cle7/python/3.9/anaconda-base
 CONDA_SETUP="$($CONDA_PATH/bin/conda shell.bash hook 2> /dev/null)"
 eval "$CONDA_SETUP"
-conda activate prognostic-run-2022-09-14
+conda activate $CONDA_ENV
