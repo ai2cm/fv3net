e6e4bec87e005def5be40877211a45f9e3cef345
Oliver Watt-Meyer <oliverwatt@gmail.com>
2022-09-22 13:45:41 -0700

    Use sine for more realistic itcz_edges test

--- workflows/diagnostics/tests/prognostic/test_compute.py
+++ workflows/diagnostics/tests/prognostic/test_compute.py
@@ -5,8 +5,8 @@ from fv3net.diagnostics.prognostic_run.compute import itcz_edges
 
 
 def test_itcz_edges():
-    lat = np.arange(-85, 90, 10)
-    psi = xr.DataArray(np.linspace(-20, 40, len(lat)), coords={"latitude": lat})
+    lat = np.arange(-85, 90, 5)
+    psi = xr.DataArray(np.sin(90 / 20 * np.deg2rad(lat)), coords={"latitude": lat})
     min_, max_ = itcz_edges(psi, lat="latitude")
-    xr.testing.assert_identical(min_, xr.DataArray(-25.0, name="latitude"))
-    xr.testing.assert_identical(max_, xr.DataArray(25.0, name="latitude"))
+    xr.testing.assert_identical(min_, xr.DataArray(-20.0, name="latitude"))
+    xr.testing.assert_identical(max_, xr.DataArray(20.0, name="latitude"))
