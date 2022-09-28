c68e2ce68ebbb380dd4662b541adac8f84888751
Spencer Clark <spencerkclark@gmail.com>
2022-09-28 20:53:37 +0000

    Cleanups

--- .environment-scripts/activate_environment.sh
+++ .environment-scripts/activate_environment.sh
@@ -3,7 +3,7 @@ set -e
 
 PLATFORM=$1
 FV3NET_DIR=$2
-INSTALL_PREFIX=$3
+INSTALL_PREFIX=$3  # Must be in environment when sourcing the environment_variable.sh script.
 CONDA_ENV=$4
 
 FV3NET_SCRIPTS=$FV3NET_DIR/.environment-scripts
--- .environment-scripts/gaea/install_base_software.sh
+++ .environment-scripts/gaea/install_base_software.sh
@@ -4,10 +4,8 @@ set -e
 INSTALL_PREFIX=$1
 CONDA_ENV=$2
 
-# We don't use module load python/3.9 since that pollutes our PATH
-CONDA_PATH=/ncrc/sw/gaea-cle7/python/3.9/anaconda-base
-CONDA_SETUP="$($CONDA_PATH/bin/conda shell.bash hook 2> /dev/null)"
-eval "$CONDA_SETUP"
+module load python/3.9
+eval "$(conda shell.bash hook)"
 
 CONDA_PREFIX=$INSTALL_PREFIX/conda
 conda config --add pkgs_dirs $CONDA_PREFIX/pkgs
