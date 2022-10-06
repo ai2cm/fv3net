12d2ed3c259ea669d7e21f29133c7e3d83ca054d
Jeremy McGibbon <jeremym@allenai.org>
2022-10-06 15:56:30 -0700

    Add halo updates to CycleGAN (#2068)
    This PR makes convolution type parameterizable for CycleGAN training, an adds a new "halo_conv2d" convolution option. The previous "conv2d" option is kept only for comparison or debugging purposes.
    
    Added public API:
    - symbol (e.g. `vcm.my_function`) or script and optional description of changes or why they are needed
    - Can group multiple related symbols on a single bullet
    
    Refactored public API:
    - Added convolution_type option to CycleGANNetworkConfig
    - Added strided_kernel_size option to GeneratorConfig, so that even kernel sizes can be used in striding layers and odd kernel sizes can be used in non-striding layers, which ensures we have an even distribution of halo points and makes it more straightforward to align outputs to inputs
    - Added in_memory option to CycleGANTrainingConfig, which will convert all training data to pytorch tensors at the start of training.
    
    Significant internal changes:
    - Removed "padding" argument from convolution factories, as we always want "same"-like padding behavior
    - Removed reflection padding from input and output convolutional layers, since the halo update convolution type does its own padding automatically. This may degrade "conv2d" results, but that configuration only exists for comparison/debugging.

---
+++ external/fv3fit/tests/pytorch/test_append_halos.py
@@ -0,0 +1,57 @@
+from fv3fit.keras._models.shared import append_halos
+from fv3fit.pytorch.cyclegan.modules import AppendHalos
+from fv3fit.pytorch.system import DEVICE
+import xarray as xr
+import numpy as np
+import pytest
+import torch
+
+
+def get_dataset(nx: int, ny: int, nz: int, n_tile: int) -> xr.Dataset:
+    nt = 5
+    ds = xr.Dataset(
+        data_vars={
+            "data": xr.DataArray(
+                data=np.random.randn(nt, n_tile, nz, nx, ny),
+                dims=["sample", "tile", "z", "x", "y"],
+                attrs={"units": ""},
+            ),
+        }
+    )
+    return ds
+
+
+@pytest.mark.parametrize(
+    "nx, ny, nz, n_tile, n_halo",
+    [
+        pytest.param(8, 8, 12, 6, 3, id="typical"),
+        pytest.param(8, 8, 12, 6, 1, id="one_halo"),
+        pytest.param(8, 8, 12, 6, 0, id="no_halo"),
+    ],
+)
+def test_compare_against_reference_implementation(
+    nx: int, ny: int, nz: int, n_tile: int, n_halo: int
+):
+    np.random.seed(0)
+    ds = get_dataset(nx=nx, ny=ny, nz=nz, n_tile=n_tile)
+    for name, da in ds.data_vars.items():
+        assert da.sizes["x"] == nx, name
+        assert da.sizes["y"] == ny, name
+        assert da.sizes.get("z", nz) == nz, name
+    append = AppendHalos(n_halo=n_halo)
+    result = append(torch.as_tensor(ds.data.values).float().to(DEVICE))
+    reference_ds: xr.Dataset = append_halos(ds, n_halo=n_halo)
+
+    # import matplotlib.pyplot as plt
+
+    # fig, ax = plt.subplots(3, 6, figsize=(12, 4))
+    # for i in range(6):
+    #     ax[0, i].imshow(result[0, i, 0, :, :].cpu().numpy())
+    #     ax[1, i].imshow(reference_ds.data[0, i, 0, :, :].values)
+    #     ax[2, i].imshow(
+    #         result[0, i, 0, :, :].cpu().numpy()
+    #         - reference_ds.data[0, i, 0, :, :].values
+    #     )
+    # plt.show()
+
+    np.testing.assert_almost_equal(result.cpu().numpy(), reference_ds.data.values)
