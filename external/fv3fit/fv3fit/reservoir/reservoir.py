6f9cf5d4786b27c1a535c33ffacfe6ebb6f7a9fc
Anna Kwa <annak@allenai.org>
2022-10-12 15:55:06 +0000

    Revert reservoir.py

--- external/fv3fit/fv3fit/reservoir/reservoir.py
+++ external/fv3fit/fv3fit/reservoir/reservoir.py
@@ -1,9 +1,8 @@
+import dataclasses
 import logging
 import numpy as np
 import scipy
-
-from .config import ReservoirHyperparameters
-
+from typing import Optional
 
 logger = logging.getLogger(__name__)
 
@@ -22,6 +21,44 @@ def _random_uniform_sparse_matrix(m, n, sparsity, min=0, max=1):
         density=1.0 - sparsity,
         data_rvs=_random_uniform_sample_func(min=0, max=1.0),
     )
+
+
+@dataclasses.dataclass
+class ReservoirHyperparameters:
+    """Hyperparameters for reservoir
+
+    input_size: Size of input vector
+    state_size: Size of hidden state vector,
+        W_res has shape state_size x state_size
+    adjacency_matrix_sparsity: Fraction of elements in adjacency matrix
+        W_res that are zero
+    output_size: Optional: size of output vector. Can be smaller than input
+        dimension if predicting on subdomains with overlapping
+        input regions. Defaults to same as input_size
+    spectral_radius: Largest absolute value eigenvalue of W_res.
+        Larger values increase the memory of the reservoir.
+    seed: Random seed for sampling
+    input_coupling_sparsity: Fraction of elements in each row of W_in
+        that are zero. Kept the same in all rows to ensure each input
+        is equally connected into the reservoir. Defaults to 0.
+    input_coupling_scaling: Scaling applied to W_in. Defaults to 1,
+        where all elements are sampled from random uniform distribution
+        [-1, 1]. Changing this affects relative weighting of reservoir memory
+        versus the most recent state.
+    """
+
+    input_size: int
+    state_size: int
+    adjacency_matrix_sparsity: float
+    spectral_radius: float
+    output_size: Optional[int] = None
+    seed: int = 0
+    input_coupling_sparsity: float = 0.0
+    input_coupling_scaling: float = 1.0
+
+    def __post_init__(self):
+        if not self.output_size:
+            self.output_size = self.input_size
 
 
 class Reservoir:
