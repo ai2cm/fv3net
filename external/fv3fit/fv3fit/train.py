16acfb54b5f09cbd744750045ba9f00e79958590
Jeremy McGibbon <jeremym@vulcan.com>
2022-11-04 21:05:04 +0000

    prevent tensorflow from preallocating all gpu memory

--- external/fv3fit/fv3fit/train.py
+++ external/fv3fit/fv3fit/train.py
@@ -179,10 +179,31 @@ def main(args, unknown_args=None):
     ).print_json()
 
 
+def disable_tensorflow_gpu_preallocation():
+    """
+    Enables "memory growth" option on all gpus for tensorflow.
+
+    Without this, tensorflow will eagerly allocate all gpu memory,
+    leaving none for pytorch.
+    """
+    gpus = tf.config.list_physical_devices("GPU")
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices("GPU")
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
+
 if __name__ == "__main__":
     logger.setLevel(logging.INFO)
     parser = get_parser()
     args, unknown_args = parser.parse_known_args()
+    disable_tensorflow_gpu_preallocation()
     os.makedirs("artifacts", exist_ok=True)
     logging.basicConfig(
         level=logging.INFO,
