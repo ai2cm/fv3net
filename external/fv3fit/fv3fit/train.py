45f8d490efacd2d80eefd88fd789c817266e124c
Jeremy McGibbon <mcgibbon@uw.edu>
2022-11-09 16:01:55 -0800

    remove unneeded try-except block

--- external/fv3fit/fv3fit/train.py
+++ external/fv3fit/fv3fit/train.py
@@ -188,15 +188,11 @@ def disable_tensorflow_gpu_preallocation():
     """
     gpus = tf.config.list_physical_devices("GPU")
     if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.list_logical_devices("GPU")
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.list_logical_devices("GPU")
+        logging.info("%d physical gpus, %d logical gpus", len(gpus), len(logical_gpus))
 
 
 if __name__ == "__main__":
