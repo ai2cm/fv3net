1752ac1f7063497327b48afcd46b5157fbb9ae55
Andre Perkins <frodre@gmail.com>
2023-01-05 00:05:37 +0000

    GPU apportionment for emulation runs

--- external/emulation/emulation/__init__.py
+++ external/emulation/emulation/__init__.py
@@ -3,9 +3,16 @@
 # when this module is loaded under callpyfort, so ensure
 # it's available here
 import sys
+import os
+from mpi4py import MPI
 
 if not hasattr(sys, "argv"):
     sys.argv = [""]
+
+# Mechanism to make only 1 GPU visible to each process
+# Expects there to be as many GPUs as ranks here #TODO make this general
+rank = MPI.COMM_WORLD.Get_rank()
+os.environ["CUDA_VISIBLE_DEVICES"] = str(rank)
 
 from emulation.config import (
     get_hooks,
