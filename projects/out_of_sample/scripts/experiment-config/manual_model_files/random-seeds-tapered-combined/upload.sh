0862ad8825c32e41cdd3308d3b9ad7f8b471f7e5
Anna Kwa <annak@allenai.org>
2023-02-02 23:55:04 +0000

    add combined tquv tapered model and sensitivity tests

---
+++ projects/out_of_sample/scripts/experiment-config/manual_model_files/random-seeds-tapered-combined/upload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gsutil cp -r \
+    tapered-tquv-seed-0 \
+    gs://vcm-ml-experiments/out-of-sample-right-side-up-winds/oos-configs/taper-wrappers/tapered-tquv-seed-0
---
+++ projects/out_of_sample/scripts/experiment-config/rerun-configs/ocsvm-tq-sensitivity-tests/delete_script_ocsvm_tq_sensitivity.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# comment out below to delete batch of argo jobs
+exit 1
+
+argo delete prognostic-run-6db04c79a59249d09cd20fbe67289a43
+argo delete prognostic-run-242fa2da0961447389d829969a3aeebe
+argo delete prognostic-run-11b225a5e85a478aa3ae03a1d0d2f76f
+argo delete prognostic-run-2d34279b12d845d08d1e5be12d615bec
+argo delete prognostic-run-b6b29ef243104e919fcff8d9cae3f8e9
+argo delete prognostic-run-1d2fba5cb37e447abf78a79aa13d3412
+argo delete prognostic-run-0c76bf3cfac0430aa0c38a2eb74c0992
+argo delete prognostic-run-161b7cf015344c2d96a42762e955e52f
