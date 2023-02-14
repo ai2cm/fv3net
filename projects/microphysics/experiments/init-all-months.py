bfa84d0885ab31bd184312955980f6c19da9976e
Andre Perkins <frodre@gmail.com>
2023-02-14 21:42:44 +0000

    Try run all months with seed1

--- projects/microphysics/experiments/init-all-months.py
+++ projects/microphysics/experiments/init-all-months.py
@@ -16,7 +16,7 @@ def get_job(month: int):
 
     tag_sha = "95218aec9ed1e6cbda0db1e1f69dc34e0d839c52"
     return PrognosticJob(
-        f"zc-emu-monthly-normfix-m{month:02d}-30d-v1",
+        f"zc-emu-monthly-normfix-m{month:02d}-seed1-30d-v1",
         config=config,
         image_tag=tag_sha,
         fv3net_image_tag=tag_sha,
