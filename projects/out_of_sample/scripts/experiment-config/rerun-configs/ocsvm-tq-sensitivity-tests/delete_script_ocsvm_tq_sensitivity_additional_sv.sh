d6aef081c85a059171dbc72f13ee86f0e4847853
Anna Kwa <annak@allenai.org>
2023-02-14 17:37:54 +0000

    add configuration for gamma 8/79 nu 4e-6

---
+++ projects/out_of_sample/scripts/experiment-config/rerun-configs/ocsvm-tq-sensitivity-tests/delete_script_ocsvm_tq_sensitivity_additional_sv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# comment out below to delete batch of argo jobs
+exit 1
+
+argo delete prognostic-run-e7a29c5c97184c878bb1ea9261399faf
+argo delete prognostic-run-a70e9878d6d649d690da99aa83ae889a
---
+++ projects/out_of_sample/scripts/experiment-config/rerun-configs/ocsvm-tq-sensitivity-tests/delete_script_ocsvm_tq_sensitivity_additional_sv_2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# comment out below to delete batch of argo jobs
+exit 1
+
+argo delete prognostic-run-48f27e214e244177aea9d7f372c221d9
