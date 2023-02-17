79acd16228f410ab6b6cd4f8e31320fd8f0c3463
Anna Kwa <annak@allenai.org>
2023-02-17 22:23:40 +0000

    rm redundant function

--- external/fv3fit/fv3fit/reservoir/one_dim/domain_1d.py
+++ external/fv3fit/fv3fit/reservoir/one_dim/domain_1d.py
@@ -1,14 +1,5 @@
-import numpy as np
 from ..domain import slice_along_axis
-
-
-def _slice(arr: np.ndarray, inds: slice, axis: int = 0):
-    # https://stackoverflow.com/a/37729566
-    # For slicing ndarray along a dynamically specified axis
-    # same as np.take() but does not make a copy of the data
-    sl = [slice(None)] * arr.ndim
-    sl[axis] = inds
-    return arr[tuple(sl)]
+import numpy as np
 
 
 class Subdomain:
