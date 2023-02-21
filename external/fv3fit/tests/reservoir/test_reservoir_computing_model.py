eab794985f809471ed6c1aedfb6f4e6a31c1d384
Anna Kwa <annak@allenai.org>
2023-02-21 19:57:27 +0000

    fix readout in test

--- external/fv3fit/tests/reservoir/test_reservoir_computing_model.py
+++ external/fv3fit/tests/reservoir/test_reservoir_computing_model.py
@@ -10,12 +10,19 @@ from fv3fit.reservoir import (
     ReservoirOnlyDomainPredictor,
     HybridDomainPredictor,
 )
-from fv3fit.reservoir.config import SubdomainConfig, ReadoutHyperparameters
+from fv3fit.reservoir.config import (
+    SubdomainConfig,
+    ReadoutHyperparameters,
+    BatchLinearRegressorHyperparameters,
+)
 
 
 def generic_readout(**readout_kwargs):
+    lr_config = BatchLinearRegressorHyperparameters(
+        l2=0, add_bias_term=True, use_least_squares_solve=True
+    )
     readout_hyperparameters = ReadoutHyperparameters(
-        linear_regressor_kwargs={}, square_half_hidden_state=False
+        linear_regressor_config=lr_config, square_half_hidden_state=False
     )
     return ReservoirComputingReadout(readout_hyperparameters, **readout_kwargs)
 
