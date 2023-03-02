0b45318f274ec39fedec8566f63ba3b7e5e081dd
Michael Rudko <michael.rudko@princeton.edu>
2023-03-02 17:22:55 -0500

    A new FMS_MAKE_OPTIONS variable has been introduced to distinguish between parallel and serial builds on different platforms.

--- .environment-scripts/install_fms.sh
+++ .environment-scripts/install_fms.sh
@@ -2,9 +2,10 @@
 set -e
 
 INSTALL_PREFIX=$1
+MAKE_OPTIONS=$2
 
 cd $INSTALL_PREFIX
 autoreconf -f --install
 ./configure
-make -j8
+make $MAKE_OPTIONS
 mv $INSTALL_PREFIX/*/*.o $INSTALL_PREFIX/*/*.h $INSTALL_PREFIX/*/*.mod $INSTALL_PREFIX/
