5fd13a34b086f2b19e5ce9954fdb54d42dbdc967
Anna Kwa <annak@allenai.org>
2023-03-08 21:51:56 +0000

    deprecate 1d code

--- external/fv3fit/fv3fit/reservoir/one_dim/__init__.py
+++ external/fv3fit/fv3fit/reservoir/one_dim/__init__.py
@@ -1,8 +1,25 @@
-from .model import (
-    HybridReservoirComputingModel,
-    DomainPredictor,
-    ReservoirOnlyDomainPredictor,
-    HybridDomainPredictor,
-    ImperfectModel,
-)
-from .config import Reservoir1DTrainingConfig, SubdomainConfig
+class DeprecationError(Exception):
+    def __init__(
+        self, message,
+    ):
+        message = (
+            f"{message} \n Last working commit for 1D reservoir code is "
+            "5f7d280e7654a345c5ba8c18c61367ffadfa6207"
+        )
+        super().__init__(message)
+
+
+deprecated_names = [
+    "HybridReservoirComputingModel",
+    "DomainPredictor",
+    "ReservoirOnlyDomainPredictor",
+    "HybridDomainPredictor",
+    "ImperfectModel",
+    "Reservoir1DTrainingConfig",
+    "SubdomainConfig",
+]
+
+
+def __getattr__(name):
+    if name in deprecated_names:
+        raise DeprecationError(f"module {__name__!r} has no attribute {name!r}")
