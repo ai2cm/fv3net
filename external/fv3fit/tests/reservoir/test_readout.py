c3ebdaa9a7e6b069b620075b50dc4ee210c3b34f
Anna Kwa <annak@allenai.org>
2023-03-09 21:36:37 +0000

    Remove combined readout class, refactor fitting and input squaring out of readout

--- external/fv3fit/tests/reservoir/test_readout.py
+++ external/fv3fit/tests/reservoir/test_readout.py
@@ -1,89 +1,43 @@
 import numpy as np
-import scipy.linalg
+import os
 import pytest
 
-from fv3fit.reservoir.config import (
-    ReadoutHyperparameters,
-    BatchLinearRegressorHyperparameters,
-)
+from fv3fit.reservoir.config import BatchLinearRegressorHyperparameters
 from fv3fit.reservoir.readout import (
     ReservoirComputingReadout,
-    CombinedReservoirComputingReadout,
-    square_even_terms,
-    _sort_subdirs_numerically,
+    combine_readouts,
     BatchLinearRegressor,
 )
 
 
-@pytest.mark.parametrize(
-    "arr, axis, expected",
-    [
-        (np.arange(4), 0, np.array([0, 1, 4, 3])),
-        (np.arange(4).reshape(1, -1), 1, np.array([[0, 1, 4, 3]])),
-        (np.arange(8).reshape(2, 4), 0, np.array([[0, 1, 4, 9], [4, 5, 6, 7]])),
-        (
-            np.arange(10).reshape(2, 5),
-            1,
-            np.array([[0, 1, 4, 3, 16], [25, 6, 49, 8, 81]]),
-        ),
-    ],
-)
-def test_square_even_terms(arr, axis, expected):
-    np.testing.assert_array_equal(square_even_terms(arr, axis=axis), expected)
+def test_readout_dump_load(tmpdir):
+    state_size = 4
+    output_size = 2
 
-
-def test_readout_square_half_hidden_state():
     readout = ReservoirComputingReadout(
-        hyperparameters=ReadoutHyperparameters(
-            linear_regressor_config=BatchLinearRegressorHyperparameters(l2=0),
-            square_half_hidden_state=True,
-        ),
-        coefficients=np.ones(shape=(5, 2)),
-        intercepts=np.zeros(2),
+        coefficients=np.random.rand(state_size, output_size),
+        intercepts=np.random.rand(output_size),
     )
-    input = np.array([2, 2, 2, 2, 2])
-    # square even entries -> [4, 2, 4, 2, 4]
-    output = readout.predict(input)
-    np.testing.assert_array_almost_equal(output, np.array([16.0, 16.0]))
+    output_path = os.path.join(tmpdir, "readout")
+    readout.dump(output_path)
+    loaded_readout = ReservoirComputingReadout.load(output_path)
 
-
-def test_reservoir_computing_readout_fit():
-    lr_config = BatchLinearRegressorHyperparameters(l2=0)
-    unsquared = ReservoirComputingReadout(
-        ReadoutHyperparameters(linear_regressor_config=lr_config)
-    )
-    squared = ReservoirComputingReadout(
-        ReadoutHyperparameters(
-            linear_regressor_config=lr_config, square_half_hidden_state=True
-        )
+    x = np.ones(state_size)
+    np.testing.assert_array_almost_equal(
+        loaded_readout.predict(x), readout.predict(x),
     )
 
-    res_states = np.random.rand(20, 5)
-    output_states = np.random.rand(20, 3)
 
-    unsquared.fit(res_states, output_states)
-    squared.fit(res_states, output_states)
-
-    assert not np.allclose(unsquared.coefficients, squared.coefficients)
-
-
-def test_combined_readout():
+def test_combine_readouts():
     np.random.seed(0)
 
     state_size = 3
     output_size = 2
-    lr_config = BatchLinearRegressorHyperparameters(l2=0)
     readout_1 = ReservoirComputingReadout(
-        hyperparameters=ReadoutHyperparameters(
-            linear_regressor_config=lr_config, square_half_hidden_state=False
-        ),
         coefficients=np.random.rand(state_size, output_size),
         intercepts=np.random.rand(output_size),
     )
     readout_2 = ReservoirComputingReadout(
-        hyperparameters=ReadoutHyperparameters(
-            linear_regressor_config=lr_config, square_half_hidden_state=False
-        ),
         coefficients=np.random.rand(state_size, output_size),
         intercepts=np.random.rand(output_size),
     )
@@ -91,79 +45,11 @@ def test_combined_readout():
     output_1 = readout_1.predict(input)
     output_2 = readout_2.predict(input)
 
-    combined_readout = CombinedReservoirComputingReadout(
-        readouts=[readout_1, readout_2]
-    )
+    combined_readout = combine_readouts(readouts=[readout_1, readout_2])
     combined_input = np.concatenate([input, input], axis=1)
     output_combined = combined_readout.predict(combined_input)
     np.testing.assert_array_almost_equal(
         output_combined, np.concatenate([output_1, output_2], axis=1)
-    )
-
-
-def test_combined_readout_inconsistent_hyperparameters():
-    lr_config = BatchLinearRegressorHyperparameters(l2=0)
-
-    readout_1 = ReservoirComputingReadout(
-        hyperparameters=ReadoutHyperparameters(
-            linear_regressor_config=lr_config, square_half_hidden_state=True
-        ),
-        coefficients=np.ones(shape=(2, 2)),
-        intercepts=np.zeros(2),
-    )
-    readout_2 = ReservoirComputingReadout(
-        hyperparameters=ReadoutHyperparameters(
-            linear_regressor_config=lr_config, square_half_hidden_state=False
-        ),
-        coefficients=np.ones(shape=(2, 2)),
-        intercepts=np.zeros(2),
-    )
-    with pytest.raises(ValueError):
-        CombinedReservoirComputingReadout(readouts=[readout_1, readout_2])
-
-
-def test__sort_subdirs_numerically():
-    subdirs = [
-        "subdir_0",
-        "subdir_1",
-        "subdir_10",
-        "subdir_11",
-        "subdir_2",
-    ]
-    assert _sort_subdirs_numerically(subdirs) == [
-        "subdir_0",
-        "subdir_1",
-        "subdir_2",
-        "subdir_10",
-        "subdir_11",
-    ]
-
-
-def test_combined_load(tmpdir):
-    np.random.seed(0)
-    readouts, readout_paths = [], []
-    coef_shape = (2, 2)
-    output_size = 2
-    for i in range(3):
-        output_path = f"{str(tmpdir)}/readout_{i}"
-        readout = ReservoirComputingReadout(
-            hyperparameters=ReadoutHyperparameters(
-                linear_regressor_config=BatchLinearRegressorHyperparameters(l2=0),
-                square_half_hidden_state=True,
-            ),
-            coefficients=np.random.rand(*coef_shape),
-            intercepts=np.random.rand(output_size),
-        )
-        readout.dump(output_path)
-        readouts.append(readout)
-        readout_paths.append(output_path)
-    combined_readout = CombinedReservoirComputingReadout.load(str(tmpdir))
-    np.testing.assert_array_almost_equal(
-        scipy.linalg.block_diag(*[r.coefficients for r in readouts]),
-        combined_readout.coefficients.todense(),
-    )
-    np.testing.assert_array_almost_equal(
-        np.concatenate([r.intercepts for r in readouts]), combined_readout.intercepts
     )
 
 
@@ -182,6 +68,7 @@ def test_BatchLinearRegressor():
     lr.batch_update(X, y)
 
     coefficients, intercepts = lr.get_weights()
+
     np.testing.assert_array_almost_equal(np.dot(X, coefficients) + intercepts, y)
 
 
