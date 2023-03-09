b0077d1c76ac5af695ee11a362ecb9f2e7b3bc64
Anna Kwa <annak@allenai.org>
2023-03-09 23:04:37 +0000

    use refactored readout in model

--- external/fv3fit/fv3fit/reservoir/model.py
+++ external/fv3fit/fv3fit/reservoir/model.py
@@ -1,12 +1,10 @@
 import fsspec
+from fv3fit.reservoir.readout import ReservoirComputingReadout
 import numpy as np
 import os
-import scipy.sparse
-from typing import Optional, Union
 import yaml
 
 from .reservoir import Reservoir
-from .domain import RankDivider
 
 
 def _square_evens(v: np.ndarray) -> np.ndarray:
@@ -18,50 +16,45 @@ def _square_evens(v: np.ndarray) -> np.ndarray:
     return c
 
 
-def square_even_terms(v: np.ndarray, axis=1) -> np.ndarray:
+def _square_even_terms(v: np.ndarray, axis=1) -> np.ndarray:
     return np.apply_along_axis(func1d=_square_evens, axis=axis, arr=v)
 
 
 class ReservoirComputingModel:
     _RESERVOIR_SUBDIR = "reservoir"
-    _RANK_DIVIDER_NAME = "rank_divider"
-    _COEFFICIENTS_NAME = "coefficients.npz"
-    _INTERCEPTS_NAME = "intercepts.npz"
+    _READOUT_SUBDIR = "readout"
     _METADATA_NAME = "metadata.yaml"
 
     def __init__(
         self,
         reservoir: Reservoir,
-        coefficients: Union[np.ndarray, scipy.sparse.coo_matrix],
-        intercepts: Union[np.ndarray, scipy.sparse.coo_matrix],
+        readout: ReservoirComputingReadout,
         square_half_hidden_state: bool = False,
-        rank_divider: Optional[RankDivider] = None,
     ):
         """_summary_
 
         Args:
             reservoir: Reservoir which takes input and updates hidden state
-            coefficients: _description_
-            intercepts:
+            readout: readout layer which takes in state and predicts next time step
             square_half_hidden_state: if True, square even terms in the reservoir
                 state before it is used as input to the regressor's .fit and
                 .predict methods. This option was found to be important for skillful
                 predictions in Wikner+2020 (https://doi.org/10.1063/5.0005541).
-            rank_divider: If used in training, save the rank divider that converts
-                the full 3D rank into stacked subdomains.
         """
         self.reservoir = reservoir
-        self.coefficients = coefficients
-        self.intercepts = intercepts
+        self.readout = readout
         self.square_half_hidden_state = square_half_hidden_state
-        self.rank_divider = rank_divider
+        # TODO: add data formatter object that handles normalization and reshpaing
 
     def predict(self):
+        # TODO: for prediction over multiple subdomains (>1 column in reservoir state
+        # array) the data formatter will flatten state into 1D vector before
+        # predicting
         if self.square_half_hidden_state is True:
-            input = square_even_terms(self.reservoir.state, axis=0)
+            readout_input = _square_even_terms(self.reservoir.state, axis=0)
         else:
-            input = self.reservoir.state
-        prediction = input * self.coefficients + self.intercepts
+            readout_input = self.reservoir.state
+        prediction = self.readout.predict(readout_input).reshape(-1)
         self.reservoir.increment_state(prediction)
         return prediction
 
@@ -71,25 +64,24 @@ class ReservoirComputingModel:
         Args:
             path: a URL pointing to a directory
         """
-        if self.rank_divider is not None:
-            self.rank_divider.dump(os.path.join(path, self._RANK_DIVIDER_NAME))
-        self.reservoir.dump(f"{path}/{self._RESERVOIR_SUBDIR}")
-
-        # Save and load as sparse COO matrices so a common dump/load can be used
-        with fsspec.open(os.path.join(path, self._COEFFICIENTS_NAME), "wb") as f:
-            scipy.sparse.save_npz(f, scipy.sparse.coo_matrix(self.coefficients))
-        with fsspec.open(os.path.join(path, self._INTERCEPTS_NAME), "wb") as f:
-            scipy.sparse.save_npz(f, scipy.sparse.coo_matrix(self.intercepts))
-
+        self.reservoir.dump(os.path.join(path, self._RESERVOIR_SUBDIR))
+        self.readout.dump(os.path.join(path, self._READOUT_SUBDIR))
         metadata = {"square_half_hidden_state": self.square_half_hidden_state}
         with fsspec.open(os.path.join(path, self._METADATA_NAME), "w") as f:
             f.write(yaml.dump(metadata))
 
-
-'''
     @classmethod
     def load(cls, path: str) -> "ReservoirComputingModel":
         """Load a model from a remote path"""
-        reservoir = Reservoir.load(f"{path}/{cls._RESERVOIR_SUBDIR}")
-        return
-'''
+        reservoir = Reservoir.load(os.path.join(path, cls._RESERVOIR_SUBDIR))
+        readout = ReservoirComputingReadout.load(
+            os.path.join(path, cls._READOUT_SUBDIR)
+        )
+        with fsspec.open(os.path.join(path, cls._METADATA_NAME), "r") as f:
+            metadata = yaml.safe_load(f)
+
+        return cls(
+            reservoir=reservoir,
+            readout=readout,
+            square_half_hidden_state=metadata["square_half_hidden_state"],
+        )
