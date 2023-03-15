06497236064f62c0b183444b1660ec671b505dff
Anna Kwa <annak@allenai.org>
2023-03-15 22:17:36 +0000

    move autoencoder to reservoir subdir

--- external/fv3fit/fv3fit/keras/__init__.py
+++ external/fv3fit/fv3fit/keras/__init__.py
@@ -2,7 +2,6 @@ from . import adapters
 from . import _models
 from ._models.shared.dense_network import DenseNetwork, DenseNetworkConfig
 from ._models.shared.pure_keras import PureKerasModel
-from ._models.shared.autoencoder import Autoencoder
 from ._models.shared.training_loop import (
     TrainingLoopConfig,
     EpochResult,
