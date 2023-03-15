fbccf4746cb6517deb2ee7316a818dbda4df26d7
Jeremy McGibbon <mcgibbon@uw.edu>
2023-03-15 19:22:30 +0000

    add test of get_percentile

--- external/fv3fit/fv3fit/pytorch/cyclegan/test_cyclegan_trainer.py
+++ external/fv3fit/fv3fit/pytorch/cyclegan/test_cyclegan_trainer.py
@@ -1,7 +1,30 @@
-from fv3fit.pytorch.cyclegan.cyclegan_trainer import ResultsAggregator
+from fv3fit.pytorch.cyclegan.cyclegan_trainer import ResultsAggregator, get_percentile
 from typing import Tuple
 import pytest
 import numpy as np
+
+
+@pytest.mark.parametrize(
+    "bins, hist, pct, expected",
+    [
+        pytest.param(
+            np.array([0, 1]), np.array([10]), 0.8, 0.8, id="single_bin_interpolation"
+        ),
+        pytest.param(
+            np.array([0, 1, 2]),
+            np.array([10, 10]),
+            0.8,
+            1.6,
+            id="two_bin_interpolation",
+        ),
+        pytest.param(
+            np.array([0, 1, 2]), np.array([10, 10]), 1.0, 2.0, id="two_bin_end_value"
+        ),
+    ],
+)
+def test_get_percentile(bins, hist, pct, expected):
+    result = get_percentile(bins, hist, pct)
+    np.testing.assert_allclose(result, expected)
 
 
 @pytest.mark.parametrize("shape", [(1, 1, 1, 1, 1, 1), (2, 2, 2, 2, 2, 2)])
