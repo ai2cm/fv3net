f6d73687a1b9cf69336ad6d5981c08e3ae90ec4c
Jeremy McGibbon <mcgibbon@uw.edu>
2023-03-15 16:25:20 -0400

    add SchedulerConfig to cyclegan training config (#2176)
    The CycleGAN model seems to perform better when it is able to train on a high learning rate to get into the neighbourhood of correct results, then decrease to lower learning rates where artifacting behaviours can be learned out of the model.
    
    This PR adds a "scheduler" configuration option with SchedulerConfig to allow the user to configure an arbitrary learning rate scheduler to use in CycleGAN training.
    
    Added public API:
    - added SchedulerConfig to fv3fit.pytorch.optimizer
    
    Refactored public API:
    - added "scheduler" option to CycleGANTrainingConfig, which defaults to no learning rate scheduling

--- external/fv3fit/fv3fit/pytorch/optimizer.py
+++ external/fv3fit/fv3fit/pytorch/optimizer.py
@@ -1,8 +1,11 @@
 import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
 import dataclasses
 from typing import (
     Any,
     Mapping,
+    Optional,
+    Protocol,
 )
 
 
@@ -17,3 +20,27 @@ class OptimizerConfig:
         kwargs = dict(params=params, **self.kwargs)
 
         return cls(**kwargs)
+
+
+class Scheduler(Protocol):
+    def step(self):
+        ...
+
+
+class NullScheduler:
+    def step(self):
+        pass
+
+
+@dataclasses.dataclass
+class SchedulerConfig:
+    name: Optional[str] = None
+    kwargs: Mapping[str, Any] = dataclasses.field(default_factory=dict)
+
+    def instance(self, optimizer) -> Scheduler:
+        if self.name is None:
+            return NullScheduler()
+        else:
+            cls = getattr(lr_scheduler, self.name)
+
+            return cls(optimizer, **self.kwargs)
