e97bc4327ac9a97e675548ad26388ae2715b0855
Jeremy McGibbon <mcgibbon@uw.edu>
2023-03-16 18:28:59 +0000

    reduce size of cyclegan test

--- external/fv3fit/fv3fit/pytorch/cyclegan/test_cyclegan.py
+++ external/fv3fit/fv3fit/pytorch/cyclegan/test_cyclegan.py
@@ -189,7 +189,7 @@ def test_cyclegan_runs_without_errors(
     os.chdir(tmpdir)
     # need a larger nx, ny for the sample data here since we're training
     # on whether we can autoencode sin waves, and need to resolve full cycles
-    nx = 32
+    nx = 24
     sizes = {"nbatch": 1, "ntime": 1, "nx": nx, "nz": 2}
     state_variables = ["var_3d", "var_2d"]
     train_tfdataset = get_tfdataset(nsamples=5, **sizes)
@@ -198,15 +198,18 @@ def test_cyclegan_runs_without_errors(
         state_variables=state_variables,
         network=CycleGANNetworkConfig(
             generator=fv3fit.pytorch.GeneratorConfig(
-                n_convolutions=2,
-                n_resnet=5,
-                max_filters=128,
+                n_convolutions=1,
+                n_resnet=3,
+                max_filters=32,
                 kernel_size=3,
                 strided_kernel_size=strided_kernel_size,
             ),
             optimizer=fv3fit.pytorch.OptimizerConfig(name="Adam", kwargs={"lr": 0.001}),
             discriminator=fv3fit.pytorch.DiscriminatorConfig(
-                kernel_size=3, strided_kernel_size=strided_kernel_size,
+                n_convolutions=1,
+                max_filters=32,
+                kernel_size=3,
+                strided_kernel_size=strided_kernel_size,
             ),
             convolution_type=conv_type,
             identity_weight=0.01,
