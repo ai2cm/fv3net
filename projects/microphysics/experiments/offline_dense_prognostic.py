c251df814eee46576c00c4326b63f8b8a34f1cbf
Andre Perkins <frodre@gmail.com>
2023-03-20 17:08:19 +0000

    Redo dense vs. prior inclusion runs

--- projects/microphysics/experiments/offline_dense_prognostic.py
+++ projects/microphysics/experiments/offline_dense_prognostic.py
@@ -10,16 +10,16 @@ from end_to_end import (
 
 
 dense_model_paths = {
-    "gscond": "gs://vcm-ml-experiments/microphysics-emulation/2022-12-13/gscond-only-dense-qvout-v2/model.tf",  # noqa
+    "gscond": "gs://vcm-ml-experiments/microphysics-emulation/2023-03-17/gscond-only-qvout-norm-fix-dense-v1/model.tf",  # noqa
     "precpd": "gs://vcm-ml-experiments/microphysics-emulation/2022-12-13/precpd-diff-only-dense-no-pr-v2/model.tf",  # noqa
 }
 model_paths = {
-    "gscond": "gs://vcm-ml-experiments/microphysics-emulation/2022-12-13/gscond-only-qvout-v2/model.tf",  # noqa
-    "precpd": "gs://vcm-ml-experiments/microphysics-emulation/2022-12-13/precpd-diff-only-no-pr-v2/model.tf",  # noqa
+    "gscond": "gs://vcm-ml-experiments/microphysics-emulation/2023-02-10/zc-train-gscond-normfix-seed3-v1/model.tf",  # noqa
+    "precpd": "gs://vcm-ml-experiments/microphysics-emulation/2022-12-16/zc-train-precpd-seed5-v2/model.tf",  # noqa
 }
 
 dense_precpd_paths = {
-    "gscond": "gs://vcm-ml-experiments/microphysics-emulation/2022-12-13/gscond-only-qvout-v2/model.tf",  # noqa,
+    "gscond": "gs://vcm-ml-experiments/microphysics-emulation/2023-02-10/zc-train-gscond-normfix-seed3-v1/model.tf",  # noqa,
     "precpd": "gs://vcm-ml-experiments/microphysics-emulation/2022-12-13/precpd-diff-only-dense-no-pr-v2/model.tf",  # noqa
 }
 
@@ -32,13 +32,13 @@ def get_job(model_paths, name):
     config["zhao_carr_emulation"]["model"]["path"] = model_paths["precpd"]
 
     return PrognosticJob(
-        name=name, image_tag="99b74dcc46b233c05942b32098c1207b0e6dd323", config=config,
+        name=name, image_tag="95218aec9ed1e6cbda0db1e1f69dc34e0d839c52", config=config,
     )
 
 
 jobs = [
-    get_job(dense_model_paths, "zc-emu-using-dense-v2"),
-    get_job(model_paths, "zc-emu-not-dense-v2"),
-    get_job(dense_precpd_paths, "zc-emu-gsc-prior-precpd-simple-v2"),
+    get_job(dense_model_paths, "zc-emu-using-dense-v3"),
+    get_job(model_paths, "zc-emu-not-dense-v3"),
+    get_job(dense_precpd_paths, "zc-emu-gsc-prior-precpd-simple-v3"),
 ]
-submit_jobs(jobs, "compare-offline-dense-dec2022")
+submit_jobs(jobs, "compare-dense-mar2023")
