88b15f7fd3e2527b7514b3d6fc15e4e04ce119a8
Anna Kwa <annak@allenai.org>
2023-03-20 23:21:18 +0000

    add to inits

--- external/fv3fit/fv3fit/keras/__init__.py
+++ external/fv3fit/fv3fit/keras/__init__.py
@@ -2,7 +2,9 @@ from . import adapters
 from . import _models
 from ._models.shared.dense_network import DenseNetwork, DenseNetworkConfig
 from ._models.shared.pure_keras import PureKerasModel
+from ._models.shared.callbacks import CallbackConfig
 from ._models.shared.training_loop import (
     TrainingLoopConfig,
     EpochResult,
 )
+from ._models.shared.loss import LossConfig
