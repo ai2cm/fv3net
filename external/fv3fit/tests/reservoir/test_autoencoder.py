06b2b6cdf44c979889629803eca89050d8265114
Anna Kwa <annak@allenai.org>
2023-03-29 17:02:40 -0700

    Add build_model function for minimal autoencoder (#2199)
    * add build function for 'autoencoder' that only does concatentation and normalization/denormalization

---
+++ external/fv3fit/tests/reservoir/test_autoencoder.py
@@ -0,0 +1,35 @@
+import numpy as np
+from fv3fit.reservoir.autoencoder import (
+    Autoencoder,
+    build_concat_and_scale_only_autoencoder,
+)
+
+a = np.array([[10, 10], [0.0, 0.0]])  # size=2, mean=5, std=5
+b = np.array([[2.0, 2.0], [0.0, 0.0]])  # size=2, mean=1, std=1
+
+test_inputs = [
+    np.array([10, 10]).reshape(1, -1),
+    np.array([3, 3]).reshape(1, -1),
+]
+
+
+def test_build_concat_and_scale_only_autoencoder_normalize():
+    model = build_concat_and_scale_only_autoencoder(["a", "b"], [a, b])
+    np.testing.assert_array_almost_equal(
+        model.encode(test_inputs).reshape(-1), np.array([1, 1, 2, 2])
+    )
+
+
+def test_build_concat_and_scale_only_autoencoder_predict():
+    model = build_concat_and_scale_only_autoencoder(["a", "b"], [a, b])
+    np.testing.assert_array_almost_equal(model.predict(test_inputs), test_inputs)
+
+
+def test_concat_and_scale_only_autoencoder_dump_load(tmpdir):
+    output_path = f"{str(tmpdir)}/model"
+    model = build_concat_and_scale_only_autoencoder(["a", "b"], [a, b])
+    encoded = model.encode(test_inputs)
+    model.dump(output_path)
+    loaded_model = Autoencoder.load(output_path)
+    loaded_encoded = loaded_model.encode(test_inputs)
+    np.testing.assert_array_equal(encoded, loaded_encoded)
