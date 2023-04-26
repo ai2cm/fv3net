d70b3e2b9afe74602ff9e5f26ceefb554137cdc4
Elynn Wu <elynn510@gmail.com>
2023-04-26 14:02:10 -0700

    change auth to init time

--- external/vcm/vcm/cloud/__init__.py
+++ external/vcm/vcm/cloud/__init__.py
@@ -1,4 +1,9 @@
 from .fsspec import get_fs, get_protocol, copy
-
+import os
 
 __all__ = [item for item in dir() if not item.startswith("_")]
+
+if "GOOGLE_APPLICATION_CREDENTIALS" in os.environ:
+    from .gsutil import authenticate
+
+    authenticate(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
