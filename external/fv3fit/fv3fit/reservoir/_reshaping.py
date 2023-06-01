818ac16a5f9068904396af810a8fa1cf1bf90043
Anna Kwa <annak@allenai.org>
2023-06-01 15:26:02 -0700

    Hybrid reservoir training (#2225)
    This PR adds the ability to train hybrid reservoir models. Instead of
    adding a separate training function, I instead implemented the hybrid
    training as an option that is enabled if the training configuration
    includes a `hybrid_variables` field. If this list is provided, those
    variables are used as additional inputs to the readout layer training
    (concatenated after the reservoir states).
    
    
    Added public API:
    - `ReservoirTrainingConfig.hybrid_variables` config field is used to
    enable hybrid training
     
    Significant internal changes:
    - some refactors of the helper functions called in the reservoir
    training function to more smoothly enable hybrid training

--- external/fv3fit/fv3fit/reservoir/_reshaping.py
+++ external/fv3fit/fv3fit/reservoir/_reshaping.py
@@ -4,3 +4,10 @@ import numpy as np
 def flatten_2d_keeping_columns_contiguous(arr: np.ndarray):
     # ex. [[1,2],[3,4], [5,6]] -> [1,3,5,2,4,6]
     return np.reshape(arr, -1, "F")
+
+
+def concat_inputs_along_subdomain_features(a, b):
+    # [time, subdomain-feature, subdomain]
+    # Concatenates two input arrays with same time and subdomain dim
+    # sizes along the subdomain-feature axis (1)
+    return np.concatenate([a, b], axis=1)
