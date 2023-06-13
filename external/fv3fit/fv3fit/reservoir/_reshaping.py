f1da4f7ec45a01eef139e896fa0c8680524631d7
Anna Kwa <annak@allenai.org>
2023-06-13 21:04:21 +0000

    rm unrelated changes

--- external/fv3fit/fv3fit/reservoir/_reshaping.py
+++ external/fv3fit/fv3fit/reservoir/_reshaping.py
@@ -6,20 +6,6 @@ def flatten_2d_keeping_columns_contiguous(arr: np.ndarray):
     return np.reshape(arr, -1, "F")
 
 
-def split_1d_into_2d_rows(arr: np.ndarray, n_rows: int) -> np.ndarray:
-    # Consecutive chunks of 1d array form rows of 2d array
-    # ex. 1d to 2d reshaping (8,) -> (2,4)) for n_rows=2
-    # [1,2,3,4,5,6,7,8] -> [[1,2,3,4], [5,6,7,8]]
-    return np.reshape(arr, (n_rows, -1), order="C")
-
-
-def split_1d_into_2d_columns(arr: np.ndarray, n_columns: int) -> np.ndarray:
-    # Consecutive chunks of 1d array form columns of 2d array
-    # ex. 1d to 2d reshaping (8,) -> (2, 4)
-    # [1,2,3,4,5,6,7,8] -> [[1,3,5,7], [2,4,6,8]]
-    return np.reshape(arr, (-1, n_columns), "F")
-
-
 def concat_inputs_along_subdomain_features(a, b):
     # [time, subdomain-feature, subdomain]
     # Concatenates two input arrays with same time and subdomain dim
