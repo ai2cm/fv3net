a338a2466bc69dd028e884f36c0fd0d19b632b82
Anna Kwa <annak@allenai.org>
2023-06-21 00:37:23 +0000

    add split_1d_samples_into_2d_rows

--- external/fv3fit/tests/reservoir/test__reshaping.py
+++ external/fv3fit/tests/reservoir/test__reshaping.py
@@ -3,6 +3,7 @@ import numpy as np
 from fv3fit.reservoir._reshaping import (
     flatten_2d_keeping_columns_contiguous,
     stack_data,
+    split_1d_samples_into_2d_rows,
 )
 
 
@@ -27,3 +28,23 @@ def test_flatten_2d_keeping_columns_contiguous():
     np.testing.assert_array_equal(
         flatten_2d_keeping_columns_contiguous(x), np.array([1, 3, 5, 2, 4, 6])
     )
+
+
+def test_split_1d_samples_into_2d_rows():
+    x = np.arange(12)
+    x_2d = split_1d_samples_into_2d_rows(x, n_rows=4, keep_first_dim_shape=False)
+    np.testing.assert_array_equal(
+        x_2d, np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]])
+    )
+
+
+def test_split_1d_samples_into_2d_rows_keep_first_dim_shape():
+    nt = 3
+    x = np.array([np.arange(12) for i in range(nt)])
+    x_2d = split_1d_samples_into_2d_rows(x, n_rows=4, keep_first_dim_shape=True)
+
+    expected = np.array(
+        [np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]]) for i in range(nt)]
+    )
+
+    np.testing.assert_array_equal(x_2d, expected)
