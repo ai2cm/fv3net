93642cb70e962d4b7e5bd092f0d1666eac2df062
Paula <paulaharder@posteo.de>
2023-07-19 00:36:34 +0000

    changed readout.py back

--- external/fv3fit/fv3fit/reservoir/readout.py
+++ external/fv3fit/fv3fit/reservoir/readout.py
@@ -33,7 +33,7 @@ class BatchLinearRegressor:
             )
 
     def batch_update(self, X, y):
-        
+
         if self.hyperparameters.add_bias_term:
             X = self._add_bias_feature(X)
         else:
@@ -45,11 +45,11 @@ class BatchLinearRegressor:
 
         self.A = np.add(self.A, (np.dot(X.T, X)))
         self.B = np.add(self.B, np.dot(X.T, y))
-        
+
     def get_weights(self):
         # use_least_squares_solve is useful for simple test cases
         # where np.linalg.solve encounters for singular XT.X
-        #print(self.A,self.B)
+        # print(self.A,self.B)
         reg = self.hyperparameters.l2 * np.identity(self.A.shape[1])
 
         if self.A is None and self.B is None:
