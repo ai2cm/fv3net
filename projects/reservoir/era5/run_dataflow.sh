50b139f08585bc79e28b4ea7453d0dc4e8b6b79b
Andre Perkins <frodre@gmail.com>
2023-10-30 22:45:23 +0000

    Working dataflow processing

--- projects/reservoir/era5/run_dataflow.sh
+++ projects/reservoir/era5/run_dataflow.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Deploy on gcp
-python daily_avg.py \
-    gs://gcp-public-data-arco-era5/co/single-level-reanalysis.zarr \
-    --output_path gs://vcm-ml-scratch/andrep/era5_regrid/daily_avg.zarr \
-    --daily_template single_day_template.nc \
+python3 regrid_latlon_era5.py \
+    gs://vcm-ml-scratch/andrep/reservoir/era5/era5_daily_all_360x180_v2.zarr \
+    gs://vcm-ml-scratch/andrep/era5_regrid/era5_daily_all_c48_regrid_v1.zarr \
+    --template_path gs://vcm-ml-scratch/andrep/reservoir/era5/c48_template.nc \
     --project vcm-ml \
     --region us-central1 \
     --temp_location gs://vcm-ml-scratch/andrep/temp/ \
     --experiments use_runner_v2 \
     --runner DataflowRunner \
-    --sdk_container_image gcr.io/vcm-ml/dataflow-xbeam:v2 \
+    --sdk_container_image gcr.io/vcm-ml/dataflow-xbeam:v3-oct2023 \
     --sdk_location container \
     --num_workers 4 \
     --machine_type n1-standard-4
