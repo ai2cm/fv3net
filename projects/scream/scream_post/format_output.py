11bb37a747e41fd14f45faf6af1e04931a3481f0
Elynn Wu <elynn510@gmail.com>
2023-12-04 16:47:03 -0800

    translate scream native output to work with diag

--- projects/scream/scream_post/format_output.py
+++ projects/scream/scream_post/format_output.py
@@ -3,7 +3,7 @@ import xarray as xr
 import numpy as np
 import os
 import cftime
-import pandas as pd
+import util
 
 
 def make_placeholder_data(
@@ -69,33 +69,44 @@ def rename_delp(ds: xr.Dataset):
     return ds
 
 
-def convert_npdatetime_to_cftime(ds: xr.Dataset):
-    if isinstance(ds.time.values[0], np.datetime64):
-        cf_time = []
-        for date in ds.time.values:
-            date = pd.to_datetime(date)
-            cf_time.append(
-                cftime.DatetimeJulian(
-                    date.year,
-                    date.month,
-                    date.day,
-                    date.hour,
-                    date.minute,
-                    date.second,
-                )
-            )
-        ds["time"] = xr.DataArray(cf_time, coords=ds.time.coords, attrs=ds.time.attrs)
+def rename_water_vapor_path(ds: xr.Dataset):
+    return ds.rename({"VapWaterPath": "water_vapor_path"})
+
+
+def add_rad_fluxes(ds: xr.Dataset):
+    shortwave_transmissivity_of_atmospheric_column = (
+        ds.SW_flux_dn_at_model_bot / ds.SW_flux_dn_at_model_top
+    )
+    shortwave_transmissivity_of_atmospheric_column = shortwave_transmissivity_of_atmospheric_column.where(  # noqa
+        ds.SW_flux_dn_at_model_top != 0.0, 0.0
+    )
+    shortwave_transmissivity_of_atmospheric_column = shortwave_transmissivity_of_atmospheric_column.assign_attrs(  # noqa
+        units="-", long_name="shortwave transmissivity of atmosphericcolumn"
+    )
+    override_for_time_adjusted_total_sky_downward_longwave_flux_at_surface = (  # noqa
+        ds.LW_flux_dn_at_model_bot
+    )
+    override_for_time_adjusted_total_sky_downward_longwave_flux_at_surface = override_for_time_adjusted_total_sky_downward_longwave_flux_at_surface.assign_attrs(  # noqa
+        units="W/m**2", long_name="surface downward longwave flux"
+    )
+    ds[
+        "shortwave_transmissivity_of_atmospheric_column"
+    ] = shortwave_transmissivity_of_atmospheric_column
+    ds[
+        "override_for_time_adjusted_total_sky_downward_longwave_flux_at_surface"
+    ] = override_for_time_adjusted_total_sky_downward_longwave_flux_at_surface
     return ds
 
 
-def rename_lev_to_z(ds: xr.Dataset):
-    rename_vars = {"lev": "z", "ilev": "z_interface"}
-    rename_vars = {k: v for k, v in rename_vars.items() if k in ds.dims}
-    return ds.rename(rename_vars)
-
-
-def rename_water_vapor_path(ds: xr.Dataset):
-    return ds.rename({"VapWaterPath": "water_vapor_path"})
+def add_sfc_geopotential_height(
+    ds: xr.Dataset,
+    path="/usr/gdata/climdat/ccsm3data/inputdata/atm/cam/topo/USGS-gtopo30_ne30np4pg2_x6t-SGH.c20210614.nc",  # noqa
+):
+    sfc_geo = xr.open_dataset(path)
+    phis = sfc_geo.PHIS
+    phis = phis.expand_dims(dim={"time": ds.time}, axis=0)
+    ds["surface_geopotential"] = phis
+    return ds
 
 
 def _get_parser() -> argparse.ArgumentParser:
@@ -114,13 +125,12 @@ def _get_parser() -> argparse.ArgumentParser:
         type=str,
         help="List of nudging variables deliminated with commas",
     )
-    parser.add_argument("chunk_size", type=int, help="Chunk size for output zarrs.")
     parser.add_argument(
-        "--split-horiz-winds",
-        type=bool,
-        default=False,
-        help="Split horiz_winds to u and v",
+        "tend_processes",
+        type=str,
+        help="List of tendency processes deliminated with commas",
     )
+    parser.add_argument("chunk_size", type=int, help="Chunk size for output zarrs.")
     parser.add_argument(
         "--calc-physics-tend",
         type=bool,
@@ -160,6 +170,22 @@ def _get_parser() -> argparse.ArgumentParser:
         default=False,
         help="Rename VapWaterPath to water_vapor_path",
     )
+
+    parser.add_argument(
+        "--split-horiz-winds-tend",
+        type=bool,
+        default=False,
+        help="Rename horiz_winds ",
+    )
+    parser.add_argument(
+        "--add-rad-fluxes",
+        type=bool,
+        default=False,
+        help="Add derived radiative fluxes",
+    )
+    parser.add_argument(
+        "--add-phis", type=bool, default=False, help="Add surface geopotential height",
+    )
     return parser
 
 
@@ -168,18 +194,35 @@ if __name__ == "__main__":
     args = parser.parse_args()
     ds = xr.open_mfdataset(args.input_data)
     nudging_vars = [str(item) for item in args.nudging_variables.split(",")]
+    if args.split_horiz_winds_tend:
+        tend_processes = [str(item) for item in args.tend_processes.split(",")]
+        for i in range(len(tend_processes)):
+            print(f"Splitting {tend_processes[i]} horiz winds tendency")
+            ds = util.split_horiz_winds_tend(ds, tend_processes[i])
     if args.calc_physics_tend:
+        print("Calculating scream physics tendencies")
         ds = compute_tendencies_due_to_scream_physics(ds, nudging_vars)
     if args.rename_nudging_tend:
+        print("Renaming nudging tendencies")
         ds = rename_nudging_tendencies(ds, nudging_vars)
     if args.rename_delp:
+        print("Renaming nudging delp")
         ds = rename_delp(ds)
     if args.convert_to_cftime:
-        ds = convert_npdatetime_to_cftime(ds)
+        print("Converting timestamps to cftime")
+        ds = util.convert_npdatetime_to_cftime(ds)
     if args.rename_lev_to_z:
-        ds = rename_lev_to_z(ds)
+        print("Renaming lev to z")
+        ds = util.rename_lev_to_z(ds)
     if args.rename_water_vapor_path:
+        print("Renaming water vapor path")
         ds = rename_water_vapor_path(ds)
+    if args.add_rad_fluxes:
+        print("Adding derived radiative fluxes")
+        ds = add_rad_fluxes(ds)
+    if args.add_phis:
+        print("Adding surface geopotential height")
+        ds = add_sfc_geopotential_height(ds)
     nudging_variables_tendencies = [
         f"{var}_tendency_due_to_nudging" for var in nudging_vars
     ]
---
+++ projects/scream/scream_post/util.py
@@ -0,0 +1,36 @@
+import xarray as xr
+import numpy as np
+import pandas as pd
+import cftime
+
+def convert_npdatetime_to_cftime(ds: xr.Dataset):
+    if isinstance(ds.time.values[0], np.datetime64):
+        cf_time = []
+        for date in ds.time.values:
+            date = pd.to_datetime(date)
+            cf_time.append(
+                cftime.DatetimeJulian(
+                    date.year,
+                    date.month,
+                    date.day,
+                    date.hour,
+                    date.minute,
+                    date.second,
+                )
+            )
+        ds["time"] = xr.DataArray(cf_time, coords=ds.time.coords, attrs=ds.time.attrs)
+    return ds
+
+
+def rename_lev_to_z(ds: xr.Dataset):
+    rename_vars = {"lev": "z", "ilev": "z_interface"}
+    rename_vars = {k: v for k, v in rename_vars.items() if k in ds.dims}
+    return ds.rename(rename_vars)
+
+def split_horiz_winds_tend(ds: xr.Dataset, label: str):
+    u = ds[f"{label}_horiz_winds_tend"].isel(dim2=0).rename({f"{label}_U_tend"})
+    v = ds[f"{label}_horiz_winds_tend"].isel(dim2=1).rename({f"{label}_V_tend"})
+    ds = ds.drop(f"{label}_horiz_winds_tend")
+    ds[f"{label}_U_tend"] = u
+    ds[f"{label}_V_tend"] = v
+    return ds
