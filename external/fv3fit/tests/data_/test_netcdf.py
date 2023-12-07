2620690b819462997db2eeb7263710c4353cf3a2
Andre Perkins <frodre@gmail.com>
2023-12-07 23:25:05 +0000

    Add test for new loader

--- external/fv3fit/tests/data_/test_netcdf.py
+++ external/fv3fit/tests/data_/test_netcdf.py
@@ -146,3 +146,28 @@ def test_error_missing_data_dim_in_specified_order():
 def test_sort_netcdfs(names, sorted_names):
     names = _natural_sort(names)
     assert names == sorted_names
+
+
+def test_NCFileLoader(tmp_path: Path):
+    path = tmp_path / "a.nc"
+    cache = tmp_path / ".cache"
+    ds = vcm.cdl_to_dataset(
+        """
+    netcdf A {
+        dimensions:
+            sample = 4;
+            z = 1;
+        variables:
+            float a(sample, z);
+        data:
+            a = 0, 1, 2, 3;
+    }
+    """
+    )
+
+    ds.to_netcdf(path.as_posix())
+    loader = fv3fit.data.NCFileLoader(path.as_posix())
+    tfds = loader.open_tfdataset(cache.as_posix(), ["a"])
+    for data in tfds.as_numpy_iterator():
+        a = data["a"]
+    np.testing.assert_array_equal(ds["a"], a)
