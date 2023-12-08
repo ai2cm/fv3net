fc1a723ed098f6b9f8b60e714be493972a19866b
Andre Perkins <frodre@gmail.com>
2023-12-08 00:46:56 +0000

    Fix original encode/decode columns tests to work with default _txyz methods

--- external/fv3fit/tests/reservoir/test_transformer.py
+++ external/fv3fit/tests/reservoir/test_transformer.py
@@ -15,41 +15,36 @@ def test_DoNothingAutoencoder(nz, nvars):
     assert len(transformer.decode(encoded_data)) == len(data)
 
 
-# @pytest.mark.parametrize(
-#     "nt, nx, ny, nz, nvars",
-#     [(20, 4, 4, 3, 2), (None, 2, 2, 1, 1), (None, 2, 2, None, 1)],
-# )
-# def test_encode_columns(nt, nx, ny, nz, nvars):
-#     shape = tuple([y for y in [nt, nx, ny, nz] if y is not None])
-#     expected_shape = (*shape[:-1], nz * nvars) if nz is not None else shape
-#     transformer = DoNothingAutoencoder([nz for var in range(nvars)])
-#     data_arrs = [np.random.rand(*shape) for var in range(nvars)]
+@pytest.mark.parametrize(
+    "nt, nx, ny, nz, nvars",
+    [(20, 4, 4, 3, 2), (None, 2, 2, 1, 1), (None, 2, 2, None, 1)],
+)
+def test_base_encode_txyz(nt, nx, ny, nz, nvars):
+    shape = tuple([y for y in [nt, nx, ny, nz] if y is not None])
+    expected_shape = (*shape[:-1], nz * nvars) if nz is not None else shape
+    transformer = DoNothingAutoencoder([nz for var in range(nvars)])
+    data_arrs = [np.random.rand(*shape) for var in range(nvars)]
 
-#     encoded = encode_columns(data_arrs, transformer=transformer)
+    encoded = transformer.encode_txyz(data_arrs)
 
-#     assert encoded.shape == expected_shape
+    assert encoded.shape == expected_shape
 
 
-# @pytest.mark.parametrize(
-#     "nx, ny, nz, nvars", [(4, 4, 3, 2), (2, 2, 1, 1), (2, 2, 1, 2)]
-# )
-# def test_decode_columns(nx, ny, nz, nvars):
-#     expected_shapes = [
-#         tuple([y for y in [nx, ny, nz] if y is not None]) for var in range(nvars)
-#     ]
+@pytest.mark.parametrize(
+    "nx, ny, nz, nvars", [(4, 4, 3, 2), (2, 2, 1, 1), (2, 2, 1, 2)]
+)
+def test_base_decode_txyz(nx, ny, nz, nvars):
+    expected_shapes = [(nx, ny, nz) for var in range(nvars)]
 
-#     encoded_input_shape = (
-#         (*expected_shapes[0][:-1], nz * nvars) if nz is not None else expected_shapes[0] # noqa
-#     )
-#     transformer = DoNothingAutoencoder([nz for var in range(nvars)])
+    transformer = DoNothingAutoencoder([nz for var in range(nvars)])
 
-#     # need to call encode before decode
-#     data_arrs = [np.random.rand(*shape) for shape in expected_shapes]
-#     transformer.encode(data_arrs)
+    # need to call encode before decode
+    data_arrs = [np.random.rand(*shape) for shape in expected_shapes]
+    transformer.encode_txyz(data_arrs)
 
-#     encoded_input = np.random.rand(*encoded_input_shape)
-#     decoded = decode_columns(encoded_input, transformer=transformer)
+    encoded_input = np.random.rand(nx, ny, transformer.n_latent_dims)
+    decoded = transformer.decode_txyz(encoded_input)
 
-#     assert len(expected_shapes) == len(decoded)
-#     for expected_shape, decoded_output in zip(expected_shapes, decoded):
-#         assert expected_shape == decoded_output.shape
+    assert len(expected_shapes) == len(decoded)
+    for expected_shape, decoded_output in zip(expected_shapes, decoded):
+        assert expected_shape == decoded_output.shape
