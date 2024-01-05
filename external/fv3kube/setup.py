8fe01cd6c49ea635a1b07afd4ee4615db7555ce6
Spencer Clark <spencerkclark@gmail.com>
2024-01-05 20:23:19 +0000

    Attempt to fix package_data in fv3kube

--- external/fv3kube/setup.py
+++ external/fv3kube/setup.py
@@ -9,6 +9,6 @@ setup(
     author_email="oliverwm@allenai.org",
     packages=find_packages(),
     package_dir={"": "."},
-    package_data={"fv3kube": ["base_yamls/*/fv3config.yml"]},
+    package_data={"fv3kube": ["base_yamls/**/**/fv3config.yml"]},
     install_requires=["fsspec>=0.7.4", "pyyaml>=5.3.0", "kubernetes>=11", "fv3config"],
 )
